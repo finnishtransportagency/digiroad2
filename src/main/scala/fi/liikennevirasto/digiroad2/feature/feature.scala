96af621db41f76f58e79cb6620780597773ff0d7
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-11-08 10:08:26 +0200

    Add features map to BusStop

--- src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
@@ -3,11 +3,11 @@ package fi.liikennevirasto.digiroad2.feature
 import fi.liikennevirasto.digiroad2.geo.Point
 
 abstract sealed class Feature
-case class BusStop(id: String, lat: Double, lon: Double) extends Feature {
+case class BusStop(id: String, lat: Double, lon: Double, features: Map[String, String] = Map()) extends Feature {
   BusStop
 }
 object BusStop {
-  def apply(id: String, point: Point) = {
-    new BusStop(id, point.lat, point.lon)
+  def apply(id: String, point: Point, features: Map[String, String] = Map()) = {
+    new BusStop(id, point.lat, point.lon, features)
   }
 }
