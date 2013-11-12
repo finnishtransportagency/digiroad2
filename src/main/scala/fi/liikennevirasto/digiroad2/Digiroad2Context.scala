32365a2807ebe91e89b530c5358c8a04b9c8b3db
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-11-12 09:59:37 +0200

    Move DB connection pool to oracle module, placeholder for FeatureProvider initialization

--- src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
@@ -3,24 +3,13 @@ package fi.liikennevirasto.digiroad2
 import com.jolbox.bonecp.{BoneCPConfig, BoneCP}
 import com.jolbox.bonecp
 import java.util.Properties
-import java.io.IOException
 
 object Digiroad2Context {
-  lazy val connectionPool: BoneCP = initConnectionPool
-
-  lazy val localProperties: Properties = {
+  lazy val properties: Properties = {
     val props = new Properties()
-    try {
-      props.load(getClass.getResourceAsStream("/local.properties"))
-    } catch {
-      case e: Exception => throw new RuntimeException("Can't load local.properties for env: " + System.getProperty("env"), e)
-    }
+    props.load(getClass.getResourceAsStream("/digiroad2.properties"))
     props
   }
 
-  private[this] def initConnectionPool: BoneCP = {
-    Class.forName("oracle.jdbc.driver.OracleDriver")
-    val cfg = new bonecp.BoneCPConfig(localProperties)
-    new BoneCP(cfg)
-  }
+//  val featureProvider: FeatureProvider = Class.forName(properties.getProperty("digiroad2.featureProvider")).newInstance().asInstanceOf[FeatureProvider]
 }
