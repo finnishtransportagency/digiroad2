cd8a7150b675fbc1cd00c18f2ce3e31a2bb3617f
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-11-21 16:55:38 +0200

    make release script exit on error

--- release.sh
+++ release.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
+
 # checkout revision if given
-git checkout $1
+git checkout $1 || exit 1
+git clean -d -f || exit 1
 
-git clean -d -f
+# rm -rf bower_components && bower install
 
-rm -rf bower_components && bower install
-
-rsync -av --exclude-from 'copy_exclude.txt' --progress UI/ src/main/webapp/
-rsync -av --progress bower_components src/main/webapp/
-
-grunt && rsync -av dist/ src/main/webapp/ && ./sbt assembly
-
-cp digiroad2-oracle/newrelic* .
-tar -cvzf file.tar.gz target/scala-2.10/digiroad2-assembly-0.1.0-SNAPSHOT.jar src/main/webapp/ newrelic*
+rsync -a --exclude-from 'copy_exclude.txt' UI/ src/main/webapp/ || exit 1
+rsync -a bower_components src/main/webapp/ || exit 1
+grunt && rsync -av dist/ src/main/webapp/ && ./sbt assembly || exit 1
+cp digiroad2-oracle/newrelic* . || exit 1
+tar -cvzf file.tar.gz target/scala-2.10/digiroad2-assembly-0.1.0-SNAPSHOT.jar src/main/webapp/ newrelic* || exit 1
 
 scp file.tar.gz gateway:.
 
