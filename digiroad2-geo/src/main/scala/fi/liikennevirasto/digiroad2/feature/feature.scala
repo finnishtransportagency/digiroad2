f756ffe0562460d114fb1c320018c77d72af5bfa
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-11-27 09:39:12 +0200

    Remove wgs84 coordinates from bus stop, change coordinate order

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
@@ -1,7 +1,6 @@
 package fi.liikennevirasto.digiroad2.feature
 
 abstract sealed class Feature
-// TODO: remove WGS coordinates (handled in client), change lon/lat order in BusStop, id from String -> Long
-case class BusStop(id: String, lat: Double, lon: Double, latWgs84: Option[Double] = None, lonWgs84: Option[Double] = None,
-                   busStopType: String, featureData: Map[String, String] = Map(), roadLinkId: Long) extends Feature
+// TODO: id from String -> Long
+case class BusStop(id: String, lon: Double, lat: Double, busStopType: String, featureData: Map[String, String] = Map(), roadLinkId: Long) extends Feature
 case class RoadLink(id: Long, lonLat: Seq[(Double, Double)]) extends Feature
