3c99516714c00644492bad4a8373d3742674cd20
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-12-02 13:20:13 +0200

    Filter bus stops and road links by municipality number

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/user/DummyUserProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/user/DummyUserProvider.scala
@@ -1,5 +1,5 @@
 package fi.liikennevirasto.digiroad2.user;
 
 class DummyUserProvider extends UserProvider {
-  def getUserConfiguration(): Map[String, String] = { Map("zoom" -> "8", "east" -> "373560", "north"-> "6677676") }
+  def getUserConfiguration(): Map[String, String] = { Map("zoom" -> "8", "east" -> "373560", "north"-> "6677676", "municipalityNumber" -> "235") }
 }
