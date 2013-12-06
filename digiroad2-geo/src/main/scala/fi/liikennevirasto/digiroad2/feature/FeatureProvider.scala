000fad6d742ad602aba92d722f99e31e7b527b1f
Samuli Karjula <samuli.karjula@reaktor.fi>
2013-12-06 18:09:57 +0200

    Add possibility to load Assets from feature providers.

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/FeatureProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/FeatureProvider.scala
@@ -2,6 +2,7 @@ package fi.liikennevirasto.digiroad2.feature
 
 trait FeatureProvider {
   def getBusStops(municipalityNumber: Option[Int] = None): Seq[BusStop]
+  def getAssets(assetTypeId: Long, municipalityNumber: Option[Long] = None): Seq[Asset] // TODO return Assets
   def updateBusStop(busStop: BusStop): BusStop
   def getRoadLinks(municipalityNumber: Option[Int] = None): Seq[RoadLink]
 }
--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/NoOpFeatureProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/NoOpFeatureProvider.scala
@@ -4,4 +4,5 @@ class NoOpFeatureProvider extends FeatureProvider {
   def getBusStops(municipalityNumber: Option[Int]): Seq[BusStop] = { List() }
   def updateBusStop(busStop: BusStop): BusStop = busStop
   def getRoadLinks(municipalityNumber: Option[Int]): Seq[RoadLink] = List()
+  def getAssets(assetTypeId: Long, municipalityNumber: Option[Long]): Seq[Asset] = List()
 }
