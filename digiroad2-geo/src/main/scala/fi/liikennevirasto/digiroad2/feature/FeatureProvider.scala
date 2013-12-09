83b07d5dfdf143812cda8e8183cb7db82a75df47
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-12-09 20:07:22 +0200

    Asset property update method

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/FeatureProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/FeatureProvider.scala
@@ -4,6 +4,7 @@ trait FeatureProvider {
   def getBusStops(municipalityNumber: Option[Int] = None): Seq[BusStop]
   def getAssetTypes: Seq[AssetType]
   def getAssets(assetTypeId: Long, municipalityNumber: Option[Long] = None): Seq[Asset]
+  def updateAssetProperty(assetId: Long, propertyId: Long, propertyValues: Seq[PropertyValue])
   def updateBusStop(busStop: BusStop): BusStop
   def getRoadLinks(municipalityNumber: Option[Int] = None): Seq[RoadLink]
 }
--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/NoOpFeatureProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/NoOpFeatureProvider.scala
@@ -2,6 +2,7 @@ package fi.liikennevirasto.digiroad2.feature
 
 class NoOpFeatureProvider extends FeatureProvider {
   def getBusStops(municipalityNumber: Option[Int]): Seq[BusStop] = { List() }
+  def updateAssetProperty(assetId: Long, propertyId: Long, propertyValues: Seq[PropertyValue]) {}
   def updateBusStop(busStop: BusStop): BusStop = busStop
   def getRoadLinks(municipalityNumber: Option[Int]): Seq[RoadLink] = List()
   def getAssets(assetTypeId: Long, municipalityNumber: Option[Long]): Seq[Asset] = List()
