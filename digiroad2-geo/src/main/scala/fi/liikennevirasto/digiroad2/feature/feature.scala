72511ef682c832e2f41379a6a6d7f448d530e3f9
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2013-12-16 15:58:41 +0200

    Remove obsolete bus stop implementation

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/feature/feature.scala
@@ -6,5 +6,4 @@ case class Asset(id: Long, assetTypeId: Long, lon: Double, lat: Double, roadLink
 case class Property(propertyId: Long, propertyName: String, propertyType: String, values: Seq[PropertyValue]) extends Feature
 case class PropertyValue(propertyValue: Long, propertyDisplayValue: String) extends Feature
 case class EnumeratedPropertyValue(propertyId: Long, propertyName: String, propertyType: String, values: Seq[PropertyValue]) extends Feature
-case class BusStop(id: Long, lon: Double, lat: Double, busStopType: String, featureData: Map[String, String] = Map(), roadLinkId: Long) extends Feature
 case class RoadLink(id: Long, lonLat: Seq[(Double, Double)]) extends Feature
