7931caecde40e88a97be5b33e46e25b87a51b7e6
Samuli Karjula <samuli.karjula@reaktor.fi>
2014-01-15 14:35:17 +0200

    Add start and end date to asset fetching api

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/NoOpAssetProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/NoOpAssetProvider.scala
@@ -1,13 +1,14 @@
 package fi.liikennevirasto.digiroad2.asset
 
 import fi.liikennevirasto.digiroad2.mtk.MtkRoadLink
+import org.joda.time.LocalDate
 
 class NoOpAssetProvider extends AssetProvider {
   def updateAssetProperty(assetId: Long, propertyId: String, propertyValues: Seq[PropertyValue]) {}
   def deleteAssetProperty(assetId: Long, propertyId: String) {}
   def getRoadLinks(municipalityNumber: Option[Int]): Seq[RoadLink] = List()
   def getAssetById(assetId: Long): Option[Asset] = None
-  def getAssets(assetTypeId: Long, municipalityNumber: Option[Long], bounds: Option[BoundingCircle]): Seq[Asset] = List()
+  def getAssets(assetTypeId: Long, municipalityNumber: Option[Long], bounds: Option[BoundingCircle], validFrom: Option[LocalDate], validTo: Option[LocalDate]): Seq[Asset] = List()
   def getAssetTypes = List()
   def getEnumeratedPropertyValues(assetTypeId: Long): Seq[EnumeratedPropertyValue] = List()
   def updateAssetLocation(asset: Asset): Asset = asset
