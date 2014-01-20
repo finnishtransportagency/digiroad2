b6e22fb88e37c7a1abc4bab20640e021e61923c6
Esko Vääräsmäki <esko.vaarasmaki@gmail.com>
2014-01-20 16:22:38 +0200

    action panel bundle layout

--- UI/src/packages/digiroad2/bundle/actionpanel/bundle.js
+++ UI/src/packages/digiroad2/bundle/actionpanel/bundle.js
@@ -26,6 +26,9 @@ Oskari.clazz.define("Oskari.digiroad2.bundle.actionpanel.ActionPanelBundle", fun
                 "type" : "text/javascript",
                 "src" : "../../../../bundles/digiroad2/bundle/actionpanel/instance.js"
             },{
+                "type" : "text/javascript",
+                "src" : "../../../../bundles/digiroad2/bundle/actionpanel/event/ActionPanelToolSelectionChangedEvent.js"
+            },{
                 "type" : "text/css",
                 "src" : "../../../../resources/digiroad2/bundle/actionpanel/css/style.css"
             }],
