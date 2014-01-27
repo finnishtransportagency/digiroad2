9373490c6fc23e9eb004913cc1f9598beffa589b
Samuli Karjula <samuli.karjula@reaktor.fi>
2014-01-27 14:39:29 +0200

    Add isRequired information to properties.

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/asset.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/asset.scala
@@ -4,9 +4,9 @@ import org.joda.time.LocalDate
 
 case class AssetType(id: Long, assetTypeName: String, geometryType: String)
 case class Asset(id: Long, assetTypeId: Long, lon: Double, lat: Double, roadLinkId: Long, propertyData: Seq[Property] = List(), bearing: Option[Int] = None, status: Option[String] = None)
-case class Property(propertyId: String, propertyName: String, propertyType: String, values: Seq[PropertyValue])
+case class Property(propertyId: String, propertyName: String, propertyType: String, required: Boolean = false, values: Seq[PropertyValue])
 case class PropertyValue(propertyValue: Long, propertyDisplayValue: String, imageId: String = null)
-case class EnumeratedPropertyValue(propertyId: String, propertyName: String, propertyType: String, values: Seq[PropertyValue])
+case class EnumeratedPropertyValue(propertyId: String, propertyName: String, propertyType: String, required: Boolean = false, values: Seq[PropertyValue])
 case class RoadLink(id: Long, lonLat: Seq[(Double, Double)], enDate: Option[LocalDate] = None)
 
 object PropertyTypes {
