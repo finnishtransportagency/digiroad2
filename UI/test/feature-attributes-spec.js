2d06a44b60799c14de0f589b3b489805aaf0c261
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-01-29 10:07:56 +0200

    Add missing semicolons

--- UI/test/feature-attributes-spec.js
+++ UI/test/feature-attributes-spec.js
@@ -2,8 +2,8 @@ describe('FeatureAttributes', function() {
     var featureAttributesInstance = Oskari.clazz.define('Oskari.digiroad2.bundle.featureattributes.FeatureAttributesBundleInstance');
 
     describe('when backend returns undefined date', function() {
-        var featureAttributes = Object.create(featureAttributesInstance._class.prototype)
-        featureAttributes.init()
+        var featureAttributes = Object.create(featureAttributesInstance._class.prototype);
+        featureAttributes.init();
 
         it('should construct date attribute with empty content', function() {
             var actualHtml = featureAttributes._makeContent([{
@@ -11,7 +11,7 @@ describe('FeatureAttributes', function() {
                 propertyName: 'propertyName',
                 propertyType: 'date',
                 values: [{imageId: null, propertyDisplayValue: null, propertyValue: 0}]
-            }])
+            }]);
             assert.equal(actualHtml,
                 '<div class="formAttributeContentRow">' +
                     '<div class="formLabels">propertyName</div>' +
@@ -19,7 +19,7 @@ describe('FeatureAttributes', function() {
                         '<input class="featureAttributeDate" type="text" data-propertyId="propertyId" name="propertyName" value=""/>' +
                         '<span class="attributeFormat">pp.kk.vvvv</span>' +
                     '</div>' +
-                '</div>')
+                '</div>');
         });
     });
 });
