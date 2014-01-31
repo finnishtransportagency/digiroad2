705da9f4d2c7f871bd811563a9f80a2e246f0b21
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-01-31 15:20:32 +0200

    Add possibility to add assets to map without connection to database

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/NoOpAssetProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/NoOpAssetProvider.scala
@@ -1,18 +1,36 @@
 package fi.liikennevirasto.digiroad2.asset
 
 import org.joda.time.LocalDate
+import fi.liikennevirasto.digiroad2.asset.PropertyTypes._
 import fi.liikennevirasto.digiroad2.mtk.MtkRoadLink
+import scala.Some
 
 class NoOpAssetProvider extends AssetProvider {
   def updateAssetProperty(assetId: Long, propertyId: String, propertyValues: Seq[PropertyValue]) {}
   def deleteAssetProperty(assetId: Long, propertyId: String) {}
   def getRoadLinks(municipalityNumber: Option[Int], bounds: Option[BoundingCircle]): Seq[RoadLink] = List()
   def getRoadLinkById(roadLinkId: Long): Option[RoadLink] = None
-  def getAssetById(assetId: Long): Option[Asset] = None
+  def getAssetById(assetId: Long): Option[Asset] = {
+    Some(Asset(0, 10, 0, 0, 0, List(
+      Property("validityDirection", "Vaikutussuunta", SingleChoice, values = Seq(
+        PropertyValue(1, "Molempiin suuntiin"),
+        PropertyValue(2, "Digitointisuuntaan"),
+        PropertyValue(3, "Digitointisuuntaa vastaan"))),
+      Property("validFrom", "Käytössä alkaen", Date, values = Seq(PropertyValue(0, null))),
+      Property("validTo", "Käytössä päättyen", Date, values = Seq(PropertyValue(0, null)))
+    )))
+  }
   def getAssets(assetTypeId: Long, municipalityNumber: Option[Long], bounds: Option[BoundingCircle], validFrom: Option[LocalDate], validTo: Option[LocalDate]): Seq[Asset] = List()
-  def createAsset(assetTypeId: Long, lon: Double, lat: Double, roadLinkId: Long, bearing: Int, creator: String) = null
+  def createAsset(assetTypeId: Long, lon: Double, lat: Double, roadLinkId: Long, bearing: Int, creator: String) = {
+    Asset(0, assetTypeId, lon, lat, roadLinkId)
+  }
   def getAssetTypes = List()
-  def getEnumeratedPropertyValues(assetTypeId: Long): Seq[EnumeratedPropertyValue] = List()
+  def getEnumeratedPropertyValues(assetTypeId: Long): Seq[EnumeratedPropertyValue] = {
+    List(EnumeratedPropertyValue("validityDirection", "Vaikutussuunta", SingleChoice, values = Seq(
+      PropertyValue(1, "Molempiin suuntiin"),
+      PropertyValue(2, "Digitointisuuntaan"),
+      PropertyValue(3, "Digitointisuuntaa vastaan"))))
+  }
   def updateAssetLocation(asset: Asset): Asset = asset
   def getImage(imageId: Long): Array[Byte] = new Array[Byte](0)
   def updateRoadLinks(roadlinks: Seq[MtkRoadLink]) { }
