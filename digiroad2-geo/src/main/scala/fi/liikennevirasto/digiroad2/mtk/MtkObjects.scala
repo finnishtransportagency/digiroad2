ec901f16031e6d4aa8c84ff696e12b8bf40e8313
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-02-03 15:20:13 +0200

    Fix decimal point character

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/mtk/MtkObjects.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/mtk/MtkObjects.scala
@@ -1,7 +1,8 @@
 package fi.liikennevirasto.digiroad2.mtk
 
 import org.joda.time.{LocalDate, DateTime}
-import java.text.DecimalFormat
+import java.text.{NumberFormat, DecimalFormat}
+import java.util.Locale
 
 case class MtkRoadLink(id: Long, startDate: DateTime, endDate: Option[LocalDate],
                        municipalityCode: Int, points: Seq[Point])
@@ -9,7 +10,9 @@ case class Point(x: Double, y: Double, z: Double)
 
 object MtkRoadLinkUtils {
   private def pointToStoringItem(point: Point): String = {
-    val formatter = new DecimalFormat("#.000")
+    val decimalPattern = "#.000"
+    val formatter = NumberFormat.getNumberInstance(Locale.US).asInstanceOf[DecimalFormat]
+    formatter.applyPattern(decimalPattern)
     s"${formatter.format(point.x)}, ${formatter.format(point.y)}, ${formatter.format(point.z)}, null"
   }
 
