df93be532b6c8a254a82233dc66ca07c7c72e96e
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-02-24 13:27:28 +0200

    Redirect browser to index.html (force login) on missing Digiroad header

--- UI/src/utils/session.js
+++ UI/src/utils/session.js
@@ -1,13 +1,19 @@
 (function(session, undefined){
-    var sessionId = null;
     session.redirectToLogin = function() {
-        window.location = "login.html";
+        window.location = "index.html";
     };
 
 }(window.session = window.session || {}));
 
 $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
     if (jqXHR.status == 401) {
+        session.redirectToLogin();
+    }
+});
+
+$(document).ajaxComplete(function(event, jqXHR, ajaxSettings) {
+    var digiroadResponse = jqXHR.getResponseHeader("X-Digiroad-Response");
+    if (!digiroadResponse) {
         session.redirectToLogin();
     }
 });
