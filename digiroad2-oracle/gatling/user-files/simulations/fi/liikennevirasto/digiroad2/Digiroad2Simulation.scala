67d9e229af0e6bd38146fea586994cad41613ae2
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-02-24 08:31:40 +0200

    Run simulation as if authenticated by SSO

--- digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
+++ digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
@@ -15,12 +15,12 @@ class Digiroad2Simulation extends Simulation {
   val httpProtocol = http
       .baseURL("http://localhost:8080")
       .acceptHeader("*/*")
+  val authHeader = Map("OAM_REMOTE_USER" -> "test2")
 
   val scn = scenario("Load bus stops and road links")
       .repeat(10) {
-        exec(http("Get road links for Kauniainen").get("""/api/roadlinks?municipalityNumber=235"""))
-        .exec(http("Get bus stops").get("""/api/assets?assetTypeId=10"""))
-        .exec(http("Get road links for Kerava").get("""/api/roadlinks?municipalityNumber=245"""))
+        exec(http("Get road links for Kauniainen").get("""/api/roadlinks""").headers(authHeader))
+        .exec(http("Get bus stops").get("""/api/assets?assetTypeId=10""").headers(authHeader))
       }
   setUp(scn.inject(atOnce(10 user))).protocols(httpProtocol)
 }
