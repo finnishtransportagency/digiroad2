e7239c3bda6f1ee5ad5edfaeee521ee6fa92723b
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-02-27 13:48:28 +0200

    Add Digiroad API key for Google Street View API

--- UI/src/bundles/digiroad2/bundle/featureattributes/instance.js
+++ UI/src/bundles/digiroad2/bundle/featureattributes/instance.js
@@ -104,7 +104,7 @@ Oskari.clazz.define("Oskari.digiroad2.bundle.featureattributes.FeatureAttributes
 
             me._streetViewTemplate  = _.template(
                 '<a target="_blank" href="http://maps.google.com/?ll={{wgs84Y}},{{wgs84X}}&cbll={{wgs84Y}},{{wgs84X}}&cbp=12,{{heading}}.09,,0,5&layer=c&t=m">' +
-                    '<img alt="Google StreetView-näkymä" src="http://maps.googleapis.com/maps/api/streetview?size=360x180&location={{wgs84Y}}' +
+                    '<img alt="Google StreetView-näkymä" src="http://maps.googleapis.com/maps/api/streetview?key=AIzaSyBh5EvtzXZ1vVLLyJ4kxKhVRhNAq-_eobY&size=360x180&location={{wgs84Y}}' +
                     ', {{wgs84X}}&fov=110&heading={{heading}}&pitch=-10&sensor=false">' +
                 '</a>');
 
