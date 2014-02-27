c4ba7465a9365ad66c4ac4be4b13bdaceee55efb
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-02-27 11:56:32 +0200

    Modify test fixture user data

--- digiroad2-oracle/sql/insert_users.sql
+++ digiroad2-oracle/sql/insert_users.sql
@@ -1,6 +1,8 @@
 insert into service_user (id, username, configuration)
-values (1, 'test', '{"zoom": 8, "east": 373560, "north": 6677676, "municipalityNumber": 235, "authorizedMunicipalities": [235]}');
+values (1, 'test', '{"zoom": 8, "east": 373560, "north": 6677676, "municipalityNumber": 235, "authorizedMunicipalities": [504,407,78,235,734,286,858,444,616,505,611,638,710,753,434,18,186,624]}');
 insert into service_user (id, username, configuration)
-values (2, 'test2', '{"zoom": 8, "east": 373560, "north": 6677676, "municipalityNumber": 235, "authorizedMunicipalities": [235,49]}');
+values (2, 'test2', '{"zoom": 8, "east": 373560, "north": 6677676, "municipalityNumber": 235, "authorizedMunicipalities": [504,407,78,235,734,286,858,444,616,505,611,638,710,753,434,18,186,624]}');
 insert into service_user (id, username, configuration)
 values (3, 'test49', '{"zoom": 8, "east": 373560, "north": 6677676, "municipalityNumber": 49, "authorizedMunicipalities": [49]}');
+insert into service_user (id, username, configuration)
+values (4, 'testpirkanmaa', '{"zoom": 8, "east": 328308, "north": 6822545, "municipalityNumber": 49, "authorizedMunicipalities": [504,407,78,235,734,286,858,444,616,505,611,638,710,753,434,18,49,186,624,837]}');
