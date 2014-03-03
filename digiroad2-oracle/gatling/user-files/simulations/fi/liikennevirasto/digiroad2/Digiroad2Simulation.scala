7f47ce7c93ce060791e59f4c6089222d2b12d1ad
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-03-03 13:15:01 +0200

    Basic roadlinks & assets w/ bounding box scenario

--- digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
+++ digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
@@ -13,15 +13,18 @@ import assertions._
 class Digiroad2Simulation extends Simulation {
 
   val httpProtocol = http
-      .baseURL("http://localhost:8080")
+      .baseURL("http://10.129.47.148:8080/digiroad")
       .acceptHeader("*/*")
-  val authHeader = Map("OAM_REMOTE_USER" -> "test2")
+  val authHeader = Map("OAM_REMOTE_USER" -> "test4")
 
   val scn = scenario("Load bus stops and road links")
       .repeat(10) {
-        exec(http("Get road links for Kauniainen").get("""/api/roadlinks""").headers(authHeader))
-        .exec(http("Get bus stops").get("""/api/assets?assetTypeId=10""").headers(authHeader))
+        exec(http("Get bus stops 1").get("""/api/assets?assetTypeId=10&bbox=367799,6676551,370171,6677735""").headers(authHeader))
+        .exec(http("Get bus stops 2").get("""/api/assets?assetTypeId=10&bbox=366777,6676051,371521,6678419""").headers(authHeader))
+        .exec(http("Get bus stops 3").get("""/api/assets?assetTypeId=10&bbox=364405,6674867,373893,6679603""").headers(authHeader))
+        .exec(http("Get road links 1").get("""/api/roadlinks?bbox=367799,6676551,370171,6677735""").headers(authHeader))
+        .exec(http("Get road links 2").get("""/api/roadlinks?bbox=366777,6676051,371521,6678419""").headers(authHeader))
+        .exec(http("Get road links 3").get("""/api/roadlinks?bbox=364405,6674867,373893,6679603""").headers(authHeader))
       }
   setUp(scn.inject(atOnce(10 user))).protocols(httpProtocol)
 }
-
