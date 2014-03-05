7c5c47dcf1dc0295e58bca9f8d85058bfe8e88c7
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-03-05 12:48:56 +0200

    Modify simulation data

--- digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
+++ digiroad2-oracle/gatling/user-files/simulations/fi/liikennevirasto/digiroad2/Digiroad2Simulation.scala
@@ -20,12 +20,18 @@ class Digiroad2Simulation extends Simulation {
 
   val scn = scenario("Load bus stops and road links")
       .repeat(10) {
-        exec(http("Get bus stops 1").get("""/api/assets?assetTypeId=10&bbox=367799,6676551,370171,6677735""").headers(authHeader))
-//        .exec(http("Get bus stops 2").get("""/api/assets?assetTypeId=10&bbox=366777,6676051,371521,6678419""").headers(authHeader))
-//        .exec(http("Get bus stops 3").get("""/api/assets?assetTypeId=10&bbox=364405,6674867,373893,6679603""").headers(authHeader))
-        .exec(http("Get road links 1").get("""/api/roadlinks?bbox=325936,6821541,330680,6823549""").headers(authHeader))
-//        .exec(http("Get road links 2").get("""/api/roadlinks?bbox=366777,6676051,371521,6678419""").headers(authHeader))
-//        .exec(http("Get road links 3").get("""/api/roadlinks?bbox=364405,6674867,373893,6679603""").headers(authHeader))
+        exec(http("Get bus stops L1").get("""/api/assets?assetTypeId=10&bbox=327316,6821857,332052,6824433""").headers(authHeader))
+        .exec(http("Get bus stops L2").get("""/api/assets?assetTypeId=10&bbox=326784,6820903,331520,6823479""").headers(authHeader))
+        .exec(http("Get bus stops L3").get("""/api/assets?assetTypeId=10&bbox=326478,6820155,331214,6822731""").headers(authHeader))
+        .exec(http("Get bus stops M1").get("""/api/assets?assetTypeId=10&bbox=326990,6821918,329358,6823206""").headers(authHeader))
+        .exec(http("Get bus stops M2").get("""/api/assets?assetTypeId=10&bbox=327010,6822456,329378,6823744""").headers(authHeader))
+        .exec(http("Get bus stops M3").get("""/api/assets?assetTypeId=10&bbox=327090,6822855,329458,6824143""").headers(authHeader))
+        .exec(http("Get road links L1").get("""/api/roadlinks?bbox=327316,6821857,332052,6824433""").headers(authHeader))
+        .exec(http("Get road links L2").get("""/api/roadlinks?bbox=326784,6820903,331520,6823479""").headers(authHeader))
+        .exec(http("Get road links L3").get("""/api/roadlinks?bbox=326478,6820155,331214,6822731""").headers(authHeader))
+        .exec(http("Get road links M1").get("""/api/roadlinks?bbox=326990,6821918,329358,6823206""").headers(authHeader))
+        .exec(http("Get road links M2").get("""/api/roadlinks?bbox=327010,6822456,329378,6823744""").headers(authHeader))
+        .exec(http("Get road links M3").get("""/api/roadlinks?bbox=327090,6822855,329458,6824143""").headers(authHeader))
       }
-  setUp(scn.inject(atOnce(1 user))).protocols(httpProtocol)
+  setUp(scn.inject(atOnce(10 user))).protocols(httpProtocol)
 }
