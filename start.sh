4e028f1d0a07b59ebfd8a817e3fa6e6086e07de5
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2014-03-06 15:21:34 +0200

    Add jvm memory parameters to start script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 jarfile="target/scala-2.10/digiroad2-assembly-0.1.0-SNAPSHOT.jar"
-javaopts="-javaagent:newrelic.jar -jar $jarfile"
+javaopts="-javaagent:newrelic.jar -Xms2048m -Xmx4096m -jar $jarfile"
 logfile="digiroad2.boot.log"
 
 nohup java $javaopts > $logfile &
