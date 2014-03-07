60a8643a3fa116d53216a727f0f2ee1066aef98a
Esko Vääräsmäki <esko.vaarasmaki@gmail.com>
2014-03-07 13:36:44 +0200

    Fix CI provider properties
    While at it modify fixture-reset to accept system property

--- fixture-reset.sh
+++ fixture-reset.sh
@@ -1,1 +1,1 @@
-./sbt 'project digiroad2-oracle' "test:run-main fi.liikennevirasto.digiroad2.util.DataFixture ${1}"
+./sbt ${2} 'project digiroad2-oracle' "test:run-main fi.liikennevirasto.digiroad2.util.DataFixture ${1}"
