74da7230aab64700c48c39b624f6f21d989af530
Vesa Marttila <vtmarttila@gmail.com>
2014-03-17 13:26:12 +0200

    Make event logging configurable at runtime

--- UI/src/utils/eventbus.js
+++ UI/src/utils/eventbus.js
@@ -1,6 +1,8 @@
 (function(BackboneEvents) {
     window.eventbus = BackboneEvents;
     eventbus.on('all', function(eventName, entity) {
-      console.log(eventName, entity);
+        if (window.DR2_LOGGING) {
+            console.log(eventName, entity);
+        }
     });
 })(BackboneEvents);
