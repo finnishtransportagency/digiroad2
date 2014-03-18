10b0197f79c3c574f46804424026f6d88cb58ad2
simula77 <samuli.karjula@reaktor.fi>
2014-03-18 11:38:59 +0200

    Rename modifier when running flyway migration version to v2

--- digiroad2-oracle/src/main/resources/db/migration/V2__rename_bus_stop_properties.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V2__rename_bus_stop_properties.sql
@@ -25,114 +25,114 @@ alter table enumerated_value modify (name_sv null);
 
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Nimi ruotsiksi', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Nimi ruotsiksi', 'text');
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Maastokoordinaatti X', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Maastokoordinaatti X', 'text');
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Maastokoordinaatti Y', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Maastokoordinaatti Y', 'text');
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Maastokoordinaatti Z', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Maastokoordinaatti Z', 'text');
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Liikennöintisuuntima', 'read-only');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Liikennöintisuuntima', 'read-only');
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Aikataulu)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Aikataulu)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Aikataulu)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Mainoskatos)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Mainoskatos)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Mainoskatos)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Penkki)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Penkki)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Penkki)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Penkki)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Penkki)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Penkki)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Penkki)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Penkki)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Pyöräteline)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Pyöräteline)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Pyöräteline)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Sähköinen aikataulunäyttö)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Sähköinen aikataulunäyttö)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Varusteet (Valaistus)', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Varusteet (Valaistus)', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Varusteet (Valaistus)'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Valaistus)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Varusteet (Valaistus)'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Valaistus)'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Varusteet (Valaistus)'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Varusteet (Valaistus)'));
 
 
 insert into property (id, asset_type_id, required, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'automatic_import', 'Saattomahdollisuus henkilöautolla', 'single_choice');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), '0', 'db_migration_v2', 'Saattomahdollisuus henkilöautolla', 'single_choice');
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Ei', 'automatic_import', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
+values (primary_key_seq.nextval, 1, 'Ei', 'db_migration_v2', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 2, 'Kyllä', 'automatic_import', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
+values (primary_key_seq.nextval, 2, 'Kyllä', 'db_migration_v2', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
 
 insert into enumerated_value (id, value, name_fi, created_by, property_id)
-values (primary_key_seq.nextval, 99, 'Ei tietoa', 'automatic_import', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
+values (primary_key_seq.nextval, 99, 'Ei tietoa', 'db_migration_v2', (select id from property where name_fi = 'Saattomahdollisuus henkilöautolla'));
 
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Liityntäpysäköintipaikkojen määrä', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Liityntäpysäköintipaikkojen määrä', 'text');
 
 insert into property (id, asset_type_id, created_by, name_fi, property_type)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'automatic_import', 'Pysäkin omistaja', 'text');
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2', 'Pysäkin omistaja', 'text');
