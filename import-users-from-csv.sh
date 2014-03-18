2f34ed3bd1e9bf0288af08d3ea34d6e31c24a608
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-03-18 10:05:43 +0200

    Fix URL to run against production or integration environment

--- import-users-from-csv.sh
+++ import-users-from-csv.sh
@@ -5,5 +5,5 @@ then
     echo "where <user-name> is the user name of administrator account to be used to make modifications in digiroad service users"
     echo "and <csv-file> is the csv (columns separated by semicolons) file containing user information to be added and / or created"
 else
-    curl -H "OAM_REMOTE_USER:${2}" -T ${3} http://${1}/api/userconfig/municipalitiesbatch
+    curl -H "OAM_REMOTE_USER:${2}" -T ${3} http://${1}/digiroad/api/userconfig/municipalitiesbatch
 fi
