fabca87588f46e1421fd335be65f43801fdd7c0c
simula77 <samuli.karjula@reaktor.fi>
2014-03-24 14:00:25 +0200

    Missing file

---
+++ digiroad2-oracle/src/main/resources/db/migration/V2_2__rename_equipment_asset_properties.sql
@@ -0,0 +1,7 @@
+update property set name_fi = 'Aikataulu' where name_fi = 'Varusteet (Aikataulu)';
+update property set name_fi = 'Katos' where name_fi = 'Varusteet (Katos)';
+update property set name_fi = 'Mainoskatos' where name_fi = 'Varusteet (Mainoskatos)';
+update property set name_fi = 'Penkki' where name_fi = 'Varusteet (Penkki)';
+update property set name_fi = 'Pyöräteline' where name_fi = 'Varusteet (Pyöräteline)';
+update property set name_fi = 'Sähköinen aikataulunäyttö' where name_fi = 'Varusteet (Sähköinen aikataulunäyttö)';
+update property set name_fi = 'Valaistus' where name_fi = 'Varusteet (Valaistus)';
