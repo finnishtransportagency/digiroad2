b973dfd01a5eff690855b31a10a4e33bba3eb77a
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-03-31 10:40:16 +0300

    Fix migration script

--- digiroad2-oracle/src/main/resources/db/migration/V2_5__localized_property_names.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V2_5__localized_property_names.sql
@@ -8,7 +8,5 @@ create table localized_string
   modified_date timestamp default current_timestamp not null,
   modified_by varchar2(128)
 );
-alter table enumerated_value add localized_string_id number(38);
-alter table text_property_value add localized_string_id number(38);
-alter table enumerated_value add constraint fk_enumerated_value_localized foreign key (localized_string_id) references localized_string(id);
-alter table text_property_value add constraint fk_text_value_localized foreign key (localized_string_id) references localized_string(id);
+alter table property add name_localized_string_id number(38);
+alter table property add constraint fk_prop_name_localized foreign key (name_localized_string_id) references localized_string(id);
