a397ec73091fbaf2fb5b9ff83b14635e3ae5fd56
Sebastian Nykopp <sebastian.nykopp@reaktor.fi>
2014-03-31 16:05:37 +0300

    Cleanup

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/asset/oracle/LocalizationDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/asset/oracle/LocalizationDao.scala
@@ -20,7 +20,6 @@ object LocalizationDao {
   }
 
   def insertLocalizedString(ls: LocalizedString): LocalizedString = {
-    // TODO: insert dates
     val seqId = sql"""select primary_key_seq.nextval from dual""".as[Long].first()
     sqlu"""
       insert into localized_string (id, value_fi, value_sv, created_date) values ($seqId, ${ls.forLanguage(LangFi).getOrElse("")}, ${ls.forLanguage(LangSv).getOrElse("")}, current_timestamp)
