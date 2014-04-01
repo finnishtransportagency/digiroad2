e941c31e3922fecb412779d95e6c37d7c63ef541
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2014-04-01 09:38:39 +0300

    Add wallu import e2e functionality

---
+++ wallu_import.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+./sbt 'project digiroad2' "runMain fi.liikennevirasto.digiroad2.util.WalluImport"
+zip vallu_import vallu_import.csv
+set -- $(<ftp.conf)
+curl -u $1:$2 -T vallu_import.zip ftp://$3/vallu_import.zip
+date "+%Y%m%d%H%M%S" >> flag.txt
+curl -u $1:$2 -T flag.txt ftp://$3/flag.txt
