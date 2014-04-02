6a06f127483160c399f9da819f725b1ce3b75f31
simula77 <samuli.karjula@reaktor.fi>
2014-04-02 11:11:13 +0300

    Fix migration to current version of database structure.

--- digiroad2-oracle/src/main/resources/db/migration/V2_7_1__add_liityntapysakoinnin_lisatiedot_property.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V2_7_1__add_liityntapysakoinnin_lisatiedot_property.sql
@@ -1,2 +1,4 @@
-insert into property (id, asset_type_id, created_by, name_fi, name_sv, property_type, ui_position_index)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2.7.1', 'Liityntäpysäköinnin lisätiedot', '', 'text', 135);
+insert into localized_string (id, value_fi, value_sv, created_by, created_date) values(primary_key_seq.nextval, 'Liityntäpysäköinnin lisätiedot', '', 'db_migration_v2.7.1', sysdate);
+
+insert into property (id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
+values (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'db_migration_v2.7.1', (select id from localized_string where value_fi = 'Liityntäpysäköinnin lisätiedot'), 'text', 135);
