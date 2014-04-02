aefeb80c211979b2b281fcaabec507fc1f11516f
Esko Vääräsmäki <esko.vaarasmaki@gmail.com>
2014-04-02 09:20:28 +0300

    Layer selection for layers

--- UI/src/bundles/digiroad2/bundle/actionpanel/ActionPanel.js
+++ UI/src/bundles/digiroad2/bundle/actionpanel/ActionPanel.js
@@ -25,92 +25,25 @@ Oskari.clazz.define("Oskari.digiroad2.bundle.actionpanel.ActionPanel",
                     sandbox.registerForEventByName(this, p);
                 }
             }
-            this._render();
-        },
-        init: function() {
-            eventbus.on('asset:fetched assetPropertyValue:fetched asset:created', this._handleAssetModified, this);
-            this._templates = Oskari.clazz.create('Oskari.digiroad2.bundle.actionpanel.template.Templates');
-            this._cursor = {'Select' : 'default', 'Add' : 'crosshair', 'Remove' : 'no-drop'};
-            this._layerPeriods = [
-                {id: "current", label: "Voimassaolevat", selected: true},
-                {id: "future", label: "Tulevat"},
-                {id: "past", label: "Käytöstä poistuneet"}
-            ];
-        },
 
-        _handleAssetModified: function(asset) {
-          var $el = jQuery('input.layerSelector[data-validity-period=' + asset.validityPeriod + ']');
-          if (!$el.is(':checked')) {
-            $el.click();
-          }
-        },
+            var panelControl =
+                '<div class="panelControl">' +
+                    '<div class="panelControlLine"></div>' +
+                    '<div class="panelControlLine"></div>' +
+                    '<div class="panelControlLine"></div>' +
+                '</div>' +
+                '<div class="panelLayerGroup"></div>';
 
-        _render: function() {
-          this._renderView();
-          this._bindEvents();
-        },
-        _renderView: function() {
-            var self = this;
-            jQuery("#maptools").html(self._templates.panelControl);
-            _.forEach(self._layerPeriods, function (layer) {
-                jQuery(".layerGroupLayers").append(self._templates.mapBusStopLayer({ selected: layer.selected ? "checked" : "", id:layer.id, name: layer.label}));
-            });
-            jQuery(".actionPanel").append(self._templates.actionButtons);
-            jQuery(".actionPanel").append(self._templates.editButton);
-            jQuery(".actionPanel").append(self._templates.readyButton);
-            jQuery(".container").append(self._templates.editMessage);
-        },
-        _bindEvents: function() {
-            var me = this;
-            jQuery(".panelControl").on("click", function() {
-                me._togglePanel();
-            });
 
-            jQuery(".layerSelector").on("change", function() {
-                var selectedValidityPeriods = $('input.layerSelector').filter(function(_, v) {
-                    return $(v).is(':checked');
-                }).map(function(_, v) {
-                        return $(v).attr('data-validity-period');
-                    }).toArray();
-                eventbus.trigger('validityPeriod:changed', selectedValidityPeriods);
-            });
+            jQuery("#maptools").append(panelControl);
+            new AssetActionPanel('asset');
+            new AssetActionPanel('linearAsset');
+            eventbus.trigger('layer:selected','asset');
 
-            jQuery(".actionButton").on("click", function() {
-                var data = jQuery(this);
-                var action = data.attr('data-action');
-                me._changeTool(action);
-            });
 
-            jQuery('.editMode').on('click', function() {
-                me._toggleEditMode(false);
-            });
 
-            jQuery('.readOnlyMode').on('click', function() {
-                me._toggleEditMode(true);
-            });
         },
-        _changeTool: function(action) {
-            jQuery(".actionButtonActive").removeClass("actionButtonActive");
-            jQuery(".actionPanelButton"+action).addClass("actionButtonActive");
-            jQuery(".actionPanelButtonSelectActiveImage").removeClass("actionPanelButtonSelectActiveImage");
-            jQuery(".actionPanelButtonAddActiveImage").removeClass("actionPanelButtonAddActiveImage");
-            jQuery(".olMap").css('cursor', this._cursor[action]);
-            eventbus.trigger('tool:changed', action);
-        },
-        _toggleEditMode: function(readOnly) {
-            this._changeTool('Select');
-            eventbus.trigger('asset:unselected');
-            eventbus.trigger('application:readOnly', readOnly);
-            jQuery('.actionButtons').toggleClass('readOnlyModeHidden');
-            jQuery('.editMode').toggleClass('editModeHidden');
-            jQuery('.readOnlyMode').toggleClass('editModeHidden');
-            jQuery('.actionButtons').toggleClass('editModeHidden');
-            jQuery('.editMessage').toggleClass('readOnlyModeHidden');
-            jQuery('.layerGroup').toggleClass('layerGroupEditMode');
-        },
-        _togglePanel: function() {
-            jQuery(".actionPanel").toggleClass('actionPanelClosed');
-        }
+        init: function() { }
     }, {
         protocol : ['Oskari.bundle.BundleInstance', 'Oskari.mapframework.module.Module']
     });
