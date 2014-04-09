792204044190f9d4df58716c6125de0218f85d02
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2014-04-09 15:15:31 +0300

    Refactoring actionpanel not to use oskari

--- UI/src/utils/ActionPanelBox.js
+++ UI/src/utils/ActionPanelBox.js
@@ -30,7 +30,6 @@ window.AssetActionPanel = function(identifier, header, icon) {
     var editMessage = '<div class="editMessage readOnlyModeHidden">Olet muokkaustilassa</div>';
 
     var cursor = {'Select' : 'default', 'Add' : 'crosshair', 'Remove' : 'no-drop'};
-    var readOnly = true;
 
     var layerPeriods = [
         {id: "current", label: "Voimassaolevat", selected: true},
