9c8a49806b14f8d257ba42590d939e7f0365e76b
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2014-04-23 13:37:21 +0300

    Calculate bearings using validity direction in csv export

--- src/test/scala/fi/liikennevirasto/digiroad2/util/AssetCsvFormatterSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/util/AssetCsvFormatterSpec.scala
@@ -27,6 +27,15 @@ class AssetCsvFormatterSpec extends FlatSpec with MustMatchers with BeforeAndAft
     csv must equal("5;;;;;374792.096855508;6677566.77442972;;;210;2;;1;1;1;0;;;;" + created + ";dr1conversion;" + validFrom + ";" + validTo + ";Liikennevirasto;235;Kauniainen;;")
   }
 
+  val testasset = AssetWithProperties(1, None, 1, 2.1, 2.2, 1, bearing = Some(3), validityDirection = None, wgslon = 2.2, wgslat = 0.56)
+  it must "recalculate bearings in validity direction" in {
+    AssetCsvFormatter.addBearing(testasset, List())._2 must equal (List("3"))
+    AssetCsvFormatter.addBearing(testasset.copy(validityDirection = Some(3)), List())._2 must equal (List("183"))
+    AssetCsvFormatter.addBearing(testasset.copy(validityDirection = Some(2)), List())._2 must equal (List("3"))
+    AssetCsvFormatter.addBearing(testasset.copy(validityDirection = Some(2), bearing = Some(195)), List())._2 must equal (List("195"))
+    AssetCsvFormatter.addBearing(testasset.copy(validityDirection = Some(3), bearing = Some(195)), List())._2 must equal (List("15"))
+  }
+
   private def extractPropertyValue(asset: AssetWithProperties, propertyPublicId: String): String = {
     asset.propertyData.find(_.publicId == propertyPublicId).get.values.head.propertyDisplayValue.getOrElse("")
   }
