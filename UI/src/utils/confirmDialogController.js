0bb2154c169014a0018f485342a986054dea7237
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-28 14:25:24 +0300

    Show confirmation dialog when asset is unselected

--- UI/src/utils/confirmDialogController.js
+++ UI/src/utils/confirmDialogController.js
@@ -1,9 +1,7 @@
-(function (){
-    return {
-        initialize: function() {
-            eventbus.on('asset:unselected', function() {
-                // TODO: Show confirmation dialog if necessary
-            });
-        }
+(function (confirmDialogController){
+    confirmDialogController.initialize = function () {
+        eventbus.on('asset:unselected', function () {
+            eventbus.trigger('confirm:show');
+        });
     };
-})();
+})(window.ConfirmDialogController = window.ConfirmDialogController || {});
--- UI/test/confirm-dialog-controller-spec.js
+++ UI/test/confirm-dialog-controller-spec.js
@@ -1,2 +1,24 @@
 describe('ConfirmDialogController', function() {
+    ConfirmDialogController.initialize();
+    var confirmDialogShown = false;
+
+    before(function() {
+        eventbus.on('confirm:show', function() { confirmDialogShown = true; });
+    });
+
+    describe('when asset is moved', function() {
+        before(function() {
+            eventbus.trigger('asset:moved');
+        });
+
+        describe('and another asset is selected', function() {
+            before(function() {
+                eventbus.trigger('asset:unselected');
+            });
+
+            it('should show confirm dialog', function() {
+                assert.isTrue(confirmDialogShown);
+            });
+        });
+    });
 });
