4729f04ad986889a9285d2d1899b93c2e4001df6
Esko Vääräsmäki <esko.vaarasmaki@gmail.com>
2014-04-28 12:02:01 +0300

    Add modal dialog for confirmation
    Conflicts:
    
    	UI/index.html

---
+++ UI/src/modalconfirm/Confirm.js
@@ -0,0 +1,38 @@
+window.Confirm = function() {
+    var confirmDiv =
+        '<div class="confirm-modal">' +
+            '<div class="confirm-modal-dialog">' +
+                '<div class="confirm-modal-dialog-info">' +
+                    'Olet muokannut tietolajia.</br>' +
+                    'Tallennat tai peru muutoksesi.' +
+                '</div>' +
+                '<div class="confirm-modal-buttons">' +
+                    '<span class="confirm-modal-button confirm-modal-button-cancel">Peruuta</span>' +
+                    '<span class="confirm-modal-button confirm-modal-button-ok">Tallenna</span>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+
+    var renderConfirmDialog = function() {
+        jQuery(".container").append(confirmDiv);
+    };
+
+    var bindEvents = function() {
+        jQuery('.confirm-modal-button-cancel').on('click', function() {
+           purge();
+        });
+        jQuery('.confirm-modal-button-ok').on('click', function() {
+            purge();
+        });
+    };
+
+    var show = function() {
+        renderConfirmDialog();
+        bindEvents();
+    };
+
+    var purge = function() {
+        jQuery(".confirm-modal").remove();
+    };
+    show();
+};
