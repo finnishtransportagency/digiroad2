0012c9dbdbc1ff23513b8a2ae663a57f6d3c7ff1
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-28 15:09:40 +0300

    Reset test context when new test starts

--- UI/test/confirm-dialog-controller-spec.js
+++ UI/test/confirm-dialog-controller-spec.js
@@ -1,15 +1,20 @@
 describe('ConfirmDialogController', function() {
-    ConfirmDialogController.initialize();
     var confirmDialogShown = false;
 
     before(function() {
         eventbus.on('confirm:show', function() { confirmDialogShown = true; });
     });
 
+    var resetTest = function() {
+        ConfirmDialogController.initialize();
+        confirmDialogShown = false;
+    };
+
     // TODO: show confirmation dialog when properties has been changed
 
     describe('when asset is moved', function() {
         before(function() {
+            resetTest();
             eventbus.trigger('asset:moved');
         });
 
@@ -26,6 +31,29 @@ describe('ConfirmDialogController', function() {
         describe('and changes are saved', function() {
             before(function() {
                 confirmDialogShown = false;
+                eventbus.trigger('asset:saved');
+            });
+
+            describe('and another asset is selected', function() {
+                before(function() {
+                    eventbus.trigger('asset:unselected');
+                });
+
+                it('should not show confirm dialog', function() {
+                    assert.isFalse(confirmDialogShown);
+                });
+            });
+        });
+    });
+
+    describe('when asset is moved', function() {
+        before(function() {
+            resetTest();
+            eventbus.trigger('asset:moved');
+        });
+
+        describe('and changes are saved', function() {
+            before(function() {
                 eventbus.trigger('asset:saved');
             });
 
