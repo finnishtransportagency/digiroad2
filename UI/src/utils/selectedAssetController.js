4d60fe08df711a559670be08695756dd2f2b6f54
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-29 18:18:19 +0300

    Cache fetched asset in asset controller and make modifications to its properties

--- UI/src/utils/selectedAssetController.js
+++ UI/src/utils/selectedAssetController.js
@@ -1,8 +1,10 @@
 (function (selectedAssetController){
     selectedAssetController.initialize = function(backend) {
+        var usedKeysFromFetchedAsset = ['assetTypeId', 'bearing', 'lat', 'lon', 'roadLinkId'];
         var propertyData = [];
         var assetIsSaved = false;
         var assetHasBeenModified = false;
+        var currentAsset = {};
 
         var reset = function() {
             assetIsSaved = false;
@@ -14,7 +16,7 @@
             if(assetHasBeenModified && !assetIsSaved) {
                 eventbus.once('confirm:ok', function() {
                     eventbus.once('asset:saved', function() { reset(); }, this);
-                    backend.updateAsset(0, { propertyData: propertyData });
+                    backend.updateAsset(0, currentAsset);
                 }, this);
                 eventbus.trigger('confirm:show');
             }
@@ -22,12 +24,49 @@
         eventbus.on('asset:moved', function() {
             assetHasBeenModified = true;
         });
-        eventbus.on('assetPropertyValue:changed', function(changedProperty) {
-            propertyData = changedProperty.propertyData;
+        eventbus.on('assetPropertyValue:changed', function(changedProperties) {
+            var transformProperties = function(properties) {
+              return _.map(properties, function(property) {
+                  var changedProperty = _.find(changedProperties.propertyData, function(p) { return p.publicId === property.publicId; });
+                  if(changedProperty) {
+                      return _.merge({}, property, _.pick(changedProperty, 'values'));
+                  } else {
+                      return property;
+                  }
+              });
+            };
+            currentAsset.properties = transformProperties(currentAsset.properties);
+            propertyData = changedProperties.propertyData;
             assetHasBeenModified = true;
         });
         eventbus.on('asset:saved', function() {
             assetIsSaved = true;
+        });
+        eventbus.on('asset:fetched', function(asset) {
+            var transformPropertyData = function(propertyData) {
+                var transformValues = function(publicId, values) {
+                    var transformValue = function(value) {
+                        return {
+                            propertyValue: value.propertyValue,
+                            propertyDisplayValue: publicId.publicId
+                        };
+                    };
+
+                    return _.map(values.values, transformValue);
+                };
+                var transformProperty = function(property) {
+                    return _.merge(
+                        {},
+                        _.pick(property, 'publicId'),
+                        {
+                            values: transformValues(_.pick(property, 'publicId'), _.pick(property, 'values'))
+                        });
+                };
+                return {
+                    properties: _.map(propertyData.propertyData, transformProperty)
+                };
+            };
+            currentAsset = _.merge({}, _.pick(asset, usedKeysFromFetchedAsset), transformPropertyData(_.pick(asset, 'propertyData')));
         });
 
         return { reset: reset };
--- UI/test/selected-asset-controller-spec.js
+++ UI/test/selected-asset-controller-spec.js
@@ -16,6 +16,7 @@ describe('SelectedAssetController', function() {
         controller.reset();
         confirmDialogShown = false;
         assetSentToBackend = null;
+        eventbus.trigger('asset:fetched', createAsset());
     };
 
     var unselectAssetAndAssertDialogShown = function(shown) {
@@ -77,7 +78,7 @@ describe('SelectedAssetController', function() {
             eventbus.trigger('assetPropertyValue:changed', {
                 propertyData: [{
                     publicId: 'vaikutussuunta',
-                    values: [{ propertyValue: 2 }]
+                    values: [{ propertyValue: '3' }]
                 }]
             });
         });
@@ -90,8 +91,83 @@ describe('SelectedAssetController', function() {
             });
 
             it('should send updated asset to backend', function() {
-                assert.equal(assetSentToBackend.propertyData[0].values[0].propertyValue, "2");
+                assert.deepEqual(assetSentToBackend, createBackendMessagePayload());
             });
         });
     });
+
+    function createAsset() {
+        return {
+            assetTypeId: 10,
+            bearing: 80,
+            externalId: 1,
+            id: 300000,
+            imageIds: ['2_1398341376263'],
+            lat: 6677267.45072414,
+            lon: 374635.608258218,
+            municipalityNumber: 235,
+            propertyData: [{
+                id: 0,
+                localizedName: 'Vaikutussuunta',
+                propertyType: 'single_choice',
+                propertyUiIndex: 65,
+                propertyValue: '2',
+                publicId: 'vaikutussuunta',
+                required: false,
+                values: [{
+                    imageId: null,
+                    propertyDisplayValue: 'Digitointisuuntaan',
+                    propertyValue: '2'}
+                ]}, {
+                id: 200,
+                localizedName: 'Pysäkin tyyppi',
+                propertyType: 'multiple_choice',
+                propertyUiIndex: 90,
+                propertyValue: '<div data-publicId="pysakin_tyyppi" name="pysakin_tyyppi" class="featureattributeChoice"><input  type="checkbox" value="5"></input><label for="pysakin_tyyppi_5">Virtuaalipysäkki</label><br/><input  type="checkbox" value="1"></input><label for="pysakin_tyyppi_1">Raitiovaunu</label><br/><input checked  type="checkbox" value="2"></input><label for="pysakin_tyyppi_2">Linja-autojen paikallisliikenne</label><br/><input  type="checkbox" value="3"></input><label for="pysakin_tyyppi_3">Linja-autojen kaukoliikenne</label><br/><input  type="checkbox" value="4"></input><label for="pysakin_tyyppi_4">Linja-autojen pikavuoro</label><br/></div>',
+                publicId: 'pysakin_tyyppi',
+                required: true,
+                values: [{
+                    imageId: '2_1398341376263',
+                    propertyDisplayValue: 'Linja-autojen paikallisliikenne',
+                    propertyValue: '2'
+                }, {
+                    imageId: '3_1398341376270',
+                    propertyDisplayValue: 'Linja-autojen kaukoliikenne',
+                    propertyValue: '3' }
+                ]}
+            ],
+            readOnly: true,
+            roadLinkId: 1140018963,
+            validityDirection: 2,
+            validityPeriod: 'current',
+            wgslat: 60.2128746641816,
+            wgslon: 24.7375812322645
+        };
+    }
+
+    function createBackendMessagePayload() {
+        return {
+            assetTypeId: 10,
+            bearing: 80,
+            lat: 6677267.45072414,
+            lon: 374635.608258218,
+            roadLinkId: 1140018963,
+            properties: [{
+                publicId: 'vaikutussuunta',
+                values: [{
+                    propertyValue: '3',
+                    propertyDisplayValue: 'vaikutussuunta'
+                }]
+            }, {
+                publicId: 'pysakin_tyyppi',
+                values: [{
+                    propertyValue: '2',
+                    propertyDisplayValue: 'pysakin_tyyppi'
+                }, {
+                    propertyValue: '3',
+                    propertyDisplayValue: 'pysakin_tyyppi'
+                }]
+            }]
+        };
+    }
 });
