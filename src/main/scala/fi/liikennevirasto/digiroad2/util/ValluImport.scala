6c4ed20032989cb626d6397951288b7b8baab051
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-05-07 11:24:00 +0300

    Make vallu complementary bus stop name data optional

--- src/main/scala/fi/liikennevirasto/digiroad2/util/ValluImport.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/util/ValluImport.scala
@@ -10,17 +10,41 @@ object ValluImport {
   val userProvider = new OracleUserProvider
   val provider = new OracleSpatialAssetProvider(userProvider)
   val header = "STOP_ID;ADMIN_STOP_ID;STOP_CODE;NAME_FI;NAME_SV;COORDINATE_X;COORDINATE_Y;ADDRESS;ROAD_NUMBER;BEARING;BEARING_DESCRIPTION;DIRECTION;LOCAL_BUS;EXPRESS_BUS;NON_STOP_EXPRESS_BUS;VIRTUAL_STOP;EQUIPMENT;REACHABILITY;SPECIAL_NEEDS;MODIFIED_TIMESTAMP;MODIFIED_BY;VALID_FROM;VALID_TO;ADMINISTRATOR_CODE;MUNICIPALITY_CODE;MUNICIPALITY_NAME;COMMENTS;CONTACT_EMAILS"
+
   def writeCsvToFile() = {
     val printer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("digiroad_stops.csv"), "UTF-8"))
+    val fileName = "valluNamesAndBusStopIds.csv"
+    val valluComplementaryBusStopNames : Map[Long, String] = readValluBusStopNames(fileName)
+
     // BOM for excel
     printer.write("\uFEFF")
     printer.write(header + "\n")
     getMunicipalities.foreach(x => {
       getAssetsForMunicipality(x)
+        .map(fetchNameFromValluImport(valluComplementaryBusStopNames, _))
         .map(getCsvRowsForAsset _)
         .foreach(x => printer.write(x + "\n"))
     })
     printer.close
+  }
+
+
+  def readValluBusStopNames(fileName: String): Map[Long, String] = {
+    if (new java.io.File(fileName).exists) {
+      val valluImportData = scala.io.Source.fromFile(fileName).getLines
+      valluImportData
+        .map(_.split(";"))
+        .foldLeft(Map[Long, String]()) {
+        (map, entry) =>
+          map + (entry(1).toLong -> entry(0))
+      }
+    } else {
+      Map()
+    }
+  }
+
+  def fetchNameFromValluImport(complementaryBusStopNames: Map[Long, String], asset: AssetWithProperties): AssetWithProperties = {
+    asset
   }
 
   def getCsvRowsForAsset(asset: AssetWithProperties) = {
