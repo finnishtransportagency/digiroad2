717c7f79c128ddb7c879c5ee6737d6d63a96c254
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-05-08 13:20:37 +0300

    Filter out new lines from comments field

--- src/test/scala/fi/liikennevirasto/digiroad2/util/AssetCsvFormatterSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/util/AssetCsvFormatterSpec.scala
@@ -62,6 +62,7 @@ class AssetCsvFormatterSpec extends FlatSpec with MustMatchers with BeforeAndAft
         case "nimi_ruotsiksi" => property.copy(values = List(PropertyValue(propertyValue = "\nnimi ruotsiksi\n", propertyDisplayValue = Some("\nnimi ruotsiksi\n"))))
         case "liikennointisuunta" => property.copy(values = List(PropertyValue(propertyValue = "\nliikennointisuunta\n", propertyDisplayValue = Some("\nliikennointisuunta\n"))))
         case "esteettomyys_liikuntarajoitteiselle" => property.copy(values = List(PropertyValue(propertyValue = "\nesteettomyys\n liikuntarajoitteiselle\n", propertyDisplayValue = Some("\nesteettomyys\n liikuntarajoitteiselle\n"))))
+        case "lisatiedot" => property.copy(values = List(PropertyValue(propertyValue = "\nlisatiedot", propertyDisplayValue = Some("\nlisatiedot"))))
         case _ => property
       }
     }
@@ -69,7 +70,7 @@ class AssetCsvFormatterSpec extends FlatSpec with MustMatchers with BeforeAndAft
     val csv = AssetCsvFormatter.formatFromAssetWithPropertiesValluCsv(asset)
     csv must equal("5;id;matkustajatunnus;nimi suomeksi;nimi ruotsiksi;374792.096855508;6677566.77442972;;;210;Etelään;liikennointisuunta;1;1;1;0;;esteettomyys liikuntarajoitteiselle;;"
       + created
-      + ";dr1conversion;" + validFrom + ";" + validTo + ";Liikennevirasto;235;Kauniainen;;")
+      + ";dr1conversion;" + validFrom + ";" + validTo + ";Liikennevirasto;235;Kauniainen;lisatiedot;")
   }
 
   private def extractPropertyValue(asset: AssetWithProperties, propertyPublicId: String): String = {
