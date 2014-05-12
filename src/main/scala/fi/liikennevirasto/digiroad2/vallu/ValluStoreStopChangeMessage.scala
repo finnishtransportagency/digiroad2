a5bad18160f7aff7eebda361176476b90df129f9
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-05-12 15:31:15 +0300

    Add asset external id to vallu xml message

--- src/main/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessage.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessage.scala
@@ -4,9 +4,10 @@ import fi.liikennevirasto.digiroad2.asset.AssetWithProperties
 
 object ValluStoreStopChangeMessage {
   def create(asset: AssetWithProperties): String = {
-    """
-      |<?xml version="1.0" encoding="UTF-8"?>
-      |<Stops>
-      |</Stops>""".stripMargin
+      val message =
+      <Stops>
+        <StopId>{asset.externalId.get}</StopId>
+      </Stops>
+    """<?xml version="1.0" encoding="UTF-8"?>""" + message.toString
   }
 }
--- src/test/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessageSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessageSpec.scala
@@ -2,23 +2,28 @@ package fi.liikennevirasto.digiroad2.vallu
 
 import org.scalatest._
 import fi.liikennevirasto.digiroad2.asset.AssetWithProperties
+import scala.xml.XML
 
 class ValluStoreStopChangeMessageSpec extends FlatSpec with MustMatchers {
-  it must "create containing element for stops" in {
-    val message: String = ValluStoreStopChangeMessage.create(AssetWithProperties(
-      id = 1,
-      externalId = None,
-      assetTypeId = 1,
-      lon = 1,
-      lat = 1,
-      roadLinkId = 1,
-      wgslon = 1,
-      wgslat = 1))
+  val testAsset = AssetWithProperties(
+    id = 1,
+    externalId = Some(123),
+    assetTypeId = 1,
+    lon = 1,
+    lat = 1,
+    roadLinkId = 1,
+    wgslon = 1,
+    wgslat = 1)
 
-    message must equal(
-      """
-        |<?xml version="1.0" encoding="UTF-8"?>
-        |<Stops>
-        |</Stops>""".stripMargin)
+  it must "specify encoding" in {
+    val message: String = ValluStoreStopChangeMessage.create(testAsset)
+    message startsWith("""<?xml version="1.0" encoding="UTF-8"?>""")
+  }
+
+  it must "create xml message containing correct external id" in {
+    val message: String = ValluStoreStopChangeMessage.create(testAsset)
+    val rootElement = XML.loadString(message)
+    val stopId = rootElement \ "StopId"
+    stopId.text must equal("123")
   }
 }
