84d70cfbb47d66ecd0e3cf1b4a278fbd77f84e94
Esko Vääräsmäki <esko.vaarasmaki@gmail.com>
2014-05-12 11:04:11 +0300

    Send hard coded xml payload

--- src/main/scala/fi/liikennevirasto/digiroad2/vallu/ValluSender.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/vallu/ValluSender.scala
@@ -5,6 +5,7 @@ import org.apache.http.entity.{StringEntity, ContentType}
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 import java.nio.charset.Charset
+import fi.liikennevirasto.digiroad2.asset.AssetWithProperties
 
 object ValluSender {
 
@@ -12,8 +13,39 @@ object ValluSender {
   val httppost = new HttpPost("http://localhost:9002")
   val httpclient = HttpClients.createDefault()
 
-  // TODO: use XML Element
-  def postToVallu(payload: String) = {
+  def postToVallu(asset: AssetWithProperties) {
+    val payload =
+      """
+        <Stops>
+          <Stop>
+            <StopId>144948</StopId>
+            <Coordinate>
+              <xCoordinate>340920</xCoordinate>
+              <yCoordinate>6987560</yCoordinate>
+            </Coordinate>
+            <Bearing>135</Bearing>
+            <StopAttribute>
+              <StopType name="LOCAL_BUS">0</StopType>
+              <StopType name="EXPRESS_BUS">1</StopType>
+              <StopType name="NON_STOP_EXPRESS_BUS">0</StopType>
+              <StopType name="VIRTUAL_STOP">0</StopType>
+            </StopAttribute>
+            <Equipment/>
+            <ModifiedTimestamp>2014-04-29T09:30:47</ModifiedTimestamp>
+            <ModifiedBy>CGI</ModifiedBy>
+            <AdministratorCode>Livi117347</AdministratorCode>
+            <MunicipalityName>Alajärvi</MunicipalityName>
+            <Comments>pysäkin koordinaatit pyöristetty</Comments>
+            <ContactEmails>
+              <Contact>pysakit@liikennevirasto.fi</Contact>
+            </ContactEmails>
+          </Stop>
+        </Stops>"""
+
+    postToVallu(payload)
+  }
+
+  private def postToVallu(payload: String) = {
     val entity = new StringEntity(payload, ContentType.create("text/plain", "UTF-8"))
     httppost.setEntity(entity)
     val response = httpclient.execute(httppost)
