845f551c997f8d9e948e545e5cb4ad6a4200dd09
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-05-14 11:46:25 +0300

    Start vallu test server in staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -16,6 +16,18 @@ role :web, %w{web@gateway}
 # extended properties on the server.
 server 'gateway', user: 'web', roles: %w{web app}, my_property: :my_value
 
+namespace :deploy do
+  task :start_vallu_server do
+    on roles(:all) do
+      execute "killall -q node; exit 0"
+      # Capistrano kills the vallu server before it gets to start up if sleep 1 is not defined
+      execute "cd #{release_path} && (nohup grunt vallu-test-server > vallu-server.log &) && sleep 1"
+    end
+  end
+
+  after :publishing, :start_vallu_server
+end
+
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
