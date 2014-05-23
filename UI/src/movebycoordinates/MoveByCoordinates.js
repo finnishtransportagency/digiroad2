275af6fb8744d4ea7c58188675a273faa07d31e8
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-05-23 12:17:30 +0300

    Fix indentation

--- UI/src/movebycoordinates/MoveByCoordinates.js
+++ UI/src/movebycoordinates/MoveByCoordinates.js
@@ -11,19 +11,19 @@ window.MoveByCoordinates = function() {
 
     var bindEvents = function() {
         coordinatesMove.on('click', function() {
-        var lonlat = $('.coordinates .lonlat').val();
-        if (lonlat.match("[A-z]")) {
-            var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-            dialog.show('Käytää koortinaateissa lon,lat numeroarvoja');
-            dialog.fadeout(2000);
-        }
-        if(lonlat.match("\\d+,\\d+")) {
-            var position = {
-                lon : lonlat.split(',')[0].trim(),
-                lat : lonlat.split(',')[1].trim()
-            };
-            eventbus.trigger('coordinates:selected',position);
-        }
+            var lonlat = $('.coordinates .lonlat').val();
+            if (lonlat.match("[A-z]")) {
+                var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
+                dialog.show('Käytää koortinaateissa lon,lat numeroarvoja');
+                dialog.fadeout(2000);
+            }
+            if (lonlat.match("\\d+,\\d+")) {
+                var position = {
+                    lon: lonlat.split(',')[0].trim(),
+                    lat: lonlat.split(',')[1].trim()
+                };
+                eventbus.trigger('coordinates:selected', position);
+            }
         });
     };
 
