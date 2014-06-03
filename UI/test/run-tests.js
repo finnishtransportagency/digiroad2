74a10d154814980f52dcb36fd76339ff55a0cde9
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-06-03 10:22:30 +0300

    Run selected asset controller spec using require

--- UI/test/run-tests.js
+++ UI/test/run-tests.js
@@ -1,13 +1,38 @@
 require.config({
     paths: {
-        'mocha': '../../bower_components/mocha/mocha'
+        'underscore':               '../bower_components/underscore/underscore',
+        'jquery':                   '../bower_components/jquery/dist/jquery.min',
+        'lodash':                   '../bower_components/lodash/dist/lodash.min',
+        'backbone':                 '../../bower_components/backbone/backbone',
+        'chai':                     '../../bower_components/chai/chai',
+        'mocha':                    '../../bower_components/mocha/mocha',
+        'EventBus':                 '../src/utils/eventbus',
+        'SelectedAssetController':  '../src/utils/selectedAssetController'
     },
     shim: {
-        'mocha': { exports: 'mocha' }
+        'jquery': { exports: '$' },
+        'lodash': { exports: '_' },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'mocha': { exports: 'mocha' },
+        'EventBus': {
+            deps: ['backbone']
+        },
+        'SelectedAssetController': {
+            deps: ['EventBus', 'lodash']
+        }
     },
     waitSeconds: 10
 });
 require(['mocha'], function(mocha) {
-    mocha.checkLeaks();
-    if (navigator.userAgent.indexOf('PhantomJS') < 0) { mocha.run(); }
+    mocha.setup({ui: 'bdd', reporter: 'html'});
+    require(['lodash', 'selected-asset-controller-spec'], function(lodash) {
+        window._ = lodash;
+        mocha.checkLeaks();
+        if (navigator.userAgent.indexOf('PhantomJS') < 0) {
+            mocha.run();
+        }
+    });
 });
--- UI/test/selected-asset-controller-spec.js
+++ UI/test/selected-asset-controller-spec.js
@@ -1,140 +1,143 @@
-describe('SelectedAssetController', function() {
-    var confirmDialogShown = false;
-    var assetSentToBackend = {};
-    var mockBackend = {
-        updateAsset: function(id, data) {
-            assetSentToBackend = {
-                id: id,
-                data: data
+define(['chai', 'SelectedAssetController'], function(chai) {
+    var assert = chai.assert;
+    describe('SelectedAssetController', function() {
+        var confirmDialogShown = false;
+        var assetSentToBackend = {};
+        var mockBackend = {
+            updateAsset: function(id, data) {
+                assetSentToBackend = {
+                    id: id,
+                    data: data
+                };
+            }
+        };
+        var controller = SelectedAssetController.initialize(mockBackend);
+
+        before(function() {
+            eventbus.on('confirm:show', function() { confirmDialogShown = true; });
+        });
+
+        var resetTest = function() {
+            controller.reset();
+            confirmDialogShown = false;
+            assetSentToBackend = {};
+            eventbus.trigger('asset:fetched', createAsset());
+        };
+
+        var assetStateIsDirty = function() {
+            return function() {
+                it('asset state should be dirty', function() {
+                    assert.equal(controller.isDirty(), true);
+                });
+            };
+        };
+
+        var assetStateIsClean = function() {
+            return function() {
+                it('asset state should be clean', function() {
+                    assert.equal(controller.isDirty(), false);
+                });
+            };
+        };
+
+        describe('when asset is moved', function() {
+            before(function() {
+                resetTest();
+                eventbus.trigger('asset:moved',{
+                    lon: 1,
+                    lat: 1,
+                    bearing: 180,
+                    roadLinkId: 3
+                });
+            });
+
+            describe('and another asset is selected', assetStateIsDirty());
+
+            describe('and changes are saved', function() {
+                before(function() {
+                    confirmDialogShown = false;
+                    eventbus.trigger('asset:saved');
+                });
+
+                describe('and another asset is selected', assetStateIsClean());
+            });
+        });
+
+        describe('when asset is moved', function() {
+            before(function() {
+                resetTest();
+                eventbus.trigger('asset:moved',{
+                    lon: 1,
+                    lat: 2,
+                    bearing: 90,
+                    roadLinkId: 4
+                });
+            });
+
+            describe('and changes are saved', function() {
+                before(function() {
+                    eventbus.trigger('asset:saved');
+                });
+
+                describe('and another asset is selected', assetStateIsClean());
+            });
+        });
+
+        describe('when asset is not moved', function () {
+            before(function() {
+                resetTest();
+            });
+
+            describe('and another asset is selected', assetStateIsClean());
+        });
+
+        function createAsset() {
+            return {
+                assetTypeId: 10,
+                bearing: 80,
+                externalId: 1,
+                id: 300000,
+                imageIds: ['2_1398341376263'],
+                lat: 6677267.45072414,
+                lon: 374635.608258218,
+                municipalityNumber: 235,
+                propertyData: [{
+                    id: 0,
+                    localizedName: 'Vaikutussuunta',
+                    propertyType: 'single_choice',
+                    propertyUiIndex: 65,
+                    propertyValue: '2',
+                    publicId: 'vaikutussuunta',
+                    required: false,
+                    values: [{
+                        imageId: null,
+                        propertyDisplayValue: 'Digitointisuuntaan',
+                        propertyValue: '2'}
+                    ]}, {
+                    id: 200,
+                    localizedName: 'Pysäkin tyyppi',
+                    propertyType: 'multiple_choice',
+                    propertyUiIndex: 90,
+                    propertyValue: '<div data-publicId="pysakin_tyyppi" name="pysakin_tyyppi" class="featureattributeChoice"><input  type="checkbox" value="5"></input><label for="pysakin_tyyppi_5">Virtuaalipysäkki</label><br/><input  type="checkbox" value="1"></input><label for="pysakin_tyyppi_1">Raitiovaunu</label><br/><input checked  type="checkbox" value="2"></input><label for="pysakin_tyyppi_2">Linja-autojen paikallisliikenne</label><br/><input  type="checkbox" value="3"></input><label for="pysakin_tyyppi_3">Linja-autojen kaukoliikenne</label><br/><input  type="checkbox" value="4"></input><label for="pysakin_tyyppi_4">Linja-autojen pikavuoro</label><br/></div>',
+                    publicId: 'pysakin_tyyppi',
+                    required: true,
+                    values: [{
+                        imageId: '2_1398341376263',
+                        propertyDisplayValue: 'Linja-autojen paikallisliikenne',
+                        propertyValue: '2'
+                    }, {
+                        imageId: '3_1398341376270',
+                        propertyDisplayValue: 'Linja-autojen kaukoliikenne',
+                        propertyValue: '3' }
+                    ]}
+                ],
+                readOnly: true,
+                roadLinkId: 1140018963,
+                validityDirection: 2,
+                validityPeriod: 'current',
+                wgslat: 60.2128746641816,
+                wgslon: 24.7375812322645
             };
         }
-    };
-    var controller = SelectedAssetController.initialize(mockBackend);
-
-    before(function() {
-        eventbus.on('confirm:show', function() { confirmDialogShown = true; });
     });
-
-    var resetTest = function() {
-        controller.reset();
-        confirmDialogShown = false;
-        assetSentToBackend = {};
-        eventbus.trigger('asset:fetched', createAsset());
-    };
-
-    var assetStateIsDirty = function() {
-        return function() {
-            it('asset state should be dirty', function() {
-                assert.equal(controller.isDirty(), true);
-            });
-        };
-    };
-
-    var assetStateIsClean = function() {
-        return function() {
-            it('asset state should be clean', function() {
-                assert.equal(controller.isDirty(), false);
-            });
-        };
-    };
-
-    describe('when asset is moved', function() {
-        before(function() {
-            resetTest();
-            eventbus.trigger('asset:moved',{
-                lon: 1,
-                lat: 1,
-                bearing: 180,
-                roadLinkId: 3
-            });
-        });
-
-        describe('and another asset is selected', assetStateIsDirty());
-
-        describe('and changes are saved', function() {
-            before(function() {
-                confirmDialogShown = false;
-                eventbus.trigger('asset:saved');
-            });
-
-            describe('and another asset is selected', assetStateIsClean());
-        });
-    });
-
-    describe('when asset is moved', function() {
-        before(function() {
-            resetTest();
-            eventbus.trigger('asset:moved',{
-                lon: 1,
-                lat: 2,
-                bearing: 90,
-                roadLinkId: 4
-            });
-        });
-
-        describe('and changes are saved', function() {
-            before(function() {
-                eventbus.trigger('asset:saved');
-            });
-
-            describe('and another asset is selected', assetStateIsClean());
-        });
-    });
-
-    describe('when asset is not moved', function () {
-        before(function() {
-            resetTest();
-        });
-
-        describe('and another asset is selected', assetStateIsClean());
-    });
-
-    function createAsset() {
-        return {
-            assetTypeId: 10,
-            bearing: 80,
-            externalId: 1,
-            id: 300000,
-            imageIds: ['2_1398341376263'],
-            lat: 6677267.45072414,
-            lon: 374635.608258218,
-            municipalityNumber: 235,
-            propertyData: [{
-                id: 0,
-                localizedName: 'Vaikutussuunta',
-                propertyType: 'single_choice',
-                propertyUiIndex: 65,
-                propertyValue: '2',
-                publicId: 'vaikutussuunta',
-                required: false,
-                values: [{
-                    imageId: null,
-                    propertyDisplayValue: 'Digitointisuuntaan',
-                    propertyValue: '2'}
-                ]}, {
-                id: 200,
-                localizedName: 'Pysäkin tyyppi',
-                propertyType: 'multiple_choice',
-                propertyUiIndex: 90,
-                propertyValue: '<div data-publicId="pysakin_tyyppi" name="pysakin_tyyppi" class="featureattributeChoice"><input  type="checkbox" value="5"></input><label for="pysakin_tyyppi_5">Virtuaalipysäkki</label><br/><input  type="checkbox" value="1"></input><label for="pysakin_tyyppi_1">Raitiovaunu</label><br/><input checked  type="checkbox" value="2"></input><label for="pysakin_tyyppi_2">Linja-autojen paikallisliikenne</label><br/><input  type="checkbox" value="3"></input><label for="pysakin_tyyppi_3">Linja-autojen kaukoliikenne</label><br/><input  type="checkbox" value="4"></input><label for="pysakin_tyyppi_4">Linja-autojen pikavuoro</label><br/></div>',
-                publicId: 'pysakin_tyyppi',
-                required: true,
-                values: [{
-                    imageId: '2_1398341376263',
-                    propertyDisplayValue: 'Linja-autojen paikallisliikenne',
-                    propertyValue: '2'
-                }, {
-                    imageId: '3_1398341376270',
-                    propertyDisplayValue: 'Linja-autojen kaukoliikenne',
-                    propertyValue: '3' }
-                ]}
-            ],
-            readOnly: true,
-            roadLinkId: 1140018963,
-            validityDirection: 2,
-            validityPeriod: 'current',
-            wgslat: 60.2128746641816,
-            wgslon: 24.7375812322645
-        };
-    }
 });
