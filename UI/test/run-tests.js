231fcc5ac5ca9dd019f6a087e37367dcbc5b915c
Mikael Koponen <mikael.koponen@gmail.com>
2014-06-04 14:49:22 +0300

    Run geometry calculations spec using require

--- UI/test/run-tests.js
+++ UI/test/run-tests.js
@@ -9,7 +9,8 @@ require.config({
         'SelectedAssetController':  '../src/utils/selectedAssetController',
         'LinearAssetLayer':         '../src/bundles/digiroad2/bundle/map/LinearAssetLayer',
         'OpenLayers':               '../bower_components/oskari.org/packages/openlayers/bundle/openlayers-build/OpenLayers',
-        'zoomlevels':               '../src/utils/zoomLevels'
+        'zoomlevels':               '../src/utils/zoomLevels',
+        'geometrycalculator':       '../src/utils/geometry-calculations'
     },
     shim: {
         'jquery': { exports: '$' },
@@ -27,13 +28,17 @@ require.config({
         'LinearAssetLayer': {
             exports: 'LinearAssetLayer',
             deps: ['OpenLayers']
+        },
+        'geometrycalculator': {
+            exports: 'geometrycalculator'
         }
     },
     waitSeconds: 10
 });
 require(['lodash',
          'selected-asset-controller-spec',
-         'linear-asset-layer-spec'], function(lodash) {
+         'linear-asset-layer-spec',
+         'geometry-calculations-spec'], function(lodash) {
     window._ = lodash;
     mocha.checkLeaks();
     if(window.mochaPhantomJS) { mochaPhantomJS.run(); }
