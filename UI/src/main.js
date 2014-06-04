0b9bc578612d4779904c4bb876b5f6b1aa2e2c65
Vesa Marttila <vtmarttila@gmail.com>
2014-06-04 11:27:41 +0300

    Extract start up code to `main.js`

---
+++ UI/src/main.js
@@ -0,0 +1,25 @@
+var parseQueryParameters = function(queryString) {
+    return _.chain(queryString.split('&'))
+            .map(function(param) {
+                return param.split('=');
+            })
+            .reduce(function(acc, param) {
+                acc[param[0]] = param[1];
+                return acc;
+            }, {})
+            .value();
+}
+
+$(function() {
+    var queryString = window.location.search.substring(1);
+    var parameters = parseQueryParameters(queryString);
+    var fakeMode = parameters.withFakeData === 'true';
+
+    if (fakeMode) {
+        $.getScript('test_data/TestData.js', function() {
+            Application.start(Backend.withRoadLinkData(TestData.roadLinkData()));
+        });
+    } else {
+        Application.start();
+    }
+});
