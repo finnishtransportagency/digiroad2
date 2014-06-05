29c33028dfa581e0f7aac52558be86997a0c9ff6
simula77 <samuli.karjula@reaktor.fi>
2014-06-05 12:34:36 +0300

    Rename zip file of Vallu export digiroad_stops.zip -> all.zip as requested by Rikhard Pärssinen

--- vallu_import.sh
+++ vallu_import.sh
@@ -2,6 +2,6 @@
 ./sbt ${1} 'project digiroad2' "runMain fi.liikennevirasto.digiroad2.util.ValluImport"
 zip digiroad_stops digiroad_stops.csv
 set -- $(<ftp.conf)
-curl -u $1:$2 -T digiroad_stops.zip ftp://$3/digiroad_stops.zip
+curl -u $1:$2 -T digiroad_stops.zip ftp://$3/all.zip
 date "+%Y%m%d%H%M%S" > flag.txt
 curl -u $1:$2 -T flag.txt ftp://$3/flag.txt
