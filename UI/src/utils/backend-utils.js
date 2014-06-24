41ef95e313a6cfbf946f3c9beda69594d55238d9
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-06-24 12:45:37 +0300

    Revert "Revert "Merge pull request #269 from finnishtransportagency/grouping-assets""
    This reverts commit 3981ca3211b514303b57f7177eace2edbc055b47.

--- UI/src/utils/backend-utils.js
+++ UI/src/utils/backend-utils.js
@@ -15,7 +15,8 @@
 
         backend.getAssets =  _.throttle(function(assetTypeId, boundingBox) {
             jQuery.getJSON('api/assets?assetTypeId=' + assetTypeId + '&bbox=' + boundingBox, function(assets) {
-                eventbus.trigger('assets:fetched', assets);
+                var groupedAssets = window.groupBusStops ? assetGrouping.groupByDistance(assets) : assets;
+                eventbus.trigger('assets:fetched', groupedAssets);
             }).fail(function() {
                 console.log("error");
             });
