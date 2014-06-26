3f6ce56dabb404147f24b26ba2c0a4b6d0e22e41
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-06-26 13:54:30 +0300

    Use $ for jQuery consistently

--- UI/src/utils/backend-utils.js
+++ UI/src/utils/backend-utils.js
@@ -3,7 +3,7 @@
 
   function initialize(backend) {
     backend.getEnumeratedPropertyValues = function (assetTypeId) {
-      jQuery.getJSON('api/enumeratedPropertyValues/' + assetTypeId, function (enumeratedPropertyValues) {
+      $.getJSON('api/enumeratedPropertyValues/' + assetTypeId, function (enumeratedPropertyValues) {
         eventbus.trigger('enumeratedPropertyValues:fetched', enumeratedPropertyValues);
       })
         .fail(function () {
@@ -16,7 +16,7 @@
     };
 
     backend.getAssets = _.throttle(function (assetTypeId, boundingBox) {
-      jQuery.getJSON('api/assets?assetTypeId=' + assetTypeId + '&bbox=' + boundingBox, function (assets) {
+      $.getJSON('api/assets?assetTypeId=' + assetTypeId + '&bbox=' + boundingBox, function (assets) {
         var groupedAssets = window.groupBusStops ? assetGrouping.groupByDistance(assets) : assets;
         eventbus.trigger('assets:fetched', groupedAssets);
       }).fail(function () {
@@ -25,13 +25,13 @@
     }, 1000);
 
     backend.getLinearAssets = _.throttle(function (boundingBox) {
-      jQuery.getJSON('api/linearassets?bbox=' + boundingBox, function (linearAssets) {
+      $.getJSON('api/linearassets?bbox=' + boundingBox, function (linearAssets) {
         eventbus.trigger('linearAssets:fetched', linearAssets);
       });
     }, 1000);
 
     backend.getRoadLinks = _.throttle(function (boundingBox) {
-      jQuery.getJSON('api/roadlinks?bbox=' + boundingBox, function (roadLinks) {
+      $.getJSON('api/roadlinks?bbox=' + boundingBox, function (roadLinks) {
         eventbus.trigger('roadLinks:fetched', roadLinks);
       });
     }, 1000);
@@ -61,7 +61,7 @@
     };
 
     backend.getApplicationSetup = function() {
-      jQuery.ajax({
+      $.ajax({
         type : 'GET',
         dataType : 'json',
         url : 'full_appsetup.json',
@@ -83,7 +83,7 @@
 
     backend.createAsset = function (data) {
       eventbus.trigger('asset:creating');
-      jQuery.ajax({
+      $.ajax({
         contentType: "application/json",
         type: "POST",
         url: "api/assets",
@@ -98,7 +98,7 @@
 
     backend.updateAsset = function (id, data) {
       eventbus.trigger('asset:saving');
-      jQuery.ajax({
+      $.ajax({
         contentType: "application/json",
         type: "PUT",
         url: "api/assets/" + id,
