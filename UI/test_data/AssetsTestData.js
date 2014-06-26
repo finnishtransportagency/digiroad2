672ec03795ee2f20422dad6ab5ac2f31e3a49fd9
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-06-26 16:45:45 +0300

    Reorder test assets

--- UI/test_data/AssetsTestData.js
+++ UI/test_data/AssetsTestData.js
@@ -3,22 +3,6 @@
     generate: function() {
       return [
         {
-          id: 300347,
-          externalId: 300065,
-          assetTypeId: 10,
-          lon: 374024.24401,
-          lat: 6676620.72589626,
-          roadLinkId: 5771,
-          imageIds: [
-            "2_1403010580826"
-          ],
-          bearing: 219,
-          validityDirection: 3,
-          readOnly: true,
-          municipalityNumber: 235,
-          validityPeriod: "current"
-        },
-        {
           id: 300348,
           externalId: 300066,
           assetTypeId: 10,
@@ -33,6 +17,22 @@
           readOnly: true,
           municipalityNumber: 235,
           validityPeriod: "future"
+        },
+        {
+          id: 300347,
+          externalId: 300065,
+          assetTypeId: 10,
+          lon: 374024.24401,
+          lat: 6676620.72589626,
+          roadLinkId: 5771,
+          imageIds: [
+            "2_1403010580826"
+          ],
+          bearing: 219,
+          validityDirection: 3,
+          readOnly: true,
+          municipalityNumber: 235,
+          validityPeriod: "current"
         }
       ];
     }
