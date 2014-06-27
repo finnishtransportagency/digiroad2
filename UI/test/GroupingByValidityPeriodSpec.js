5363d71669c53b7470f8d36f3968857f6270114b
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-06-27 14:50:31 +0300

    Verify that stop in group has root

--- UI/test/GroupingByValidityPeriodSpec.js
+++ UI/test/GroupingByValidityPeriodSpec.js
@@ -4,8 +4,14 @@ define(['chai'], function(chai) {
 
   describe('when loading application with overlapping bus stops in different validity periods', function() {
     it('only includes bus stops in the selected validity period to the group', function() {
-      expect($('[data-asset-id=300348]')).to.be.hidden;
-      expect($('[data-asset-id=300347]')).to.be.visible;
+      var currentStop = $('[data-asset-id=300347]');
+      var futureStop = $('[data-asset-id=300348]');
+
+      expect(futureStop).to.be.hidden;
+      expect(currentStop).to.be.visible;
+
+      expect(currentStop.find('.bus-basic-marker')).to.have.class('root');
+      expect(futureStop.find('.bus-basic-marker')).not.to.have.class('root');
     });
   });
 
