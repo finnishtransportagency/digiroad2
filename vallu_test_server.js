83a94842a2982ad488d3878ddad90620e9d4b431
Mikael Koponen <mikael.koponen@gmail.com>
2014-07-03 09:37:25 +0300

    Proxy vallu integration requests to real api url

--- vallu_test_server.js
+++ vallu_test_server.js
@@ -1,5 +1,12 @@
 var http = require('http');
 var util = require('util');
+var httpProxy = require('http-proxy')
+
+var VALLU_API_URL = process.env.VALLU_API_URL;
+
+var proxy = httpProxy.createProxyServer({
+  secure: false
+});
 
 var server = http.createServer(function (req, res) {
   if (req.method == 'POST') {
@@ -7,12 +14,16 @@ var server = http.createServer(function (req, res) {
     req.on('data', function (data) {
       body += data;
     });
-
     req.on('end', function () {
-      res.writeHead(200, {'Content-Type': 'text/xml'});
       console.log(body);
-      res.end(body);
+      if (!VALLU_API_URL) {
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(body);
+      }
     });
+    if (VALLU_API_URL) {
+      proxy.web(req, res, { target: VALLU_API_URL });
+    }
   } else {
     console.log('Vallu local test only handles POST request!');
     res.end();
@@ -20,6 +31,11 @@ var server = http.createServer(function (req, res) {
 });
 
 var host = process.argv[2];
-var port = process.argv[3]
+var port = process.argv[3];
+if (VALLU_API_URL) {
+  console.log('Using Vallu API URL:', VALLU_API_URL);
+} else {
+  console.log('Vallu API URL not set')
+}
 console.log(util.format('Starting echoing vallu test server on %s in port %d', host, port));
 server.listen(port, host);
