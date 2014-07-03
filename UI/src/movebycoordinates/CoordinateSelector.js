53f3ea138171e14c5cec5c298a962f3b591cb0b1
Mikael Koponen <mikael.koponen@gmail.com>
2014-07-03 10:20:16 +0300

    Fix placeholder text for coordinates selector

--- UI/src/movebycoordinates/CoordinateSelector.js
+++ UI/src/movebycoordinates/CoordinateSelector.js
@@ -2,7 +2,7 @@ window.CoordinateSelector = function(parentElement, extent) {
     var tooltip = "Koordinaattien sy&ouml;tt&ouml;: pohjoinen (7 merkki&auml;), it&auml; (6 merkki&auml;). Esim. 6901839, 435323";
     var crosshairToggle = $('<div class="crosshair-wrapper"><div class="checkbox"><label><input type="checkbox" name="crosshair" value="crosshair" checked="true"/> Kohdistin</label></div></div>');
     var coordinatesDiv = $('<div class="coordinates-wrapper"/>');
-    var coordinatesText = $('<input type="text" class="lonlat form-control input-sm" name="lonlat" placeholder="lat, lon" title="' + tooltip +'"/>');
+    var coordinatesText = $('<input type="text" class="lonlat form-control input-sm" name="lonlat" placeholder="P, I" title="' + tooltip +'"/>');
     var moveButton = $('<button class="btn btn-sm btn-tertiary">Siirry</button>');
     var markButton = $('<button class="btn btn-sm btn-tertiary">Merkitse</button>');
 
