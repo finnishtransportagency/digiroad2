966738e50531f075dc32e7da76e71f86854f79df
Mikael Koponen <mikael.koponen@gmail.com>
2014-07-15 11:09:20 +0300

    Split integration tests to smaller subtests

--- UI/test/GroupingByValidityPeriodSpec.js
+++ UI/test/GroupingByValidityPeriodSpec.js
@@ -4,6 +4,8 @@ define(['chai'], function(chai) {
   var assert = chai.assert;
 
   describe('when loading application with overlapping bus stops in different validity periods', function() {
+    this.timeout(5000);
+
     it('only includes bus stops in the selected validity period to the group', function() {
       var currentStop = $('[data-asset-id=300347]');
       var futureStop = $('[data-asset-id=300348]');
@@ -12,27 +14,43 @@ define(['chai'], function(chai) {
       expect(currentStop).to.be.visible;
 
       expect(currentStop.find('.bus-basic-marker')).to.have.class('root');
+    });
 
-      $('#map-tools input[name=future]:first').click();
+    describe('and when selecting future validity period', function() {
+      before(function() {
+        $('#map-tools input[name=future]:first').click();
+      });
 
-      expect(futureStop).to.be.visible;
-      expect(currentStop).to.be.visible;
+      it('includes bus stops in future and current validity period', function() {
+        var currentStop = $('[data-asset-id=300347]');
+        var futureStop = $('[data-asset-id=300348]');
 
-      var currentStopMarker = currentStop.find('.bus-basic-marker');
-      var futureStopMarker = futureStop.find('.bus-basic-marker');
+        expect(futureStop).to.be.visible;
+        expect(currentStop).to.be.visible;
 
-      expect(currentStopMarker).to.have.class('root');
-      expect(futureStopMarker).not.to.have.class('root');
-      assert(currentStopMarker.offset().top > futureStopMarker.offset().top, 'Future stop is above current stop');
+        var currentStopMarker = currentStop.find('.bus-basic-marker');
+        var futureStopMarker = futureStop.find('.bus-basic-marker');
 
-      $('#map-tools input[name=current]:first').click();
+        expect(currentStopMarker).to.have.class('root');
+        expect(futureStopMarker).not.to.have.class('root');
+        assert(currentStopMarker.offset().top > futureStopMarker.offset().top, 'Future stop is above current stop');
+      });
 
-      expect(futureStop).to.be.visible;
-      expect(currentStop).to.be.hidden;
+      describe('and current validity period is deselected', function() {
+        before(function() {
+          $('#map-tools input[name=current]:first').click();
+        });
 
-      expect(futureStop.find('.bus-basic-marker')).to.have.class('root');
+        it('includes only bus stop in future', function() {
+          var currentStop = $('[data-asset-id=300347]');
+          var futureStop = $('[data-asset-id=300348]');
 
+          expect(futureStop).to.be.visible;
+          expect(currentStop).to.be.hidden;
+
+          expect(futureStop.find('.bus-basic-marker')).to.have.class('root');
+        });
+      });
     });
   });
-
 });
