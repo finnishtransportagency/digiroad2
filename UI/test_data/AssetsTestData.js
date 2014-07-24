a94733ff210da3fb7f8080c199da0cc5ea89f64f
Mikael Koponen <mikael.koponen@gmail.com>
2014-07-24 17:00:43 +0300

    Introduce function to generate test assets

--- UI/test_data/AssetsTestData.js
+++ UI/test_data/AssetsTestData.js
@@ -3,8 +3,8 @@
     generate: function() {
       return this.withValidityPeriods(['future', 'current']);
     },
-    withValidityPeriods: function(validityPeriods) {
-      return [
+    generateAsset: function(properties) {
+      return _.defaults(properties,
         {
           id: 300348,
           externalId: 300066,
@@ -12,32 +12,18 @@
           lon: 374750,
           lat: 6677409,
           roadLinkId: 5809,
-          imageIds: [
-            "2_1403010580826"
-          ],
+          imageIds: ['2_1403010580826'],
           bearing: 219,
           validityDirection: 3,
           readOnly: true,
           municipalityNumber: 235,
-          validityPeriod: validityPeriods[0]
-        },
-        {
-          id: 300347,
-          externalId: 300065,
-          assetTypeId: 10,
-          lon: 374750,
-          lat: 6677409,
-          roadLinkId: 5809,
-          imageIds: [
-            "2_1403010580826"
-          ],
-          bearing: 219,
-          validityDirection: 3,
-          readOnly: true,
-          municipalityNumber: 235,
-          validityPeriod: validityPeriods[1]
+          validityPeriod: 'current'
         }
-      ];
+      );
+    },
+    withValidityPeriods: function(validityPeriods) {
+      return [this.generateAsset({id: 300348, externalId: 300066, validityPeriod: validityPeriods[0]}),
+              this.generateAsset({id: 300347, externalId: 300065, validityPeriod: validityPeriods[1]})];
     }
   }
 }(this));
