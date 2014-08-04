e925bb4dbcd0fa85adc112451803b51f50e4f1f8
simula77 <samuli.karjula@reaktor.fi>
2014-08-04 15:43:30 +0300

    Reference properties by public_id

--- digiroad2-oracle/src/main/resources/db/migration/V4__add_speed_limit_properties.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V4__add_speed_limit_properties.sql
@@ -1,31 +1,31 @@
 insert into localized_string (id, value_fi, value_sv, created_by, created_date) values(primary_key_seq.nextval, 'Rajoitus', '', 'db_migration_v4', sysdate);
 
-insert into property (id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
-values (primary_key_seq.nextval, (select id from asset_type where name = 'Nopeusrajoitukset'), 'db_migration_v4', (select id from localized_string where value_fi = 'Rajoitus'), 'single_choice', 30);
+insert into property (id, public_id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
+values (primary_key_seq.nextval, 'rajoitus', (select id from asset_type where name = 'Nopeusrajoitukset'), 'db_migration_v4', (select id from localized_string where value_fi = 'Rajoitus'), 'single_choice', 30);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 20, '20', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 20, '20', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 30, '30', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 30, '30', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 40, '40', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 40, '40', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 50, '50', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 50, '50', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 60, '60', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 60, '60', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 70, '70', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 70, '70', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 80, '80', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 80, '80', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 100, '100', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 100, '100', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
 
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 120, '120', '', (select property.id from property, localized_string where localized_string.value_fi = 'Rajoitus' and property.NAME_LOCALIZED_STRING_ID = localized_string.id), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 120, '120', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
