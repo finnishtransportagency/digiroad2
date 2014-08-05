5115912dd248190c82cb69bdc364f61cf5b7fb72
Mikael Koponen <mikael.koponen@gmail.com>
2014-08-05 12:50:27 +0300

    Fix indentation

--- UI/src/bundles/digiroad2/bundle/map/LinearAssetLayer.js
+++ UI/src/bundles/digiroad2/bundle/map/LinearAssetLayer.js
@@ -1,57 +1,57 @@
 window.LinearAssetLayer = function(map, backend) {
-    backend = backend || Backend;
+  backend = backend || Backend;
 
-    var vectorLayer = new OpenLayers.Layer.Vector("linearAsset", {
-        styleMap: new OpenLayers.StyleMap({
-            "default": new OpenLayers.Style(OpenLayers.Util.applyDefaults({
-                strokeColor: "#B22222",
-                strokeWidth: 8
-            }))
-        })
+  var vectorLayer = new OpenLayers.Layer.Vector("linearAsset", {
+    styleMap: new OpenLayers.StyleMap({
+      "default": new OpenLayers.Style(OpenLayers.Util.applyDefaults({
+        strokeColor: "#B22222",
+        strokeWidth: 8
+      }))
+    })
+  });
+  vectorLayer.setOpacity(1);
+
+  var showLayer = function() {
+    map.addLayer(vectorLayer);
+    vectorLayer.setVisibility(true);
+    if (zoomlevels.isInAssetZoomLevel(map.getZoom())) {
+      backend.getLinearAssets(map.getExtent());
+    }
+  };
+  var hideLayer = function() {
+    map.removeLayer(vectorLayer);
+  };
+
+  eventbus.on('layer:selected', function(layer) {
+    if (layer === 'linearAsset') {
+      showLayer();
+    } else {
+      hideLayer();
+    }
+  }, this);
+
+  eventbus.on('map:moved', function(state) {
+    if (zoomlevels.isInAssetZoomLevel(state.zoom) && state.selectedLayer === 'linearAsset') {
+      backend.getLinearAssets(state.bbox);
+    } else {
+      vectorLayer.removeAllFeatures();
+    }
+  }, this);
+
+  var drawLinearAssets = function(linearAssets) {
+    vectorLayer.removeAllFeatures();
+    var features = _.map(linearAssets, function(linearAsset) {
+      var points = _.map(linearAsset.points, function(point) {
+        return new OpenLayers.Geometry.Point(point.x, point.y);
+      });
+      return new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString(points), null);
     });
-    vectorLayer.setOpacity(1);
+    vectorLayer.addFeatures(features);
+  };
 
-    var showLayer = function() {
-        map.addLayer(vectorLayer);
-        vectorLayer.setVisibility(true);
-        if (zoomlevels.isInAssetZoomLevel(map.getZoom())) {
-            backend.getLinearAssets(map.getExtent());
-        }
-    };
-    var hideLayer = function() {
-        map.removeLayer(vectorLayer);
-    };
-
-    eventbus.on('layer:selected', function(layer) {
-        if (layer === 'linearAsset') {
-            showLayer();
-        } else {
-            hideLayer();
-        }
-    }, this);
-
-    eventbus.on('map:moved', function(state) {
-        if (zoomlevels.isInAssetZoomLevel(state.zoom) && state.selectedLayer === 'linearAsset') {
-            backend.getLinearAssets(state.bbox);
-        } else {
-            vectorLayer.removeAllFeatures();
-        }
-    }, this);
-
-    var drawLinearAssets = function(linearAssets) {
-        vectorLayer.removeAllFeatures();
-        var features = _.map(linearAssets, function (linearAsset) {
-            var points = _.map(linearAsset.points, function (point) {
-                return new OpenLayers.Geometry.Point(point.x, point.y);
-            });
-            return new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString(points), null);
-        });
-        vectorLayer.addFeatures(features);
-    };
-
-    eventbus.on('linearAssets:fetched', function(linearAssets) {
-        if (zoomlevels.isInAssetZoomLevel(map.getZoom())) {
-            drawLinearAssets(linearAssets);
-        }
-    }, this);
+  eventbus.on('linearAssets:fetched', function(linearAssets) {
+    if (zoomlevels.isInAssetZoomLevel(map.getZoom())) {
+      drawLinearAssets(linearAssets);
+    }
+  }, this);
 };
