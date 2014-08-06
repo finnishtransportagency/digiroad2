6f708a29ea7bbe71115f60c1273dbf9141a27548
Mikael Koponen <mikael.koponen@gmail.com>
2014-08-06 16:32:24 +0300

    Add test data for linear assets

---
+++ UI/test_data/LinearAssetsTestData.js
@@ -0,0 +1,22 @@
+(function(root) {
+  root.LinearAssetsTestData = {
+    generate: function() {
+      return [
+        {
+          "id": 1123812,
+          "limit": 40,
+          "points": [
+            {
+              "x": 374895.585,
+              "y": 6677506.848
+            },
+            {
+              "x": 374945.976,
+              "y": 6677538.7
+            }
+          ]
+        }
+      ];
+    }
+  }
+})(this);
