a0d3998861c38f9ed05c2be30a80cb2c621e695f
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-08 14:10:58 +0300

    Fix indentation

--- UI/src/bundles/digiroad2/bundle/map/template/Templates.js
+++ UI/src/bundles/digiroad2/bundle/map/template/Templates.js
@@ -1,25 +1,25 @@
 Oskari.clazz.define('Oskari.digiroad2.bundle.map.template.Templates',
-    function () {
-        _.templateSettings = {
-            interpolate: /\{\{(.+?)\}\}/g
-        };
-        var styleMap = new OpenLayers.StyleMap({
-            "select": new OpenLayers.Style({
-                strokeWidth: 6,
-                strokeOpacity: 1,
-                strokeColor: "#5eaedf"
-            }),
-            "default": new OpenLayers.Style({
-                strokeWidth: 5,
-                strokeColor: "#a4a4a2",
-                strokeOpacity: 0.7
-            })
-        });
+  function() {
+    _.templateSettings = {
+      interpolate: /\{\{(.+?)\}\}/g
+    };
+    var styleMap = new OpenLayers.StyleMap({
+      "select": new OpenLayers.Style({
+        strokeWidth: 6,
+        strokeOpacity: 1,
+        strokeColor: "#5eaedf"
+      }),
+      "default": new OpenLayers.Style({
+        strokeWidth: 5,
+        strokeColor: "#a4a4a2",
+        strokeOpacity: 0.7
+      })
+    });
 
-        this.roadStyles = styleMap;
-        styleMap.styles.default.rules.push(new OpenLayers.Rule({
-            elseFilter: true,
-            symbolizer: styleMap.styles.default.defaultStyle
-        }));
-    }
+    this.roadStyles = styleMap;
+    styleMap.styles.default.rules.push(new OpenLayers.Rule({
+      elseFilter: true,
+      symbolizer: styleMap.styles.default.defaultStyle
+    }));
+  }
 );
