6806dfc978a91e1caf7abb0eaba5a7ed71c83545
Vesa Marttila <vtmarttila@gmail.com>
2014-08-08 10:54:57 +0300

    Use RequireJS to load test data

--- UI/test/run-integration-tests.js
+++ UI/test/run-integration-tests.js
@@ -1,23 +1,72 @@
 require.config({
   paths: {
-    'jquery':         '../../bower_components/jquery/dist/jquery.min',
-    'chai':           '../../bower_components/chai/chai',
-    'chai-jquery':    '../../bower_components/chai-jquery/chai-jquery',
-    'eventbus':       '../src/utils/eventbus',
-    'AssetsTestData': '../test_data/AssetsTestData'
+    jquery:                           '../../bower_components/jquery/dist/jquery.min',
+    chai:                             '../../bower_components/chai/chai',
+    'chai-jquery':                    '../../bower_components/chai-jquery/chai-jquery',
+    eventbus:                         '../src/utils/eventbus',
+    AssetsTestData:                   '../test_data/AssetsTestData',
+    RoadLinkTestData:                 '../test_data/RoadLinkTestData',
+    UserRolesTestData:                '../test_data/UserRolesTestData',
+    EnumeratedPropertyValuesTestData: '../test_data/EnumeratedPropertyValuesTestData',
+    ApplicationSetupTestData:         '../test_data/ApplicationSetupTestData',
+    ConfigurationTestData:            '../test_data/ConfigurationTestData',
+    AssetPropertyNamesTestData:       '../test_data/AssetPropertyNamesTestData',
+    LinearAssetsTestData:             '../test_data/LinearAssetsTestData'
   },
   shim: {
     'chai-jquery': ['jquery', 'chai'],
     'eventbus': { exports: 'eventbus' },
-    'AssetsTestData': { exports: 'AssetsTestData' }
+    'AssetsTestData': { exports: 'AssetsTestData' },
+    'RoadLinkTestData': { exports: 'RoadLinkTestData' },
+    'UserRolesTestData': { exports: 'UserRolesTestData' },
+    'EnumeratedPropertyValuesTestData': { exports: 'EnumeratedPropertyValuesTestData' },
+    'ApplicationSetupTestData': { exports: 'ApplicationSetupTestData' },
+    'ConfigurationTestData': { exports: 'ConfigurationTestData' },
+    'AssetPropertyNamesTestData': { exports: 'AssetPropertyNamesTestData' },
+    'LinearAssetsTestData': { exports: 'LinearAssetsTestData' }
   },
   waitSeconds: 10
 });
-require(['chai', 'chai-jquery', 'GroupingByValidityPeriodSpec', 'AssetCreationSpec', 'AssetMoveSpec', 'SpeedLimitVisualizationSpec'], function(chai, chaiJquery) {
+require(['chai',
+         'chai-jquery',
+         'AssetsTestData',
+         'RoadLinkTestData',
+         'UserRolesTestData',
+         'EnumeratedPropertyValuesTestData',
+         'ApplicationSetupTestData',
+         'ConfigurationTestData',
+         'AssetPropertyNamesTestData',
+         'LinearAssetsTestData',
+         'GroupingByValidityPeriodSpec',
+         'AssetCreationSpec',
+         'AssetMoveSpec',
+         'SpeedLimitVisualizationSpec'],
+        function(chai,
+                 chaiJquery,
+                 AssetsTestData,
+                 RoadLinkTestData,
+                 UserRolesTestData,
+                 EnumeratedPropertyValuesTestData,
+                 ApplicationSetupTestData,
+                 ConfigurationTestData,
+                 AssetPropertyNamesTestData,
+                 LinearAssetsTestData) {
   chai.use(chaiJquery);
 
   eventbus.once('map:initialized', function() {
-    if(window.mochaPhantomJS) { mochaPhantomJS.run(); }
+    if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
     else { mocha.run(); }
   });
+
+  jQuery.browser = {msie: false}; // Fixes broken oskari ie browser test
+  Application.start(
+    Backend.withRoadLinkData(RoadLinkTestData.generate())
+      .withUserRolesData(UserRolesTestData.generate())
+      .withEnumeratedPropertyValues(EnumeratedPropertyValuesTestData.generate())
+      .withApplicationSetupData(ApplicationSetupTestData.generate())
+      .withConfigurationData(ConfigurationTestData.generate())
+      .withAssetPropertyNamesData(AssetPropertyNamesTestData.generate())
+      .withAssetsData(AssetsTestData.generate())
+      .withLinearAssetsData(LinearAssetsTestData.generate()));
 });
+
