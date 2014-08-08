fcc62672ffb5f08e788e15bf503824599b3d7b27
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-08 14:09:33 +0300

    Remove obsolete linear asset

--- UI/src/packages/digiroad2/bundle/map/bundle.js
+++ UI/src/packages/digiroad2/bundle/map/bundle.js
@@ -31,9 +31,6 @@ Oskari.clazz.define("Oskari.digiroad2.bundle.map.Map", function() {
                 "src" : "../../../../bundles/digiroad2/bundle/map/template/Templates.js"
             },{
                 "type" : "text/javascript",
-                "src" : "../../../../bundles/digiroad2/bundle/map/domain/LinearAsset.js"
-            },{
-                "type" : "text/javascript",
                 "src" : "../../../../bundles/digiroad2/bundle/map/LinearAssetLayer.js"
             },{
                 "type" : "text/javascript",
