a579c9aaa57db440d7d41cbf55488b2e5b832ce3
Mikael Koponen <mikael.koponen@gmail.com>
2014-08-18 16:47:21 +0300

    Add start and end points to speed limit form

--- UI/src/view/SpeedLimitForm.js
+++ UI/src/view/SpeedLimitForm.js
@@ -1,13 +1,27 @@
 (function (root) {
   var template = function(speedLimit) {
+    var startPoint = _.first(speedLimit.points);
+    var endPoint = _.last(speedLimit.points);
+    var fields = [
+      {key: "Rajoitus", value: speedLimit.limit},
+      {key: "Alkupiste X", value: startPoint.x},
+      {key: "Y", value: startPoint.y},
+      {key: "Loppupiste X", value: endPoint.x},
+      {key: "Y", value: endPoint.y}
+    ];
+
+    var formElementTemplate = _.template(
+      '<div class="form-group">' +
+        '<label class="control-label">{{ key }}</label>' +
+        '<p class="form-control-static">{{ value }}</p>' +
+      '</div>'
+    );
+
     return '<header>Segmentin ID: ' + speedLimit.id + '</header>' +
            '<div class="wrapper read-only">' +
              '<div class="form form-horizontal form-dark">' +
-                '<div class="form-group">' +
-                  '<label class="control-label">Rajoitus</label>' +
-                  '<p class="form-control-static">' + speedLimit.limit + '</p>' +
-                '</div>' +
-              '</div>' +
+               _.map(fields, formElementTemplate).join('') +
+             '</div>' +
            '</div>';
   };
 
