24621bc4b36147dee7f932a0a50c6860eaaf6482
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-18 15:54:37 +0300

    Introduce MapView to replace Map module

--- UI/src/bundles/digiroad2/bundle/map/Map.js
+++ UI/src/bundles/digiroad2/bundle/map/Map.js
@@ -33,68 +33,7 @@ Oskari.clazz.define('Oskari.digiroad2.bundle.map.Map',
       this.getMapModule().setLayerPlugin('map', null);
     },
     init: function(sandbox) {
-      eventbus.on('application:initialized', function() {
-        var zoom = this._map.getZoom();
-        applicationModel.setZoomLevel(zoom);
-        if (!zoomlevels.isInAssetZoomLevel(zoom)) {
-          this.showAssetZoomDialog();
-        }
-        new CoordinateSelector($('.mapplugin.coordinates'), this._map.getMaxExtent());
-        this._isInitialized = true;
-        eventbus.trigger('map:initialized', this._map);
-      }, this);
-      eventbus.on('asset:moving', function(nearestLine) {
-        var nearestFeature = _.find(this.roadLayer.features, function(feature) {
-          return feature.attributes.roadLinkId == nearestLine.roadLinkId;
-        });
-        this._selectControl.unselectAll();
-        this._selectControl.select(nearestFeature);
-      }, this);
-      eventbus.on('asset:saved asset:updateCancelled asset:updateFailed', function() {
-        this._selectControl.unselectAll();
-      }, this);
-      eventbus.on('road-type:selected', this._toggleRoadType, this);
-      eventbus.on('tool:changed', function(action) {
-        var cursor = {'Select': 'default', 'Add': 'crosshair', 'Remove': 'no-drop'};
-        $('.olMap').css('cursor', cursor[action]);
-      });
-      eventbus.on('coordinates:selected coordinates:marked', function(position) {
-        this._sandbox.postRequestByName('MapMoveRequest', [position.lon, position.lat, zoomlevels.getAssetZoomLevelIfNotCloser(this._map.getZoom())]);
-      }, this);
-      eventbus.on('map:moved', this.mapMovedHandler, this);
-      eventbus.on('coordinates:marked', function(position) {
-        this._drawCenterMarker(position);
-      }, this);
-      eventbus.on('roadLinks:fetched', function(roadLinks) {
-        this.drawRoadLinks(roadLinks);
-      }, this);
-      eventbus.on('layer:selected', function(layer) {
-        var assetLayer = layers.asset;
-        if (layer === 'speedLimit') {
-          showSpeedLimitLayer();
-          assetLayer.hide();
-          this._disableColorsOnRoadLayer();
-          this.roadLayer.redraw();
-        } else {
-          assetLayer.show();
-          hideSpeedLimitLayer();
-          this._toggleRoadType(this._roadTypeSelected);
-        }
-      }, this);
-      var map = this._map;
-      var showSpeedLimitLayer = function() {
-        var speedLimitLayer = layers.speedLimit;
-        map.addLayer(speedLimitLayer.vectorLayer);
-        speedLimitLayer.vectorLayer.setVisibility(true);
-        speedLimitLayer.update(map.getZoom(), map.getExtent());
-      };
-      var hideSpeedLimitLayer = function() {
-        layers.speedLimit.reset();
-        map.removeLayer(layers.speedLimit.vectorLayer);
-      };
-      this._map.events.register('moveend', this, function() {
-        applicationModel.moveMap(map.getZoom(), map.getExtent());
-      });
+      new MapView(this._map, sandbox);
 
       // register domain builder
       var mapLayerService = sandbox.getService('Oskari.mapframework.service.MapLayerService');
@@ -102,67 +41,13 @@ Oskari.clazz.define('Oskari.digiroad2.bundle.map.Map',
         mapLayerService.registerLayerModel('map', 'Oskari.digiroad2.bundle.map.domain.BusStopLayer');
       }
       sandbox.postRequestByName('RearrangeSelectedMapLayerRequest', ['base_35', 0]);
-
-      var layers = this.addLayersToMap(Oskari.clazz.create('Oskari.digiroad2.bundle.map.template.Templates'));
     },
-    _drawCenterMarker: function(position) {
-      var size = new OpenLayers.Size(16, 16);
-      var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h / 2);
-      var icon = new OpenLayers.Icon('./images/center-marker.svg', size, offset);
-
-      this._centerMarkerLayer.clearMarkers();
-      var marker = new OpenLayers.Marker(new OpenLayers.LonLat(position.lon, position.lat), icon);
-      this._centerMarkerLayer.addMarker(marker);
-    },
-
-    drawRoadLinks: function(roadLinks) {
-      this.roadLayer.removeAllFeatures();
-      var features = _.map(roadLinks, function(roadLink) {
-        var points = _.map(roadLink.points, function(point) {
-          return new OpenLayers.Geometry.Point(point.x, point.y);
-        });
-        return new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString(points), roadLink);
-      });
-      this.roadLayer.addFeatures(features);
-    },
-
     startPlugin: function(sandbox) {
       this._sandbox = sandbox;
       this._map = this.getMapModule().getMap();
       sandbox.register(this);
     },
-    showAssetZoomDialog: function() {
-      var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-      dialog.show('Zoomaa l&auml;hemm&auml;ksi, jos haluat n&auml;hd&auml; kohteita');
-      dialog.fadeout(2000);
-    },
     start: function(sandbox) {
-    },
-    changeRoadsWidthByZoomLevel: function() {
-      var widthBase = 2 + (this._map.getZoom() - zoomlevels.minZoomForRoadLinks);
-      var roadWidth = widthBase * widthBase;
-      if (this._roadTypeSelected) {
-        this.roadLayer.styleMap.styles.default.defaultStyle.strokeWidth = roadWidth;
-        this.roadLayer.styleMap.styles.select.defaultStyle.strokeWidth = roadWidth;
-      } else {
-        this.roadLayer.styleMap.styles.default.defaultStyle.strokeWidth = 5;
-        this.roadLayer.styleMap.styles.select.defaultStyle.strokeWidth = 7;
-      }
-    },
-    mapMovedHandler: function(mapState) {
-      if (zoomlevels.isInRoadLinkZoomLevel(mapState.zoom)) {
-        this.changeRoadsWidthByZoomLevel();
-        backend.getRoadLinks(mapState.bbox);
-      } else {
-        this.roadLayer.removeAllFeatures();
-      }
-
-      this._handleRoadsVisibility();
-      if (!zoomlevels.isInAssetZoomLevel(mapState.zoom)) {
-        if (this._isInitialized && mapState.hasZoomLevelChanged) {
-          this.showAssetZoomDialog();
-        }
-      }
     },
     preselectLayers: function(layers) {
       for (var i = 0; i < layers.length; i++) {
@@ -173,52 +58,8 @@ Oskari.clazz.define('Oskari.digiroad2.bundle.map.Map',
         this._addMapLayerToMap(layer);
       }
     },
-    _handleRoadsVisibility: function() {
-      if (_.isObject(this.roadLayer)) {
-        this.roadLayer.setVisibility(zoomlevels.isInRoadLinkZoomLevel(this._map.getZoom()));
-      }
-    },
     _afterMapLayerAddEvent: function(event) {
       this._addMapLayerToMap(event.getMapLayer(), event.getKeepLayersOrder(), event.isBasemap());
-    },
-    _toggleRoadType: function(toggle) {
-      if (toggle) {
-        this._enableColorsOnRoadLayer();
-      } else {
-        this._disableColorsOnRoadLayer();
-      }
-      this._roadTypeSelected = toggle;
-      this.changeRoadsWidthByZoomLevel();
-      this.roadLayer.redraw();
-    },
-    _enableColorsOnRoadLayer: function() {
-      var roadLinkTypeStyleLookup = {
-        PrivateRoad: { strokeColor: "#0011bb" },
-        Street: { strokeColor: "#11bb00" },
-        Road: { strokeColor: "#ff0000" }
-      };
-      this.roadLayer.styleMap.addUniqueValueRules("default", "type", roadLinkTypeStyleLookup);
-    },
-    _disableColorsOnRoadLayer: function() {
-      this.roadLayer.styleMap.styles.default.rules = [];
-    },
-    addLayersToMap: function(templates) {
-      this.roadLayer = new OpenLayers.Layer.Vector("road", {
-        styleMap: templates.roadStyles
-      });
-      this.roadLayer.setVisibility(false);
-      this._selectControl = new OpenLayers.Control.SelectFeature(this.roadLayer);
-
-      this._centerMarkerLayer = new OpenLayers.Layer.Markers('centerMarker');
-      this._map.addLayer(this.roadLayer);
-
-      var layers = {
-        asset: new AssetLayer(this._map, this.roadLayer),
-        speedLimit: new SpeedLimitLayer(this._map, backend)
-      };
-      this._map.addLayer(this._centerMarkerLayer);
-
-      return layers;
     },
     getOLMapLayers: function(layer) {
       if (!layer.isLayerOfType(this._layerType)) {
---
+++ UI/src/view/MapView.js
@@ -0,0 +1,182 @@
+(function(root) {
+  root.MapView = function(map, sandbox) {
+    var roadLayer;
+    var selectControl;
+    var roadTypeSelected;
+    var isInitialized = false;
+    var centerMarkerLayer;
+    var layers;
+
+    var showAssetZoomDialog = function() {
+      var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
+      dialog.show('Zoomaa l&auml;hemm&auml;ksi, jos haluat n&auml;hd&auml; kohteita');
+      dialog.fadeout(2000);
+    };
+
+    var enableColorsOnRoadLayer = function() {
+      var roadLinkTypeStyleLookup = {
+        PrivateRoad: { strokeColor: "#0011bb" },
+        Street: { strokeColor: "#11bb00" },
+        Road: { strokeColor: "#ff0000" }
+      };
+      roadLayer.styleMap.addUniqueValueRules("default", "type", roadLinkTypeStyleLookup);
+    };
+
+    var disableColorsOnRoadLayer = function() {
+      roadLayer.styleMap.styles.default.rules = [];
+    };
+
+    var changeRoadsWidthByZoomLevel = function() {
+      var widthBase = 2 + (map.getZoom() - zoomlevels.minZoomForRoadLinks);
+      var roadWidth = widthBase * widthBase;
+      if (roadTypeSelected) {
+        roadLayer.styleMap.styles.default.defaultStyle.strokeWidth = roadWidth;
+        roadLayer.styleMap.styles.select.defaultStyle.strokeWidth = roadWidth;
+      } else {
+        roadLayer.styleMap.styles.default.defaultStyle.strokeWidth = 5;
+        roadLayer.styleMap.styles.select.defaultStyle.strokeWidth = 7;
+      }
+    };
+
+    var toggleRoadType = function(toggle) {
+      if (toggle) {
+        enableColorsOnRoadLayer();
+      } else {
+        disableColorsOnRoadLayer();
+      }
+      roadTypeSelected = toggle;
+      changeRoadsWidthByZoomLevel();
+      roadLayer.redraw();
+    };
+
+    var handleRoadsVisibility = function() {
+      if (_.isObject(roadLayer)) {
+        roadLayer.setVisibility(zoomlevels.isInRoadLinkZoomLevel(map.getZoom()));
+      }
+    };
+
+    var mapMovedHandler = function(mapState) {
+      if (zoomlevels.isInRoadLinkZoomLevel(mapState.zoom)) {
+        changeRoadsWidthByZoomLevel();
+        backend.getRoadLinks(mapState.bbox);
+      } else {
+        roadLayer.removeAllFeatures();
+      }
+
+      handleRoadsVisibility();
+      if (!zoomlevels.isInAssetZoomLevel(mapState.zoom)) {
+        if (isInitialized && mapState.hasZoomLevelChanged) {
+          showAssetZoomDialog();
+        }
+      }
+    };
+
+    var drawCenterMarker = function(position) {
+      var size = new OpenLayers.Size(16, 16);
+      var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h / 2);
+      var icon = new OpenLayers.Icon('./images/center-marker.svg', size, offset);
+
+      centerMarkerLayer.clearMarkers();
+      var marker = new OpenLayers.Marker(new OpenLayers.LonLat(position.lon, position.lat), icon);
+      centerMarkerLayer.addMarker(marker);
+    };
+
+    var drawRoadLinks = function(roadLinks) {
+      roadLayer.removeAllFeatures();
+      var features = _.map(roadLinks, function(roadLink) {
+        var points = _.map(roadLink.points, function(point) {
+          return new OpenLayers.Geometry.Point(point.x, point.y);
+        });
+        return new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString(points), roadLink);
+      });
+      roadLayer.addFeatures(features);
+    };
+
+    var addLayersToMap = function(templates) {
+      roadLayer = new OpenLayers.Layer.Vector("road", {
+        styleMap: templates.roadStyles
+      });
+      roadLayer.setVisibility(false);
+      selectControl = new OpenLayers.Control.SelectFeature(roadLayer);
+
+      centerMarkerLayer = new OpenLayers.Layer.Markers('centerMarker');
+      map.addLayer(roadLayer);
+
+      var layers = {
+        asset: new AssetLayer(map, roadLayer),
+        speedLimit: new SpeedLimitLayer(map, backend)
+      };
+      map.addLayer(centerMarkerLayer);
+
+      return layers;
+    };
+
+    eventbus.on('application:initialized', function() {
+      var zoom = map.getZoom();
+      applicationModel.setZoomLevel(zoom);
+      if (!zoomlevels.isInAssetZoomLevel(zoom)) {
+        showAssetZoomDialog();
+      }
+      new CoordinateSelector($('.mapplugin.coordinates'), map.getMaxExtent());
+      isInitialized = true;
+      eventbus.trigger('map:initialized', map);
+    }, this);
+    eventbus.on('asset:moving', function(nearestLine) {
+      var nearestFeature = _.find(roadLayer.features, function(feature) {
+        return feature.attributes.roadLinkId == nearestLine.roadLinkId;
+      });
+      selectControl.unselectAll();
+      selectControl.select(nearestFeature);
+    }, this);
+    eventbus.on('asset:saved asset:updateCancelled asset:updateFailed', function() {
+      selectControl.unselectAll();
+    }, this);
+    eventbus.on('road-type:selected', toggleRoadType, this);
+    eventbus.on('tool:changed', function(action) {
+      var cursor = {'Select': 'default', 'Add': 'crosshair', 'Remove': 'no-drop'};
+      $('.olMap').css('cursor', cursor[action]);
+    });
+    eventbus.on('coordinates:selected coordinates:marked', function(position) {
+      // TODO: Implement this without calling into Oskari
+      sandbox.postRequestByName('MapMoveRequest', [position.lon, position.lat, zoomlevels.getAssetZoomLevelIfNotCloser(map.getZoom())]);
+    }, this);
+    eventbus.on('map:moved', mapMovedHandler, this);
+    eventbus.on('coordinates:marked', function(position) {
+      drawCenterMarker(position);
+    }, this);
+    eventbus.on('roadLinks:fetched', function(roadLinks) {
+      drawRoadLinks(roadLinks);
+    }, this);
+    eventbus.on('layer:selected', function(layer) {
+      var assetLayer = layers.asset;
+      if (layer === 'speedLimit') {
+        showSpeedLimitLayer();
+        assetLayer.hide();
+        disableColorsOnRoadLayer();
+        roadLayer.redraw();
+      } else {
+        assetLayer.show();
+        hideSpeedLimitLayer();
+        toggleRoadType(roadTypeSelected);
+      }
+    }, this);
+
+    var showSpeedLimitLayer = function() {
+      var speedLimitLayer = layers.speedLimit;
+      map.addLayer(speedLimitLayer.vectorLayer);
+      speedLimitLayer.vectorLayer.setVisibility(true);
+      speedLimitLayer.update(map.getZoom(), map.getExtent());
+    };
+
+    var hideSpeedLimitLayer = function() {
+      layers.speedLimit.reset();
+      map.removeLayer(layers.speedLimit.vectorLayer);
+    };
+
+    map.events.register('moveend', this, function() {
+      applicationModel.moveMap(map.getZoom(), map.getExtent());
+    });
+
+    layers = addLayersToMap(Oskari.clazz.create('Oskari.digiroad2.bundle.map.template.Templates'));
+  };
+})(this);
