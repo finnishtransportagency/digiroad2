ef508c74f8c78a11f2d095e8b15d7b5f9370c940
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-20 14:27:29 +0300

    Replace double quotes with single quotes

--- UI/test/SpeedLimitSelectionSpec.js
+++ UI/test/SpeedLimitSelectionSpec.js
@@ -13,8 +13,8 @@ define(['chai', 'TestHelpers'], function(chai, testHelpers) {
   };
 
   var clickElement = function(element) {
-    var event = document.createEvent("MouseEvent");
-    event.initMouseEvent("click", true, true, window, null, 0, 0, 0, 0, false, false, false, false, 0, null);
+    var event = document.createEvent('MouseEvent');
+    event.initMouseEvent('click', true, true, window, null, 0, 0, 0, 0, false, false, false, false, 0, null);
     element.dispatchEvent(event);
   };
 
