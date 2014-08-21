e870579957afedbbf5f428d31291ee2ceaf79731
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-21 14:06:16 +0300

    Calculate speed limit end points on speed limits that contain more than one link

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetProvider.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetProvider.scala
@@ -2,7 +2,11 @@ package fi.liikennevirasto.digiroad2.linearasset
 
 import fi.liikennevirasto.digiroad2.asset.BoundingRectangle
 
-case class Point(x: Double, y: Double)
+case class Point(x: Double, y: Double) {
+  def distanceTo(point: Point): Double =
+    Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2))
+}
+
 case class SpeedLimit(id: Long, roadLinkId: Long, sideCode: Int, limit: Int, points: Seq[Point])
 
 trait LinearAssetProvider {
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProviderSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProviderSpec.scala
@@ -20,6 +20,13 @@ class OracleLinearAssetProviderSpec extends FunSuite with Matchers {
 
   test("calculate end points of one link speed limit") {
     val links = List((Point(373028.812006694,6678475.44858997), Point(373044.204553789,6678442.81292882)))
-    provider.calculateSpeedLimitEndPoints(links) shouldBe((Point(373028.812006694,6678475.44858997), Point(373044.204553789,6678442.81292882)))
+    provider.calculateSpeedLimitEndPoints(links) shouldBe(Set(Point(373028.812006694,6678475.44858997),
+                                                              Point(373044.204553789,6678442.81292882)))
+  }
+
+  test("calculate end points of two link speed limit") {
+    val links = List((Point(374134.233471419,6677240.50731189), Point(374120.876216048,6677240.61213817)), (Point(374120.876216048,6677240.61213817), Point(374083.159979821,6677239.66865146)))
+    provider.calculateSpeedLimitEndPoints(links) shouldBe (Set(Point(374134.233471419,6677240.50731189),
+                                                               Point(374083.159979821,6677239.66865146)))
   }
 }
