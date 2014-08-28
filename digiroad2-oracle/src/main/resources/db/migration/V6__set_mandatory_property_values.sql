41136bea0a0b47da980012fa3f999206e72721ac
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-08-28 14:35:11 +0300

    Remove extra linefeed

--- digiroad2-oracle/src/main/resources/db/migration/V6__set_mandatory_property_values.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V6__set_mandatory_property_values.sql
@@ -1,4 +1,3 @@
 update property set required = 1, modified_date = sysdate, modified_by = 'db_migration_v6' where public_id = 'nimi_suomeksi';
-
 update property set required = 0, modified_date = sysdate, modified_by = 'db_migration_v6' where public_id = 'tietojen_yllapitaja';
 update property set required = 0, modified_date = sysdate, modified_by = 'db_migration_v6' where public_id = 'katos';
