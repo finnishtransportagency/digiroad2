24ee38afaafd6970c55b62b587471590a8371656
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-09-04 15:11:26 +0300

    Call map zoom from zoom component

--- UI/src/view/ZoomBox.js
+++ UI/src/view/ZoomBox.js
@@ -6,5 +6,7 @@
         '<div class="pzbDiv-minus"></div>' +
       '</div>';
     container.append(element);
+    container.find('.pzbDiv-plus').click(function() { map.zoomIn(); });
+    container.find('.pzbDiv-minus').click(function() { map.zoomOut(); });
   };
 })(this);
