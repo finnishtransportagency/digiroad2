a35c6475ae572bbfacaeb78965935234bd5c9aae
Vesa Marttila <vtmarttila@gmail.com>
2014-09-08 14:50:13 +0300

    Revert "Rename speed limit generator method"
    This reverts commit abf6d508d102534d7746463d2af0e5f334cee287.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/SpeedLimitGenerator.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/SpeedLimitGenerator.scala
@@ -29,7 +29,7 @@ object SpeedLimitGenerator {
       roadLinks.foreach { case (roadLinkId, length) =>
         val assetId = nextPrimaryKeyId.as[Long].first
         val lrmPositionId = nextPrimaryKeyId.as[Long].first
-        val speedLimit = 100
+        val speedLimit = 50
         assetPS.setLong(1, assetId)
         assetPS.addBatch()
 
