8a1c90b587a33732b1c4f8fd4154a80e2b785e44
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-09-10 10:21:42 +0300

    Fix indentation

--- UI/src/main.js
+++ UI/src/main.js
@@ -1,27 +1,27 @@
 var parseQueryParameters = function(queryString) {
-    return _.chain(queryString.split('&'))
-            .map(function(param) {
-                return param.split('=');
-            })
-            .reduce(function(acc, param) {
-                acc[param[0]] = param[1];
-                return acc;
-            }, {})
-            .value();
+  return _.chain(queryString.split('&'))
+    .map(function(param) {
+      return param.split('=');
+    })
+    .reduce(function(acc, param) {
+      acc[param[0]] = param[1];
+      return acc;
+    }, {})
+    .value();
 };
 
 $(function() {
-    var queryString = window.location.search.substring(1);
-    var parameters = parseQueryParameters(queryString);
-    var fakeMode = parameters.withFakeData === 'true';
+  var queryString = window.location.search.substring(1);
+  var parameters = parseQueryParameters(queryString);
+  var fakeMode = parameters.withFakeData === 'true';
 
-    if (fakeMode) {
-        getScripts(['test_data/RoadLinkTestData.js', 'test_data/SpeedLimitsTestData.js'], function() {
-            Application.start(new Backend()
-              .withRoadLinkData(RoadLinkTestData.generate())
-              .withSpeedLimitsData(SpeedLimitsTestData.generate()));
-        });
-    } else {
-        Application.start();
-    }
+  if (fakeMode) {
+    getScripts(['test_data/RoadLinkTestData.js', 'test_data/SpeedLimitsTestData.js'], function() {
+      Application.start(new Backend()
+        .withRoadLinkData(RoadLinkTestData.generate())
+        .withSpeedLimitsData(SpeedLimitsTestData.generate()));
+    });
+  } else {
+    Application.start();
+  }
 });
