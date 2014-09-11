11bbcc48fe8dd69cfd34cec87f61e168d8ab62eb
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-09-11 16:14:36 +0300

    Generate position indices in digitization order for segment sequence where segments are in arbitrary order

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProviderSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProviderSpec.scala
@@ -34,6 +34,20 @@ class OracleLinearAssetProviderSpec extends FunSuite with Matchers {
     provider.generatePositionIndices(segments) shouldBe (Seq(0, 1))
   }
 
+  test("generate position indices for segment sequence where segments are not in order") {
+    val segments = List((Point(374120.876216048,6677240.61213817), Point(374083.159979821,6677239.66865146)), (Point(374134.233471419,6677240.50731189), Point(374120.876216048,6677240.61213817)))
+    provider.generatePositionIndices(segments) shouldBe (Seq(1, 0))
+  }
+
+  test("generate position indices for three segment sequence") {
+    val segments = List(
+      (Point(372572.589549587, 6678017.88260562), Point(372564.91838001, 6678035.95670311)),
+      (Point(372564.918268001, 6678035.95699387), Point(372450.464234144, 6678051.64592463)),
+      (Point(372573.640063694, 6678008.0175942), Point(372572.589549587, 6678017.88260562))
+    )
+    provider.generatePositionIndices(segments) shouldBe (Seq(1, 2, 0))
+  }
+
   test("calculate end points of one link speed limit") {
     val links = List((Point(373028.812006694,6678475.44858997), Point(373044.204553789,6678442.81292882)))
     provider.calculateSpeedLimitEndPoints(links) shouldBe(Set(Point(373028.812006694,6678475.44858997),
