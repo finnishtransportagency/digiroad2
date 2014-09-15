348d5ba3fb3641fb7c3e759e3576bb07ad59e3a4
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-09-15 11:56:06 +0300

    Use ordering of links to determine speed limit end points

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProvider.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProvider.scala
@@ -116,38 +116,11 @@ class OracleLinearAssetProvider extends LinearAssetProvider {
   }
 
   def calculateSpeedLimitEndPoints(links: List[(Point, Point)]): Set[Point] = {
-    def distancesFromPoint(point: Point, pointsToCompare: List[Point]): Map[Point, Double] = {
-      pointsToCompare.foldLeft(Map.empty[Point, Double]) { (acc, x) =>
-        acc + (x -> point.distanceTo(x))
-      }
-    }
-
-    if (links.length == 1) Set(links.head._1, links.head._2)
-    else {
-      val distances: Map[Point, Map[Point, Double]] = links.foldLeft(Map.empty[Point, Map[Point, Double]]) { (acc, link) =>
-        val remainingLinks = links.toSet - link
-        val pointsToCompare = remainingLinks.foldLeft(List.empty[Point]) { (acc, link) =>
-          val (firstPoint, secondPoint) = link
-          firstPoint :: secondPoint :: acc
-        }
-        val distancesFromFirstPoint = distancesFromPoint(link._1, pointsToCompare)
-        val distancesFromSecondPoint = distancesFromPoint(link._2, pointsToCompare)
-        acc + (link._1 -> distancesFromFirstPoint) + (link._2 -> distancesFromSecondPoint)
-      }
-
-      val shortestDistances: Map[Point, Double] = distances.map { distancesFromPoint =>
-        val (point, distances) = distancesFromPoint
-        val shortestDistance = distances.toList.sortBy(_._2).head._2
-        (point -> shortestDistance)
-      }
-
-      val sortedPoints: List[(Point, Double)] = shortestDistances.toList.sortWith { (point1, point2) =>
-        point1._2 > point2._2
-      }
-
-      sortedPoints.take(2).map(_._1).toSet
-    }
-  }
+    val positionIndices = generatePositionIndices(links)
+    val linksWithPositionIndices: Seq[((Point, Point), Int)] = links.zip(positionIndices)
+    val orderedLinks = linksWithPositionIndices.sortBy(_._2)
+    Set(orderedLinks.head._1._1, orderedLinks.last._1._2)
+ }
 
   override def getSpeedLimit(segmentId: Long): Option[SpeedLimit] = {
     Database.forDataSource(ds).withDynTransaction {
