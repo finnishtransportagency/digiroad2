59675c899357bb3c19e8619b84cd66db4148bb3d
Mikko Hautala <mikko.hautala@reaktor.fi>
2014-09-19 11:29:21 +0300

    Use open layers map to trigger click event on AssetCreationSpec
    This replaces triggering events directly on eventbus and
    increases the abstraction level of the test.

--- UI/test/AssetCreationSpec.js
+++ UI/test/AssetCreationSpec.js
@@ -3,15 +3,24 @@ define(['chai', 'eventbus', 'TestHelpers'], function(chai, eventbus, testHelpers
   var expect = chai.expect;
 
   describe('when loading application', function() {
-    before(function(done) { testHelpers.restartApplication(
-      function() { done(); });
+
+    var openLayersMap;
+    before(function(done) {
+      testHelpers.restartApplication(function(map) {
+        openLayersMap = map;
+        $('.edit-mode-btn').click();
+        $('.action.add').click();
+        done();
+      });
     });
 
     describe('and creating a new asset', function() {
-      before(function() {
-        $('.edit-mode-btn').click();
-        $('.action.add').click();
-        eventbus.trigger('map:clicked', {x: 100, y: 100});
+      before(function(done) {
+        openLayersMap.events.triggerEvent('click',  {target: {}, srcElement: {}, xy: {x: 100, y: 100}});
+        eventbus.once('map:clicked', function() {
+          done();
+        });
+
       });
 
       it('it shows new marker', function() {
