e23f898d4563e482511a5d0b9f3478237071ab65
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-09-29 10:50:19 +0300

    Move speed limit link position generation out of linear asset provider

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProvider.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetProvider.scala
@@ -6,95 +6,13 @@ import fi.liikennevirasto.digiroad2.asset.oracle.{AssetPropertyConfiguration, Qu
 import fi.liikennevirasto.digiroad2.linearasset._
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase._
 import fi.liikennevirasto.digiroad2.user.UserProvider
+import fi.liikennevirasto.digiroad2.util.SpeedLimitLinkPositions
 
 import scala.slick.driver.JdbcDriver.backend.Database
 import Database.dynamicSession
 import scala.slick.jdbc.{StaticQuery => Q}
 
 class OracleLinearAssetProvider extends LinearAssetProvider {
-  type PointIndex = Int
-  type PointPosition = Int
-  type SegmentIndex = Int
-
-  def generatePositionIndices(segments: Seq[(Point, Point)]): Seq[Int] = {
-    def fetchFromIndexedSegments(indexedSegments: Seq[((Point, Point), SegmentIndex)], index: PointIndex): Point = {
-      val segment = indexedSegments(pointIndexToSegmentIndex(index))
-      index % 2 match {
-        case 0 => segment._1._1
-        case 1 => segment._1._2
-      }
-    }
-
-    def segmentIndexToPointIndices(segmentIndex: SegmentIndex): (PointIndex, PointIndex) = {
-      (2 * segmentIndex, 2 * segmentIndex + 1)
-    }
-
-    def pointIndexToSegmentIndex(pointIndex: PointIndex): SegmentIndex = {
-      pointIndex / 2
-    }
-
-    def findFriendIndex(pointIndex: PointIndex): PointIndex = {
-      pointIndex % 2 match {
-        case 0 => pointIndex + 1
-        case 1 => pointIndex - 1
-      }
-    }
-
-    def isSegmentStartPoint(point: (PointIndex, (PointIndex, Double))): Boolean = {
-      val (index, _) = point
-      index % 2 == 0
-    }
-
-    val indexedSegments = segments.zipWithIndex
-    if (indexedSegments.length == 1) Seq(0)
-    else {
-      val distances: Seq[(PointIndex, Map[PointIndex, Double])] = indexedSegments.foldLeft(Seq.empty[(PointIndex, Map[PointIndex, Double])]) { (acc, indexedSegment) =>
-        val (segment, index) = indexedSegment
-        val pointsToCompare: List[PointIndex] = indexedSegments.foldLeft(List.empty[PointIndex]) { (acc, otherIndexedSegment) =>
-          val (_, otherIndex) = otherIndexedSegment
-          if (index == otherIndex) acc
-          else {
-            val (firstIndex, secondIndex) = segmentIndexToPointIndices(otherIndex)
-            acc ++ List(firstIndex, secondIndex)
-          }
-        }
-
-        val indexedPoints = pointsToCompare.foldLeft(Map.empty[PointIndex, Point]) { (acc, idx) =>
-          acc + (idx -> fetchFromIndexedSegments(indexedSegments, idx))
-        }
-
-        val distancesFromFirstPoint: Map[PointIndex, Double] = indexedPoints.mapValues(segment._1.distanceTo)
-        val distancesFromSecondPoint: Map[PointIndex, Double] = indexedPoints.mapValues(segment._2.distanceTo)
-        val (firstIndex, secondIndex) = segmentIndexToPointIndices(index)
-        acc ++ Seq((firstIndex, distancesFromFirstPoint), (secondIndex, distancesFromSecondPoint))
-      }
-
-      val shortestDistances: Seq[(PointIndex, (PointIndex, Double))] = distances.map { distancesFromPoint =>
-        val (point, distances) = distancesFromPoint
-        val shortestDistance = distances.toList.sortBy(_._2).head
-        (point, shortestDistance)
-      }
-
-      val startingPoint: PointIndex = shortestDistances.filter(isSegmentStartPoint).sortWith { (point1, point2) =>
-        point1._2._2 > point2._2._2
-      }.head._1
-
-      val pointsWithPositionNumbers: Map[PointIndex, PointPosition] = indexedSegments.foldLeft((startingPoint, Map.empty[Int, Int])) { (acc, indexedSegment) =>
-        val (_, segmentIndex) = indexedSegment
-        val (pointIndex, positionNumbers) = acc
-        val friend = findFriendIndex(pointIndex)
-        val closestNeighbourOfFriend = shortestDistances(friend)._2._1
-        val (pointPositionNumber, friendPositionNumber) = segmentIndexToPointIndices(segmentIndex)
-        (closestNeighbourOfFriend, positionNumbers + (pointIndex -> pointPositionNumber) + (friend -> friendPositionNumber))
-      }._2
-
-      val positionNumbersInIndexOrder: Seq[(PointIndex, PointPosition)] = pointsWithPositionNumbers.toList.sortBy(_._1)
-      val segmentPositionNumbers: Seq[Int] = positionNumbersInIndexOrder.sliding(2, 2).map(_.head).map(_._2).map(pointIndexToSegmentIndex).toList
-
-      segmentPositionNumbers
-    }
-  }
-
   private def toSpeedLimit(linkAndPositionNumber: ((Long, Long, Int, Int, Seq[(Double, Double)]), Int)): SpeedLimitLink = {
     val ((id, roadLinkId, sideCode, limit, points), positionNumber) = linkAndPositionNumber
     SpeedLimitLink(id, roadLinkId, sideCode, limit, points.map { case (x, y) => Point(x, y) }, positionNumber)
@@ -109,7 +27,7 @@ class OracleLinearAssetProvider extends LinearAssetProvider {
 
   private def getLinksWithPositions(links: Seq[(Long, Long, Int, Int, Seq[(Double, Double)])]): Seq[SpeedLimitLink] = {
     val linkEndpoints: Seq[(Point, Point)] = links.map(getLinkEndpoints)
-    val positionNumbers = generatePositionIndices(linkEndpoints)
+    val positionNumbers = SpeedLimitLinkPositions.generate(linkEndpoints)
     links.zip(positionNumbers).map(toSpeedLimit)
   }
 
@@ -120,7 +38,7 @@ class OracleLinearAssetProvider extends LinearAssetProvider {
   }
 
   def calculateSpeedLimitEndPoints(links: List[(Point, Point)]): Set[Point] = {
-    val positionIndices = generatePositionIndices(links)
+    val positionIndices = SpeedLimitLinkPositions.generate(links)
     val linksWithPositionIndices: Seq[((Point, Point), Int)] = links.zip(positionIndices)
     val orderedLinks = linksWithPositionIndices.sortBy(_._2)
     Set(orderedLinks.head._1._1, orderedLinks.last._1._2)
