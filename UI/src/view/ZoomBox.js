56e586551914010315c84649b556c31902a073cc
Hannu Oksa <hannu.oksa@reaktor.fi>
2014-10-08 15:15:58 +0300

    ZoomBox plugin restructuring

--- UI/src/view/ZoomBox.js
+++ UI/src/view/ZoomBox.js
@@ -1,12 +1,12 @@
 (function (root) {
   root.ZoomBox = function(map, container) {
     var element =
-      '<div class="oskariui mapplugin pzbDiv zoombar" data-position="2">' +
-        '<div class="pzbDiv-plus"></div>' +
-        '<div class="pzbDiv-minus"></div>' +
+      '<div class="zoombar" data-position="2">' +
+        '<div class="plus"></div>' +
+        '<div class="minus"></div>' +
       '</div>';
     container.append(element);
-    container.find('.pzbDiv-plus').click(function() { map.zoomIn(); });
-    container.find('.pzbDiv-minus').click(function() { map.zoomOut(); });
+    container.find('.plus').click(function() { map.zoomIn(); });
+    container.find('.minus').click(function() { map.zoomOut(); });
   };
 })(this);
