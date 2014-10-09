1120ae278e6ac12173bea0f787931da553cebb07
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-10-09 14:53:21 +0300

    Reuse lies in between - function in geometry truncation

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/GeometryUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/GeometryUtils.scala
@@ -32,10 +32,15 @@ object GeometryUtils {
     (firstPoint, lastPoint)
   }
 
+  private def liesInBetween(measure: Double, interval: (Double, Double)): Boolean = {
+    measure >= interval._1 && measure <= interval._2
+  }
+
   def truncateGeometry(geometry: Seq[Point], startMeasure: Double, endMeasure: Double): Seq[Point] = {
     def measureOnSegment(measure: Double, segment: (Point, Point), accumulatedLength: Double): Boolean = {
       val (firstPoint, secondPoint) = segment
-      (firstPoint.distanceTo(secondPoint) + accumulatedLength) >= measure && accumulatedLength <= measure
+      val interval = (accumulatedLength, firstPoint.distanceTo(secondPoint) + accumulatedLength)
+      liesInBetween(measure, interval)
     }
 
     def newPointOnSegment(measureOnSegment: Double, segment: (Point, Point)): Point = {
@@ -67,9 +72,6 @@ object GeometryUtils {
 
   def subtractIntervalFromIntervals(intervals: Seq[(Double, Double)], interval: (Double, Double)): Seq[(Double, Double)] = {
     val (spanStart, spanEnd) = (math.min(interval._1, interval._2), math.max(interval._1, interval._2))
-    def liesInBetween(measure: Double, interval: (Double, Double)): Boolean = {
-      measure >= interval._1 && measure <= interval._2
-    }
     intervals.flatMap {
       case (start, end) if !liesInBetween(spanStart, (start, end)) && liesInBetween(spanEnd, (start, end)) => List((spanEnd, end))
       case (start, end) if !liesInBetween(spanEnd, (start, end)) && liesInBetween(spanStart, (start, end)) => List((start, spanStart))
