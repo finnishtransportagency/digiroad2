46a74c33275c7720fab9b1fdab61b0db10576c49
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-10-09 17:41:51 +0300

    Fix indentation

--- UI/src/movebycoordinates/CoordinateSelector.js
+++ UI/src/movebycoordinates/CoordinateSelector.js
@@ -1,60 +1,60 @@
 window.CoordinateSelector = function(parentElement, extent) {
-    var tooltip = "Koordinaattien sy&ouml;tt&ouml;: pohjoinen (7 merkki&auml;), it&auml; (6 merkki&auml;). Esim. 6901839, 435323";
-    var crosshairToggle = $('<div class="crosshair-wrapper"><div class="checkbox"><label><input type="checkbox" name="crosshair" value="crosshair" checked="true"/> Kohdistin</label></div></div>');
-    var coordinatesDiv = $('<div class="coordinates-wrapper"/>');
-    var coordinatesText = $('<input type="text" class="lonlat form-control input-sm" name="lonlat" placeholder="P, I" title="' + tooltip +'"/>');
-    var moveButton = $('<button class="btn btn-sm btn-tertiary">Siirry</button>');
-    var markButton = $('<button class="btn btn-sm btn-tertiary">Merkitse</button>');
+  var tooltip = "Koordinaattien sy&ouml;tt&ouml;: pohjoinen (7 merkki&auml;), it&auml; (6 merkki&auml;). Esim. 6901839, 435323";
+  var crosshairToggle = $('<div class="crosshair-wrapper"><div class="checkbox"><label><input type="checkbox" name="crosshair" value="crosshair" checked="true"/> Kohdistin</label></div></div>');
+  var coordinatesDiv = $('<div class="coordinates-wrapper"/>');
+  var coordinatesText = $('<input type="text" class="lonlat form-control input-sm" name="lonlat" placeholder="P, I" title="' + tooltip + '"/>');
+  var moveButton = $('<button class="btn btn-sm btn-tertiary">Siirry</button>');
+  var markButton = $('<button class="btn btn-sm btn-tertiary">Merkitse</button>');
 
-    var render = function() {
-        parentElement.append(coordinatesDiv.append(coordinatesText).append(moveButton).append(markButton)).append(crosshairToggle);
-    };
+  var render = function() {
+    parentElement.append(coordinatesDiv.append(coordinatesText).append(moveButton).append(markButton)).append(crosshairToggle);
+  };
 
-    var bindEvents = function() {
-        var moveToCoordinates = function(eventName) {
-            var lonlat = $('.coordinates .lonlat').val();
-            var regex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
-            var result = lonlat.match(regex);
+  var bindEvents = function() {
+    var moveToCoordinates = function(eventName) {
+      var lonlat = $('.coordinates .lonlat').val();
+      var regex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
+      var result = lonlat.match(regex);
 
-            var showDialog = function(message) {
-                var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-                dialog.show(message);
-                dialog.fadeout(3000);
-            };
+      var showDialog = function(message) {
+        var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
+        dialog.show(message);
+        dialog.fadeout(3000);
+      };
 
-            if (!result) {
-                showDialog('K&auml;yt&auml; koordinaateissa P ja I numeroarvoja.');
-            } else if (!geometrycalculator.isInBounds(extent, result[2], result[1])) {
-                showDialog('Koordinaatit eiv&auml;t osu kartalle.');
-            } else {
-                var position = {
-                    lat: result[1],
-                    lon: result[2]
-                };
-                eventbus.trigger(eventName, position);
-            }
+      if (!result) {
+        showDialog('K&auml;yt&auml; koordinaateissa P ja I numeroarvoja.');
+      } else if (!geometrycalculator.isInBounds(extent, result[2], result[1])) {
+        showDialog('Koordinaatit eiv&auml;t osu kartalle.');
+      } else {
+        var position = {
+          lat: result[1],
+          lon: result[2]
         };
-
-        coordinatesText.keypress(function(event) {
-            if (event.keyCode == 13) {
-                moveToCoordinates();
-            }
-        });
-        moveButton.on('click', function() {
-            moveToCoordinates('coordinates:selected');
-        });
-        markButton.on('click', function() {
-            moveToCoordinates('coordinates:marked');
-        });
-
-        $('input', crosshairToggle).change(function() {
-            $('.crosshair').toggle(this.checked);
-        });
+        eventbus.trigger(eventName, position);
+      }
     };
 
-    var show = function() {
-        render();
-        bindEvents();
-    };
-    show();
+    coordinatesText.keypress(function(event) {
+      if (event.keyCode == 13) {
+        moveToCoordinates();
+      }
+    });
+    moveButton.on('click', function() {
+      moveToCoordinates('coordinates:selected');
+    });
+    markButton.on('click', function() {
+      moveToCoordinates('coordinates:marked');
+    });
+
+    $('input', crosshairToggle).change(function() {
+      $('.crosshair').toggle(this.checked);
+    });
+  };
+
+  var show = function() {
+    render();
+    bindEvents();
+  };
+  show();
 };
