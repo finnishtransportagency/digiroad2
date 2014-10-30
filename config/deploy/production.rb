8cabd2573a59565ac5a6b7ccc6dcfe1d94dfd80b
Vesa Marttila <vtmarttila@gmail.com>
2014-10-30 16:09:59 +0200

    Deploy only on one production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,9 @@ role :web, %w{web@production1}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server 'production1', user: 'web', roles: %w{web app}, my_property: :my_value
-server 'production2', user: 'web', roles: %w{web app}, my_property: :my_value
+
+# FIXME: Disabled because in production speed limit generation is not tied to single instance
+#server 'production2', user: 'web', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
