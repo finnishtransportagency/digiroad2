a2ae4117898a59c95098a399b43713fd392edfdb
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-11-06 16:03:29 +0200

    Improved interval subtraction tests

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/GeometryUtilsSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/GeometryUtilsSpec.scala
@@ -152,8 +152,28 @@ class GeometryUtilsSpec extends FunSuite with Matchers {
     linksToMove.map(_._1) shouldBe Seq(0)
   }
 
-  test("subtract completely outlying interval from intervals") {
+  test("subtract contained interval from intervals") {
+    val result = subtractIntervalFromIntervals(Seq((3.0, 6.0)), (4.0, 5.0))
+    result shouldBe Seq((3.0, 4.0), (5.0, 6.0))
+  }
+
+  test("subtract outlying interval from intervals") {
     val result = subtractIntervalFromIntervals(Seq((3.0, 6.0)), (1.0, 2.0))
     result shouldBe Seq((3.0, 6.0))
+  }
+
+  test("subtract interval from beginning of intervals") {
+    val result = subtractIntervalFromIntervals(Seq((3.0, 6.0)), (2.0, 4.0))
+    result shouldBe Seq((4.0, 6.0))
+  }
+
+  test("subtract interval from end of intervals") {
+    val result = subtractIntervalFromIntervals(Seq((3.0, 6.0)), (5.0, 7.0))
+    result shouldBe Seq((3.0, 5.0))
+  }
+
+  test("subtract containing interval from intervals") {
+    val result = subtractIntervalFromIntervals(Seq((3.0, 6.0)), (2.0, 7.0))
+    result shouldBe Seq()
   }
 }
