aa445a302db661ca3aa417b233843b8c088ac9cb
Vesa Marttila <vtmarttila@gmail.com>
2014-11-11 13:42:44 +0200

    Remove `getRoadLinkById`

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/GeometryUtils.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/GeometryUtils.scala
@@ -8,22 +8,22 @@ object GeometryUtils {
   /**
    * Calculates compass bearing of roadlink (in digitation direction) at asset position
    */
-  def calculateBearing(asset: AssetWithProperties, roadLink: RoadLink): Int = {
-    if (roadLink.lonLat.size < 2) return 0
+  def calculateBearing(point: (Double, Double), segment: Seq[(Double, Double)]): Int = {
+    if (segment.size < 2) return 0
     def direction(p1: (Double, Double), p2: (Double, Double)): Double = {
       val dLon = p2._1 - p1._1
       val dLat = p2._2 - p1._2
       (Math.toDegrees(Math.atan2(dLon, dLat)) + 360) % 360
     }
-    val closest = roadLink.lonLat.minBy { ll =>
-      Math.sqrt(Math.pow((asset.lon - ll._1), 2) + Math.pow((asset.lat - ll._2), 2))
+    val closest = segment.minBy { ll =>
+      Math.sqrt(Math.pow((point._1 - ll._1), 2) + Math.pow((point._2 - ll._2), 2))
     }
-    val closestIdx = roadLink.lonLat.indexOf(closest)
+    val closestIdx = segment.indexOf(closest)
     // value is an approximation anyway so just truncate decimal part instead of adding checks for round-up corner case
-    if (closestIdx == (roadLink.lonLat.size - 1)) {
-      direction(roadLink.lonLat(closestIdx - 1), closest).toInt
+    if (closestIdx == (segment.size - 1)) {
+      direction(segment(closestIdx - 1), closest).toInt
     } else {
-      direction(closest, roadLink.lonLat(closestIdx + 1)).toInt
+      direction(closest, segment(closestIdx + 1)).toInt
     }
   }
 
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/RoadLinkServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/RoadLinkServiceSpec.scala
@@ -4,15 +4,15 @@ import org.scalatest.{FunSuite, Matchers}
 
 class RoadLinkServiceSpec extends FunSuite with Matchers {
   test("Get production road link with test id that maps to one production road link") {
-    RoadLinkService.getByTestId(48l, 50.0).map(_._1) should be (Some(57))
-    RoadLinkService.getByTestId(48l, 50.0).map(_._2) should be (Some(18))
+    RoadLinkService.getByTestIdAndMeasure(48l, 50.0).map(_._1) should be (Some(57))
+    RoadLinkService.getByTestIdAndMeasure(48l, 50.0).map(_._2) should be (Some(18))
   }
 
   test("Get production road link with test id that doesn't map to production") {
-    RoadLinkService.getByTestId(1414l, 50.0) should be (None)
+    RoadLinkService.getByTestIdAndMeasure(1414l, 50.0) should be (None)
   }
 
   test("Get production road link with test id that maps to several links in production") {
-    RoadLinkService.getByTestId(147298l, 50.0) should be (None)
+    RoadLinkService.getByTestIdAndMeasure(147298l, 50.0) should be (None)
   }
 }
