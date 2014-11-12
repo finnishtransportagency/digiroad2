81dfb96a3a5f334627ff70b5d7bc1e6d853d358c
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-11-12 15:13:32 +0200

    Remove unnecessary join to asset type table when fetching all assets

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/asset/oracle/queries.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/asset/oracle/queries.scala
@@ -195,7 +195,7 @@ object Queries {
 
   def allAssets =
     """
-    select a.id as asset_id, a.external_id as asset_external_id, t.id as asset_type_id, a.bearing as bearing, lrm.side_code as validity_direction,
+    select a.id as asset_id, a.external_id as asset_external_id, a.asset_type_id, a.bearing as bearing, lrm.side_code as validity_direction,
     a.valid_from as valid_from, a.valid_to as valid_to, geometry AS position,
     p.id as property_id, p.public_id as property_public_id, p.property_type, p.ui_position_index, p.required, e.value as value,
     case
@@ -206,16 +206,15 @@ object Queries {
     lrm.id, lrm.start_measure, lrm.end_measure, lrm.prod_road_link_id, lrm.road_link_id, i.id as image_id, i.modified_date as image_modified_date,
     a.created_date, a.created_by, a.modified_date, a.modified_by,
     SDO_CS.TRANSFORM(a.geometry, 4326) AS position_wgs84
-    from asset_type t
-      join asset a on a.asset_type_id = t.id
-        join asset_link al on a.id = al.asset_id
-          join lrm_position lrm on al.position_id = lrm.id
-        join property p on t.id = p.asset_type_id
-          left join single_choice_value s on s.asset_id = a.id and s.property_id = p.id and p.property_type = 'single_choice'
-          left join text_property_value tp on tp.asset_id = a.id and tp.property_id = p.id and (p.property_type = 'text' or p.property_type = 'long_text')
-          left join multiple_choice_value mc on mc.asset_id = a.id and mc.property_id = p.id and p.property_type = 'multiple_choice'
-          left join enumerated_value e on mc.enumerated_value_id = e.id or s.enumerated_value_id = e.id
-          left join image i on e.image_id = i.id
+    from asset a
+      join asset_link al on a.id = al.asset_id
+        join lrm_position lrm on al.position_id = lrm.id
+      join property p on a.asset_type_id = p.asset_type_id
+        left join single_choice_value s on s.asset_id = a.id and s.property_id = p.id and p.property_type = 'single_choice'
+        left join text_property_value tp on tp.asset_id = a.id and tp.property_id = p.id and (p.property_type = 'text' or p.property_type = 'long_text')
+        left join multiple_choice_value mc on mc.asset_id = a.id and mc.property_id = p.id and p.property_type = 'multiple_choice'
+        left join enumerated_value e on mc.enumerated_value_id = e.id or s.enumerated_value_id = e.id
+        left join image i on e.image_id = i.id
     where a.asset_type_id = 10"""
 
     // FIXME: Temporary solution for getting rid of `road_link` table
