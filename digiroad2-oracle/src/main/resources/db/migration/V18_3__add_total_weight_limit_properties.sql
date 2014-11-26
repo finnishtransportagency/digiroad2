f98a470426821cfe108c35efbb9a636e89ab4d5f
Mikko Hautala <mikko.hautala@reaktor.fi>
2014-11-26 11:29:41 +0200

    Create table number_property_value and use it with total weight limits

--- digiroad2-oracle/src/main/resources/db/migration/V18_3__add_total_weight_limit_properties.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V18_3__add_total_weight_limit_properties.sql
@@ -6,5 +6,5 @@ BEGIN
     insert into localized_string (id, value_fi, value_sv, created_by, created_date) values(localized_string_id, 'Rajoitus', '', 'db_migration_v4', sysdate);
 
     insert into property (id, public_id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
-    values (primary_key_seq.nextval, 'kokonaispainorajoitus', (select id from asset_type where name = 'Kokonaispainorajoitukset'), 'db_migration_v4', localized_string_id, 'text', 30);
+    values (primary_key_seq.nextval, 'kokonaispainorajoitus', (select id from asset_type where name = 'Kokonaispainorajoitukset'), 'db_migration_v4', localized_string_id, 'number', 30);
 END;
