d0e24bb448c7435f81083bef0631b99390afca2b
Mikko Hautala <mikko.hautala@reaktor.fi>
2014-11-26 10:40:01 +0200

    Use unique localized string for total weight limit property

--- digiroad2-oracle/src/main/resources/db/migration/V18_2__add_total_weight_limit_properties.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V18_2__add_total_weight_limit_properties.sql
@@ -1,2 +1,10 @@
-insert into property (id, public_id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
-values (primary_key_seq.nextval, 'kokonaispainorajoitus', (select id from asset_type where name = 'Kokonaispainorajoitukset'), 'db_migration_v4', (select id from localized_string where value_fi = 'Rajoitus'), 'text', 30);
+DECLARE
+    localized_string_id PLS_INTEGER;
+BEGIN
+    localized_string_id := primary_key_seq.nextval;
+
+    insert into localized_string (id, value_fi, value_sv, created_by, created_date) values(localized_string_id, 'Rajoitus', '', 'db_migration_v4', sysdate);
+
+    insert into property (id, public_id, asset_type_id, created_by, name_localized_string_id, property_type, ui_position_index)
+    values (primary_key_seq.nextval, 'kokonaispainorajoitus', (select id from asset_type where name = 'Kokonaispainorajoitukset'), 'db_migration_v4', localized_string_id, 'text', 30);
+END;
