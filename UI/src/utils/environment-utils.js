60cb33eb47d08d2694299b9c48b0e5a43c42daec
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-01 13:12:32 +0200

    Move common url address processing to environment utility

--- UI/src/utils/environment-utils.js
+++ UI/src/utils/environment-utils.js
@@ -1,4 +1,9 @@
-(function(root){
+(function(root) {
+  var urlParts = function() {
+    var url = window.location.href.split('/');
+    return _.filter(url, function(urlPart) { return !_.isEmpty(urlPart); });
+  };
+
   var name = function() {
     var environmentName = {
       'testiextranet.liikennevirasto.fi': 'production',
@@ -6,12 +11,16 @@
       'devtest.liikennevirasto.fi': 'staging'
     };
 
-    var url = window.location.href.split('/');
-    var urlParts = _.filter(url, function(urlPart) { return !_.isEmpty(urlPart); });
-    return environmentName[urlParts[1]] || 'unknown';
+    return environmentName[urlParts()[1]] || 'unknown';
+  };
+
+  var urlPath = function() {
+    var urlWithoutResource = _.initial(urlParts());
+    return _.head(urlWithoutResource) + '//' + _.tail(urlWithoutResource).join('/');
   };
 
   root.Environment = {
-    name: name
+    name: name,
+    urlPath: urlPath
   };
 }(this));
