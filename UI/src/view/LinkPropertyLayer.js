65fc5285734a9285343dde843ed4f2dfa02a77da
Mikko Hautala <mikko.hautala@reaktor.fi>
2014-12-17 12:11:55 +0200

    Add control to select road link on link properties layer

--- UI/src/view/LinkPropertyLayer.js
+++ UI/src/view/LinkPropertyLayer.js
@@ -1,10 +1,34 @@
 (function(root) {
   root.LinkPropertyLayer = function(map, roadLayer) {
 
-    var show = function() {
+    var selectControl = new OpenLayers.Control.SelectFeature(roadLayer.layer);
+    map.addControl(selectControl);
+
+    var handleMapMoved = function(state) {
+      if (zoomlevels.isInRoadLinkZoomLevel(state.zoom) && state.selectedLayer === 'linkProperties') {
+        start();
+      } else {
+        stop();
+      }
+    };
+    eventbus.on('map:moved', handleMapMoved);
+
+    var start = function() {
+      selectControl.activate();
+    };
+
+    var stop = function() {
+      selectControl.deactivate();
+    };
+
+    var show = function(map) {
+      if (zoomlevels.isInRoadLinkZoomLevel(map.getZoom())) {
+        start();
+      }
     };
 
     var hide = function() {
+      stop();
     };
 
     return {
