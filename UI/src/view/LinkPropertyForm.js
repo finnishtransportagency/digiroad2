a77575e863562794db4db662646c72c9f398fc5d
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-17 16:21:02 +0200

    Add road type to the road link property form

--- UI/src/view/LinkPropertyForm.js
+++ UI/src/view/LinkPropertyForm.js
@@ -1,10 +1,28 @@
 (function (root) {
   root.LinkPropertyForm = function() {
-    var template = _.template('<header><span>Linkin ID: <%- roadLinkId %></span></header>');
+    var localizedTypes = {
+      PrivateRoad: 'Yksityistie',
+      Street: 'Katu',
+      Road: 'Maantie'
+    };
+
+    var template = _.template('' +
+      '<header>' +
+        '<span>Linkin ID: <%- roadLinkId %></span>' +
+      '</header>' +
+      '<div class="wrapper read-only">' +
+        '<div class="form form-horizontal form-dark">' +
+          '<div class="form-group">' +
+            '<label class="control-label">Väylätyyppi</label>' +
+            '<p class="form-control-static"><%- localizedType %></p>' +
+          '</div>' +
+        '</div>' +
+      '</div>');
 
     var bindEvents = function() {
       var rootElement = $('#feature-attributes');
       eventbus.on('linkProperties:selected', function(linkProperties) {
+        linkProperties.localizedType = localizedTypes[linkProperties.type];
         rootElement.html(template(linkProperties));
       });
       eventbus.on('linkProperties:unselected', function() {
