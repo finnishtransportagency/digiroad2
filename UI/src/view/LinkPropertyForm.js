3da5e65b31b87e7d8cccfd22363c409bf52d7d87
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-01-02 15:38:51 +0200

    Add basic edit mode form for traffic direction in link properties layer

--- UI/src/view/LinkPropertyForm.js
+++ UI/src/view/LinkPropertyForm.js
@@ -24,9 +24,14 @@
       UnknownDirection: 'Tuntematon'
     };
 
-    var template = _.template('' +
+    var disabled = 'disabled';
+    var template = '' +
       '<header>' +
         '<span>Linkin ID: <%- roadLinkId %></span>' +
+        '<div class="road form-controls">' +
+          '<button class="save btn btn-primary" ' + disabled + '>Tallenna</button>' +
+          '<button class="cancel btn btn-secondary" ' + disabled + '>Peruuta</button>' +
+        '</div>' +
       '</header>' +
       '<div class="wrapper read-only">' +
         '<div class="form form-horizontal form-dark">' +
@@ -37,23 +42,41 @@
             '<p class="form-control-static"><%- localizedType %></p>' +
             '<label class="control-label">Toiminnallinen luokka</label>' +
             '<p class="form-control-static"><%- localizedFunctionalClass %></p>' +
+          '</div>' +
+          '<div class="form-group editable">' +
             '<label class="control-label">Liikennevirran suunta</label>' +
             '<p class="form-control-static"><%- localizedTrafficDirection %></p>' +
+            '<select class="form-control speed-limit" style="display: none"><%= trafficDirectionOptionTags %></select>' +
           '</div>' +
         '</div>' +
-      '</div>');
+      '</div>' +
+      '<footer class="road form-controls" style="display: none">' +
+        '<button class="save btn btn-primary" ' + disabled + '>Tallenna</button>' +
+        '<button class="cancel btn btn-secondary" ' + disabled + '>Peruuta</button>' +
+      '</footer>';
 
     var bindEvents = function() {
       var rootElement = $('#feature-attributes');
+      var toggleMode = function(readOnly) {
+        rootElement.find('.editable .form-control-static').toggle(readOnly);
+        rootElement.find('select').toggle(!readOnly);
+        rootElement.find('.form-controls').toggle(!readOnly);
+      };
       eventbus.on('linkProperties:selected', function(linkProperties) {
         linkProperties.localizedFunctionalClass = localizedFunctionalClasses[linkProperties.functionalClass] || 'Tuntematon';
         linkProperties.localizedType = localizedTypes[linkProperties.type];
         linkProperties.localizedTrafficDirection = localizedTrafficDirections[linkProperties.trafficDirection];
-        rootElement.html(template(linkProperties));
+        var trafficDirectionOptionTags = _.map(localizedTrafficDirections, function(value, key) {
+          var selected = key === linkProperties.trafficDirection ? " selected" : "";
+          return '<option value="' + value + '"' + selected + '>' + value + '</option>';
+        }).join('');
+        rootElement.html(_.template(template, linkProperties, { imports: { trafficDirectionOptionTags: trafficDirectionOptionTags }}));
+        toggleMode(applicationModel.isReadOnly());
       });
       eventbus.on('linkProperties:unselected', function() {
         rootElement.empty();
       });
+      eventbus.on('application:readOnly', toggleMode);
     };
 
     bindEvents();
