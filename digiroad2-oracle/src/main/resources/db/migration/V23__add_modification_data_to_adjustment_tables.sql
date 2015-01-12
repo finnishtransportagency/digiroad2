68b1927b8c618ffdcb106b1828710d6b5f773206
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-01-12 14:36:31 +0200

    Add creation date and creator to adjustment tables

---
+++ digiroad2-oracle/src/main/resources/db/migration/V23__add_modification_data_to_adjustment_tables.sql
@@ -0,0 +1,7 @@
+alter table adjusted_traffic_direction
+  add (created_date timestamp default current_timestamp,
+       created_by varchar2(128));
+
+alter table adjusted_functional_class
+  add (created_date timestamp default current_timestamp,
+       created_by varchar2(128));
