05f69f351eea1c460eb5fed3c5485f7cf89a34de
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-01-13 11:42:10 +0200

    Force adjustment creation date

--- digiroad2-oracle/src/main/resources/db/migration/V23__add_modification_data_to_adjustment_tables.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V23__add_modification_data_to_adjustment_tables.sql
@@ -1,7 +1,7 @@
 alter table adjusted_traffic_direction
-  add (created_date timestamp default current_timestamp,
+  add (created_date timestamp default current_timestamp not null,
        created_by varchar2(128));
 
 alter table adjusted_functional_class
-  add (created_date timestamp default current_timestamp,
+  add (created_date timestamp default current_timestamp not null,
        created_by varchar2(128));
