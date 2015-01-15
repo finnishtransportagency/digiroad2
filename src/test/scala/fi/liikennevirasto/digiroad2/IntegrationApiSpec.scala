90d4f72a5530ee7079df563e9617f40855c0d385
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-01-15 16:07:24 +0200

    Fix integration api test to use user and password of correct environment

--- src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
@@ -1,14 +1,29 @@
 package fi.liikennevirasto.digiroad2
 
+import java.util.Properties
 import org.scalatest.{Tag, FunSuite}
 import org.scalatra.test.scalatest.ScalatraSuite
 import org.apache.commons.codec.binary.Base64
 
 
-
 class IntegrationApiSpec extends FunSuite with ScalatraSuite {
 
   addServlet(classOf[IntegrationApi], "/*")
+
+  val properties: Properties = {
+    val props = new Properties()
+    props.load(getClass.getResourceAsStream("/authentication.properties"))
+    props
+  }
+
+  private def getProperty(name: String): String = {
+    val property = properties.getProperty(name)
+    if (property != null) {
+      property
+    } else {
+      throw new RuntimeException(s"cannot find property $name")
+    }
+  }
 
   def getWithBasicUserAuth[A](uri: String, username: String, password: String)(f: => A): A = {
     val credentials = username + ":" + password
@@ -17,6 +32,9 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
     get(uri, Seq.empty, Map("Authorization" -> authorizationToken))(f)
   }
 
+  val username = getProperty("authentication.basic.username")
+  val password = getProperty("authentication.basic.password")
+
   test("Should require correct authentication", Tag("db")) {
     get("/data") {
       status should equal(401)
@@ -24,7 +42,7 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
     getWithBasicUserAuth("/data", "nonexisting", "incorrect") {
       status should equal(401)
     }
-    getWithBasicUserAuth("/data", "kalpa", "kalpa") {
+    getWithBasicUserAuth("/data", username, password) {
       status should equal(200)
     }
   }
@@ -33,7 +51,7 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
     getWithBasicUserAuth("/assets", "kalpa", "kalpa") {
       status should equal(400)
     }
-    getWithBasicUserAuth("/assets?municipality=235", "kalpa", "kalpa") {
+    getWithBasicUserAuth("/assets?municipality=235", username, password) {
       status should equal(200)
     }
   }
