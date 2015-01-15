886ed2cb5ab3b6c78accd5f1aff482896a100bb0
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-01-15 16:20:46 +0200

    Fix integration api test in ci

--- src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
@@ -10,30 +10,12 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
 
   addServlet(classOf[IntegrationApi], "/*")
 
-  val properties: Properties = {
-    val props = new Properties()
-    props.load(getClass.getResourceAsStream("/authentication.properties"))
-    props
-  }
-
-  private def getProperty(name: String): String = {
-    val property = properties.getProperty(name)
-    if (property != null) {
-      property
-    } else {
-      throw new RuntimeException(s"cannot find property $name")
-    }
-  }
-
   def getWithBasicUserAuth[A](uri: String, username: String, password: String)(f: => A): A = {
     val credentials = username + ":" + password
     val encodedCredentials = Base64.encodeBase64URLSafeString(credentials.getBytes())
     val authorizationToken = "Basic " + encodedCredentials + "="
     get(uri, Seq.empty, Map("Authorization" -> authorizationToken))(f)
   }
-
-  val username = getProperty("authentication.basic.username")
-  val password = getProperty("authentication.basic.password")
 
   test("Should require correct authentication", Tag("db")) {
     get("/data") {
@@ -42,7 +24,7 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
     getWithBasicUserAuth("/data", "nonexisting", "incorrect") {
       status should equal(401)
     }
-    getWithBasicUserAuth("/data", username, password) {
+    getWithBasicUserAuth("/data", "kalpa", "kalpa") {
       status should equal(200)
     }
   }
@@ -51,7 +33,7 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite {
     getWithBasicUserAuth("/assets", "kalpa", "kalpa") {
       status should equal(400)
     }
-    getWithBasicUserAuth("/assets?municipality=235", username, password) {
+    getWithBasicUserAuth("/assets?municipality=235", "kalpa", "kalpa") {
       status should equal(200)
     }
   }
