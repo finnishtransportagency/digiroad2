26464fe366c9270fb56dddc518231360f689917b
Vesa Marttila <vtmarttila@gmail.com>
2015-01-16 15:03:29 +0200

    Remove unused method

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RoadLinkService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RoadLinkService.scala
@@ -38,22 +38,6 @@ object RoadLinkService {
     }
   }
 
-  def getByTestId(testId: Long): Option[(Long, Int, RoadLinkType)] = {
-    Database.forDataSource(dataSource).withDynTransaction {
-      val query = sql"""
-         select prod.dr1_id, prod.kunta_nro, prod.functionalroadclass
-           from tielinkki_ctas prod
-           join tielinkki test
-           on prod.mml_id = test.mml_id
-           where test.objectid = $testId
-        """
-      query.as[(Long, Int, RoadLinkType)].list() match {
-        case List(productionLink) => Some((productionLink._1, productionLink._2, productionLink._3))
-        case _ => None
-      }
-    }
-  }
-
   def getByIdAndMeasure(id: Long, measure: Double): Option[(Long, Int, Option[Point], RoadLinkType)] = {
     Database.forDataSource(dataSource).withDynTransaction {
       val query = sql"""
