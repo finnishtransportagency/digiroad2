0e5de47ecedfb926810f19ae7116da3c7072b9e6
Hannu Oksa <hannu.oksa@reaktor.fi>
2015-01-23 12:13:55 +0200

    Dynamic sizing and positioning of modals

--- UI/src/view/InstructionsPopup.js
+++ UI/src/view/InstructionsPopup.js
@@ -2,12 +2,12 @@
   root.InstructionsPopup = function(container) {
     var element =
       '<div class="instructions-popup">' +
-        '<h3 class="popupHeader"/>' +
+        '<header/>' +
       '</div>';
     container.append(element);
 
     var show = function(message, timeout) {
-      container.find('.popupHeader').text(message);
+      container.find('.instructions-popup').find('header').text(message);
       container.find('.instructions-popup').fadeIn(200);
       setTimeout(function() { container.find('.instructions-popup').fadeOut(200); }, timeout);
     };
