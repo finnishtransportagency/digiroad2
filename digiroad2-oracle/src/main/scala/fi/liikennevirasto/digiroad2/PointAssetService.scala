7ce9a392b3312c358f56b662756ff002b0650f2a
Vesa Marttila <vtmarttila@gmail.com>
2015-01-26 14:43:26 +0200

    Create generic point asset service

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetService.scala
@@ -3,17 +3,17 @@ package fi.liikennevirasto.digiroad2
 import fi.liikennevirasto.digiroad2.ConversionDatabase._
 
 import scala.slick.driver.JdbcDriver.backend.Database
-import scala.slick.driver.JdbcDriver.backend.Database.dynamicSession
 import scala.slick.jdbc.StaticQuery.interpolation
+import scala.slick.driver.JdbcDriver.backend.Database.dynamicSession
 
-object ClosedRouteService {
-  def getByMunicipality(municipalityNumber: Int): Seq[Map[String, Any]] = {
+object PointAssetService {
+  def getByMunicipality(typeId: Int, municipalityNumber: Int): Seq[Map[String, Any]] = {
     Database.forDataSource(dataSource).withDynTransaction {
       val query = sql"""
          select s.segm_id, s.tielinkki_id, to_2d(sdo_lrs.dynamic_segment(t.shape, s.alkum, s.loppum))
            from segments s
            join tielinkki_ctas t on s.tielinkki_id = t.dr1_id
-           where t.kunta_nro = $municipalityNumber and s.tyyppi = 16
+           where t.kunta_nro = $municipalityNumber and s.tyyppi = $typeId
         """
       query.as[(Long, Long, Seq[Point])].iterator().map {
         case (id, roadLinkId, geometry) => Map("id" -> id, "point" -> geometry.head)
