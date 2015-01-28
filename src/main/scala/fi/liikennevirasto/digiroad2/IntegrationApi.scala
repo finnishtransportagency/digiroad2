344ffd938644ba5c421203402487e80c570347c5
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-01-28 11:20:53 +0200

    Add administrator code to integration API

--- src/main/scala/fi/liikennevirasto/digiroad2/IntegrationApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/IntegrationApi.scala
@@ -110,6 +110,7 @@ class IntegrationApi extends ScalatraServlet with JacksonJsonSupport with Authen
             extractPropertyValue("nimi_ruotsiksi", asset.propertyData, propertyValuesToString),
             extractPropertyValue("tietojen_yllapitaja", asset.propertyData, firstPropertyValueToInt),
             extractPropertyValue("yllapitajan_tunnus", asset.propertyData, propertyValuesToString),
+            extractPropertyValue("yllapitajan_koodi", asset.propertyData, propertyValuesToString),
             extractPropertyValue("katos", asset.propertyData, firstPropertyValueToInt),
             extractPropertyValue("pyorateline", asset.propertyData, firstPropertyValueToInt))
        )
