5635ae3b28a4b5a31889a7d565dc1d9b075c99a3
Vesa Marttila <vtmarttila@gmail.com>
2015-01-29 14:10:36 +0200

    Return side code from PointAssetService

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetService.scala
@@ -10,13 +10,13 @@ object PointAssetService {
   def getByMunicipality(typeId: Int, municipalityNumber: Int): Seq[Map[String, Any]] = {
     Database.forDataSource(dataSource).withDynTransaction {
       val query = sql"""
-         select s.segm_id, s.tielinkki_id, to_2d(sdo_lrs.dynamic_segment(t.shape, s.alkum, s.loppum))
+         select s.segm_id, s.tielinkki_id, to_2d(sdo_lrs.dynamic_segment(t.shape, s.alkum, s.loppum)), s.puoli
            from segments s
            join tielinkki_ctas t on s.tielinkki_id = t.dr1_id
            where t.kunta_nro = $municipalityNumber and s.tyyppi = $typeId
         """
-      query.as[(Long, Long, Seq[Point])].iterator().map {
-        case (id, roadLinkId, geometry) => Map("id" -> id, "point" -> geometry.head)
+      query.as[(Long, Long, Seq[Point], Int)].iterator().map {
+        case (id, roadLinkId, geometry, sideCode) => Map("id" -> id, "point" -> geometry.head, "sideCode" -> sideCode)
       }.toSeq
     }
   }
