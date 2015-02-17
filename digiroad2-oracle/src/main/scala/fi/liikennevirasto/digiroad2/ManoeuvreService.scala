3d237fa61a4f45f37fe2fe1b188d073d6ba3306e
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-02-17 14:35:24 +0200

    Return manoeuvre source and destination road link ids from backend

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
@@ -8,17 +8,26 @@ import scala.slick.jdbc.StaticQuery.interpolation
 import scala.slick.driver.JdbcDriver.backend.Database.dynamicSession
 import scala.slick.jdbc.{StaticQuery => Q}
 
+case class Manoeuvre(id: Long, sourceRoadLinkId: Long, destRoadLinkId: Long)
+
 object ManoeuvreService {
-  def getByBoundingBox(bounds: BoundingRectangle, municipalities: Set[Int]): Seq[Long] = {
+  def getByBoundingBox(bounds: BoundingRectangle, municipalities: Set[Int]): Seq[Manoeuvre] = {
     Database.forDataSource(ConversionDatabase.dataSource).withDynTransaction {
       val municipalityFilter = if (municipalities.nonEmpty) "kunta_nro in (" + municipalities.mkString(",") + ") and" else ""
       val boundingBoxFilter = OracleDatabase.boundingBoxFilter(bounds)
-      sql"""
-        select k.kaan_id
+      val manoeuvres = sql"""
+        select k.kaan_id, k.tl_dr1_id, elem_jarjestyslaji
         from kaantymismaarays k
         join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
         where #$municipalityFilter #$boundingBoxFilter
-      """.as[Long].list
+      """.as[(Long, Long, Int)].list
+
+      val manoeuvresById: Map[Long, Seq[(Long, Long, Int)]] = manoeuvres.groupBy(_._1)
+      manoeuvresById.filter { case(id, links) =>
+        links.size == 2 && !links.exists{ case(_, _, linkType) => linkType == 2 }
+      }.map { case(id, links) =>
+        Manoeuvre(id, links.find(_._3 == 1).get._2, links.find(_._3 == 3).get._2)
+      }.toSeq
     }
   }
 }
