b0935306c1f90d4ebfd3d15bb0e32671a19b72ee
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-02-18 15:27:00 +0200

    Create dashed lines for manoeuvre target links and tune styles

--- UI/src/view/ManoeuvreLayer.js
+++ UI/src/view/ManoeuvreLayer.js
@@ -2,31 +2,60 @@
   root.ManoeuvreLayer = function(map, roadLayer, roadCollection, backend) {
 
     var layerName = 'manoeuvre';
-    var manoeuvreColorLookup = {
+    var manoeuvreSourceLookup = {
       0: { strokeColor: '#a4a4a2' },
       1: { strokeColor: '#0000ff' }
     };
+    var featureTypeLookup = {
+      normal: { strokeWidth: 8, strokeOpacity: 0.5 },
+      overlay: { strokeOpacity: 0.7, strokeColor: '#be0000', strokeLinecap: 'square', strokeWidth: 6, strokeDashstyle: '1 10'  }
+    };
     var defaultStyleMap = new OpenLayers.StyleMap({
-      'default': new OpenLayers.Style(OpenLayers.Util.applyDefaults({
-        strokeWidth: 5,
-        strokeOpacity: 0.7
-      }))
+      'default': new OpenLayers.Style(OpenLayers.Util.applyDefaults({}))
     });
-    defaultStyleMap.addUniqueValueRules('default', 'startsManoeuvre', manoeuvreColorLookup);
+    defaultStyleMap.addUniqueValueRules('default', 'manoeuvreSource', manoeuvreSourceLookup);
+    defaultStyleMap.addUniqueValueRules('default', 'type', featureTypeLookup);
 
     var eventListener = _.extend({running: false}, eventbus);
 
     roadLayer.setLayerSpecificStyleMap(layerName, defaultStyleMap);
 
+    var createDashedLineFeatures = function(roadLinks) {
+      return _.flatten(_.map(roadLinks, function(roadLink) {
+        var points = _.map(roadLink.points, function(point) {
+          return new OpenLayers.Geometry.Point(point.x, point.y);
+        });
+        var attributes = _.merge({}, roadLink, {
+          type: 'overlay'
+        });
+        return new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString(points), attributes);
+      }));
+    };
+
+    var drawDashedLineFeatures = function(roadLinks) {
+      var dashedRoadLinks = _.filter(roadLinks, function(roadLink) {
+        return roadLink.manoeuvreDestination === 1;
+      });
+      roadLayer.layer.addFeatures(createDashedLineFeatures(dashedRoadLinks));
+    };
+
     var draw = function() {
       backend.getManoeuvres(map.getExtent(), function(manoeuvres) {
         var roadLinks = _.map(roadCollection.getAll(), function(roadLink) {
-          var manoeuvreStartLink = _.find(manoeuvres, function(manoeuvre) {
+          var manoeuvreSourceLink = _.find(manoeuvres, function(manoeuvre) {
             return manoeuvre.sourceRoadLinkId === roadLink.roadLinkId;
           });
-          return _.merge({}, roadLink, { startsManoeuvre: manoeuvreStartLink ? 1 : 0 });
+          var manoeuvreDestinationLink = _.find(manoeuvres, function(manoeuvre) {
+            return manoeuvre.destRoadLinkId === roadLink.roadLinkId;
+          });
+          return _.merge({}, roadLink, {
+            manoeuvreSource: manoeuvreSourceLink ? 1 : 0,
+            manoeuvreDestination: manoeuvreDestinationLink ? 1 : 0,
+            type: 'normal'
+          });
         });
         roadLayer.drawRoadLinks(roadLinks, map.getZoom());
+        drawDashedLineFeatures(roadLinks);
       });
     };
 
