598229e8f481040b7f29477e986fa775ed18da70
Simo Råman <simo.raman@reaktor.fi>
2015-02-20 11:07:41 +0200

    add mml-ids for source and destination in manoeuvre

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
@@ -8,7 +8,7 @@ import scala.slick.jdbc.StaticQuery.interpolation
 import scala.slick.driver.JdbcDriver.backend.Database.dynamicSession
 import scala.slick.jdbc.{StaticQuery => Q}
 
-case class Manoeuvre(id: Long, sourceRoadLinkId: Long, destRoadLinkId: Long)
+case class Manoeuvre(id: Long, sourceRoadLinkId: Long, destRoadLinkId: Long, sourceMmlId: Long, destMmlId: Long)
 
 object ManoeuvreService {
   def getByBoundingBox(bounds: BoundingRectangle, municipalities: Set[Int]): Seq[Manoeuvre] = {
@@ -16,17 +16,19 @@ object ManoeuvreService {
       val municipalityFilter = if (municipalities.nonEmpty) "kunta_nro in (" + municipalities.mkString(",") + ") and" else ""
       val boundingBoxFilter = OracleDatabase.boundingBoxFilter(bounds)
       val manoeuvres = sql"""
-        select k.kaan_id, k.tl_dr1_id, elem_jarjestyslaji
+        select k.kaan_id, k.tl_dr1_id, tl.mml_id, elem_jarjestyslaji
         from kaantymismaarays k
         join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
         where #$municipalityFilter #$boundingBoxFilter
-      """.as[(Long, Long, Int)].list
+      """.as[(Long, Long, Long, Int)].list
 
-      val manoeuvresById: Map[Long, Seq[(Long, Long, Int)]] = manoeuvres.groupBy(_._1)
+      val manoeuvresById: Map[Long, Seq[(Long, Long, Long, Int)]] = manoeuvres.groupBy(_._1)
       manoeuvresById.filter { case(id, links) =>
-        links.size == 2 && links.exists(_._3 == 1) && links.exists(_._3 == 3)
+        links.size == 2 && links.exists(_._4 == 1) && links.exists(_._4 == 3)
       }.map { case(id, links) =>
-        Manoeuvre(id, links.find(_._3 == 1).get._2, links.find(_._3 == 3).get._2)
+        val source: (Long, Long, Long, Int) = links.find(_._4 == 1).get
+        val dest: (Long, Long, Long, Int) = links.find(_._4 == 3).get
+        Manoeuvre(id, source._2, dest._2, source._3, dest._3)
       }.toSeq
     }
   }
