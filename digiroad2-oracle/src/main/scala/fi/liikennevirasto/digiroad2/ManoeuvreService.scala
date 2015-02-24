2eba09a27a6d5e56416856a09718b25ca739d6d4
Simo Råman <simo.raman@reaktor.fi>
2015-02-24 10:33:18 +0200

    extract common functionality to query function

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
@@ -12,33 +12,17 @@ case class Manoeuvre(id: Long, sourceRoadLinkId: Long, destRoadLinkId: Long, sou
 
 object ManoeuvreService {
   def getByBoundingBox(bounds: BoundingRectangle, municipalities: Set[Int]): Seq[Manoeuvre] = {
-    val municipalityFilter = if (municipalities.nonEmpty) "kunta_nro in (" + municipalities.mkString(",") + ") and" else ""
+    val municipalityFilter = if (municipalities.nonEmpty) "kunta_nro in (" + municipalities.mkString(",") + ") and " else ""
     val boundingBoxFilter = OracleDatabase.boundingBoxFilter(bounds)
-    Database.forDataSource(ConversionDatabase.dataSource).withDynTransaction {
-      val manoeuvres = sql"""
-        select k.kaan_id, k.tl_dr1_id, tl.mml_id, k.elem_jarjestyslaji
-        from kaantymismaarays k
-        join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
-        where k.kaan_id in (
-          select distinct(k.kaan_id)
-          from kaantymismaarays k
-          join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
-          where #$municipalityFilter #$boundingBoxFilter)
-      """.as[(Long, Long, Long, Int)].list
-
-      val manoeuvresById: Map[Long, Seq[(Long, Long, Long, Int)]] = manoeuvres.groupBy(_._1)
-      manoeuvresById.filter { case (id, links) =>
-        links.size == 2 && links.exists(_._4 == 1) && links.exists(_._4 == 3)
-      }.map { case (id, links) =>
-        val source: (Long, Long, Long, Int) = links.find(_._4 == 1).get
-        val dest: (Long, Long, Long, Int) = links.find(_._4 == 3).get
-        Manoeuvre(id, source._2, dest._2, source._3, dest._3)
-      }.toSeq
-    }
+    query(municipalityFilter + boundingBoxFilter)
   }
 
   def getByMunicipality(municipalityNumber: Int): Seq[Manoeuvre] = {
-    val municipalityFilter = s"kunta_nro = $municipalityNumber"
+    val filter = s"kunta_nro = $municipalityNumber and k.elem_jarjestyslaji = 1"
+    query(filter)
+  }
+
+  private def query(filter: String): Seq[Manoeuvre] = {
     Database.forDataSource(ConversionDatabase.dataSource).withDynTransaction {
       val manoeuvres = sql"""
         select k.kaan_id, k.tl_dr1_id, tl.mml_id, k.elem_jarjestyslaji
@@ -48,7 +32,7 @@ object ManoeuvreService {
           select distinct(k.kaan_id)
           from kaantymismaarays k
           join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
-          where #$municipalityFilter and k.elem_jarjestyslaji = 1)
+          where #$filter)
       """.as[(Long, Long, Long, Int)].list
 
       val manoeuvresById: Map[Long, Seq[(Long, Long, Long, Int)]] = manoeuvres.groupBy(_._1)
