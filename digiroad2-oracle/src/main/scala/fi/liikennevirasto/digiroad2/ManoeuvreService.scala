77aca0342a4a36bfa2b1118daaca9cf819f2d2d4
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-02-25 13:39:13 +0200

    Fetch manoeuvres for kalpa API (by municipality) from local db
    While at it extract common code to local private function that fetches manoeuvres for given prefiltered list of roadlinks.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ManoeuvreService.scala
@@ -16,51 +16,41 @@ case class Manoeuvre(id: Long, sourceRoadLinkId: Long, destRoadLinkId: Long, sou
 
 object ManoeuvreService {
   def getByMunicipality(municipalityNumber: Int): Seq[Manoeuvre] = {
-    val filter = s"kunta_nro = $municipalityNumber and k.elem_jarjestyslaji = 1"
-    query(filter)
-  }
+    Database.forDataSource(OracleDatabase.ds).withDynTransaction {
+      val roadLinks = RoadLinkService.getByMunicipalityWithProperties(municipalityNumber)
+        .map(link => (link("id").asInstanceOf[Long], link("mmlId").asInstanceOf[Long]))
+        .toMap
 
-  private def query(filter: String): Seq[Manoeuvre] = {
-    Database.forDataSource(ConversionDatabase.dataSource).withDynTransaction {
-      val manoeuvres = sql"""
-        select k.kaan_id, k.tl_dr1_id, tl.mml_id, k.elem_jarjestyslaji
-        from kaantymismaarays k
-        join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
-        where k.kaan_id in (
-          select distinct(k.kaan_id)
-          from kaantymismaarays k
-          join tielinkki_ctas tl on k.tl_dr1_id = tl.dr1_id
-          where #$filter)
-      """.as[(Long, Long, Long, Int)].list
-
-      val manoeuvresById: Map[Long, Seq[(Long, Long, Long, Int)]] = manoeuvres.groupBy(_._1)
-      manoeuvresById.filter { case (id, links) =>
-        links.size == 2 && links.exists(_._4 == 1) && links.exists(_._4 == 3)
-      }.map { case (id, links) =>
-        val source: (Long, Long, Long, Int) = links.find(_._4 == 1).get
-        val dest: (Long, Long, Long, Int) = links.find(_._4 == 3).get
-        Manoeuvre(id, source._2, dest._2, source._3, dest._3)
-      }.toSeq
+      getByRoadlinks(roadLinks)
     }
   }
 
   def getByBoundingBox(bounds: BoundingRectangle, municipalities: Set[Int]): Seq[Manoeuvre] = {
     Database.forDataSource(OracleDatabase.ds).withDynTransaction {
       val roadLinks = RoadLinkService.getRoadLinks(bounds, municipalities)
-      val roadLinkIds = roadLinks.map(_._1).toList
+        .map(link => (link._1, link._2))
+        .toMap
 
-      val manoeuvres = OracleArray.fetchManoeuvresByRoadLinkIds(roadLinkIds, bonecpToInternalConnection(dynamicSession.conn))
-
-      val manoeuvresById: Map[Long, Seq[(Long, Int, Long, Int, DateTime, String)]] = manoeuvres.toList.groupBy(_._1)
-      manoeuvresById.filter { case (id, links) =>
-        links.size == 2 && links.exists(_._4 == 1) && links.exists(_._4 == 3)
-      }.map { case (id, links) =>
-        val source: (Long, Int, Long, Int, DateTime, String) = links.find(_._4 == 1).get
-        val dest: (Long, Int, Long, Int, DateTime, String) = links.find(_._4 == 3).get
-        val sourceMmlId = roadLinks.find(_._1 == source._3).map(_._2).getOrElse(RoadLinkService.getRoadLink(source._3)._2)
-        val destMmlId = roadLinks.find(_._1 == dest._3).map(_._2).getOrElse(RoadLinkService.getRoadLink(source._3)._2)
-        Manoeuvre(id, source._3, dest._3, sourceMmlId, destMmlId)
-      }.toSeq
+      getByRoadlinks(roadLinks)
     }
   }
+
+  private def getByRoadlinks(roadLinks: Map[Long,Long]): Seq[Manoeuvre] = {
+    val roadLinkIds = roadLinks.keys.toList
+
+    val manoeuvres = OracleArray.fetchManoeuvresByRoadLinkIds(roadLinkIds, bonecpToInternalConnection(dynamicSession.conn))
+
+    val manoeuvresById: Map[Long, Seq[(Long, Int, Long, Int, DateTime, String)]] = manoeuvres.toList.groupBy(_._1)
+    manoeuvresById.filter { case (id, links) =>
+      links.size == 2 && links.exists(_._4 == 1) && links.exists(_._4 == 3)
+    }.map { case (id, links) =>
+      val source: (Long, Int, Long, Int, DateTime, String) = links.find(_._4 == 1).get
+      val dest: (Long, Int, Long, Int, DateTime, String) = links.find(_._4 == 3).get
+      val sourceMmlId = roadLinks.getOrElse(source._3, RoadLinkService.getRoadLink(source._3)._2)
+      val destMmlId = roadLinks.getOrElse(dest._3, RoadLinkService.getRoadLink(source._3)._2)
+
+      Manoeuvre(id, source._3, dest._3, sourceMmlId, destMmlId)
+    }.toSeq
+  }
+
 }
