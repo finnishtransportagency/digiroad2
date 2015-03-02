2058c33aeffc6bbbae4cfe54cbac40d9122a9dfd
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-03-02 09:34:01 +0200

    Verify both partially and completely contained manoeuvre

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/ManoeuvreServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/ManoeuvreServiceSpec.scala
@@ -8,8 +8,11 @@ class ManoeuvreServiceSpec extends FunSuite with Matchers {
     val bounds = BoundingRectangle(Point(373880.25, 6677085), Point(374133, 6677382))
     val manoeuvres = ManoeuvreService.getByBoundingBox(bounds, Set(235))
     manoeuvres.length should equal(5)
-    val m = manoeuvres.find(_.id == 39561).get
-    m.sourceMmlId should equal(388562342)
-    m.destMmlId should equal(388569406)
+    val partiallyContainedManoeuvre = manoeuvres.find(_.id == 39561).get
+    partiallyContainedManoeuvre.sourceMmlId should equal(388562342)
+    partiallyContainedManoeuvre.destMmlId should equal(388569406)
+    val completelyContainedManoeuvre = manoeuvres.find(_.id == 97666).get
+    completelyContainedManoeuvre.sourceMmlId should equal(388569430)
+    completelyContainedManoeuvre.destMmlId should equal(388569418)
   }
 }
