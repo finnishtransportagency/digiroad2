a1196a99616ab432240f8e3b1ca3241d734b6ef3
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-03-05 15:13:01 +0200

    Add test data for manoeuvre exceptions in development environment

--- digiroad2-oracle/sql/kauniainen_manoeuvres.sql
+++ digiroad2-oracle/sql/kauniainen_manoeuvres.sql
@@ -16,3 +16,33 @@ Insert into MANOEUVRE (ID,TYPE,ROAD_LINK_ID,ELEMENT_TYPE,MODIFIED_DATE,MODIFIED_
 Insert into MANOEUVRE (ID,TYPE,ROAD_LINK_ID,ELEMENT_TYPE,MODIFIED_DATE,MODIFIED_BY) values (39562,2,7389,3,to_timestamp('24-FEB-15','DD-MON-RR'),'dr1_conversion');
 Insert into MANOEUVRE (ID,TYPE,ROAD_LINK_ID,ELEMENT_TYPE,MODIFIED_DATE,MODIFIED_BY) values (39564,2,6804,3,to_timestamp('24-FEB-15','DD-MON-RR'),'dr1_conversion');
 Insert into MANOEUVRE (ID,TYPE,ROAD_LINK_ID,ELEMENT_TYPE,MODIFIED_DATE,MODIFIED_BY) values (39563,2,6804,3,to_timestamp('24-FEB-15','DD-MON-RR'),'dr1_conversion');
+
+-- Ajoneuvo sallittu poikkeussännöt:
+-- 4 Kuorma-auto
+-- 5 Linja-auto
+-- 6 Pakettiauto
+-- 7 Henkilöauto
+-- 8 Taksi
+-- 13 Ajoneuvoyhdistelmä
+-- 14 Traktori tai maatalousajoneuvo
+-- 15 Matkailuajoneuvo
+-- 16 Jakeluauto
+-- 18 Kimppakyytiajoneuvo
+-- 19 Sotilasajoneuvo
+-- 20 Vaarallista lastia kuljettava ajoneuvo
+-- 21 Huoltoajo
+-- 22 Tontille ajo
+
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97666, 4);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97666, 5);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97665, 8);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97665, 16);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97665, 18);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (97665, 19);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39558, 21);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39559, 21);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39560, 21);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39561, 19);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39561, 21);
+Insert into MANOEUVRE_EXCEPTIONS (MANOEUVRE_ID, EXCEPTION_TYPE) values (39561, 22);
+
