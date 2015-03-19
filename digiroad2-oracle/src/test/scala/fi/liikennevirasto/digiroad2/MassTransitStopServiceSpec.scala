42d1e6c3e2f9ebf60c38038d64e146017b949d7b
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-03-19 10:11:37 +0200

    Handle linear referencing with measurements outside geometry

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/MassTransitStopServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/MassTransitStopServiceSpec.scala
@@ -70,4 +70,10 @@ class MassTransitStopServiceSpec extends FunSuite with Matchers {
     val point: Option[Point] = MassTransitStopService.calculateLinearReferencePoint(linkGeometry, -1.5)
     point should be(None)
   }
+
+  test("Linear reference point outside geometry should be undefined") {
+    val linkGeometry = List(Point(0.0, 0.0), Point(1.0, 0.0))
+    val point: Option[Point] = MassTransitStopService.calculateLinearReferencePoint(linkGeometry, 1.5)
+    point should be(None)
+  }
 }
