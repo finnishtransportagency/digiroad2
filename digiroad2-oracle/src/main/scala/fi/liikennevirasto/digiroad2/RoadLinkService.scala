832511b44572e0b97be63dac9de0346935e4beb2
Simo Råman <simo.raman@reaktor.fi>
2015-04-02 12:41:11 +0300

    introduce VVHClient for accessing VVH resources

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RoadLinkService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RoadLinkService.scala
@@ -1,7 +1,5 @@
 package fi.liikennevirasto.digiroad2
 
-import java.net.URLEncoder
-
 import _root_.oracle.spatial.geometry.JGeometry
 import com.jolbox.bonecp.{BoneCPConfig, BoneCPDataSource}
 import fi.liikennevirasto.digiroad2.asset.oracle.AssetPropertyConfiguration.DateTimePropertyFormat
@@ -18,15 +16,11 @@ import scala.slick.driver.JdbcDriver.backend.Database.dynamicSession
 import scala.slick.jdbc.StaticQuery.interpolation
 import scala.slick.jdbc.{GetResult, PositionedResult, StaticQuery => Q}
 
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-
 trait RoadLinkService {
   def fetchVVHRoadlink(mmlId: Long): Option[(Int, Seq[Point])]
   def fetchVVHRoadlinks(bounds: BoundingRectangle, municipalities: Set[Int] = Set()): Seq[(Long, Int, Seq[Point])]
 }
+
 object RoadLinkService extends RoadLinkService {
   case class BasicRoadLink(id: Long, mmlId: Long, geometry: Seq[Point], length: Double, administrativeClass: AdministrativeClass, functionalClass: Int, trafficDirection: TrafficDirection, linkType: Int)
   type KalpaRoadLink = (Long, Long, Seq[Point], AdministrativeClass, Int, TrafficDirection, Int)
@@ -305,56 +299,12 @@ object RoadLinkService extends RoadLinkService {
     }
   }
 
-  protected implicit val jsonFormats: Formats = DefaultFormats
   override def fetchVVHRoadlinks(bounds: BoundingRectangle, municipalities: Set[Int] = Set()): Seq[(Long, Int, Seq[Point])] = {
-    val url = "http://10.129.47.146:6080/arcgis/rest/services/VVH_OTH/Basic_data/FeatureServer/query?" +
-      "layerDefs=0&geometry=" + bounds.leftBottom.x + "," + bounds.leftBottom.y + "," + bounds.rightTop.x + "," + bounds.rightTop.y +
-      "&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&returnGeometry=true&geometryPrecision=3&f=pjson"
-
-    val featureMap: Map[String, Any] = fetchVVHFeatureMap(url)
-
-    val features = featureMap("features").asInstanceOf[List[Map[String, Any]]]
-    features.map(feature => {
-      extractVVHFeature(feature)
-    })
+    VVHClient.fetchVVHRoadlinks(bounds, municipalities)
   }
 
   override def fetchVVHRoadlink(mmlId: Long): Option[(Int, Seq[Point])] = {
-    val layerDefs = URLEncoder.encode(s"""{"0":"MTK_ID=$mmlId"}""", "UTF-8")
-    val url = "http://10.129.47.146:6080/arcgis/rest/services/VVH_OTH/Basic_data/FeatureServer/query?" +
-      s"layerDefs=$layerDefs&returnGeometry=true&geometryPrecision=3&f=pjson"
-
-    val featureMap: Map[String, Any] = fetchVVHFeatureMap(url)
-
-    val features = featureMap("features").asInstanceOf[List[Map[String, Any]]]
-    features.headOption.map(feature => {
-      extractVVHFeature(feature)
-    }).map{ x => (x._2, x._3)}
-  }
-
-  private def fetchVVHFeatureMap(url: String): Map[String, Any] = {
-    val request = new HttpGet(url)
-    val client = HttpClientBuilder.create().build()
-    val response = client.execute(request)
-    val content = parse(StreamInput(response.getEntity.getContent)).values.asInstanceOf[Map[String, Any]]
-    val layers = content("layers").asInstanceOf[List[Map[String, Any]]]
-    val featureMap: Map[String, Any] = layers.find(map => {
-      map.contains("features")
-    }).get
-    featureMap
-  }
-
-  private def extractVVHFeature(feature: Map[String, Any]): (Long, Int, Seq[Point]) = {
-    val geometry = feature("geometry").asInstanceOf[Map[String, Any]]
-    val paths = geometry("paths").asInstanceOf[List[List[List[Double]]]]
-    val path: List[List[Double]] = paths.head
-    val linkGeometry: Seq[Point] = path.map(point => {
-      Point(point(0), point(1))
-    })
-    val attributes = feature("attributes").asInstanceOf[Map[String, Any]]
-    val mmlId = attributes("MTK_ID").asInstanceOf[BigInt].longValue()
-    val municipalityCode = attributes("KUNTATUNNUS").asInstanceOf[String].toInt
-    (mmlId, municipalityCode, linkGeometry)
+    VVHClient.fetchVVHRoadlink(mmlId)
   }
 
   def getRoadLinkDataByMmlIds(mmlIds: Seq[Long]): Seq[AdjustedRoadLink] = {
