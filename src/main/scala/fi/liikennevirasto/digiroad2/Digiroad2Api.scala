f91b8837e1fe6697853c94c4ef5421c65996bfb1
Simo Råman <simo.raman@reaktor.fi>
2015-04-02 15:49:41 +0300

    update properties for mass transit stop before position

--- src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
@@ -123,8 +123,8 @@ class Digiroad2Api extends ScalatraServlet with JacksonJsonSupport with CorsSupp
   private def updateMassTransitStop(id: Long, position: Option[Position], properties: Seq[SimpleProperty]): AssetWithProperties = {
     useVVHGeometry match {
       case true =>
-        val massTransitStop = position.map { position => MassTransitStopService.updatePosition(id, position) }
         val asset = assetProvider.updateAsset(id, None, properties)
+        val massTransitStop = position.map { position => MassTransitStopService.updatePosition(id, position) }
         massTransitStop.getOrElse(asset)
       case false =>
         assetProvider.updateAsset(id, position, properties)
