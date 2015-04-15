73a744730720132fbb877238f0911175b317928f
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-04-15 11:14:22 +0300

    Make local functions private

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
@@ -232,7 +232,7 @@ trait MassTransitStopService {
     requiredProperties + (validityDirection.publicId -> validityDirection.propertyType)
   }
 
-  def calculatePointFromLinearReference(geometry: Seq[Point], measure: Double): Option[Point] = {
+  private def calculatePointFromLinearReference(geometry: Seq[Point], measure: Double): Option[Point] = {
     case class AlgorithmState(previousPoint: Point, remainingMeasure: Double, result: Option[Point])
     if (geometry.size < 2 || measure < 0) { None }
     else {
@@ -254,7 +254,7 @@ trait MassTransitStopService {
     }
   }
 
-  def calculateLinearReferenceFromPoint(point: Point, points: Seq[Point]): Double = {
+  private def calculateLinearReferenceFromPoint(point: Point, points: Seq[Point]): Double = {
     case class Projection(distance: Double, segmentIndex: Int, segmentLength: Double, mValue: Double)
     val lineSegments: Seq[((Point, Point), Int)] = points.zip(points.tail).zipWithIndex
     val projections: Seq[Projection] = lineSegments.map { case((p1: Point, p2: Point), segmentIndex: Int) =>
