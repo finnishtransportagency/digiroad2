bcd6feb52456b5d59535111628d6bcffa2a6c292
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-04-20 13:51:46 +0300

    Remove dependency on order of returned data in tests

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/PointAssetServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/PointAssetServiceSpec.scala
@@ -5,13 +5,12 @@ import org.scalatest.{Matchers, FunSuite}
 class PointAssetServiceSpec extends FunSuite with Matchers {
   test("Get traffic light point data") {
     val data = PointAssetService.getByMunicipality(9, 235)
-    val element = data(0)
-
-    val id = element.get("id").get.asInstanceOf[Long]
-    id should equal(24189944)
-    val point = element.get("point").get.asInstanceOf[Point]
-    point.x should equal(374673.0256)
-    point.y should equal(6676887.3011)
+    val trafficLight = data.find { light =>
+      light.get("id").get.asInstanceOf[Long] == 24189944
+    }
+    val point = trafficLight.map { light => light.get("point").get.asInstanceOf[Point] }
+    point should be(Some(Point(374673.0256, 6676887.3011)))
+    data.length should be(23)
   }
 
   test("Get Service point data") {
