ca2236007b84e783255573181433aa11fa769a76
Simo Råman <simo.raman@reaktor.fi>
2015-05-11 14:16:15 +0300

    return 99 if administrative class is null in VVH

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/VVHClient.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/VVHClient.scala
@@ -80,7 +80,11 @@ class VVHClient(hostname: String) {
     val attributes = feature("attributes").asInstanceOf[Map[String, Any]]
     val mmlId = attributes("MTK_ID").asInstanceOf[BigInt].longValue()
     val municipalityCode = attributes("KUNTATUNNUS").asInstanceOf[String].toInt
-    val administrativeClass = attributes("HALLINNOLLINENLUOKKA").asInstanceOf[BigInt].toInt
-    (mmlId, municipalityCode, linkGeometry, administrativeClass)
+    (mmlId, municipalityCode, linkGeometry, extractAdministrativeClass(attributes))
+  }
+
+  private def extractAdministrativeClass(attributes: Map[String, Any]): Int = {
+    val administrativeClass = attributes("HALLINNOLLINENLUOKKA").asInstanceOf[BigInt]
+    if(administrativeClass == null ) 99 else administrativeClass.toInt
   }
 }
