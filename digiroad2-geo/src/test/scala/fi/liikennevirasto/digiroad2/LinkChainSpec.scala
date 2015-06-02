4fff77c8f818b5174a010fe6d633b7f098312053
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-06-02 10:33:19 +0300

    Harden the link chains gap test to cover different geometry running direction combinations

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/LinkChainSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/LinkChainSpec.scala
@@ -41,11 +41,15 @@ class LinkChainSpec extends FunSuite with Matchers {
   }
 
   test("generate position indices and calculate gap for two segment sequence with a gap") {
-    val segments = List(
-      (Point(0, 0), Point(0, 100)),
-      (Point(0, 200), Point(0, 300))
-    )
-    identityLinkChain(segments).linkGaps(identity[(Point, Point)]) shouldBe Seq(100.0)
-    identityLinkChain(segments).map{ link => (link.linkIndex, link.linkPosition) }.sortBy(_._1).map(_._2) shouldBe Seq(0, 1)
+    val testSegments = List(
+      List((Point(0, 0), Point(0, 100)), (Point(0, 200), Point(0, 300))),
+      List((Point(0, 0), Point(0, 100)), (Point(0, 300), Point(0, 200))),
+      List((Point(0, 100), Point(0, 0)), (Point(0, 200), Point(0, 300))),
+      List((Point(0, 100), Point(0, 0)), (Point(0, 300), Point(0, 200))))
+
+    testSegments.foreach { segments =>
+      identityLinkChain(segments).linkGaps() shouldBe Seq(100.0)
+      identityLinkChain(segments).map{ link => (link.linkIndex, link.linkPosition) }.sortBy(_._1).map(_._2) shouldBe Seq(0, 1)
+    }
   }
 }
