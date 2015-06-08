6c74c8bb8141e348a844ac764c46fc3ca342f7f9
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-06-08 10:52:05 +0300

    Verify that M-Value adjustments are generated

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/SpeedLimitFillerSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/SpeedLimitFillerSpec.scala
@@ -1,5 +1,6 @@
 package fi.liikennevirasto.digiroad2
 
+import fi.liikennevirasto.digiroad2.SpeedLimitFiller.{MValueAdjustment, SpeedLimitChangeSet}
 import fi.liikennevirasto.digiroad2.asset.Unknown
 import fi.liikennevirasto.digiroad2.linearasset.{SpeedLimitDTO, RoadLinkForSpeedLimit}
 import org.scalatest._
@@ -11,12 +12,13 @@ class SpeedLimitFillerSpec extends FunSuite with Matchers {
       3l -> RoadLinkForSpeedLimit(Seq(Point(2.0, 0.0), Point(3.0, 0.0)), 1.0, Unknown, 3))
     val speedLimits = Map(1l -> Seq(
       SpeedLimitDTO(1, 1, 0, None, Seq(Point(0.0, 0.0), Point(1.0, 0.0)), 0.0, 1.0),
-      SpeedLimitDTO(1, 2, 0, None, Seq(Point(1.0, 0.0), Point(1.5, 0.0)), 0.0, 0.5),
+      SpeedLimitDTO(1, 2, 0, None, Seq(Point(1.0, 0.0), Point(1.2, 0.0)), 0.0, 0.2),
       SpeedLimitDTO(1, 3, 0, None, Seq(Point(2.0, 0.0), Point(3.0, 0.0)), 0.0, 1.0)))
     val (filledTopology, changeSet) = SpeedLimitFiller.fillTopology2(topology, speedLimits)
     filledTopology.map(_.mmlId) should be (Seq(1, 2, 3))
     filledTopology.map(_.points) should be (Seq(Seq(Point(0.0, 0.0), Point(1.0, 0.0)),
       Seq(Point(1.0, 0.0), Point(2.0, 0.0)),
       Seq(Point(2.0, 0.0), Point(3.0, 0.0))))
+    changeSet.adjustedMValues should be (Seq(MValueAdjustment(1, 2, 1.0)))
   }
 }
