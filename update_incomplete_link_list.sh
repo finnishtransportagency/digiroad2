9d8da594b95307ba9d8f4beaf5fc0235e785a402
Mikael Koponen <mikael.koponen@gmail.com>
2015-07-03 12:46:14 +0300

    Save heap dump on OutOfMemoryError when updating incomplete links

--- update_incomplete_link_list.sh
+++ update_incomplete_link_list.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-SBT_OPTS="-Xms1512M -Xmx4096m -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024M"
+SBT_OPTS="-Xms1512M -Xmx4096m -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024M -XX:+HeapDumpOnOutOfMemoryError"
 java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar ${1} 'project digiroad2' "runMain fi.liikennevirasto.digiroad2.util.UpdateIncompleteLinkList"
