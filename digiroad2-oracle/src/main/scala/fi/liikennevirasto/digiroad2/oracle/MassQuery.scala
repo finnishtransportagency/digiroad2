c2aa012f3c027758378ac13cb14ffd6dbe9201e5
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-07-03 13:13:46 +0300

    Create new temporary id table that should perform better

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/oracle/MassQuery.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/oracle/MassQuery.scala
@@ -5,14 +5,14 @@ import scala.slick.jdbc.StaticQuery.interpolation
 
 object MassQuery {
   def withIds[T](ids: Set[Long])(f: String => T): T = {
-    val insertMmlIdPS = dynamicSession.prepareStatement("insert into temporary_id (id) values (?)")
+    val insertMmlIdPS = dynamicSession.prepareStatement("insert into temp_id (id) values (?)")
     ids.foreach { id =>
       insertMmlIdPS.setLong(1, id)
       insertMmlIdPS.addBatch()
     }
     insertMmlIdPS.executeBatch()
-    val ret = f("temporary_id")
-    sqlu"""delete from temporary_id""".execute()
+    val ret = f("temp_id")
+    sqlu"""delete from temp_id""".execute()
     ret
   }
 }
