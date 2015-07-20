1be6f61373668e99038258cbba1b9bf28a43e97f
Mikael Koponen <mikael.koponen@gmail.com>
2015-07-20 16:44:37 +0300

    Fix formatting

--- UI/test/MultiSegmentSpeedLimitSpec.js
+++ UI/test/MultiSegmentSpeedLimitSpec.js
@@ -28,33 +28,33 @@ define(['chai', 'TestHelpers'], function(chai, testHelpers) {
     });
 
     describe('and selecting speed limit', function() {
-      before(function () {
+      before(function() {
         testHelpers.selectSpeedLimit(openLayersMap, speedLimit.id);
       });
 
-      describe('and clicking on the background map', function () {
-        before(function () {
-          var layer = _.find(openLayersMap.layers, function (layer) {
+      describe('and clicking on the background map', function() {
+        before(function() {
+          var layer = _.find(openLayersMap.layers, function(layer) {
             return layer.isBaseLayer;
           }).div;
           testHelpers.clickElement(layer);
         });
-        it('deselects speed limit', function () {
+        it('deselects speed limit', function() {
           expect($('#feature-attributes header')).not.to.exist;
         });
       });
 
       describe('and selecting assets layer', function() {
-        before(function () {
-          $('.panel-header').filter(':visible').filter(function (i, element) {
+        before(function() {
+          $('.panel-header').filter(':visible').filter(function(i, element) {
             return _.contains($(element).text(), 'Joukkoliikenteen pysäkit');
           }).click();
         });
-        describe('and reselecting speed limits layer', function () {
-          before(function () {
+        describe('and reselecting speed limits layer', function() {
+          before(function() {
             $('.speed-limits').click();
           });
-          it('deselects speed limit', function () {
+          it('deselects speed limit', function() {
             expect($('#feature-attributes header')).not.to.exist;
           });
         });
@@ -65,14 +65,14 @@ define(['chai', 'TestHelpers'], function(chai, testHelpers) {
   describe('when loading application in edit mode with speed limits', function() {
     var openLayersMap;
     var speedLimitId = 13;
-    var speedLimitGroup = _.filter(SpeedLimitsTestData.generate(), function (linkGroup) {
-      return _.some(linkGroup, { id: speedLimitId });
+    var speedLimitGroup = _.filter(SpeedLimitsTestData.generate(), function(linkGroup) {
+      return _.some(linkGroup, {id: speedLimitId});
     });
     var speedLimitsInGroup = _.flatten(speedLimitGroup);
 
     var speedLimitConstructor = SpeedLimitsTestData.generateSpeedLimitConstructor(speedLimitGroup);
-    before(function (done) {
-      testHelpers.restartApplication(function (map) {
+    before(function(done) {
+      testHelpers.restartApplication(function(map) {
         openLayersMap = map;
         $('.speed-limits').click();
         testHelpers.clickVisibleEditModeButton();
@@ -83,27 +83,27 @@ define(['chai', 'TestHelpers'], function(chai, testHelpers) {
         .withSpeedLimitUpdate(speedLimitConstructor(speedLimitId)));
     });
 
-    describe('and changing value of speed limit', function () {
-      before(function () {
+    describe('and changing value of speed limit', function() {
+      before(function() {
         testHelpers.selectSpeedLimit(openLayersMap, speedLimitId);
         $('#feature-attributes .form-control.speed-limit').val('100').change();
       });
-      it('should update all speed limit links on map', function () {
-        var features = _.filter(testHelpers.getSpeedLimitFeatures(openLayersMap), function (feature) {
-          return _.some(speedLimitsInGroup, { id : feature.attributes.id });
+      it('should update all speed limit links on map', function() {
+        var features = _.filter(testHelpers.getSpeedLimitFeatures(openLayersMap), function(feature) {
+          return _.some(speedLimitsInGroup, {id: feature.attributes.id});
         });
 
         expect(features).not.to.equal(0);
-        _.each(features, function (feature) {
+        _.each(features, function(feature) {
           expect(feature.attributes.value).to.equal(100);
         });
       });
 
-      describe('and cancelling the change', function () {
-        before(function () {
+      describe('and cancelling the change', function() {
+        before(function() {
           $('#feature-attributes .speed-limit button.cancel').click();
         });
-        it('resets the update but maintains selection state', function () {
+        it('resets the update but maintains selection state', function() {
           assertSpeedLimitIsSelectedWithLimitValue(openLayersMap, speedLimitId, 60);
         });
       });
@@ -113,13 +113,13 @@ define(['chai', 'TestHelpers'], function(chai, testHelpers) {
   describe('when loading application in edit mode with speed limits', function() {
     var openLayersMap;
     var speedLimitId = 13;
-    var speedLimits = _.filter(SpeedLimitsTestData.generate(), function (link) {
-      return link[0].id === speedLimitId;
+    var speedLimits = _.filter(SpeedLimitsTestData.generate(), function(link) {
+      return _.some(link, { id:  speedLimitId });
     });
 
     var speedLimitConstructor = SpeedLimitsTestData.generateSpeedLimitConstructor(speedLimits);
-    before(function (done) {
-      testHelpers.restartApplication(function (map) {
+    before(function(done) {
+      testHelpers.restartApplication(function(map) {
         openLayersMap = map;
         $('.speed-limits').click();
         testHelpers.clickVisibleEditModeButton();
