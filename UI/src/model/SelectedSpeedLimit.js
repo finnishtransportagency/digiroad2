33586525b283c6c3997ed98262d86c68f8f399f1
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-07-29 10:59:17 +0300

    Remove obsolete getEndpoints function

--- UI/src/model/SelectedSpeedLimit.js
+++ UI/src/model/SelectedSpeedLimit.js
@@ -163,10 +163,6 @@
       return getProperty('id');
     };
 
-    this.getEndpoints = function() {
-      return getProperty('endpoints');
-    };
-
     this.getValue = function() {
       return getProperty('value');
     };
