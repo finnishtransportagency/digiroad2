1ed4bf2cf26420cd9aca7869ed7d57bf022b4083
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-07-30 10:21:46 +0300

    Fix usage of _.rest

--- UI/src/view/AssetForm.js
+++ UI/src/view/AssetForm.js
@@ -423,7 +423,7 @@
         var properties = sortProperties(selectedAssetModel.getProperties());
         var contents = _.take(properties, 2)
           .concat(floatingStatus(selectedAssetModel))
-          .concat(_.rest(properties, 2));
+          .concat(_.drop(properties, 2));
         var components =_.map(contents, function(feature){
           feature.localizedName = window.localizedStrings[feature.publicId];
           var propertyType = feature.propertyType;
