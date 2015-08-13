89271d46b809282b26cc4da2c64f00c22a1a38db
Mikael Koponen <mikael.koponen@gmail.com>
2015-08-13 16:07:05 +0300

    Parameterize bounding box deltas

--- digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/BrowseSpeedLimitSimulation.scala
+++ digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/BrowseSpeedLimitSimulation.scala
@@ -4,52 +4,71 @@ import java.util.concurrent.TimeUnit
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.core.feeder.RecordSeqFeederBuilder
 
 import scala.concurrent.duration.FiniteDuration
 
 class BrowseSpeedLimitSimulation extends Simulation {
 
-  val scn = scenario("Browse speed limits")
+  val deltas = Seq(Seq(-284.0, 8.0, -284.0, 8.0),
+      Seq(-1487.84599685, -801.154003200121, 912.154003150004, 816.845996799879),
+      Seq(-287.845996849996, 7.84599679987878, -287.845996849996, 7.84599679987878),
+      Seq(-2087.84599685, -1205.65400320012, 1512.15400315, 1221.34599679988),
+      Seq(-2414.45438104001, -965.857063200325, 1185.54561895999, 1461.14293679968),
+      Seq(-2557.01037753001, -702.325241800398, 1042.98962246999, 1724.6747581996),
+      Seq(-2530.09395842999, -404.69544180017, 1069.90604157001, 2022.30455819983),
+      Seq(-2753.67487558001, -195.987239300273, 846.325124419993, 2231.01276069973))
+
+  val startBoundingBoxes = IndexedSeq(Seq(370624.0,6670024.0,375424.0,6673260.0))
+
+  val feeder = startBoundingBoxes.map { startBoundingBox =>
+    val adjustedBoundingBoxes = deltas.map(_.zip(startBoundingBox).map { x => x._2 - x._1 })
+    val boundingBoxes = (startBoundingBox +: adjustedBoundingBoxes).map(_.mkString(","))
+    Map("boundingBoxes" -> boundingBoxes)
+  }.queue
+
+  val scn = scenario("Browse speed limits").exec(feed(feeder)
     .exec(http("load page")
     .get("/"))
     .pause(5)
-    .exec(http("roadlinks highest level").get("/api/roadlinks2?bbox=370624,6670024,375424,6673260")
+    .exec(http("roadlinks highest level").get("/api/roadlinks2?bbox=${boundingBoxes(0)}")
     .resources(
       http("roadlinks zoom 1")
-        .get("/api/roadlinks2?bbox=370908,6670016,375708,6673252"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(1)}"),
       http("speedlimits highest level")
-        .get("/api/speedlimits?bbox=370624,6670024,375424,6673260")))
+        .get("/api/speedlimits?bbox=${boundingBoxes(0)}")))
     .pause(1)
     .exec(http("speedlimits zoom 1")
-    .get("/api/speedlimits?bbox=370908,6670016,375708,6673252"))
+    .get("/api/speedlimits?bbox=${boundingBoxes(1)}"))
     .pause(1)
-    .exec(http("roadlinks zoom 2").get("/api/roadlinks2?bbox=372111.84599685,6670825.1540032,374511.84599685,6672443.1540032")
+    .exec(http("roadlinks zoom 2").get("/api/roadlinks2?bbox=${boundingBoxes(2)}")
     .resources(
       http("roadlinks zoom 3")
-        .get("/api/roadlinks2?bbox=370911.84599685,6670016.1540032,375711.84599685,6673252.1540032"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(3)}"),
       http("roadlinks zoom 4")
-        .get("/api/roadlinks2?bbox=372711.84599685,6671229.6540032,373911.84599685,6672038.6540032"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(4)}"),
       http("speedlimits zoom 4")
-        .get("/api/speedlimits?bbox=372711.84599685,6671229.6540032,373911.84599685,6672038.6540032"),
+        .get("/api/speedlimits?bbox=${boundingBoxes(4)}"),
       http("speedlimits zoom 4")
-        .get("/api/speedlimits?bbox=370911.84599685,6670016.1540032,375711.84599685,6673252.1540032"),
+        .get("/api/speedlimits?bbox=${boundingBoxes(4)}"),
       http("roadlinks zoom 5")
-        .get("/api/roadlinks2?bbox=373038.45438104,6670989.8570632,374238.45438104,6671798.8570632"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(5)}"),
       http("roadlinks zoom 6")
-        .get("/api/roadlinks2?bbox=373181.01037753,6670726.3252418,374381.01037753,6671535.3252418"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(6)}"),
       http("roadlinks zoom 7")
-        .get("/api/roadlinks2?bbox=373154.09395843,6670428.6954418,374354.09395843,6671237.6954418"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(7)}"),
       http("speedlimits zoom 5")
-        .get("/api/speedlimits?bbox=373038.45438104,6670989.8570632,374238.45438104,6671798.8570632"),
+        .get("/api/speedlimits?bbox=${boundingBoxes(5)}"),
       http("speedlimits zoom 6")
-        .get("/api/speedlimits?bbox=373181.01037753,6670726.3252418,374381.01037753,6671535.3252418"),
+        .get("/api/speedlimits?bbox=${boundingBoxes(6)}"),
       http("speedlimits zoom 7")
-        .get("/api/speedlimits?bbox=373154.09395843,6670428.6954418,374354.09395843,6671237.6954418"),
+        .get("/api/speedlimits?bbox=${boundingBoxes(7)}"),
       http("roadlinks zoom 8")
-        .get("/api/roadlinks2?bbox=373377.67487558,6670219.9872393,374577.67487558,6671028.9872393"),
+        .get("/api/roadlinks2?bbox=${boundingBoxes(8)}"),
       http("speedlimits zoom 8")
-        .get("/api/speedlimits?bbox=373377.67487558,6670219.9872393,374577.67487558,6671028.9872393")))
+        .get("/api/speedlimits?bbox=${boundingBoxes(8)}"))))
 
   private val duration: FiniteDuration = FiniteDuration(TestConfiguration.users, TimeUnit.SECONDS)
   setUp(scn.inject(rampUsers(TestConfiguration.users) over duration)).protocols(TestConfiguration.httpConf)
+
 }
