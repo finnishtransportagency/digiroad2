8aec21f921eaf14f46db6072451cae81a8271a80
Simo Råman <simo.raman@reaktor.fi>
2015-08-14 12:49:29 +0300

    load bounding box start values from file

--- digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/BrowseSpeedLimitSimulation.scala
+++ digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/BrowseSpeedLimitSimulation.scala
@@ -4,11 +4,20 @@ import java.util.concurrent.TimeUnit
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.core.feeder.RecordSeqFeederBuilder
 
+import scala.collection.immutable.IndexedSeq
 import scala.concurrent.duration.FiniteDuration
 
 class BrowseSpeedLimitSimulation extends Simulation {
+  def getStartBoundingBoxes: IndexedSeq[Seq[Double]] = {
+    val source = scala.io.Source.fromFile("./boundingBoxes.csv")
+    val startBoundingBoxes = source.getLines().map { l =>
+      val cols = l.split(',').toSeq
+      cols.map(_.toDouble)
+    }.toIndexedSeq
+    source.close()
+    startBoundingBoxes
+  }
 
   val deltas = Seq(Seq(-284.0, 8.0, -284.0, 8.0),
       Seq(-1487.84599685, -801.154003200121, 912.154003150004, 816.845996799879),
@@ -19,28 +28,7 @@ class BrowseSpeedLimitSimulation extends Simulation {
       Seq(-2530.09395842999, -404.69544180017, 1069.90604157001, 2022.30455819983),
       Seq(-2753.67487558001, -195.987239300273, 846.325124419993, 2231.01276069973))
 
-  private val espooBoundingBox: Seq[Double] = Seq(370624.0, 6670024.0, 375424.0, 6673260.0)
-  private val haminaBoundingBox: Seq[Double] = Seq(507434,6713400,514290,6716352)
-  private val hankoBoundingBox: Seq[Double] = Seq(270506,6638124,277362,6641076)
-  private val hollolaBoundingBox: Seq[Double] = Seq(416050,6760868,422906,6763820)
-  private val huittinenBoundingBox: Seq[Double] = Seq(265350,6789536,272206,6792488)
-  private val hyvinkaaBoundingBox: Seq[Double] = Seq(379622,6721932,386478,6724884)
-  private val iisalmiBoundingBox: Seq[Double] = Seq(506487.0,7046614.0,513343.0,7049566.0)
-  private val iittiBoundingBox: Seq[Double] = Seq(460783.0,6749306.0,467639.0,6752258.0)
-  private val ilmajokiBoundingBox: Seq[Double] = Seq(270655.0,6961910.0,277511.0,6964862.0)
-  private val jyvaskylaBoundingBox: Seq[Double] = Seq(431483.0,6900333.0,438339.0,6903285.0)
-
-  val startBoundingBoxes = IndexedSeq(
-    espooBoundingBox,
-    haminaBoundingBox,
-    hankoBoundingBox,
-    hollolaBoundingBox,
-    huittinenBoundingBox,
-    hyvinkaaBoundingBox,
-    iisalmiBoundingBox,
-    iittiBoundingBox,
-    ilmajokiBoundingBox,
-    jyvaskylaBoundingBox)
+  val startBoundingBoxes = getStartBoundingBoxes
 
   val feeder = startBoundingBoxes.map { startBoundingBox =>
     val adjustedBoundingBoxes = deltas.map(_.zip(startBoundingBox).map { x => x._2 - x._1 })
@@ -52,7 +40,8 @@ class BrowseSpeedLimitSimulation extends Simulation {
     .exec(http("load page")
     .get("/"))
     .pause(5)
-    .exec(http("roadlinks highest level").get("/api/roadlinks2?bbox=${boundingBoxes(0)}")
+    .exec(http("roadlinks highest level")
+    .get("/api/roadlinks2?bbox=${boundingBoxes(0)}")
     .resources(
       http("roadlinks zoom 1")
         .get("/api/roadlinks2?bbox=${boundingBoxes(1)}"),
