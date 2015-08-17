14ef2dfebe1d0c6c8553eb9d2952810473f18101
Simo Råman <simo.raman@reaktor.fi>
2015-08-17 13:01:05 +0300

    fix created by value

--- digiroad2-oracle/src/main/resources/db/migration/V47__add_speed_limit_property_for_90.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V47__add_speed_limit_property_for_90.sql
@@ -1,2 +1,2 @@
 insert into enumerated_value (id, value, name_fi, name_sv, property_id, created_by, created_date)
-values (primary_key_seq.nextval, 90, '90', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v4', sysdate);
+values (primary_key_seq.nextval, 90, '90', '', (select id from property where public_id = 'rajoitus'), 'db_migration_v47', sysdate);
