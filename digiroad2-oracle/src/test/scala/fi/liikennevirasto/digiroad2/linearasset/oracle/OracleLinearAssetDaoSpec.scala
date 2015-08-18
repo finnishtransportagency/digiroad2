6829d8129417c3a8d00b5ed84f6fb7df12fc6d4e
Mikael Koponen <mikael.koponen@gmail.com>
2015-08-18 15:34:35 +0300

    Add link with multiple unknown speed limits to fixture data

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetDaoSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/oracle/OracleLinearAssetDaoSpec.scala
@@ -230,10 +230,11 @@ class OracleLinearAssetDaoSpec extends FunSuite with Matchers {
 
   test("speed limit purge removes fully covered link from unknown speed limit list") {
     Database.forDataSource(ds).withDynTransaction {
-      val mmlId = 1068804939
+      val mmlId = 1068804942
+      sqlu"""delete from unknown_speed_limit""".execute
       sqlu"""insert into unknown_speed_limit (mml_id, municipality_code, administrative_class) values ($mmlId, 235, 1)""".execute
       val dao = daoWithRoadLinks(Nil)
-      dao.purgeFromUnknownSpeedLimits(mmlId, 86.123)
+      dao.purgeFromUnknownSpeedLimits(mmlId, 59.934)
       sql"""select mml_id from unknown_speed_limit where mml_id = $mmlId""".as[Long].firstOption should be(None)
       dynamicSession.rollback()
     }
