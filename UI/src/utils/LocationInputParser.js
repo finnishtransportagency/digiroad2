dbd723a14987fd18d9b1838f60ee1f3a84381eb4
Mikael Koponen <mikael.koponen@gmail.com>
2015-08-21 13:23:00 +0300

    Fix indentation

--- UI/src/utils/LocationInputParser.js
+++ UI/src/utils/LocationInputParser.js
@@ -1,22 +1,22 @@
 (function(root) {
   var parse = function(input) {
-      var coordinateRegex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
-      var matchedCoordinates = input.match(coordinateRegex);
-      var parsedOutput;
-      if (matchedCoordinates) {
-          return parseCoordinates(matchedCoordinates);
-      } else {
-        return parseStreet(input);
-      }
+    var coordinateRegex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
+    var matchedCoordinates = input.match(coordinateRegex);
+    var parsedOutput;
+    if (matchedCoordinates) {
+      return parseCoordinates(matchedCoordinates);
+    } else {
+      return parseStreet(input);
+    }
   };
 
   var parseCoordinates = function(coordinates) {
-      return { type: 'coordinate', lat: parseInt(coordinates[1], 10), lon: parseInt(coordinates[2], 10) };
+    return { type: 'coordinate', lat: parseInt(coordinates[1], 10), lon: parseInt(coordinates[2], 10) };
   };
 
   var parseStreet = function(input) {
-      var result = _.map(input.split(','), _.trim);
-      return { type: 'street', street: result[0], municipality: result[1] };
+    var result = _.map(input.split(','), _.trim);
+    return { type: 'street', street: result[0], municipality: result[1] };
   };
 
   var LocationInputParser = {
--- UI/test/LocationInputParserSpec.js
+++ UI/test/LocationInputParserSpec.js
@@ -1,11 +1,11 @@
 define(['chai', 'LocationInputParser'], function(chai, LocationInputParser) {
-    var assert = chai.assert;
+  var assert = chai.assert;
 
-    it('parses coordinate pairs', function() {
-        assert.deepEqual({ type: 'coordinate', lat: 123, lon: 345 }, LocationInputParser.parse('123,345'));
-    });
+  it('parses coordinate pairs', function() {
+    assert.deepEqual({ type: 'coordinate', lat: 123, lon: 345 }, LocationInputParser.parse('123,345'));
+  });
 
-    it('parses street addresses', function() {
-        assert.deepEqual({ type: 'street', street: 'Salorankatu 7', municipality: 'Salo' }, LocationInputParser.parse('Salorankatu 7, Salo'));
-    });
+  it('parses street addresses', function() {
+    assert.deepEqual({ type: 'street', street: 'Salorankatu 7', municipality: 'Salo' }, LocationInputParser.parse('Salorankatu 7, Salo'));
+  });
 });
