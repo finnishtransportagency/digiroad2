adec92e3a506417fc7b8207f11003352973c3e00
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-08-25 13:05:03 +0300

    Fix tooltip and placeholder

--- UI/src/actionpanel/SearchBox.js
+++ UI/src/actionpanel/SearchBox.js
@@ -1,9 +1,9 @@
 (function(root) {
   root.SearchBox = function() {
-    var tooltip = "Koordinaattien sy&ouml;tt&ouml;: pohjoinen (7 merkki&auml;), it&auml; (6 merkki&auml;). Esim. 6901839, 435323";
+    var tooltip = "Hae katuosoitteella, tieosoitteella tai koordinaateilla";
     var groupDiv = $('<div class="panel-group"/>');
     var coordinatesDiv = $('<div class="panel search-box"/>');
-    var coordinatesText = $('<input type="text" class="location input-sm" placeholder="P, I" title="' + tooltip + '"/>');
+    var coordinatesText = $('<input type="text" class="location input-sm" placeholder="Osoite tai koordinaatit" title="' + tooltip + '"/>');
     var moveButton = $('<button class="btn btn-sm btn-primary">Hae</button>');
 
     this.element = groupDiv.append(coordinatesDiv.append(coordinatesText).append(moveButton));
