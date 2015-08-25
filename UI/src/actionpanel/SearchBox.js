ddb1fce64a995bc144739a129dcde1d568fa40f5
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-08-25 12:49:33 +0300

    Add search controls to action panel

--- UI/src/actionpanel/SearchBox.js
+++ UI/src/actionpanel/SearchBox.js
@@ -1,5 +1,11 @@
 (function(root) {
   root.SearchBox = function() {
-    this.element = '<div>Hevonen</div>';
+    var tooltip = "Koordinaattien sy&ouml;tt&ouml;: pohjoinen (7 merkki&auml;), it&auml; (6 merkki&auml;). Esim. 6901839, 435323";
+    var groupDiv = $('<div class="panel-group"/>');
+    var coordinatesDiv = $('<div class="panel search-box"/>');
+    var coordinatesText = $('<input type="text" class="location input-sm" placeholder="P, I" title="' + tooltip + '"/>');
+    var moveButton = $('<button class="btn btn-sm btn-primary">Hae</button>');
+
+    this.element = groupDiv.append(coordinatesDiv.append(coordinatesText).append(moveButton));
   };
 })(this);
