31e52b1e849b1592b41f97d95ba28240e5065a85
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-08-25 10:39:53 +0300

    Make vkm request url's relative

--- UI/src/utils/backend-utils.js
+++ UI/src/utils/backend-utils.js
@@ -265,11 +265,11 @@
     };
 
     this.getGeocode = function(address) {
-      return $.post("/vkm/geocode", { address: address }).then(function(x) { return JSON.parse(x); });
+      return $.post("vkm/geocode", { address: address }).then(function(x) { return JSON.parse(x); });
     };
 
     this.getCoordinatesFromRoadAddress = function(roadNumber, section, distance, lane) {
-      return $.get("/vkm/tieosoite", {tie: roadNumber, osa: section, etaisyys: distance, ajorata: lane})
+      return $.get("vkm/tieosoite", {tie: roadNumber, osa: section, etaisyys: distance, ajorata: lane})
         .then(function(x) { return JSON.parse(x); });
     };
 
