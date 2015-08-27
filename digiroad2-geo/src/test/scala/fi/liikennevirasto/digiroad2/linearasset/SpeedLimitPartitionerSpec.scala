702245fe55b78ea3089f49c46477e9301f980edb
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-08-27 13:00:01 +0300

    Separate speed limit and road link partitioner tests

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/SpeedLimitPartitionerSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/linearasset/SpeedLimitPartitionerSpec.scala
@@ -1,18 +1,12 @@
-package fi.liikennevirasto.digiroad2
+package fi.liikennevirasto.digiroad2.linearasset
 
+import fi.liikennevirasto.digiroad2.Point
 import fi.liikennevirasto.digiroad2.asset._
-import fi.liikennevirasto.digiroad2.linearasset.{RoadLinkPartitioner, SpeedLimitPartitioner, VVHRoadLinkWithProperties, SpeedLimit}
 import org.scalatest._
 
 class SpeedLimitPartitionerSpec extends FunSuite with Matchers {
   private def speedLimitLink(mmlId: Long, value: Int, geometry: Seq[Point]) = {
     SpeedLimit(0, mmlId, SideCode.BothDirections, TrafficDirection.BothDirections, Some(value), geometry, 0.0, 0.0, None, None, None, None)
-  }
-
-  private def roadLink(mmlId: Long, geometry: Seq[Point]) = {
-    VVHRoadLinkWithProperties(mmlId, geometry, 0.0, Municipality, 0,
-      TrafficDirection.BothDirections, Motorway, None, None,
-      Map("ROADNUMBER" -> BigInt(123)))
   }
 
   test("group speed limits with same limit value and road number") {
@@ -87,76 +81,5 @@ class SpeedLimitPartitionerSpec extends FunSuite with Matchers {
 
     val groupedLinks = SpeedLimitPartitioner.partition(speedLimitLinks, roadIdentifiers)
     groupedLinks should have size 2
-  }
-
-  test("group road links") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 1
-    groupedLinks.head should have size 2
-    groupedLinks.head.map(_.mmlId).toSet should be(roadLinks.map(_.mmlId).toSet)
-  }
-
-  test("separate road link group with functional class") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(functionalClass = 1))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
-  }
-
-  test("separate road link group with traffic direction") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(trafficDirection = TrafficDirection.AgainstDigitizing))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
-  }
-
-  test("separate road link group with link type") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(linkType = Freeway))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
-  }
-
-  test("separate road link group with road number") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(attributes = Map("ROADNUMBER" -> BigInt(5))))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
-  }
-
-  test("separate road link group with road name") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(attributes = Map("ROADNAME_FI" -> "Opastinsilta")))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
-  }
-
-  test("separate road link group with undefined road identifier") {
-    val roadLinks = Seq(
-      roadLink(0l, Seq(Point(0.0, 0.0), Point(10.0, 0.0))),
-      roadLink(1l, Seq(Point(10.2, 0.0), Point(20.0, 0.0))).copy(attributes = Map()))
-
-    val groupedLinks = RoadLinkPartitioner.partition(roadLinks)
-    groupedLinks should have size 2
-    groupedLinks.map(_.length) should be(Seq(1, 1))
   }
 }
