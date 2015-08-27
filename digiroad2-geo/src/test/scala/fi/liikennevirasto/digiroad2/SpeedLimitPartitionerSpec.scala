e31785162956c3bd64ec144936e339a950fcfed8
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-08-27 09:56:56 +0300

    Remove todo

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/SpeedLimitPartitionerSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/SpeedLimitPartitionerSpec.scala
@@ -87,7 +87,6 @@ class SpeedLimitPartitionerSpec extends FunSuite with Matchers {
     groupedLinks should have size 2
   }
 
-  // TODO: Separate cluster on different link type
   // TODO: Separate cluster on different road name or road number
   test("group road links") {
     val roadLinks = Seq(
