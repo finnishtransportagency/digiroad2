3f23dc64fc5eb273e16e2e87b5a583bc3369bc2e
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-09-01 15:37:49 +0300

    Use correct one way icon on undefined links

--- UI/src/view/LinkPropertyLayerStyles.js
+++ UI/src/view/LinkPropertyLayerStyles.js
@@ -8,7 +8,8 @@
       new OpenLayersRule().where('functionalClass').is(5).use({ strokeColor: '#01b', externalGraphic: 'images/link-properties/arrow-drop-blue.svg' }),
       new OpenLayersRule().where('functionalClass').is(6).use({ strokeColor: '#01b', externalGraphic: 'images/link-properties/arrow-drop-blue.svg' }),
       new OpenLayersRule().where('functionalClass').is(7).use({ strokeColor: '#888', externalGraphic: 'images/link-properties/arrow-drop-grey.svg' }),
-      new OpenLayersRule().where('functionalClass').is(8).use({ strokeColor: '#888', externalGraphic: 'images/link-properties/arrow-drop-grey.svg' })
+      new OpenLayersRule().where('functionalClass').is(8).use({ strokeColor: '#888', externalGraphic: 'images/link-properties/arrow-drop-grey.svg' }),
+      new OpenLayersRule().where('functionalClass').is(99).use({ strokeColor: '#000', externalGraphic: 'images/link-properties/arrow-drop-black.svg' })
     ];
 
     var zoomLevelRules = [
@@ -125,13 +126,13 @@
     // --- Link type style maps
 
     var linkTypeRules = [
-      
       new OpenLayersRule().where('linkType').isIn([2, 3]).use({ strokeColor: '#01b',  externalGraphic: 'images/link-properties/arrow-drop-blue.svg'  }),
       new OpenLayersRule().where('linkType').isIn([1, 4]).use({ strokeColor: '#f00',  externalGraphic: 'images/link-properties/arrow-drop-red.svg'   }),
       new OpenLayersRule().where('linkType').isIn([5, 6]).use({ strokeColor: '#0cd',  externalGraphic: 'images/link-properties/arrow-drop-cyan.svg'  }),
       new OpenLayersRule().where('linkType').isIn([8, 9]).use({ strokeColor: '#888', externalGraphic: 'images/link-properties/arrow-drop-grey.svg'  }),
       new OpenLayersRule().where('linkType').isIn([7, 10, 11, 12]).use({ strokeColor: '#1b0', externalGraphic: 'images/link-properties/arrow-drop-green.svg' }),
-      new OpenLayersRule().where('linkType').isIn([13, 21]).use({ strokeColor: '#f5d', externalGraphic: 'images/link-properties/arrow-drop-pink.svg'  })
+      new OpenLayersRule().where('linkType').isIn([13, 21]).use({ strokeColor: '#f5d', externalGraphic: 'images/link-properties/arrow-drop-pink.svg'  }),
+      new OpenLayersRule().where('linkType').is(99).use({ strokeColor: '#000', externalGraphic: 'images/link-properties/arrow-drop-black.svg'  })
     ];
 
     var linkTypeDefaultStyle = new OpenLayers.Style(OpenLayers.Util.applyDefaults({
