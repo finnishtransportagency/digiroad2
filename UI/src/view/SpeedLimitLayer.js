2709d0e7afb31cbd72dd57242e7d82303078ba5a
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-09-02 10:49:00 +0300

    Introduce own module to linear asset box selection

--- UI/src/view/SpeedLimitLayer.js
+++ UI/src/view/SpeedLimitLayer.js
@@ -402,17 +402,7 @@ window.SpeedLimitLayer = function(params) {
     }
   };
 
-  var getModifierKey = function() {
-    if (navigator.platform.toLowerCase().indexOf('mac') === 0) {
-      return OpenLayers.Handler.MOD_META;
-    } else {
-      return OpenLayers.Handler.MOD_CTRL;
-    }
-  };
-
-  var boxControl = new OpenLayers.Control();
-  map.addControl(boxControl);
-  var boxHandler = new OpenLayers.Handler.Box(boxControl, { done: massUpdateSpeedLimits }, { keyMask: getModifierKey() });
+  var boxHandler = new BoxSelectControl(map, massUpdateSpeedLimits);
 
   var handleSpeedLimitUnSelected = function(selection) {
     _.each(_.filter(vectorLayer.features, function(feature) {
