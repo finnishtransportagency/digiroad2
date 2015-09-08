ccdf6d957700372f8c8dd61721e0258e42243fd2
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-09-08 14:12:40 +0300

    Fix test fixture sequences after adding lit roads test data

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -2,7 +2,7 @@ drop sequence lrm_position_primary_key_seq;
 create sequence lrm_position_primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 50000000
+  start with 60000000
   increment by 1
   cache 100
   cycle;
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 400000
+  start with 500000
   increment by 1
   cache 100
   cycle;
