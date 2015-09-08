579a8e68e2caab37863ede186477d93ade1475cf
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-09-08 10:59:21 +0300

    Switch between expanded and collapsed lit road box

--- UI/src/actionpanel/LitRoadBox.js
+++ UI/src/actionpanel/LitRoadBox.js
@@ -1,12 +1,38 @@
 (function(root) {
  root.LitRoadBox = function() {
-   var collapsedTemplate =
-     '<div class="panel">' +
-       '<header class="panel-header">Valaistu tie</header>' +
-     '</div>';
+   var collapsed =
+     $('<div class="panel">' +
+         '<header class="panel-header">Valaistu tie</header>' +
+       '</div>');
+
+   var expanded =
+     $('<div class="panel">' +
+         '<header class="panel-header expanded">Valaistu tie</header>' +
+       '</div>').hide();
+
+   var executeOrShowConfirmDialog = function(f) {
+     if (applicationModel.isDirty()) {
+       new Confirm();
+     } else {
+       f();
+     }
+   };
+
+   var bindDOMEventHandlers = function() {
+     collapsed.click(function() {
+       executeOrShowConfirmDialog(function() {
+         collapsed.hide();
+         expanded.show();
+       });
+     });
+   };
+
+   bindDOMEventHandlers();
 
    return {
-     element: $('<div class="panel-group"/>').append(collapsedTemplate)
+     element: $('<div class="panel-group"/>')
+       .append(collapsed)
+       .append(expanded)
    };
  };
 })(this);
