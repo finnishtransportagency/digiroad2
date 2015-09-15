ddc25e5154d1014d999187258e2d9b829de3506a
Simo Råman <simo.raman@reaktor.fi>
2015-09-15 10:02:06 +0300

    clear incomplete links list before re-populating

--- src/main/scala/fi/liikennevirasto/digiroad2/util/UpdateIncompleteLinkList.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/util/UpdateIncompleteLinkList.scala
@@ -3,13 +3,26 @@ package fi.liikennevirasto.digiroad2.util
 import fi.liikennevirasto.digiroad2.Digiroad2Context
 import fi.liikennevirasto.digiroad2.asset.oracle.OracleSpatialAssetDao
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
+import slick.driver.JdbcDriver.backend.Database.dynamicSession
+import slick.jdbc.StaticQuery.interpolation
 
 object UpdateIncompleteLinkList {
-  def runUpdate(): Unit = {
+  def main(args:Array[String]) : Unit = {
+    try {
+    UpdateIncompleteLinkList.runUpdate()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    Digiroad2Context.system.shutdown()
+  }
+
+  private def runUpdate(): Unit = {
+    println("*** Delete incomplete links")
+    clearIncompleteLinks()
+    println("*** Get municipalities")
     val municipalities: Seq[Int] = OracleDatabase.withDynSession {
       OracleSpatialAssetDao.getMunicipalities
     }
-
     municipalities.foreach { municipality =>
       println("*** Processing municipality: " + municipality)
       val roadLinks = Digiroad2Context.roadLinkService.getRoadLinksFromVVH(municipality)
@@ -17,12 +30,9 @@ object UpdateIncompleteLinkList {
     }
   }
 
-  def main(args:Array[String]) : Unit = {
-    try {
-      UpdateIncompleteLinkList.runUpdate()
-    } catch {
-      case e: Exception => e.printStackTrace()
+  private def clearIncompleteLinks(): Unit = {
+    OracleDatabase.withDynTransaction {
+      sqlu"""truncate table incomplete_link""".execute
     }
-    Digiroad2Context.system.shutdown()
   }
 }
