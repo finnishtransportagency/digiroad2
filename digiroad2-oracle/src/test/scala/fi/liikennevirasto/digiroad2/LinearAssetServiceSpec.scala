9c0366fba807770a54e725a652124cf578b676fe
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-09-18 16:14:18 +0300

    Run test against higher abstraction level

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/LinearAssetServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/LinearAssetServiceSpec.scala
@@ -6,17 +6,26 @@ import fi.liikennevirasto.digiroad2.linearasset.VVHRoadLinkWithProperties
 import fi.liikennevirasto.digiroad2.linearasset.oracle.OracleLinearAssetDao
 import fi.liikennevirasto.digiroad2.util.TestTransactions
 import org.mockito.Mockito._
+import org.mockito.Matchers._
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FunSuite, Matchers}
 
 class LinearAssetServiceSpec extends FunSuite with Matchers {
   val mockRoadLinkService = MockitoSugar.mock[RoadLinkService]
   when(mockRoadLinkService.fetchVVHRoadlink(388562360l)).thenReturn(Some(VVHRoadlink(388562360l, 235, Seq(Point(0, 0), Point(10, 0)), Municipality, TrafficDirection.UnknownDirection, FeatureClass.AllOthers)))
+  when(mockRoadLinkService.getRoadLinksFromVVH(any[BoundingRectangle], any[Set[Int]])).thenReturn(Seq(
+      VVHRoadLinkWithProperties(
+        1, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 10.0, Municipality,
+        1, TrafficDirection.BothDirections, Motorway, None, None)))
+
+  val mockLinearAssetDao = MockitoSugar.mock[OracleLinearAssetDao]
+  when(mockLinearAssetDao.fetchLinearAssetsByMmlIds(30, Seq(1), "mittarajoitus"))
+    .thenReturn(Seq(PersistedLinearAsset(1, 1, 1, Some(40000), 0.4, 9.6, None, None, None, None, false)))
 
   object PassThroughService extends LinearAssetOperations {
     override def withDynTransaction[T](f: => T): T = f
     override def roadLinkService: RoadLinkService = mockRoadLinkService
-    override def dao: OracleLinearAssetDao = null
+    override def dao: OracleLinearAssetDao = mockLinearAssetDao
   }
 
   def runWithRollback(test: => Unit): Unit = TestTransactions.runWithRollback(PassThroughService.dataSource)(test)
@@ -54,17 +63,11 @@ class LinearAssetServiceSpec extends FunSuite with Matchers {
   }
 
   test("adjust linear asset to cover whole link when the difference in asset length and link length is less than maximum allowed error") {
-    val topology = Seq(
-      VVHRoadLinkWithProperties(
-        1, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 10.0, Municipality,
-        1, TrafficDirection.BothDirections, Motorway, None, None))
-    val linearAssets = Map(1l -> Seq(
-      PersistedLinearAsset(1, 1, 1, Some(40000), 0.4, 9.6, None, None, None, None, false)))
-    val (filledTopology, changeSet) = NumericalLimitFiller.fillTopology(topology, linearAssets, 30)
-    filledTopology should have size 1
-    filledTopology.map(_.points) should be(Seq(Seq(Point(0.0, 0.0), Point(10.0, 0.0))))
-    filledTopology.map(_.mmlId) should be(Seq(1))
-    filledTopology.map(_.value) should be(Seq(Some(40000)))
-    changeSet.adjustedMValues should be (Seq(MValueAdjustment(1, 1, 0.0, 10.0)))
+    val linearAssets = PassThroughService.getByBoundingBox(30, BoundingRectangle(Point(0.0, 0.0), Point(1.0, 1.0)))
+    linearAssets should have size 1
+    linearAssets.map(_.points) should be(Seq(Seq(Point(0.0, 0.0), Point(10.0, 0.0))))
+    linearAssets.map(_.mmlId) should be(Seq(1))
+    linearAssets.map(_.value) should be(Seq(Some(40000)))
+//    changeSet.adjustedMValues should be (Seq(MValueAdjustment(1, 1, 0.0, 10.0)))
   }
 }
