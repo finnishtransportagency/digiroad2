8c1b873931066c2f10be5c6f8234ee1bf9a4c82c
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-09-22 13:40:58 +0300

    Define styles for piecewise linear asset selection

--- UI/src/view/PiecewiseLinearAssetStyle.js
+++ UI/src/view/PiecewiseLinearAssetStyle.js
@@ -1,52 +1,5 @@
 (function(root) {
   root.PiecewiseLinearAssetStyle = function(applicationModel) {
-    var singleElementEvents = function() {
-      return _.map(arguments, function(argument) { return singleElementEventCategory + ':' + argument; }).join(' ');
-    };
-
-    var multiElementEvent = function(eventName) {
-      return multiElementEventCategory + ':' + eventName;
-    };
-
-    var combineFilters = function(filters) {
-      return new OpenLayers.Filter.Logical({ type: OpenLayers.Filter.Logical.AND, filters: filters });
-    };
-
-    var zoomLevelFilter = function(zoomLevel) {
-      return new OpenLayers.Filter.Function({ evaluate: function() { return applicationModel.zoom.level === zoomLevel; } });
-    };
-
-    var oneWayFilter = function() {
-      return new OpenLayers.Filter.Comparison({ type: OpenLayers.Filter.Comparison.NOT_EQUAL_TO, property: 'sideCode', value: 1 });
-    };
-
-    var createZoomDependentOneWayRule = function(zoomLevel, style) {
-      return new OpenLayers.Rule({
-        filter: combineFilters([oneWayFilter(), zoomLevelFilter(zoomLevel)]),
-        symbolizer: style
-      });
-    };
-
-    var validityDirectionStyleRules = [
-      createZoomDependentOneWayRule(9, { strokeWidth: 2 }),
-      createZoomDependentOneWayRule(10, { strokeWidth: 4 }),
-      createZoomDependentOneWayRule(11, { strokeWidth: 4 }),
-      createZoomDependentOneWayRule(12, { strokeWidth: 8 }),
-      createZoomDependentOneWayRule(13, { strokeWidth: 8 }),
-      createZoomDependentOneWayRule(14, { strokeWidth: 8 }),
-      createZoomDependentOneWayRule(15, { strokeWidth: 8 })
-    ];
-
-    var styleLookup = {
-      false: {strokeColor: '#ff0000'},
-      true: {strokeColor: '#7f7f7c'}
-    };
-
-    var typeSpecificStyleLookup = {
-      line: { strokeOpacity: 0.7 },
-      cutter: { externalGraphic: 'images/cursor-crosshair.svg', pointRadius: 11.5 }
-    };
-
     var expirationRules = [
       new OpenLayersRule().where('expired').is(true).use({strokeColor: '#7f7f7c'}),
       new OpenLayersRule().where('expired').is(false).use({strokeColor: '#ff0000'})
@@ -79,14 +32,13 @@
     var selectionSelectStyle = new OpenLayers.Style(OpenLayers.Util.applyDefaults({
       strokeOpacity: 0.7
     }));
+    selectionDefaultStyle.addRules(expirationRules);
+    selectionDefaultStyle.addRules(zoomLevelRules);
+    selectionSelectStyle.addRules(featureTypeRules);
     var selectionStyle = new OpenLayers.StyleMap({
       default: selectionDefaultStyle,
       select: selectionSelectStyle
     });
-    selectionStyle.addUniqueValueRules('default', 'expired', styleLookup);
-    selectionStyle.addUniqueValueRules('default', 'level', RoadLayerSelectionStyle.linkSizeLookup, applicationModel.zoom);
-    selectionStyle.addUniqueValueRules('select', 'type', typeSpecificStyleLookup);
-    selectionDefaultStyle.addRules(validityDirectionStyleRules);
 
     var lineFeatures = function(linearAssets) {
       return _.flatten(_.map(linearAssets, function(linearAsset) {
