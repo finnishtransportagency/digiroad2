24e016eccc13bf30b45a1df71df86fd31f40eddc
Simo Råman <simo.raman@reaktor.fi>
2015-10-12 09:29:25 +0300

    move NewLinearAsset from SpeedLimitProvider

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/PersistedLinearAsset.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/PersistedLinearAsset.scala
@@ -5,3 +5,6 @@ import org.joda.time.DateTime
 case class PersistedLinearAsset(id: Long, mmlId: Long, sideCode: Int, value: Option[Int],
                          startMeasure: Double, endMeasure: Double, createdBy: Option[String], createdDateTime: Option[DateTime],
                          modifiedBy: Option[String], modifiedDateTime: Option[DateTime], expired: Boolean, typeId: Int)
+
+case class NewLinearAsset(mmlId: Long, startMeasure: Double, endMeasure: Double, value: Option[Int], sideCode: Int)
+
