9ce0225d969b5458bd532881a38aca35b647b979
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-10-15 14:27:10 +0300

    Add asset controls to navigation panel

--- UI/src/view/NavigationPanel.js
+++ UI/src/view/NavigationPanel.js
@@ -3,11 +3,47 @@
     initialize: initialize
   };
 
-  function initialize(container, instructionsPopup, locationSearch) {
+  function initialize(container, instructionsPopup, locationSearch, linearAssets, linkPropertiesModel, selectedSpeedLimit) {
     container.append('<div class="navigation-panel"></div>');
 
-    var element = $('.navigation-panel');
+    var navigationPanel = $('.navigation-panel');
 
-    element.append(new SearchBox(instructionsPopup, locationSearch).element);
+    navigationPanel.append(new SearchBox(instructionsPopup, locationSearch).element);
+
+    var roadLinkBox = new RoadLinkBox(linkPropertiesModel);
+    var massTransitBox = new ActionPanelBoxes.AssetBox();
+    var speedLimitBox = new ActionPanelBoxes.SpeedLimitBox(selectedSpeedLimit);
+    var manoeuvreBox = new ManoeuvreBox();
+
+    var assetElements = _.map(linearAssets, function(asset) {
+      var legendValues = [asset.editControlLabels.disabled, asset.editControlLabels.enabled];
+      var assetBox = new LinearAssetBox(asset.selectedLinearAsset, asset.layerName, asset.title, asset.className, legendValues);
+      return {
+        layerName: asset.layerName,
+        element: assetBox.element
+      };
+    }).concat([
+      {layerName: 'linkProperty', element: roadLinkBox.element},
+      {layerName: 'massTransitStop', element: massTransitBox.element},
+      {layerName: 'speedLimit', element: speedLimitBox.element},
+      {layerName: 'manoeuvre', element: manoeuvreBox.element},
+    ]);
+
+    _.forEach(assetElements, function(asset) {
+      navigationPanel.append(asset.element.hide());
+    });
+
+    var assetControls = _.chain(assetElements)
+      .map(function(asset) {
+        return [asset.layerName, asset.element];
+      })
+      .zipObject()
+      .value();
+
+    eventbus.on('layer:selected', function selectLayer(layer, previouslySelectedLayer) {
+      var previousControl = assetControls[previouslySelectedLayer];
+      if (previousControl) previousControl.hide();
+      assetControls[layer].show();
+    });
   }
 })(this);
