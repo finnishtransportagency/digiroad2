47611a385a08eb8be65b73bcbb99bcce01e661d4
Simo Råman <simo.raman@reaktor.fi>
2015-10-19 11:41:50 +0300

    Set flag to ensure usage of /dev/urandom. There is a change JDBC-driver uses /dev/random instead of /dev/urandom. This might cause problems with connection to Oracle.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 jarfile="target/scala-2.11/digiroad2-assembly-0.1.0-SNAPSHOT.jar"
-javaopts="-javaagent:newrelic.jar -Xms2048m -Xmx4096m -Dfile.encoding=UTF8 -jar $jarfile"
+javaopts="-javaagent:newrelic.jar -Xms2048m -Xmx4096m -Dfile.encoding=UTF8 -Djava.security.egd=file:///dev/urandom -jar $jarfile"
 logfile="digiroad2.boot.log"
 
 nohup java $javaopts > $logfile &
