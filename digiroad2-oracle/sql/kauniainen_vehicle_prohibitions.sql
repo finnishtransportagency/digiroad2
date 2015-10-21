1ad139ea0a757e4a7e050a9208ffae3c37d40178
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-10-21 16:18:16 +0300

    Add a prohibition with a validity period to dev fixture.

--- digiroad2-oracle/sql/kauniainen_vehicle_prohibitions.sql
+++ digiroad2-oracle/sql/kauniainen_vehicle_prohibitions.sql
@@ -3,3 +3,9 @@ insert into LRM_POSITION (ID,MML_ID,START_MEASURE,END_MEASURE,SIDE_CODE) values 
 insert into asset_link (ASSET_ID, POSITION_ID) values (600018, 70000000);
 insert into prohibition_value(ID, ASSET_ID, TYPE) values (600019, 600018, 1);
 
+insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY) values (600020,190,'dr2_test_data');
+insert into LRM_POSITION (ID,MML_ID,START_MEASURE,END_MEASURE,SIDE_CODE) values (70000001, 1621077551, 0, 100, 1);
+insert into asset_link (ASSET_ID, POSITION_ID) values (600020, 70000001);
+insert into prohibition_value(ID, ASSET_ID, TYPE) values (600021, 600020, 1);
+insert into prohibition_validity_period(id, prohibition_value_id, type, START_HOUR, END_HOUR) values (600022, 600021, 1, 12, 16);
+
--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -2,7 +2,7 @@ drop sequence lrm_position_primary_key_seq;
 create sequence lrm_position_primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 70000001
+  start with 70000002
   increment by 1
   cache 100
   cycle;
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 600020
+  start with 600023
   increment by 1
   cache 100
   cycle;
