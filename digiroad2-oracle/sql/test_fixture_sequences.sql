01d6722c5bb99a3cb26461122e5c0f537cdead0e
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-10-21 16:43:10 +0300

    Add an exception to a dev fixture prohibition.

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 600023
+  start with 600024
   increment by 1
   cache 100
   cycle;
