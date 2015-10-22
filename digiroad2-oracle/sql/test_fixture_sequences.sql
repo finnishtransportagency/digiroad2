367779e3a68ada47c9e78eceeba08ab9c55e33bc
Mikko Hautala <mikko.hautala@reaktor.fi>
2015-10-22 12:00:07 +0300

    Add prohibition with validity period and exceptions to development fixture

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -2,7 +2,7 @@ drop sequence lrm_position_primary_key_seq;
 create sequence lrm_position_primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 70000002
+  start with 70000003
   increment by 1
   cache 100
   cycle;
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 600024
+  start with 600029
   increment by 1
   cache 100
   cycle;
