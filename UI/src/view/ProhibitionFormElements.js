5591eec471ffbd857df0c4f77470473b9607b45b
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-10-30 14:35:38 +0200

    Remove redundant wrapper function.

--- UI/src/view/ProhibitionFormElements.js
+++ UI/src/view/ProhibitionFormElements.js
@@ -1,238 +1,234 @@
 (function(root) {
   root.ProhibitionFormElements = function(unit, editControlLabels, className, defaultValue, elementType, possibleValues) {
-    return prohibitionFormElements();
+    var prohibitionValues = {
+      3: 'Ajoneuvo',
+      2: 'Moottoriajoneuvo',
+      23: 'Läpiajo',
+      12: 'Jalankulku',
+      11: 'Polkupyörä',
+      26: 'Ratsastus',
+      10: 'Mopo',
+      9: 'Moottoripyörä',
+      27: 'Moottorikelkka',
+      5: 'Linja-auto',
+      8: 'Taksi',
+      7: 'Henkilöauto',
+      6: 'Pakettiauto',
+      4: 'Kuorma-auto',
+      15: 'Matkailuajoneuvo',
+      19: 'Sotilasajoneuvo',
+      13: 'Ajoneuvoyhdistelmä',
+      14: 'Traktori tai maatalousajoneuvo',
+      24: 'Ryhmän A vaarallisten aineiden kuljetus',
+      25: 'Ryhmän B vaarallisten aineiden kuljetus'
+    };
+    var exceptionValues = {
+      21: 'Huoltoajo',
+      22: 'Tontille ajo',
+      10: 'Mopo',
+      9: 'Moottoripyörä',
+      27: 'Moottorikelkka',
+      5: 'Linja-auto',
+      8: 'Taksi',
+      7: 'Henkilöauto',
+      6: 'Pakettiauto',
+      4: 'Kuorma-auto',
+      15: 'Matkailuajoneuvo',
+      19: 'Sotilasajoneuvo',
+      13: 'Ajoneuvoyhdistelmä',
+      14: 'Traktori tai maatalousajoneuvo',
+      24: 'Ryhmän A vaarallisten aineiden kuljetus',
+      25: 'Ryhmän B vaarallisten aineiden kuljetus'
+    };
 
-    function prohibitionFormElements() {
-      var prohibitionValues = {
-        3: 'Ajoneuvo',
-        2: 'Moottoriajoneuvo',
-        23: 'Läpiajo',
-        12: 'Jalankulku',
-        11: 'Polkupyörä',
-        26: 'Ratsastus',
-        10: 'Mopo',
-        9: 'Moottoripyörä',
-        27: 'Moottorikelkka',
-        5: 'Linja-auto',
-        8: 'Taksi',
-        7: 'Henkilöauto',
-        6: 'Pakettiauto',
-        4: 'Kuorma-auto',
-        15: 'Matkailuajoneuvo',
-        19: 'Sotilasajoneuvo',
-        13: 'Ajoneuvoyhdistelmä',
-        14: 'Traktori tai maatalousajoneuvo',
-        24: 'Ryhmän A vaarallisten aineiden kuljetus',
-        25: 'Ryhmän B vaarallisten aineiden kuljetus'
-      };
-      var exceptionValues = {
-        21: 'Huoltoajo',
-        22: 'Tontille ajo',
-        10: 'Mopo',
-        9: 'Moottoripyörä',
-        27: 'Moottorikelkka',
-        5: 'Linja-auto',
-        8: 'Taksi',
-        7: 'Henkilöauto',
-        6: 'Pakettiauto',
-        4: 'Kuorma-auto',
-        15: 'Matkailuajoneuvo',
-        19: 'Sotilasajoneuvo',
-        13: 'Ajoneuvoyhdistelmä',
-        14: 'Traktori tai maatalousajoneuvo',
-        24: 'Ryhmän A vaarallisten aineiden kuljetus',
-        25: 'Ryhmän B vaarallisten aineiden kuljetus'
-      };
+    return {
+      singleValueElement: singleValueElement,
+      bindEvents: bindEvents
+    };
 
-      return {
-        singleValueElement: singleValueElement,
-        bindEvents: bindEvents
-      };
+    function singleValueElement(currentValue) {
+      return '' +
+        '<div class="form-group editable ' + className + '">' +
+          valueElement(currentValue) +
+          editElement() +
+        '</div>';
+    }
 
-      function singleValueElement(currentValue) {
+    function valueElement(currentValue) {
+      var items = _.map(currentValue, function(x) {
         return '' +
-          '<div class="form-group editable ' + className + '">' +
-            valueElement(currentValue) +
-            editElement() +
+          '<li>' +
+            prohibitionDisplayElement(x) +
+            prohibitionEditElement(x) +
+          '</li>';
+      });
+      return currentValue ? '<ul>' + items.join('') + '</ul>' : '-';
+    }
+
+    function prohibitionDisplayElement(prohibition) {
+      var typeElement = '<span>' + prohibitionValues[prohibition.typeId] + '</span>';
+
+      function exceptionElement() {
+        var exceptionElements = _.map(prohibition.exceptions, function (exceptionId) {
+          return '<li>' + exceptionValues[exceptionId] + '</li>';
+        }).join('');
+        var element = '' +
+          '<div>Rajoitus ei koske seuraavia ajoneuvoja: ' +
+          '  <ul>' + exceptionElements + '</ul>' +
           '</div>';
+        return _.isEmpty(prohibition.exceptions) ?  '' : element;
       }
 
-      function valueElement(currentValue) {
-        var items = _.map(currentValue, function(x) {
-          return '' +
-            '<li>' +
-              prohibitionDisplayElement(x) +
-              prohibitionEditElement(x) +
-            '</li>';
-        });
-        return currentValue ? '<ul>' + items.join('') + '</ul>' : '-';
-      }
-
-      function prohibitionDisplayElement(prohibition) {
-        var typeElement = '<span>' + prohibitionValues[prohibition.typeId] + '</span>';
-
-        function exceptionElement() {
-          var exceptionElements = _.map(prohibition.exceptions, function (exceptionId) {
-            return '<li>' + exceptionValues[exceptionId] + '</li>';
-          }).join('');
-          var element = '' +
-            '<div>Rajoitus ei koske seuraavia ajoneuvoja: ' +
-            '  <ul>' + exceptionElements + '</ul>' +
-            '</div>';
-          return _.isEmpty(prohibition.exceptions) ?  '' : element;
-        }
-
-        function validityPeriodElement() {
-          var validityPeriodItems = _.map(prohibition.validityPeriods, function (period) {
-            var dayName;
-            if (period.days === "Saturday") {
-              dayName = "La ";
-            } else if (period.days === "Sunday") {
-              dayName = "Su ";
-            } else {
-              dayName = "Ma - Pe ";
-            }
-            return '<li>' + dayName + period.startHour + ' - ' + period.endHour + '</li>';
-          }).join('');
-          return '<ul>' + validityPeriodItems + '</ul>';
-        }
-
-        return '' +
-          '<div class="form-control-static">' +
-          typeElement +
-          validityPeriodElement() +
-          exceptionElement() +
-          '</div>';
-      }
-
-      function deleteButton() {
-        return '<button class="delete btn-delete">x</button>';
-      }
-
-      function prohibitionEditElement(prohibition) {
-        function typeElement() {
-          var optionTags = _.map(prohibitionValues, function(name, key) {
-            var selected = prohibition.typeId.toString() === key ? 'selected' : '';
-            return '<option value="' + key + '"' + ' ' + selected + '>' + name + '</option>';
-          });
-          return '' +
-            '<select class="form-control select existing-prohibition">' +
-            optionTags +
-            '</select>';
-        }
-
-        function exceptionsElement() {
-          var elementLabel = '<label>Rajoitus ei koske seuraavia ajoneuvoja:</label>';
-
-          function existingExceptionElements() {
-            var items = _(prohibition.exceptions).map(function(exception) {
-              return '' +
-                '<div class="form-group existing-exception">' +
-                deleteButton() +
-                '  <select class="form-control select">' +
-                exceptionOptions(exception) +
-                '  </select>' +
-                '</div>';
-            });
-            return items.join('');
+      function validityPeriodElement() {
+        var validityPeriodItems = _.map(prohibition.validityPeriods, function (period) {
+          var dayName;
+          if (period.days === "Saturday") {
+            dayName = "La ";
+          } else if (period.days === "Sunday") {
+            dayName = "Su ";
+          } else {
+            dayName = "Ma - Pe ";
           }
-
-          return '' +
-            '<div class="exception-group">' +
-            elementLabel +
-            existingExceptionElements() +
-            newExceptionElement() +
-            '</div>';
-        }
-
-        return '' +
-          '<div class="form-group edit-control-group">' +
-          deleteButton() +
-          typeElement() +
-          exceptionsElement() +
-          '</div>';
+          return '<li>' + dayName + period.startHour + ' - ' + period.endHour + '</li>';
+        }).join('');
+        return '<ul>' + validityPeriodItems + '</ul>';
       }
 
-      function exceptionOptions(exception) {
-        var elements = _.map(exceptionValues, function(name, key) {
-          var selected = exception && (exception.toString() === key) ? 'selected' : '';
-          return '' +
-            '<option value="' + key + '" ' + selected + '>' +
-            name +
-            '</option>';
-        });
-        return elements.join('');
-      }
+      return '' +
+        '<div class="form-control-static">' +
+        typeElement +
+        validityPeriodElement() +
+        exceptionElement() +
+        '</div>';
+    }
 
-      function newExceptionElement() {
-        return '' +
-          '<div class="form-group new-exception">' +
-          '  <select class="form-control select">' +
-          '    <option class="empty" disabled selected>Lisää poikkeus</option>' +
-          exceptionOptions() +
-          '  </select>' +
-          '</div>';
-      }
+    function deleteButton() {
+      return '<button class="delete btn-delete">x</button>';
+    }
 
-      function editElement() {
+    function prohibitionEditElement(prohibition) {
+      function typeElement() {
         var optionTags = _.map(prohibitionValues, function(name, key) {
-          return '<option value="' + key + '">' + name + '</option>';
+          var selected = prohibition.typeId.toString() === key ? 'selected' : '';
+          return '<option value="' + key + '"' + ' ' + selected + '>' + name + '</option>';
         });
         return '' +
-          '<div class="form-group edit-control-group">' +
-          '  <select class="form-control select new-prohibition">' +
-          '    <option class="empty" disabled selected>Lisää kielto</option>' +
+          '<select class="form-control select existing-prohibition">' +
           optionTags +
-          '  </select>' +
+          '</select>';
+      }
+
+      function exceptionsElement() {
+        var elementLabel = '<label>Rajoitus ei koske seuraavia ajoneuvoja:</label>';
+
+        function existingExceptionElements() {
+          var items = _(prohibition.exceptions).map(function(exception) {
+            return '' +
+              '<div class="form-group existing-exception">' +
+              deleteButton() +
+              '  <select class="form-control select">' +
+              exceptionOptions(exception) +
+              '  </select>' +
+              '</div>';
+          });
+          return items.join('');
+        }
+
+        return '' +
+          '<div class="exception-group">' +
+          elementLabel +
+          existingExceptionElements() +
+          newExceptionElement() +
           '</div>';
       }
 
-      function bindEvents(rootElement, selectedLinearAsset) {
-        $(rootElement).on('change', '.existing-exception select', function() {
-          selectedLinearAsset.setValue(extractValue(rootElement));
-        });
+      return '' +
+        '<div class="form-group edit-control-group">' +
+        deleteButton() +
+        typeElement() +
+        exceptionsElement() +
+        '</div>';
+    }
 
-        $(rootElement).on('change', '.new-exception select', function(evt) {
-          $(evt.target).parent().removeClass('new-exception').addClass('existing-exception');
-          $(evt.target).before(deleteButton());
-          $(evt.target).closest('.exception-group').append(newExceptionElement());
-          selectedLinearAsset.setValue(extractValue(rootElement));
-        });
+    function exceptionOptions(exception) {
+      var elements = _.map(exceptionValues, function(name, key) {
+        var selected = exception && (exception.toString() === key) ? 'selected' : '';
+        return '' +
+          '<option value="' + key + '" ' + selected + '>' +
+          name +
+          '</option>';
+      });
+      return elements.join('');
+    }
 
-        $(rootElement).on('change', 'select.existing-prohibition', function() {
-          selectedLinearAsset.setValue(extractValue(rootElement));
-        });
+    function newExceptionElement() {
+      return '' +
+        '<div class="form-group new-exception">' +
+        '  <select class="form-control select">' +
+        '    <option class="empty" disabled selected>Lisää poikkeus</option>' +
+        exceptionOptions() +
+        '  </select>' +
+        '</div>';
+    }
 
-        $(rootElement).on('change', 'select.new-prohibition', function(evt) {
-          $(evt.target).removeClass('new-prohibition').addClass('existing-prohibition');
-          $(evt.target).before(deleteButton());
-          $(rootElement).find('.form-group.' + className).append(editElement());
-          selectedLinearAsset.setValue(extractValue(rootElement));
-        });
+    function editElement() {
+      var optionTags = _.map(prohibitionValues, function(name, key) {
+        return '<option value="' + key + '">' + name + '</option>';
+      });
+      return '' +
+        '<div class="form-group edit-control-group">' +
+        '  <select class="form-control select new-prohibition">' +
+        '    <option class="empty" disabled selected>Lisää kielto</option>' +
+        optionTags +
+        '  </select>' +
+        '</div>';
+    }
 
-        $(rootElement).on('click', 'button.delete', function(evt) {
-          $(evt.target).parent().remove();
-          selectedLinearAsset.setValue(extractValue(rootElement));
+    function bindEvents(rootElement, selectedLinearAsset) {
+      $(rootElement).on('change', '.existing-exception select', function() {
+        selectedLinearAsset.setValue(extractValue(rootElement));
+      });
+
+      $(rootElement).on('change', '.new-exception select', function(evt) {
+        $(evt.target).parent().removeClass('new-exception').addClass('existing-exception');
+        $(evt.target).before(deleteButton());
+        $(evt.target).closest('.exception-group').append(newExceptionElement());
+        selectedLinearAsset.setValue(extractValue(rootElement));
+      });
+
+      $(rootElement).on('change', 'select.existing-prohibition', function() {
+        selectedLinearAsset.setValue(extractValue(rootElement));
+      });
+
+      $(rootElement).on('change', 'select.new-prohibition', function(evt) {
+        $(evt.target).removeClass('new-prohibition').addClass('existing-prohibition');
+        $(evt.target).before(deleteButton());
+        $(rootElement).find('.form-group.' + className).append(editElement());
+        selectedLinearAsset.setValue(extractValue(rootElement));
+      });
+
+      $(rootElement).on('click', 'button.delete', function(evt) {
+        $(evt.target).parent().remove();
+        selectedLinearAsset.setValue(extractValue(rootElement));
+      });
+    }
+
+    function extractValue(rootElement) {
+      function extractExceptions(element) {
+        var exceptionElements = $(element).find('.existing-exception select');
+        return _.map(exceptionElements, function(exception) {
+          return parseInt($(exception).val(), 10);
         });
       }
 
-      function extractValue(rootElement) {
-        function extractExceptions(element) {
-          var exceptionElements = $(element).find('.existing-exception select');
-          return _.map(exceptionElements, function(exception) {
-            return parseInt($(exception).val(), 10);
-          });
-        }
-
-        var prohibitionElements = $(rootElement).find('.existing-prohibition');
-        return _.map(prohibitionElements, function(element) {
-          return {
-            typeId: parseInt($(element).val(), 10),
-            exceptions: extractExceptions($(element).parent()),
-            validityPeriods: []
-          };
-        });
-      }
+      var prohibitionElements = $(rootElement).find('.existing-prohibition');
+      return _.map(prohibitionElements, function(element) {
+        return {
+          typeId: parseInt($(element).val(), 10),
+          exceptions: extractExceptions($(element).parent()),
+          validityPeriods: []
+        };
+      });
     }
   };
 })(this);
