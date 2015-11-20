681cdc593329132064076cf45b414af2bc7ee427
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-11-20 15:50:22 +0200

    Move PointAssetOperations trait to the right file.

---
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetOperations.scala
@@ -0,0 +1,158 @@
+package fi.liikennevirasto.digiroad2
+
+import com.jolbox.bonecp.{BoneCPDataSource, BoneCPConfig}
+import fi.liikennevirasto.digiroad2.asset.{Unknown, BoundingRectangle}
+import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
+import fi.liikennevirasto.digiroad2.user.User
+import slick.driver.JdbcDriver.backend.Database.dynamicSession
+import slick.jdbc.StaticQuery
+import slick.jdbc.StaticQuery.interpolation
+
+
+trait PointAssetOperations[Asset <: FloatingAsset, PersistedAsset <: RoadLinkAssociatedPointAsset, AssetWithTimeStamps <: FloatingAsset] {
+  def roadLinkService: RoadLinkService
+  lazy val dataSource = {
+    val cfg = new BoneCPConfig(OracleDatabase.loadProperties("/bonecp.properties"))
+    new BoneCPDataSource(cfg)
+  }
+  def withDynTransaction[T](f: => T): T = OracleDatabase.withDynTransaction(f)
+  def withDynSession[T](f: => T): T = OracleDatabase.withDynSession(f)
+  def typeId: Int
+  def fetchPointAssets(queryFilter: String => String): Seq[PersistedAsset]
+  def persistedAssetToAsset(persistedAsset: PersistedAsset, floating: Boolean): Asset
+  def persistedAssetToAssetWithTimeStamps(persistedStop: PersistedAsset, floating: Boolean): AssetWithTimeStamps
+
+  def getByBoundingBox(user: User, bounds: BoundingRectangle): Seq[Asset] = {
+    case class AssetBeforeUpdate(asset: Asset, persistedFloating: Boolean)
+
+    val roadLinks = roadLinkService.fetchVVHRoadlinks(bounds)
+    withDynSession {
+      val boundingBoxFilter = OracleDatabase.boundingBoxFilter(bounds, "a.geometry")
+      val filter = s"where a.asset_type_id = $typeId and $boundingBoxFilter"
+      val persistedAssets: Seq[PersistedAsset] = fetchPointAssets(withFilter(filter))
+
+      val assetsBeforeUpdate: Seq[AssetBeforeUpdate] = persistedAssets.filter { persistedAsset =>
+        user.isAuthorizedToRead(persistedAsset.municipalityCode)
+      }.map { persistedAsset =>
+        val floating = PointAssetOperations.isFloating(persistedAsset, roadLinks.find(_.mmlId == persistedAsset.mmlId).map(link => (link.municipalityCode, link.geometry)))
+        AssetBeforeUpdate(persistedAssetToAsset(persistedAsset, floating), persistedAsset.floating)
+      }
+
+      assetsBeforeUpdate.foreach { asset =>
+        if (asset.asset.floating != asset.persistedFloating) {
+          updateFloating(asset.asset.id, asset.asset.floating)
+        }
+      }
+
+      assetsBeforeUpdate.map(_.asset)
+    }
+  }
+
+  protected def getFloatingAssets(idField: String, includedMunicipalities: Option[Set[Int]]): Map[String, Map[String, Seq[Long]]] = {
+    case class FloatingAsset(id: Long, municipality: String, administrativeClass: String)
+
+    withDynSession {
+      val optionalMunicipalities = includedMunicipalities.map(_.mkString(","))
+      val allFloatingAssetsQuery = s"""
+        select a.$idField, m.name_fi, lrm.mml_id
+        from asset a
+        join municipality m on a.municipality_code = m.id
+        join asset_link al on a.id = al.asset_id
+        join lrm_position lrm on al.position_id = lrm.id
+        where asset_type_id = $typeId and floating = '1' and (valid_to is null or valid_to > sysdate)"""
+
+      val sql = optionalMunicipalities match {
+        case Some(municipalities) => allFloatingAssetsQuery + s" and municipality_code in ($municipalities)"
+        case _ => allFloatingAssetsQuery
+      }
+
+      val result = StaticQuery.queryNA[(Long, String, Long)](sql).list
+      val administrativeClasses = roadLinkService.fetchVVHRoadlinks(result.map(_._3).toSet).groupBy(_.mmlId).mapValues(_.head.administrativeClass)
+
+      result
+        .map { case (id, municipality, administrativeClass) =>
+          FloatingAsset(id, municipality, administrativeClasses.getOrElse(administrativeClass, Unknown).toString)
+        }
+        .groupBy(_.municipality)
+        .mapValues { municipalityAssets =>
+          municipalityAssets
+            .groupBy(_.administrativeClass)
+            .mapValues(_.map(_.id))
+        }
+    }
+  }
+
+  def getByMunicipality(municipalityCode: Int): Seq[AssetWithTimeStamps] = {
+    val roadLinks = roadLinkService.fetchVVHRoadlinks(municipalityCode)
+    def findRoadlink(mmlId: Long): Option[(Int, Seq[Point])] =
+      roadLinks.find(_.mmlId == mmlId).map(x => (x.municipalityCode, x.geometry))
+
+    withDynSession {
+      fetchPointAssets(withMunicipality(municipalityCode))
+        .map(withFloatingUpdate(convertPersistedAsset(persistedAssetToAssetWithTimeStamps, findRoadlink)))
+        .toList
+    }
+  }
+
+  def getById(id: Long): Option[AssetWithTimeStamps] = {
+    val persistedAsset = getPersistedAssetsByIds(Set(id)).headOption
+    val roadLinks: Option[VVHRoadlink] = persistedAsset.flatMap { x => roadLinkService.fetchVVHRoadlink(x.mmlId) }
+
+    def findRoadlink(mmlId: Long): Option[(Int, Seq[Point])] =
+      roadLinks.find(_.mmlId == mmlId).map(x => (x.municipalityCode, x.geometry))
+
+    persistedAsset.map(withFloatingUpdate(convertPersistedAsset(persistedAssetToAssetWithTimeStamps, findRoadlink)))
+  }
+
+  def getPersistedAssetsByIds(ids: Set[Long]): Seq[PersistedAsset] = {
+    withDynSession {
+      val idsStr = ids.toSeq.mkString(",")
+      val filter = s"where a.asset_type_id = $typeId and a.id in ($idsStr)"
+      fetchPointAssets(withFilter(filter))
+    }
+  }
+
+  protected def convertPersistedAsset[T](conversion: (PersistedAsset, Boolean) => T,
+                                         roadLinkByMmlId: Long => Option[(Int, Seq[Point])])
+                                        (persistedStop: PersistedAsset): T = {
+    val floating = PointAssetOperations.isFloating(persistedStop, roadLinkByMmlId(persistedStop.mmlId))
+    conversion(persistedStop, floating)
+  }
+
+  protected def withFilter(filter: String)(query: String): String = {
+    query + " " + filter
+  }
+
+  protected def withMunicipality(municipalityCode: Int)(query: String): String = {
+    withFilter(s"where a.asset_type_id = $typeId and a.municipality_code = $municipalityCode")(query)
+  }
+
+  protected def withFloatingUpdate[T <: FloatingAsset](toPointAsset: PersistedAsset => T)
+                                                      (persistedAsset: PersistedAsset): T = {
+    val pointAsset = toPointAsset(persistedAsset)
+    if (persistedAsset.floating != pointAsset.floating) updateFloating(pointAsset.id, pointAsset.floating)
+    pointAsset
+  }
+
+  protected def updateFloating(id: Long, floating: Boolean) = sqlu"""update asset set floating = $floating where id = $id""".execute
+}
+
+object PointAssetOperations {
+  def isFloating(persistedAsset: RoadLinkAssociatedPointAsset, roadLink: Option[(Int, Seq[Point])]): Boolean = {
+    val point = Point(persistedAsset.lon, persistedAsset.lat)
+    roadLink match {
+      case None => true
+      case Some((municipalityCode, geometry)) => municipalityCode != persistedAsset.municipalityCode ||
+        !coordinatesWithinThreshold(Some(point), GeometryUtils.calculatePointFromLinearReference(geometry, persistedAsset.mValue))
+    }
+  }
+
+  private val FLOAT_THRESHOLD_IN_METERS = 3
+
+  def coordinatesWithinThreshold(pt1: Option[Point], pt2: Option[Point]): Boolean = {
+    (pt1, pt2) match {
+      case (Some(point1), Some(point2)) => point1.distanceTo(point2) <= FLOAT_THRESHOLD_IN_METERS
+      case _ => false
+    }
+  }
+}
