a8ae16b007da4ddd96f3ed20076879b30552bd97
tvenhola <tuomas.venhola@cgi.com>
2015-12-02 14:37:08 +0200

    I accidentally the jarfile

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ if [[ -e ~/.java.opts ]]; then
 else
   javaopts="-Xms2048m -Xmx4096m"
 fi
+jarfile="target/scala-2.11/digiroad2-assembly-0.1.0-SNAPSHOT.jar"
 javaopts="-javaagent:newrelic.jar $javaopts -Dfile.encoding=UTF8 -Djava.security.egd=file:///dev/urandom -jar $jarfile"
 logfile="digiroad2.boot.log"
 
