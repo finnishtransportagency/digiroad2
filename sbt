36ebd73c488f9c04d2ce8436c2c3a339e6af5176
tvenhola <tuomas.venhola@cgi.com>
2015-12-02 14:24:22 +0200

    Environment specific java memory options enabled in sbt and start.sh scripts

--- sbt
+++ sbt
@@ -1,3 +1,8 @@
 #!/bin/sh
-SBT_OPTS="-Xms1512M -Xmx2536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024M"
+if [[ -e ~/.sbt.opts ]]; then
+  source ~/.sbt.opts
+else
+  SBT_OPTS="-Xms1512M -Xmx2536M -Xss1M -XX:MaxPermSize=1024M"
+fi
+SBT_OPTS="$SBT_OPTS -XX:+CMSClassUnloadingEnabled"
 java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-jarfile="target/scala-2.11/digiroad2-assembly-0.1.0-SNAPSHOT.jar"
-javaopts="-javaagent:newrelic.jar -Xms2048m -Xmx4096m -Dfile.encoding=UTF8 -Djava.security.egd=file:///dev/urandom -jar $jarfile"
+if [[ -e ~/.java.opts ]]; then
+  source ~/.java.opts
+else
+  javaopts="-Xms2048m -Xmx4096m"
+fi
+javaopts="-javaagent:newrelic.jar $javaopts -Dfile.encoding=UTF8 -Djava.security.egd=file:///dev/urandom -jar $jarfile"
 logfile="digiroad2.boot.log"
 
 nohup java $javaopts > $logfile &
