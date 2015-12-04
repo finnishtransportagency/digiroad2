5bfb787985bf971eca30e6257c4f33546ac8651f
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-12-04 16:08:16 +0200

    Use the same exceptions as in prohibitions.

--- UI/src/view/ManoeuvreForm.js
+++ UI/src/view/ManoeuvreForm.js
@@ -72,20 +72,20 @@
     var deleteButtonTemplate = '<button class="btn-delete delete">x</button>';
 
     var exceptions = [
-      {typeId: 7,  title: 'Henkilöauto'},
-      {typeId: 4,  title: 'Kuorma-auto'},
-      {typeId: 5,  title: 'Linja-auto'},
-      {typeId: 6,  title: 'Pakettiauto'},
-      {typeId: 8,  title: 'Taksi'},
-      {typeId: 13, title: 'Ajoneuvoyhdistelmä'},
-      {typeId: 14, title: 'Traktori tai maatalousajoneuvo'},
-      {typeId: 15, title: 'Matkailuajoneuvo'},
-      {typeId: 16, title: 'Jakeluauto'},
-      {typeId: 18, title: 'Kimppakyytiajoneuvo'},
-      {typeId: 19, title: 'Sotilasajoneuvo'},
-      {typeId: 20, title: 'Vaarallista lastia kuljettava ajoneuvo'},
-      {typeId: 21, title: 'Huoltoajo'},
-      {typeId: 22, title: 'Tontille ajo'}
+      { typeId: 21, title: 'Huoltoajo' },
+      { typeId: 22, title: 'Tontille ajo' },
+      { typeId: 10, title: 'Mopo' },
+      { typeId: 9, title: 'Moottoripyörä' },
+      { typeId: 27, title: 'Moottorikelkka' },
+      { typeId: 5, title: 'Linja-auto' },
+      { typeId: 8, title: 'Taksi' },
+      { typeId: 7, title: 'Henkilöauto' },
+      { typeId: 6, title: 'Pakettiauto' },
+      { typeId: 4, title: 'Kuorma-auto' },
+      { typeId: 15, title: 'Matkailuajoneuvo' },
+      { typeId: 19, title: 'Sotilasajoneuvo' },
+      { typeId: 13, title: 'Ajoneuvoyhdistelmä' },
+      { typeId: 14, title: 'Traktori tai maatalousajoneuvo' }
     ];
     var localizeException = function(typeId) {
       return _.find(exceptions, {typeId: typeId});
