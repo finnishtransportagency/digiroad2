c8e6e64c8159d9f3d03c162561e75d1e5166ec93
Simo Råman <simo.raman@reaktor.fi>
2015-12-09 13:45:34 +0200

    Fix sequence values for test data.

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -2,7 +2,7 @@ drop sequence lrm_position_primary_key_seq;
 create sequence lrm_position_primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 70000009
+  start with 70000011
   increment by 1
   cache 100
   cycle;
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 600046
+  start with 600048
   increment by 1
   cache 100
   cycle;
