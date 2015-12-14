bd7cffacc020a0554fabff90e129fbe46709ab2c
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-12-14 17:54:42 +0200

    Rename NewRailwayCrossing to IncomingRailwayCrossing.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RailwayCrossingService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/RailwayCrossingService.scala
@@ -3,10 +3,10 @@ package fi.liikennevirasto.digiroad2
 import fi.liikennevirasto.digiroad2.pointasset.oracle._
 import org.joda.time.DateTime
 
-case class NewRailwayCrossing(lon: Double, lat: Double, mmlId: Long, safetyEquipment: Int, name: String) extends IncomingPointAsset
+case class IncomingRailwayCrossing(lon: Double, lat: Double, mmlId: Long, safetyEquipment: Int, name: String) extends IncomingPointAsset
 
 class RailwayCrossingService(val vvhClient: VVHClient) extends PointAssetOperations {
-  type IncomingAsset = NewRailwayCrossing
+  type IncomingAsset = IncomingRailwayCrossing
   type PersistedAsset = RailwayCrossing
 
   override def typeId: Int = 230
@@ -17,14 +17,14 @@ class RailwayCrossingService(val vvhClient: VVHClient) extends PointAssetOperati
     persistedAsset.copy(floating = floating)
   }
 
-  override def create(asset: NewRailwayCrossing, username: String, geometry: Seq[Point], municipality: Int): Long = {
+  override def create(asset: IncomingRailwayCrossing, username: String, geometry: Seq[Point], municipality: Int): Long = {
     val mValue = GeometryUtils.calculateLinearReferenceFromPoint(Point(asset.lon, asset.lat, 0), geometry)
     withDynTransaction {
       OracleRailwayCrossingDao.create(RailwayCrossingToBePersisted(asset.mmlId, asset.lon, asset.lat, mValue, municipality, username, asset.safetyEquipment, asset.name), username)
     }
   }
 
-  override def update(id: Long, updatedAsset: NewRailwayCrossing, geometry: Seq[Point], municipality: Int, username: String): Long = {
+  override def update(id: Long, updatedAsset: IncomingRailwayCrossing, geometry: Seq[Point], municipality: Int, username: String): Long = {
     val mValue = GeometryUtils.calculateLinearReferenceFromPoint(Point(updatedAsset.lon, updatedAsset.lat, 0), geometry)
     withDynTransaction {
       OracleRailwayCrossingDao.update(id, RailwayCrossingToBePersisted(updatedAsset.mmlId, updatedAsset.lon, updatedAsset.lat, mValue, municipality, username, updatedAsset.safetyEquipment, updatedAsset.name))
