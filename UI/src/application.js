3b31ab224c26dc303600e1883c8e3d0560ef5fb8
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-12-14 11:08:11 +0200

    Fix "Uusi suojatie" in obstacles form

--- UI/src/application.js
+++ UI/src/application.js
@@ -444,7 +444,8 @@ var URLRouter = function(map, backend, models) {
         {symbolUrl: 'images/point-assets/point_red.svg', label: 'Geometrian ulkopuolella'}],
       formLabels: {
         singleFloatingAssetLabel: 'suojatien',
-        manyFloatingAssetsLabel: 'suojatiet'
+        manyFloatingAssetsLabel: 'suojatiet',
+        newAssetLabel: 'suojatie'
       }
     },
     {
@@ -458,7 +459,8 @@ var URLRouter = function(map, backend, models) {
         {symbolUrl: 'images/point-assets/point_red.svg', label: 'Geometrian ulkopuolella'}],
       formLabels: {
         singleFloatingAssetLabel: 'esterakennelman',
-        manyFloatingAssetsLabel: 'esterakennelmat'
+        manyFloatingAssetsLabel: 'esterakennelmat',
+        newAssetLabel: 'esterakennelma'
       }
     }
   ];
