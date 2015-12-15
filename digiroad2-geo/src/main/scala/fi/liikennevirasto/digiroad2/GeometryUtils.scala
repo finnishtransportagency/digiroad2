f945f8471908584d4dc5ed61369578bff4eb885c
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-12-15 16:40:49 +0200

    Revert "Try a fix to floating railway crossings."
    This reverts commit f051b2b1ce45914e51d3593cee7fe41dc6512f31.

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/GeometryUtils.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/GeometryUtils.scala
@@ -78,8 +78,9 @@ object GeometryUtils {
         if (acc.result.isDefined) {
           acc
         } else {
-          val distance = point.distanceTo(acc.previousPoint)
-          if (acc.remainingMeasure <= distance) {
+          val distance = Math.round(point.distanceTo(acc.previousPoint))
+          val remainingMeasure = Math.round(acc.remainingMeasure)
+          if (remainingMeasure <= distance) {
             val directionVector = (point - acc.previousPoint).normalize()
             val result = Some(acc.previousPoint + directionVector.scale(acc.remainingMeasure))
             AlgorithmState(point, acc.remainingMeasure - distance, result)
@@ -88,13 +89,7 @@ object GeometryUtils {
           }
         }
       }
-      state match {
-        case AlgorithmState(point, remainingMeasure, None) =>
-          val previousPoint = geometry(-2)
-          val directionVector = (point - previousPoint).normalize()
-          Some(previousPoint + directionVector.scale(remainingMeasure))
-        case _ => state.result
-      }
+      state.result
     }
   }
 
--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetOperations.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/PointAssetOperations.scala
@@ -176,21 +176,16 @@ object PointAssetOperations {
     val point = Point(persistedAsset.lon, persistedAsset.lat)
     roadLink match {
       case None => true
-      case Some((municipalityCode, geometry)) =>
-        val calculatedPoint = GeometryUtils.calculatePointFromLinearReference(geometry, persistedAsset.mValue)
-
-        municipalityCode != persistedAsset.municipalityCode ||
-          !(coordinatesWithinThreshold(Some(point), calculatedPoint, DISTANCE_BETWEEN_OLD_AND_CALCULATED_POINT_THRESHOLD_IN_METERS) &&
-            coordinatesWithinThreshold(Some(geometry.last), calculatedPoint, DISTANCE_TO_GEOMETRY))
+      case Some((municipalityCode, geometry)) => municipalityCode != persistedAsset.municipalityCode ||
+        !coordinatesWithinThreshold(Some(point), GeometryUtils.calculatePointFromLinearReference(geometry, persistedAsset.mValue))
     }
   }
 
-  private val DISTANCE_BETWEEN_OLD_AND_CALCULATED_POINT_THRESHOLD_IN_METERS = 3
-  private val DISTANCE_TO_GEOMETRY = 1
+  private val FLOAT_THRESHOLD_IN_METERS = 3
 
-  def coordinatesWithinThreshold(pt1: Option[Point], pt2: Option[Point], threshold: Int): Boolean = {
+  def coordinatesWithinThreshold(pt1: Option[Point], pt2: Option[Point]): Boolean = {
     (pt1, pt2) match {
-      case (Some(point1), Some(point2)) => point1.distanceTo(point2) <= threshold
+      case (Some(point1), Some(point2)) => point1.distanceTo(point2) <= FLOAT_THRESHOLD_IN_METERS
       case _ => false
     }
   }
