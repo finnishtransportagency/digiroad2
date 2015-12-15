aab774cfb57ac14d8186633071d048ec4732e7e8
tvenhola <tuomas.venhola@cgi.com>
2015-12-15 16:14:26 +0200

    Update setup script for new environment

--- setup-oracle.sh
+++ setup-oracle.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 echo "Starting oracle configuration";
 DR2_DIR=`pwd`;
 cd /tmp;
 # FIXME: This is a bit slow
 rm -rf digiroad2-oracle;
-git clone git@github.com:finnishtransportagency/digiroad2-oracle.git;
+git clone https://github.com/finnishtransportagency/digiroad2-oracle.git;
 cd $DR2_DIR;
 cp -r /tmp/digiroad2-oracle/lib digiroad2-oracle/;
 cp -r /tmp/digiroad2-oracle/conf digiroad2-oracle/;
