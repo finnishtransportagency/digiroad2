fa724365b3693a863781a16ffe7792d40834e2d7
Hanna L <halied123@hotmail.com>
2015-12-28 17:10:09 +0200

    Add new file for service point tests with placeholders for tests

---
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/ServicePointServiceSpec.scala
@@ -0,0 +1,45 @@
+package fi.liikennevirasto.digiroad2
+
+import fi.liikennevirasto.digiroad2.asset.{BoundingRectangle, Municipality, TrafficDirection}
+import fi.liikennevirasto.digiroad2.user.{Configuration, User}
+import fi.liikennevirasto.digiroad2.util.TestTransactions
+import org.joda.time.DateTime
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FunSuite, Matchers}
+
+class ServicePointServiceSpec extends FunSuite with Matchers {
+  val testUser = User(
+    id = 1,
+    username = "Hannu",
+    configuration = Configuration(authorizedMunicipalities = Set(235)))
+  val mockVVHClient = MockitoSugar.mock[VVHClient]
+  when(mockVVHClient.fetchVVHRoadlinks(any[BoundingRectangle], any[Set[Int]])).thenReturn(Seq(
+    VVHRoadlink(388553074, 235, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), Municipality,
+      TrafficDirection.BothDirections, FeatureClass.AllOthers)))
+
+  val service = new DirectionalTrafficSignService(mockVVHClient) {
+    override def withDynTransaction[T](f: => T): T = f
+    override def withDynSession[T](f: => T): T = f
+  }
+
+  def runWithRollback(test: => Unit): Unit = TestTransactions.runWithRollback(service.dataSource)(test)
+
+  test("Can fetch by bounding box") {
+
+  }
+
+
+  test("Create new service point") {
+
+  }
+
+  test("Expire service point") {
+
+  }
+
+  test("Update service point") {
+
+  }
+}
