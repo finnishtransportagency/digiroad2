6db094e001915882da7e8517ac69e6268a178879
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-01-07 11:09:48 +0200

    Show parking place count option for truck parking place service type

--- UI/src/view/PointAssetForm.js
+++ UI/src/view/PointAssetForm.js
@@ -320,9 +320,13 @@
       '    <label class="control-label">Palvelun lisätieto</label>' +
       '    <p class="form-control-static">' + (service.additionalInfo || '–') + '</p>' +
       '    <textarea class="form-control large-input" data-service-id="' + service.id + '">' + (service.additionalInfo || '')  + '</textarea>' +
-           ((selectedServiceType.value == 12 || selectedServiceType.value == 15) ? parkingPlaceElements : '') +
+           (showParkingPlaceCount(selectedServiceType) ? parkingPlaceElements : '') +
       '  </div>' +
       '</li>';
+  }
+
+  function showParkingPlaceCount(selectedServiceType) {
+    return (selectedServiceType.value == 12 || selectedServiceType.value == 15 || selectedServiceType.value == 14);
   }
 
   function renderNewServiceElement() {
