c70a699ba3b3aa6827e2d15bdef890eb23bab133
tvenhola <tuomas.venhola@cgi.com>
2016-01-13 16:44:17 +0200

    Gatling test configuration; add proxy parameter

--- performance-tests.sh
+++ performance-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
   then
     echo "Wrong number of arguments"
-    echo "usage: ./performance-tests.sh [hostname] [username] [user count]"
+    echo "usage: ./performance-tests.sh [hostname] [username] [user count] [http proxy hostname]"
     exit 1
 fi
-./sbt -Dhost=$1 -Dusername=$2 -Dusers=$3 'project gatling' test
+./sbt -Dhost=$1 -Dusername=$2 -Dusers=$3 -DproxyHost=$4 'project gatling' test
