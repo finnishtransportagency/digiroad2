da79faa1def8361cae15d40e564eabe982f319c1
tvenhola <tuomas.venhola@cgi.com>
2016-01-13 16:42:16 +0200

    Gatling test configuration; add proxy parameter

--- digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/TestConfiguration.scala
+++ digiroad2-gatling/src/test/scala/fi/liikennevirasto/digiroad2/performance/TestConfiguration.scala
@@ -7,7 +7,11 @@ object TestConfiguration {
   val users: Int = System.getProperty("users").toInt
   val username: String = System.getProperty("username")
   val baseUrl: String = "http://" + host + "/digiroad"
+  val proxyHost: String = System.getProperty("proxyHost")
   val httpConf = http
+    .proxy(Proxy(proxyHost, 80)
+      .httpsPort(80))
+    .noProxyFor(host)
     .baseURL(baseUrl)
     .header("Cookie", "testusername=tarutest")
     .header("OAM_REMOTE_USER", TestConfiguration.username)
