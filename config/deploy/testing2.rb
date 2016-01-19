b027575c354f36b16df71b2675b561b41641a425
tvenhola <tuomas.venhola@cgi.com>
2016-01-19 14:35:30 +0200

    Second nodes' deployment files

---
+++ config/deploy/testing2.rb
@@ -0,0 +1,54 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{web@testing2}
+role :web, %w{web@testing2}
+# role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server 'testing2', user: 'web', roles: %w{web app}, my_property: :my_value
+
+# FIXME: Disabled because in production speed limit generation is not tied to single instance
+#server 'production2', user: 'web', roles: %w{web app}, my_property: :my_value
+
+namespace :deploy do
+  task :start_vallu_server do
+    on roles(:all) do
+      execute "killall -q node; exit 0"
+      # Capistrano kills the vallu server before it gets to start up if sleep 1 is not defined
+      execute "cd #{release_path} && (nohup grunt vallu-test-server > ./src/main/webapp/vallu-server.log &) && sleep 1"
+    end
+  end
+
+  after :publishing, :start_vallu_server
+end
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
