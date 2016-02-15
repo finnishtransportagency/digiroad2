488e55e12cc3a73dfd6f8534cf27f2a8a7817ccb
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-02-15 11:01:04 +0200

    Add link id to obstacles fixture

--- digiroad2-oracle/sql/kauniainen_obstacles.sql
+++ digiroad2-oracle/sql/kauniainen_obstacles.sql
@@ -1,5 +1,5 @@
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600046,220,'dr2_test_data',235);
-insert into LRM_POSITION (ID,MML_ID,START_MEASURE,END_MEASURE,SIDE_CODE) values (70000009, 388553074, 103, 103, 1);
+INSERT INTO LRM_POSITION (ID, LINK_ID, START_MEASURE, END_MEASURE, MML_ID) VALUES (70000009, 1611317, 103.000, 103.000, 388553074);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600046, 70000009);
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600046, (select id from enumerated_value where name_fi='Suljettu yhteys'), (select id from property where public_id='esterakennelma'));
 UPDATE asset
@@ -13,7 +13,7 @@ UPDATE asset
 
 
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600047,220,'dr2_test_data',235);
-insert into LRM_POSITION (ID,START_MEASURE,END_MEASURE,MML_ID) values (70000010,69,69,388553080);
+INSERT INTO LRM_POSITION (ID, LINK_ID, START_MEASURE, END_MEASURE, MML_ID) VALUES (70000010, 1611341, 69.000, 69.000, 388553080);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600047, 70000010);
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600047, (select id from enumerated_value where name_fi='Avattava puomi'), (select id from property where public_id='esterakennelma'));
 UPDATE asset
@@ -27,7 +27,7 @@ UPDATE asset
 
 
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600048,220,'dr2_test_data',235);
-insert into LRM_POSITION (ID,START_MEASURE,END_MEASURE,MML_ID) values (70000011,100,100,12345);
+INSERT INTO LRM_POSITION (ID, LINK_ID, START_MEASURE, END_MEASURE, MML_ID) VALUES (70000011, null, 100.000, 100.000, 12345);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600048, 70000011);
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600048, (select id from enumerated_value where name_fi='Avattava puomi'), (select id from property where public_id='esterakennelma'));
 UPDATE asset
