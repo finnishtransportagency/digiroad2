2cd067c3f658fe5bf9e2e15c3e105678c65b8418
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-02-23 16:52:40 +0200

    Add link type and functional class with old id for changed link

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -11,7 +11,7 @@ drop sequence primary_key_seq;
 create sequence primary_key_seq
   minvalue 1
   maxvalue 999999999999999999999999999
-  start with 615151
+  start with 615731
   increment by 1
   cache 100
   cycle;
