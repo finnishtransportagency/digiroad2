1694400e7527d3b1a9faba8af2f2dd1aad2d153d
koivistojp <jani.p.koivisto@cgi.com>
2016-03-11 16:39:43 +0200

    WIP: find previous speed limit data for new links

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/SpeedLimitService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/SpeedLimitService.scala
@@ -45,6 +45,9 @@ class SpeedLimitService(eventbus: DigiroadEventBus, vvhClient: VVHClient, roadLi
       val speedLimits = speedLimitLinks.groupBy(_.linkId)
       val roadLinksByLinkId = topology.groupBy(_.linkId).mapValues(_.head)
 
+      // wip: get data for projection
+      fillNewRoadLinksWithPreviousSpeedLimitData(roadLinks, change)
+
       val (filledTopology, changeSet) = SpeedLimitFiller.fillTopology(topology, speedLimits)
       eventbus.publish("linearAssets:update", changeSet)
 
@@ -54,6 +57,16 @@ class SpeedLimitService(eventbus: DigiroadEventBus, vvhClient: VVHClient, roadLi
       eventbus.publish("speedLimits:persistUnknownLimits", unknownLimits)
 
       LinearAssetPartitioner.partition(filledTopology, roadLinksByLinkId)
+    }
+  }
+
+  def fillNewRoadLinksWithPreviousSpeedLimitData(roadLinks: Seq[RoadLink], changes: Seq[ChangeInfo]) ={
+    // TODO: find previous speedlimit data for old ids
+    // find oldIds
+    val oldRoadLinkIds = changes.flatMap(_.oldId)
+    println(oldRoadLinkIds)
+    roadLinks.map{ roadLink =>
+
     }
   }
 
