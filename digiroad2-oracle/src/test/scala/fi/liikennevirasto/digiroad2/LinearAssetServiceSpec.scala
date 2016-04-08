67b1691f9370248992ef168e914899351cf88408
Hanna L <hanna.liedenpohja@hotmail.com>
2016-04-08 16:47:05 +0300

    Unit test for mapping linear assets to combined road link (change types 1&2)

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/LinearAssetServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/LinearAssetServiceSpec.scala
@@ -6,6 +6,8 @@ import fi.liikennevirasto.digiroad2.linearasset.oracle.OracleLinearAssetDao
 import fi.liikennevirasto.digiroad2.linearasset._
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
 import fi.liikennevirasto.digiroad2.util.TestTransactions
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
@@ -417,6 +419,90 @@ class LinearAssetServiceSpec extends FunSuite with Matchers {
       val linearAssets3 = afterByLinkId(newLinkId3)
       linearAssets3.length should be (1)
       linearAssets3.filter(_.startMeasure == 0.0).head.value should be (None)
+
+      dynamicSession.rollback()
+    }
+  }
+
+  test("Should map linear assets (lit road) of three old links to one new link") {
+
+    // Combined road link (change types 1 and 2)
+
+    val mockRoadLinkService = MockitoSugar.mock[RoadLinkService]
+    val service = new LinearAssetService(mockRoadLinkService, new DummyEventBus) {
+      override def withDynTransaction[T](f: => T): T = f
+    }
+
+    val oldLinkId1 = 5001
+    val oldLinkId2 = 5002
+    val oldLinkId3 = 5003
+    val newLinkId = 6000
+    val municipalityCode = 235
+    val administrativeClass = Municipality
+    val trafficDirection = TrafficDirection.BothDirections
+    val functionalClass = 1
+    val linkType = Freeway
+    val boundingBox = BoundingRectangle(Point(123, 345), Point(567, 678))
+    val assetTypeId = 100 // lit roads
+
+    val oldRoadLinks = Seq(RoadLink(oldLinkId1, List(Point(0.0, 0.0), Point(10.0, 0.0)), 10.0, administrativeClass, functionalClass, trafficDirection, linkType, None, None, Map("MUNICIPALITYCODE" -> BigInt(municipalityCode))),
+      RoadLink(oldLinkId2, List(Point(0.0, 0.0), Point(10.0, 0.0)), 10.0, administrativeClass, functionalClass, trafficDirection, linkType, None, None, Map("MUNICIPALITYCODE" -> BigInt(municipalityCode))),
+      RoadLink(oldLinkId3, List(Point(0.0, 0.0), Point(5.0, 0.0)), 5.0, administrativeClass, functionalClass, trafficDirection, linkType, None, None, Map("MUNICIPALITYCODE" -> BigInt(municipalityCode))))
+
+    val newRoadLink = RoadLink(newLinkId, List(Point(0.0, 0.0), Point(25.0, 0.0)), 25.0, administrativeClass, functionalClass, trafficDirection, linkType, None, None, Map("MUNICIPALITYCODE" -> BigInt(municipalityCode)))
+
+    val changeInfo = Seq(ChangeInfo(Some(oldLinkId1), Some(newLinkId), 12345, 1, Some(0), Some(10), Some(0), Some(10), Some(144000000)),
+      ChangeInfo(Some(oldLinkId2), Some(newLinkId), 12345, 2, Some(0), Some(10), Some(10), Some(20), Some(144000000)),
+      ChangeInfo(Some(oldLinkId3), Some(newLinkId), 12345, 2, Some(0), Some(5), Some(20), Some(25), Some(144000000)))
+
+    OracleDatabase.withDynTransaction {
+      sqlu"""insert into lrm_position (id, link_id, start_measure, end_measure, side_code) VALUES (1, $oldLinkId1, 0.0, 10.0, ${SideCode.BothDirections.value})""".execute
+      sqlu"""insert into asset (id, asset_type_id, modified_date, modified_by) values (1,$assetTypeId, TO_TIMESTAMP('2014-02-17 10:03:51.047483', 'YYYY-MM-DD HH24:MI:SS.FF6'),'KX1')""".execute
+      sqlu"""insert into asset_link (asset_id, position_id) values (1,1)""".execute
+      sqlu"""insert into number_property_value (id, asset_id, property_id, value) values (1,1,(select id from property where public_id = 'mittarajoitus'),1)""".execute
+      sqlu"""insert into lrm_position (id, link_id, start_measure, end_measure, side_code) VALUES (2, $oldLinkId2, 0, 10.0, ${SideCode.BothDirections.value})""".execute
+      sqlu"""insert into asset (id, asset_type_id, modified_date, modified_by) values (2,$assetTypeId, TO_TIMESTAMP('2016-02-17 10:03:51.047483', 'YYYY-MM-DD HH24:MI:SS.FF6'),'KX2')""".execute
+      sqlu"""insert into asset_link (asset_id, position_id) values (2,2)""".execute
+      sqlu"""insert into number_property_value (id, asset_id, property_id, value) values (2,2,(select id from property where public_id = 'mittarajoitus'),1)""".execute
+      sqlu"""insert into lrm_position (id, link_id, start_measure, end_measure, side_code) VALUES (3, $oldLinkId3, 0, 5.0, ${SideCode.BothDirections.value})""".execute
+      sqlu"""insert into asset (id, asset_type_id, modified_date, modified_by) values (3,$assetTypeId, TO_TIMESTAMP('2015-02-17 10:03:51.047483', 'YYYY-MM-DD HH24:MI:SS.FF6'),'KX3')""".execute
+      sqlu"""insert into asset_link (asset_id, position_id) values (3,3)""".execute
+      sqlu"""insert into number_property_value (id, asset_id, property_id, value) values (3,3,(select id from property where public_id = 'mittarajoitus'),1)""".execute
+
+      when(mockRoadLinkService.getRoadLinksAndChangesFromVVH(any[BoundingRectangle], any[Set[Int]])).thenReturn((oldRoadLinks, Nil))
+      val before = service.getByBoundingBox(100, boundingBox).toList.flatten
+
+      before.length should be (3)
+      before.foreach(_.value should be (Some(NumericValue(1))))
+      before.foreach(_.sideCode should be (SideCode.BothDirections))
+
+      val beforeByLinkId = before.groupBy(_.linkId)
+      val linearAssets1 = beforeByLinkId(oldLinkId1)
+      linearAssets1.length should be (1)
+      linearAssets1.head.startMeasure should be (0)
+      linearAssets1.head.endMeasure should be (10)
+      val linearAssets2 = beforeByLinkId(oldLinkId2)
+      linearAssets2.length should be (1)
+      linearAssets2.head.startMeasure should be (0)
+      linearAssets2.head.endMeasure should be (10)
+      val linearAssets3 = beforeByLinkId(oldLinkId3)
+      linearAssets3.head.startMeasure should be (0)
+      linearAssets3.head.endMeasure should be (5)
+
+
+      when(mockRoadLinkService.getRoadLinksAndChangesFromVVH(any[BoundingRectangle], any[Set[Int]])).thenReturn((List(newRoadLink), changeInfo))
+      val after = service.getByBoundingBox(100, boundingBox).toList.flatten
+
+      after.length should be(1)
+      after.head.value should be(Some(NumericValue(1)))
+      after.head.sideCode should be (SideCode.BothDirections)
+      after.head.startMeasure should be (0)
+      after.head.endMeasure should be (25)
+      after.head.modifiedBy should be(Some("KX2"))
+
+      val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+      val latestModifiedDate = DateTime.parse("2016-02-17 10:03:51.047483", formatter)
+      after.head.modifiedDateTime should be(Some(latestModifiedDate))
 
       dynamicSession.rollback()
     }
