e4bea3e91827e416bca84c8e1f94d06e3a80c2b9
Hanna L <hanna.liedenpohja@hotmail.com>
2016-04-18 15:45:26 +0300

    Add fixture data for hazmat prohibition and axle weight limit

--- digiroad2-oracle/sql/siilinjarvi_linear_assets.sql
+++ digiroad2-oracle/sql/siilinjarvi_linear_assets.sql
@@ -103,5 +103,52 @@ SELECT * from dual;
 --   INTO NUMBER_PROPERTY_VALUE (ID, ASSET_ID,"VALUE",PROPERTY_ID) values (siilinjarvi_lrm_key_seq.NEXTVAL, siilinjarvi_key_seq.CURRVAL,1,(select id from property where public_id = 'mittarajoitus'))
 -- SELECT * from dual;
 
+INSERT ALL
+-- Hazmat prohibitions; Cases 5&6 (divided): OLD_ID: 5169516 --> NEW_ID: 6565223, NEW_ID: 6565226
+  INTO ASSET (ID,ASSET_TYPE_ID,FLOATING,CREATED_BY) values (siilinjarvi_key_seq.NEXTVAL,210,0,'testfixture')
+  INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL, 5169516, null, 0.000, 91.316, 2)
+  INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (siilinjarvi_key_seq.CURRVAL,siilinjarvi_lrm_key_seq.CURRVAL)
+  INTO PROHIBITION_VALUE (ID, ASSET_ID, TYPE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL,siilinjarvi_key_seq.CURRVAL,24)
+  INTO PROHIBITION_VALIDITY_PERIOD (ID, PROHIBITION_VALUE_ID, TYPE, START_HOUR, END_HOUR) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL,siilinjarvi_lrm_key_seq.CURRVAL,1,8,12)
+
+SELECT * from dual;
+INSERT ALL
+
+  INTO ASSET (ID,ASSET_TYPE_ID,FLOATING,CREATED_BY) values (siilinjarvi_key_seq.NEXTVAL,210,0,'testfixture')
+  INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL, 5169516, null, 0.000, 91.316, 3)
+  INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (siilinjarvi_key_seq.CURRVAL,siilinjarvi_lrm_key_seq.CURRVAL)
+  INTO PROHIBITION_VALUE (ID, ASSET_ID, TYPE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL,siilinjarvi_key_seq.CURRVAL,24)
+  INTO PROHIBITION_VALIDITY_PERIOD (ID, PROHIBITION_VALUE_ID, TYPE, START_HOUR, END_HOUR) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL,siilinjarvi_lrm_key_seq.CURRVAL,1,8,12)
+
+SELECT * from dual;
+
+INSERT ALL
+
+-- Axle weight limit; Cases 1&2 (3 old links combined):  OLD_ID: 2225999, OLD_ID: 2226035, OLD_ID: 2226036  --> NEW_ID: 6564314
+
+  INTO ASSET (ID,ASSET_TYPE_ID,FLOATING,CREATED_BY) values (siilinjarvi_key_seq.NEXTVAL,50,0,'testfixture')
+  INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL, 2225999, null, 0.000, 22.612, 1)
+  INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (siilinjarvi_key_seq.CURRVAL,siilinjarvi_lrm_key_seq.CURRVAL)
+  INTO NUMBER_PROPERTY_VALUE (ID, ASSET_ID,"VALUE",PROPERTY_ID) values (siilinjarvi_lrm_key_seq.NEXTVAL, siilinjarvi_key_seq.CURRVAL,500,(select id from property where public_id = 'mittarajoitus'))
+
+SELECT * from dual;
+INSERT ALL
+
+  INTO ASSET (ID,ASSET_TYPE_ID,FLOATING,CREATED_BY) values (siilinjarvi_key_seq.NEXTVAL,50,0,'testfixture')
+  INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL, 2226036, null, 0.000, 106.634, 2)
+  INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (siilinjarvi_key_seq.CURRVAL,siilinjarvi_lrm_key_seq.CURRVAL)
+  INTO NUMBER_PROPERTY_VALUE (ID, ASSET_ID,"VALUE",PROPERTY_ID) values (siilinjarvi_lrm_key_seq.NEXTVAL, siilinjarvi_key_seq.CURRVAL,500,(select id from property where public_id = 'mittarajoitus'))
+
+SELECT * from dual;
+
+INSERT ALL
+
+  INTO ASSET (ID,ASSET_TYPE_ID,FLOATING,CREATED_BY) values (siilinjarvi_key_seq.NEXTVAL,50,0,'testfixture')
+  INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (siilinjarvi_lrm_key_seq.NEXTVAL, 2226036, null, 0.000, 106.634, 3)
+  INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (siilinjarvi_key_seq.CURRVAL,siilinjarvi_lrm_key_seq.CURRVAL)
+  INTO NUMBER_PROPERTY_VALUE (ID, ASSET_ID,"VALUE",PROPERTY_ID) values (siilinjarvi_lrm_key_seq.NEXTVAL, siilinjarvi_key_seq.CURRVAL,500,(select id from property where public_id = 'mittarajoitus'))
+
+SELECT * from dual;
+
 drop sequence siilinjarvi_key_seq;
 drop sequence siilinjarvi_lrm_key_seq;
