92c81ceabf5049d75efd8b963f724fd0c767c271
leirasf <faustino.leiras@cgi.com>
2016-07-04 14:20:23 +0300

    Add 'non_final_has_destination' on manoeuvre element constraint after fixed data.

--- digiroad2-oracle/src/main/resources/db/migration/V85__intermediate_links_for_manoeuvres.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V85__intermediate_links_for_manoeuvres.sql
@@ -2,5 +2,9 @@ alter table manoeuvre_element add dest_link_id number(38, 0);
 
 UPDATE MANOEUVRE_ELEMENT SET dest_link_id = (SELECT link_id FROM MANOEUVRE_ELEMENT me2 WHERE me2.manoeuvre_id = MANOEUVRE_ELEMENT.manoeuvre_id AND element_type=3) WHERE element_type=1;
 
-
+alter table
+   MANOEUVRE_ELEMENT
+add constraint
+   non_final_has_destination
+   CHECK (element_type = 3 OR dest_link_id IS NOT NULL);
 
