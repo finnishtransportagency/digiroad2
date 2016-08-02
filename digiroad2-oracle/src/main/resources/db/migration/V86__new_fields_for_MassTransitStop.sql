d7b7776e66932b64c6629725cb81f30e338ef233
tvenhola <tuomas.venhola@cgi.com>
2016-08-02 13:16:46 +0300

    spellcheck: language name to lower case

--- digiroad2-oracle/src/main/resources/db/migration/V86__new_fields_for_MassTransitStop.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V86__new_fields_for_MassTransitStop.sql
@@ -5,11 +5,11 @@ INSERT INTO LOCALIZED_STRING (ID, VALUE_FI, CREATED_DATE)
     VALUES (primary_key_seq.nextval, 'Osoite suomeksi', sysdate);
     
 INSERT INTO LOCALIZED_STRING (ID, VALUE_FI, CREATED_DATE)  
-    VALUES (primary_key_seq.nextval, 'Osoite Ruotsiksi', sysdate);  
+    VALUES (primary_key_seq.nextval, 'Osoite ruotsiksi', sysdate);
 
 
 INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID) 
     VALUES (primary_key_seq.nextval, 10, 'text', 0, 'db_migration_v86', 'osoite_suomeksi', (select id from LOCALIZED_STRING where VALUE_FI = 'Osoite suomeksi')); 
 
 INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID) 
-    VALUES (primary_key_seq.nextval, 10, 'text', 0, 'db_migration_v86', 'osoite_ruotsiksi', (select id from LOCALIZED_STRING where VALUE_FI = 'Osoite Ruotsiksi'));
+    VALUES (primary_key_seq.nextval, 10, 'text', 0, 'db_migration_v86', 'osoite_ruotsiksi', (select id from LOCALIZED_STRING where VALUE_FI = 'Osoite ruotsiksi'));
