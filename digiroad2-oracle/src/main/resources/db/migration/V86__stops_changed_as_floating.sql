feb94f51809784d131787d765a835f4d77caf5f0
leirasf <faustino.leiras@cgi.com>
2016-08-12 21:38:24 +0300

    DROTH-238  Attempt to use read_only_test reverted

--- digiroad2-oracle/src/main/resources/db/migration/V86__stops_changed_as_floating.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V86__stops_changed_as_floating.sql
@@ -1,6 +1,6 @@
 
 --Insert two new not visible fields at form MassTransitSop
 INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID) VALUES
-                                            (primary_key_seq.nextval, 10, 'read_only_text', 0, 'db_migration_v87', 'linkin_hallinnollinen_luokka');
+                                            (primary_key_seq.nextval, 10, 'text', 0, 'db_migration_v87', 'linkin_hallinnollinen_luokka');
 INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID) VALUES
-                                            (primary_key_seq.nextval, 10, 'read_only_text', 0, 'db_migration_v87', 'kellumisen_syy');
+                                            (primary_key_seq.nextval, 10, 'text', 0, 'db_migration_v87', 'kellumisen_syy');
