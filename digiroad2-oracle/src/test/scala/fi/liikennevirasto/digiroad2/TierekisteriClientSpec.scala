e9bd5d5fd43ba68364d4ebe4a8ecf0280467b22e
leirasf <faustino.leiras@cgi.com>
2016-08-22 16:41:24 +0300

    DROTH 248 add TierekisteriClient test class

---
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/TierekisteriClientSpec.scala
@@ -0,0 +1,11 @@
+package fi.liikennevirasto.digiroad2
+
+import org.scalatest.{Matchers, FunSuite}
+
+class TierekisteriClientSpec extends FunSuite with Matchers  {
+
+  test("fetch tierekisteri mass transit stop") {
+
+  }
+
+}
