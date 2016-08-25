7c049037840a1cf7778bc6e20865d0a88b1dfcb7
FaustinoLeiras <faustino.leiras@cgi.com>
2016-08-25 13:49:05 +0300

    Switch sql schema version

--- digiroad2-oracle/src/main/resources/db/migration/V86__stops_changed_as_floating.sql
+++
@@ -1,6 +0,0 @@
-
---Insert two new not visible fields at form MassTransitSop
-INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID) VALUES
-                                            (primary_key_seq.nextval, 10, 'read_only_number', 0, 'db_migration_v87', 'linkin_hallinnollinen_luokka');
-INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID) VALUES
-                                            (primary_key_seq.nextval, 10, 'read_only_number', 0, 'db_migration_v87', 'kellumisen_syy');
