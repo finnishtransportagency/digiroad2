79a59d06728ec9d82aeed2a8b95e38dfc551f3ed
marquesfm <francisco.marques@cgi.com>
2016-09-06 20:21:22 +0300

    DROTH-261 (144) On create & update the bus stop data is saved to Tierekisteri - Creation of the new error to return (TierekisteriInternalServerError); - Modified Update and Create actions for send the information to Tierekisteri if its relevant; - Unit Tests for the convert MassTransitStopWithProperties into TierekisteriMassTransitStop.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
@@ -356,6 +356,7 @@ trait MassTransitStopService extends PointAssetOperations {
         updateAssetGeometry(id, point)
       }
       getPersistedStopWithPropertiesAndPublishEvent(id, { _ => roadLink }, tierekisteriClient.updateMassTransitStop)
+      //getPersistedStopWithPropertiesAndPublishEvent(id, municipalityCode, geometry, tierekisteriClient.updateMassTransitStop)
     }
   }
 
@@ -398,6 +399,7 @@ trait MassTransitStopService extends PointAssetOperations {
         updateAdministrativeClassValue(assetId, administrativeClass.getOrElse(throw new IllegalArgumentException("AdministrativeClass argument is mandatory")))
         updateLiViIdentifierProperty(assetId, nationalId, asset.properties)
         getPersistedStopWithPropertiesAndPublishEvent(assetId, fetchRoadLink, tierekisteriClient.createMassTransitStop)
+        //getPersistedStopWithPropertiesAndPublishEvent(assetId, municipality, geometry, tierekisteriClient.createMassTransitStop)
         assetId
       }
       else
@@ -421,11 +423,13 @@ trait MassTransitStopService extends PointAssetOperations {
   }
 
   private def getPersistedStopWithPropertiesAndPublishEvent(assetId: Long, roadLinkByLinkId: Long => Option[VVHRoadlink], createOrUpdateMassTransitStop: TierekisteriMassTransitStop => Unit) = {
+  //private def getPersistedStopWithPropertiesAndPublishEvent(assetId: Long, municipalityCode: Int, geometry: Seq[Point], createOrUpdateMassTransitStop: TierekisteriMassTransitStop => Unit) = {
     val persistedStop = fetchPointAssets(withId(assetId)).headOption
     val relevantToTR = isNotVirtualStopAndIsMantainedByELY(Some(persistedStop.get))
 
     if (relevantToTR) {
       val (newMTSWithProperties, floatingReason) = persistedStopToMassTransitStopWithProperties(roadLinkByLinkId)(persistedStop.get)
+      //val newMTSWithProperties = persistedStopToMassTransitStopWithProperties({ _ => Some((municipalityCode, geometry)) })(persistedStop.get)
 
       val newTierekisteriMassTransitStop = TierekisteriBusStopMarshaller.toTierekisteriMassTransitStop(newMTSWithProperties)
 
