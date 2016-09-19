d4b758cf08038626bde26c8e40f5b9a36de2c30a
tvenhola <tuomas.venhola@cgi.com>
2016-09-19 16:17:57 +0300

    Use correct port for proxy

--- src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
@@ -104,7 +104,7 @@ class ArcGisProxyServlet extends ProxyServlet {
     val properties = new Properties()
     properties.load(getClass.getResourceAsStream("/digiroad2.properties"))
     if (properties.getProperty("http.proxySet", "false").toBoolean) {
-      val proxy = new HttpProxy(properties.getProperty("http.proxyHost", "localhost"), properties.getProperty("http.proxyPort", "443").toInt)
+      val proxy = new HttpProxy(properties.getProperty("http.proxyHost", "localhost"), properties.getProperty("http.proxyPort", "80").toInt)
       proxy.getExcludedAddresses.addAll(properties.getProperty("http.nonProxyHosts", "").split("|").toList)
       client.getProxyConfiguration.getProxies.add(proxy)
       client.setIdleTimeout(60000)
