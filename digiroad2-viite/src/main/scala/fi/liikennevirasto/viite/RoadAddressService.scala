ba5388a36efb53cc03a8b58481ac7ffdcc4ea558
tvenhola <tuomas.venhola@cgi.com>
2016-10-04 14:59:58 +0300

    Force calibration points to end of link when drawing

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadAddressService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadAddressService.scala
@@ -101,9 +101,18 @@ class RoadAddressService(roadLinkService: RoadLinkService) {
           rl.linkType, rl.modifiedAt, rl.modifiedBy,
           rl.attributes, ra.roadNumber, ra.roadPartNumber, ra.track.value, ra.ely, ra.discontinuity.value,
           ra.startAddrMValue, ra.endAddrMValue, formatter.print(ra.endDate), ra.startMValue, ra.endMValue, toSideCode(ra.startMValue, ra.endMValue, ra.track),
-          ra.calibrationPoints.find(_.mValue == 0.0), ra.calibrationPoints.find(_.mValue > 0.0))
+          startingCalibrationPoint(ra.calibrationPoints, length),
+          endingCalibrationPoint(ra.calibrationPoints, length))
       }).filter(_.length > 0.0)
     }
+  }
+
+  private def startingCalibrationPoint(calibrationPoints: Seq[CalibrationPoint], linkLength: Double): Option[CalibrationPoint] = {
+    calibrationPoints.filter(_.mValue < linkLength).sortBy(_.mValue).headOption.map(_.copy(mValue = 0.0))
+  }
+
+  private def endingCalibrationPoint(calibrationPoints: Seq[CalibrationPoint], linkLength: Double): Option[CalibrationPoint] = {
+    calibrationPoints.filter(_.mValue > linkLength).sortBy(0.0-_.mValue).headOption.map(_.copy(mValue = linkLength))
   }
 
   def getRoadParts(boundingRectangle: BoundingRectangle, roadNumberLimits: Seq[(Int, Int)], municipalities: Set[Int]) = {
