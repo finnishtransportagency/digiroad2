dfd6b44494f7f95e70dc3041d3d7230240a8c966
tvenhola <tuomas.venhola@cgi.com>
2016-10-14 13:56:48 +0300

    Use dynamic LRM ids in test (fails if LRM ids have been used)

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/masstransitstop/oracle/queries.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/masstransitstop/oracle/queries.scala
@@ -64,6 +64,10 @@ object Queries {
 
   def nextViitePrimaryKeyId = sql"select viite_general_seq.nextval from dual"
 
+  def fetchLrmPositionIds(len: Int) = {
+    sql"""SELECT lrm_position_primary_key_seq.nextval FROM dual connect by level <= $len""".as[Long].list
+  }
+
   def updateAssetModified(assetId: Long, updater: String) =
     sqlu"""
       update asset set modified_by = $updater, modified_date = CURRENT_TIMESTAMP where id = $assetId
