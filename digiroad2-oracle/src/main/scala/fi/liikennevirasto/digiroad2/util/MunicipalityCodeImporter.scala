45b4f172850c6afddb32b1b3943e4ccd3fce0a46
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2016-10-28 19:51:57 +0300

    DRVVH-357 Refactoring off the ELY-table and ely-numbering on to the municipality-table
    Added a try catch in order to check if the kunnat_ja_elyt_2014.csv file is not detected.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/MunicipalityCodeImporter.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/MunicipalityCodeImporter.scala
@@ -17,25 +17,31 @@ class MunicipalityCodeImporter {
 
   def importMunicipalityCodes() = {
     OracleDatabase.withDynTransaction {
-      val src = Source.fromInputStream(getClass.getResourceAsStream("/kunnat_ja_elyt_2014.csv"))
-      src.getLines().toList.drop(1).map(row => {
-        val elems = row.replace("\"", "").split(";")
-        var roadMaintainerID = elems(3) match {
-          case "1" => 14
-          case "2" => 12
-          case "3" => 10
-          case "4" => 9
-          case "5" => 8
-          case "6" => 4
-          case "7" => 2
-          case "8" => 3
-          case "9" => 1
-          case "0" => 0
-        }
-        sqlu"""
-          insert into municipality(id, name_fi, name_sv, ely_nro, road_maintainer_id) values( ${elems(0).toInt}, ${elems(1)}, ${elems(2)}, ${elems(3).toInt} ,roadMaintainerID)
+      try {
+        val src = Source.fromInputStream(getClass.getResourceAsStream("/kunnat_ja_elyt_2014.csv"))
+        src.getLines().toList.drop(1).map(row => {
+          val elems = row.replace("\"", "").split(";")
+          val roadMaintainerID = elems(3) match {
+            case "1" => 14
+            case "2" => 12
+            case "3" => 10
+            case "4" => 9
+            case "5" => 8
+            case "6" => 4
+            case "7" => 2
+            case "8" => 3
+            case "9" => 1
+            case "0" => 0
+          }
+          sqlu"""
+          insert into municipality(id, name_fi, name_sv, ely_nro, road_maintainer_id) values( ${elems(0).toInt}, ${elems(1)}, ${elems(2)}, ${elems(3).toInt} ,$roadMaintainerID)
         """.execute
-      })
+        })
+      } catch {
+        case  npe: NullPointerException => {
+          println(s"/kunnat_ja_elyt_2014.csv was not found, skipping.")
+        }
+      }
     }
   }
 
