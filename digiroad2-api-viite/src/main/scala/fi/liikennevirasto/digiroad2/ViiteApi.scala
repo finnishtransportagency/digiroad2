21b395c38ba7f6cc54a61799c82f556d1fff23cc
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2016-11-09 18:15:42 +0200

    DRVVH-377 (335) UI: Display the complementary geometry on top (Z-indexing) in visualization rules
    Changed the RoadLayer in order to accommodate for the newly added Z-Level based on the roadlink subtype.
    Changed the draw dashed and borders in order to accommodate for the Z-Level in the LinkPropertyLayer.
    Changed the LinkPropertyLayerStyles in order to find the zLevel of a roadLink and apply it.
    Changed ViiteApi in order to send the subType on the return object.

--- digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
+++ digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
@@ -82,7 +82,7 @@ class ViiteApi(val roadLinkService: RoadLinkService, val vVHClient: VVHClient,
   private def getRoadLinksFromVVH(municipalities: Set[Int], zoomLevel: Int)(bbox: String): Seq[Seq[Map[String, Any]]] = {
     val boundingRectangle = constructBoundingRectangle(bbox)
     val viiteRoadLinks = zoomLevel match {
-        //TODO: When well-performing solution for main parts and road parts is ready
+      //TODO: When well-performing solution for main parts and road parts is ready
       case DrawMainRoadPartsOnly =>
         //roadAddressService.getCoarseRoadParts(boundingRectangle, Seq((1, 99)), municipalities)
         Seq()
@@ -151,7 +151,8 @@ class ViiteApi(val roadLinkService: RoadLinkService, val vVHClient: VVHClient,
       "discontinuity" -> roadAddressLink.discontinuity,
       "endDate" -> roadAddressLink.endDate,
       "anomaly" -> roadAddressLink.anomaly.value,
-      "roadLinkType" -> roadAddressLink.roadLinkType)
+      "roadLinkType" -> roadAddressLink.roadLinkType,
+      "subType" -> roadAddressLink.attributes.getOrElse("SUBTYPE", null))
   }
 
   private def calibrationPoint(geometry: Seq[Point], calibrationPoint: Option[CalibrationPoint]) = {
