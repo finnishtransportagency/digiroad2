c5910ff1a082361aab3a6b639397d85362473a6b
marquesfm <francisco.marques@cgi.com>
2016-12-20 13:03:53 +0200

    DROTH-404 SlickException in Production - Revert corrections;

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/MassTransitStopService.scala
@@ -70,7 +70,7 @@ trait MassTransitStopService extends PointAssetOperations {
 
   val geometryTransform = new GeometryTransform
 
-  val massTransitStopEnumeratedPropertyValues = {
+  lazy val massTransitStopEnumeratedPropertyValues = {
     withDynSession{
       val properties = Queries.getEnumeratedPropertyValues(typeId)
       properties.map(epv => epv.publicId -> epv.values).toMap
