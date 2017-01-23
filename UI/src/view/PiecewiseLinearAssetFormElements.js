a2dc94ec3fde1ba268aebdf75b42b961e46c99dc
marquesfm <francisco.marques@cgi.com>
2017-01-23 17:21:10 +0200

    DROTH-433 (US 268) Visualizations of all Tasks together - Corrections in View Mode Form.

--- UI/src/view/PiecewiseLinearAssetFormElements.js
+++ UI/src/view/PiecewiseLinearAssetFormElements.js
@@ -102,25 +102,43 @@
     }
 
     function singleChoiceValuesConversion(current, propertiesName){
-        var value = current[1] ? current[1].value : "";
+        var value = current.propertyValue ? current.propertyValue.value : "";
         var accessRightsValues = { '1': 'Tieoikeus', '2': 'Tiekunnan osakkuus', '3': 'LiVin hallinnoimalla maa-alueella', '4': 'Huoltoreittikäytössä olevat kevyen liikenteen väylät (ei rautatieliikennealuetta) väylä', '5': 'Tuntematon'};
         var possibleValues = { '0': 'Ei tietoa', '1': 'LiVi', '2': 'Muu'};
-        if(current[0] == propertiesName[0]){
-            return accessRightsValues[current[1].value];
+        if(current.propertyName == propertiesName[0].name){
+            return accessRightsValues[current.propertyValue.value];
         }else{
-            if(current[0] == propertiesName[1]){
-                return possibleValues[current[1].value];
+            if(current.propertyName == propertiesName[1].name){
+                return possibleValues[current.propertyValue.value];
             }
             else return value;
         }
     }
 
     function obtainFormControl(className, valueString, currentValue){
-        var propertiesName = ['Käyttöoikeus','Huoltovastuu','Tiehoitokunta','Nimi','Osoite','Postinumero','Postitoimipaikka','Puhelin 1','Puhelin 2','Lisätietoa'];
-        var zipped = _.zip(propertiesName, currentValue);
-        return _.map(zipped, function(current){
+        var propertiesName = [
+            {'name': "Käyttöoikeus", 'id': "huoltotie_kayttooikeus"},
+            {'name': "Huoltovastuu", 'id': "huoltotie_huoltovastuu"},
+            {'name': "Tiehoitokunta", 'id': "huoltotie_tiehoitokunta"},
+            {'name': "Nimi", 'id': "huoltotie_nimi"},
+            {'name': "Osoite", 'id': "huoltotie_osoite"},
+            {'name': "Postinumero", 'id': "huoltotie_postinumero"},
+            {'name': "Postitoimipaikka", 'id': "huoltotie_postitoimipaikka"},
+            {'name': "Puhelin 1", 'id': "huoltotie_puh1"},
+            {'name': "Puhelin 2", 'id': "huoltotie_puh2"},
+            {'name': "Lisätietoa", 'id': "huoltotie_lisatieto"}
+        ];
+
+        var newCurrentValue = _.map(propertiesName, function (property) {
+            var arrayValue =  _.find(currentValue, function (value) {
+                return (property.id == value.publicId);
+            });
+            return {propertyName: property.name, propertyValue: arrayValue};
+        });
+
+        return _.map(newCurrentValue, function(current){
             var value = singleChoiceValuesConversion(current, propertiesName);
-            return '  <p class="form-control-static ' + className + '" style="display:none;">' + current[0] + ': ' + valueString(value).replace(/[\n\r]+/g, '<br>') + '</p>';
+            return '  <p class="form-control-static ' + className + '" style="display:none;">' + current.propertyName + ': ' + valueString(value).replace(/[\n\r]+/g, '<br>') + '</p>';
         }).join('');
     }
 
