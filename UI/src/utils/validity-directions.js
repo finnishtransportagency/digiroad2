278e9fdd9f8c76470a017e15962b30c708de2aac
joanacoelho <joana.f.coelho@cgi.com>
2017-02-16 16:50:56 +0200

    (US 446) Validity Direction with radius

--- UI/src/utils/validity-directions.js
+++ UI/src/utils/validity-directions.js
@@ -8,10 +8,13 @@
       var switchedDirections = { 1: 1, 2: 3, 3: 2 };
       return switchedDirections[validityDirection];
     },
+    //TODO : validate the use of this function in order to work with radius
     calculateRotation: function(bearing, validityDirection) {
       var bearingInPolarCoordinates = geometrycalculator.convertCompassToPolarCoordinates(bearing);
+      bearingInPolarCoordinates = bearingInPolarCoordinates * (Math.PI / 180);
       if (validityDirection === validitydirections.oppositeDirection) {
-        return geometrycalculator.oppositeAngle(bearingInPolarCoordinates);
+        //return geometrycalculator.oppositeAngle(bearingInPolarCoordinates);
+          return bearingInPolarCoordinates + Math.PI;
       } else {
         return bearingInPolarCoordinates;
       }
