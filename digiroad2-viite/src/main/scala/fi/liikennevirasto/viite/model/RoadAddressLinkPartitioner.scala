94473dee89ed571b5ae25bbdfea1630a6b70e390
rochajb <jorgebfrr@hotmail.com>
2017-02-20 14:15:13 +0200

    VIITE-245 (US 135) Fixed RoadAddressLinkPartitioner, remove roadlinktype to get the complementary roads

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/RoadAddressLinkPartitioner.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/RoadAddressLinkPartitioner.scala
@@ -6,7 +6,11 @@ object RoadAddressLinkPartitioner extends GraphPartitioner {
 
   def partition(links: Seq[RoadAddressLink]): Seq[Seq[RoadAddressLink]] = {
     val linkGroups = links.groupBy { link => (
-      link.anomaly.value, link.roadNumber, link.roadPartNumber, link.trackCode, link.roadLinkType.value
+      /*
+      Ignores the roadLinkType from the linkGroups, to the task 245 -> complementary roads
+      */
+      //link.anomaly.value, link.roadNumber, link.roadPartNumber, link.trackCode, link.roadLinkType.value
+      link.anomaly.value, link.roadNumber, link.roadPartNumber, link.trackCode
       )
     }
     val clusters = for (linkGroup <- linkGroups.values.toSeq;
