fa06e18d28e44bf811d29e170b522fdb24555029
rochajb <jorgebfrr@hotmail.com>
2017-03-03 19:25:57 +0200

    Removed comment code

--- viite-UI/src/model/SelectedLinkProperty.js
+++ viite-UI/src/model/SelectedLinkProperty.js
@@ -417,10 +417,10 @@
       return current.length;
     };
 
-    // eventbus.on('roadLink:editModeAdjacents', function(){
-    //   if(!applicationModel.isActiveButtons() && count() > 0) {
-    //   }
-    // });
+     eventbus.on('roadLink:editModeAdjacents', function(){
+       if(!applicationModel.isActiveButtons() && count() > 0) {
+       }
+     });
 
     var getFeaturesToKeep = function(){
       return featuresToKeep;
