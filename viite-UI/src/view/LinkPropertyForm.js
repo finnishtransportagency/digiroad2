1c5d1f3fb1b5cc5ff912bfb8ee61b0e005c056be
rochajb <jorgebfrr@hotmail.com>
2017-03-15 13:48:11 +0200

    VIITE-271 Corrected in master

--- viite-UI/src/view/LinkPropertyForm.js
+++ viite-UI/src/view/LinkPropertyForm.js
@@ -516,11 +516,11 @@
             $('#floatingEditModeForm').show();
             $('[id*="sourceButton"]').click({"sources": sources, "adjacents": adjacents},function(event) {
               eventbus.trigger("adjacents:nextSelected", event.data.sources, event.data.adjacents, event.currentTarget.value);
+            });
               rootElement.find('.link-properties button.calculate').attr('disabled', false);
               rootElement.find('.link-properties button.cancel').attr('disabled', false);
               rootElement.find('.link-properties button.continue').attr('disabled', true);
               applicationModel.setActiveButtons(true);
-            });
             $('[id*="aditionalSourceButton"]').click(sources,function(event) {
               processAditionalFloatings(sources, event.currentTarget.value);
             });
