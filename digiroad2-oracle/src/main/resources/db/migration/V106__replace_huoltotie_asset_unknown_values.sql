df8e35eed170f8e3e5511868991552d6cc2115c7
MACar08 <miguel.cardador@cgi.com>
2017-05-03 18:38:28 +0300

    DROTH-627 (US 418) Replace unknown values to 99

---
+++ digiroad2-oracle/src/main/resources/db/migration/V106__replace_huoltotie_asset_unknown_values.sql
@@ -0,0 +1,11 @@
+UPDATE ENUMERATED_VALUE
+SET VALUE = 99
+WHERE NAME_FI = 'Tuntematon'
+AND VALUE = 5
+AND PROPERTY_ID = (select id from property where public_ID = 'huoltotie_kayttooikeus')
+
+UPDATE ENUMERATED_VALUE
+SET VALUE = 99
+WHERE NAME_FI = 'Ei tietoa'
+AND VALUE = 0
+AND PROPERTY_ID = (select id from property where public_ID = 'huoltotie_huoltovastuu')
