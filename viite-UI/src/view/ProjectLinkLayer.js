b10d5244b6e43c972d1a8fd7da6389ecc9f64061
JaniKoivisto <koivisto.jani@live.fi>
2017-05-16 16:06:43 +0300

    Add new layer.js for project links

---
+++ viite-UI/src/view/ProjectLinkLayer.js
@@ -0,0 +1,50 @@
+(function(root) {
+    root.ProjectLinkLayer = function(map, projectCollection) {
+        var vectorLayer;
+        var layerMinContentZoomLevels = {};
+        var currentZoom = 0;
+
+        var vectorSource = new ol.source.Vector({
+            loader: function(extent, resolution, projection) {
+                var zoom = Math.log(1024/resolution) / Math.log(2);
+                    var features = _.map(projectCollection.getAll(), function(projectLink) {
+                        var points = _.map(projectLink.points, function(point) {
+                            return [point.x, point.y];
+                        });
+                        var feature =  new ol.Feature({ geometry: new ol.geom.LineString(points)
+                        });
+                        feature.projectLinkData = projectLink;
+                        return feature;
+                    });
+                loadFeatures(features);
+            },
+            strategy: ol.loadingstrategy.bbox
+        });
+
+        vectorLayer = new ol.layer.Vector({
+            source: vectorSource
+        });
+
+        var loadFeatures = function (features) {
+            vectorSource.addFeatures(features);
+        };
+
+        var show = function(map) {
+            vectorLayer.setVisible(true);
+        };
+
+        var hideLayer = function() {
+            this.stop();
+            this.hide();
+        };
+
+        vectorLayer.setVisible(true);
+        map.addLayer(vectorLayer);
+
+        return {
+            show: show,
+            hide: hideLayer
+        };
+    };
+
+})(this);
