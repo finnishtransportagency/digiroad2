f9ef9d6ce9f36ec956eb2e9decac67c47686db2e
Campanha <ricardo.campanha.marques@cgi.com>
2017-06-07 20:29:16 +0300

    join Groupby by Anomaly where 1 or 2

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/RoadAddressLinkPartitioner.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/RoadAddressLinkPartitioner.scala
@@ -8,7 +8,7 @@ object RoadAddressLinkPartitioner extends GraphPartitioner {
 
   def partition[T <: RoadAddressLinkLike](links: Seq[T]): Seq[Seq[T]] = {
     val linkGroups = links.groupBy { link => (
-      link.anomaly.value, link.roadNumber, link.roadPartNumber, link.trackCode,
+      link.anomaly.value.equals(Anomaly.None), link.anomaly.value.equals(Anomaly.Illogical), link.roadNumber, link.roadPartNumber, link.trackCode,
       link.roadLinkType.equals(RoadLinkType.FloatingRoadLinkType), link.roadLinkSource.equals(LinkGeomSource.ComplimentaryLinkInterface)
       )
     }
