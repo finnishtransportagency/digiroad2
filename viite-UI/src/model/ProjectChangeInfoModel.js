b6b78f05ab80dd2844c485db42b1933d0664c0f1
alapeijario <ossi.ala-peijari@cgi.com>
2017-06-19 12:42:56 +0300

    cleanup and added undefined check

--- viite-UI/src/model/ProjectChangeInfoModel.js
+++ viite-UI/src/model/ProjectChangeInfoModel.js
@@ -1,22 +1,23 @@
 (function(root) {
   root.ProjectChangeInfoModel = function(backend) {
 
-    var sourceList=[{endAddressM:1,endRoadPartNumber:0,roadNumber:0,startAddressM:0,startRoadPartNumber:0,trackCode:0}];
-    var targetList=[{endAddressM:1,endRoadPartNumber:0,roadNumber:0,startAddressM:0,startRoadPartNumber:0,trackCode:0}];
-    var changesInfo=[{changetype:0,discontinuity:"jatkuva",roadType:9,source:sourceList,target:targetList}];
+    var roadInfoList=[{endAddressM:1,endRoadPartNumber:0,roadNumber:0,startAddressM:0,startRoadPartNumber:0,trackCode:0}];
+    var changesInfo=[{changetype:0,discontinuity:"jatkuva",roadType:9,source:roadInfoList,target:roadInfoList}];
     var projectChanges={id:0,name:"templateproject", user:"templateuser",ely:0,changeDate:"1980-01-28",changeInfoSeq:changesInfo};
 
 
 
     function getChanges(projectID){
       backend.getChangeTable(projectID,function(changedata) {
-        projectChanges= roadChangeAPIResultParser(changedata);
-        eventbus.trigger('projectChanges:fetched', projectChanges);
+    var parsedresult=roadChangeAPIResultParser(changedata);
+      if (parsedresult!==null && parsedresult.discontinuity !==null) {
+        eventbus.trigger('projectChanges:fetched', roadChangeAPIResultParser(parsedresult));
+      }
       });
     }
 
-    function roadChangeAPIResultParser(projectChanges) {
-      //TODO: Parse
+    function roadChangeAPIResultParser(changeData) {
+      projectChanges=changeData;
       return projectChanges;
     }
 
