80c0cb657dc6edb214763f52f864b0b970f3a55c
alapeijario <ossi.ala-peijari@cgi.com>
2017-06-19 10:49:34 +0300

    definitions to api content

--- viite-UI/src/model/ProjectChangeInfoModel.js
+++ viite-UI/src/model/ProjectChangeInfoModel.js
@@ -1,7 +1,11 @@
 (function(root) {
   root.ProjectChangeInfoModel = function(backend) {
 
-    var projectChanges;
+    var sourceList=[{endAddressM:1,endRoadPartNumber:0,roadNumber:0,startAddressM:0,startRoadPartNumber:0,trackCode:0}];
+    var targetList=[{endAddressM:1,endRoadPartNumber:0,roadNumber:0,startAddressM:0,startRoadPartNumber:0,trackCode:0}];
+    var changesInfo=[{changetype:0,discontinuity:"jatkuva",roadType:9,source:sourceList,target:targetList}];
+    var projectChanges={id:0,name:"templateproject", user:"templateuser",ely:0,changeDate:"1980-01-28",changeInfoSeq:changesInfo};
+
 
 
     function getChanges(projectID){
