162865172ffe4a30bc6aba59a07969f9b6128a42
alapeijario <ossi.ala-peijari@cgi.com>
2017-06-20 17:47:24 +0300

    new svg spinner

--- viite-UI/src/model/ApplicationModel.js
+++ viite-UI/src/model/ApplicationModel.js
@@ -105,8 +105,48 @@
     };
 
     var addSpinner = function () {
-      jQuery('.container').append('<div class="spinner-overlay modal-overlay"><div class="spinner"></div></div>');
+      jQuery('.container').append('<div class="spinner-overlay modal-overlay"><svg  class="spinner"  viewBox="0 0 500 500" version="1.1"></svg></div>');
+      createMHDSpinner(".spinner",false,0.6,100,30,500);
     };
+
+    function createMHDSpinner(className,bbcircle,duration,spinnerwidth,margintoblackcircle,width) {
+      var svg = document.querySelector(className);
+      var wrapper = svg.parentNode;
+      //var width=svg.viewBox.animVal.width; for some reason test fails with this
+      var halfwidth=(width/2),outerCurve=(halfwidth-margintoblackcircle);
+      var innerCurve=(halfwidth-margintoblackcircle-spinnerwidth);
+      var path, preCurvePoints = [[outerCurve, outerCurve - innerCurve], [outerCurve, outerCurve - innerCurve]], curvePoints, angle;
+      while (svg.firstChild)
+        svg.removeChild(svg.firstChild);
+      if (bbcircle){
+        var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circle.setAttributeNS(null, 'cx',halfwidth );
+        circle.setAttributeNS(null, 'cy', halfwidth);
+        circle.setAttributeNS(null, 'r',  halfwidth);
+        circle.setAttributeNS(null, 'style', 'fill: black' );
+        svg.appendChild(circle);
+      }
+      for (i = 0,shade = 0; 255 > i; i++) {
+        path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        angle = (i + 1)*Math.PI*2/255;
+        curvePoints = [[halfwidth + outerCurve*Math.sin(angle), halfwidth - outerCurve*Math.cos(angle)], [halfwidth + innerCurve*Math.sin(angle), halfwidth - innerCurve*Math.cos(angle)]];
+        path.setAttribute("d", "M " + preCurvePoints[0] + " A " + outerCurve + " " + outerCurve + ", 0, 0, 1, " + curvePoints[0] + " A " + (outerCurve - innerCurve)/2 + " " + (outerCurve - innerCurve)/2 + ", 0, 0, 1, " + curvePoints[1] + " A " + innerCurve + " " + innerCurve + ", 0, 0, 0, " + preCurvePoints[1]);
+        path.setAttribute("fill", "rgb(" + [shade, shade, shade]+ ")");
+        preCurvePoints = curvePoints;
+        shade++;
+        svg.appendChild(path);
+      }
+      var animateMotion = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
+      animateMotion.setAttributeNS(null, "attributeName", "transform");
+      animateMotion.setAttributeNS(null, "attributeType", "XML");
+      animateMotion.setAttributeNS(null, "type", "rotate");
+      animateMotion.setAttributeNS(null, "dur",+duration+"s");
+      animateMotion.setAttributeNS(null, "repeatCount","indefinite");
+      animateMotion.setAttributeNS(null, "from", "0 0 0");
+      animateMotion.setAttributeNS(null, "to", "360 0 0");
+      svg.appendChild(animateMotion);
+      animateMotion.beginElement();
+    }
 
     var removeSpinner = function(){
       jQuery('.spinner-overlay').remove();
