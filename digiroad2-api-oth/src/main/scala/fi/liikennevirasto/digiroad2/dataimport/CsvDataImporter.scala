09f84d9e15e292ba3acc81f28700475446af78e9
MACar08 <miguel.cardador@cgi.com>
2017-06-22 17:23:07 +0300

    fixing related with DROTH-496

--- digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/dataimport/CsvDataImporter.scala
+++ digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/dataimport/CsvDataImporter.scala
@@ -54,13 +54,15 @@ trait RoadLinkCsvImporter {
     "Toiminnallinen luokka" -> "functional_Class",
     "Liikennevirran suunta" -> "DIRECTIONTYPE",
     "Tielinkin tyyppi" -> "link_Type",
-    "Tasosijainti" -> "VERTICALLEVEL",
     "Kuntanumero" -> "MUNICIPALITYCODE",
     "Osoitenumerot oikealla alku" -> "FROM_RIGHT",
     "Osoitenumerot oikealla loppu" -> "TO_RIGHT",
     "Osoitenumerot vasemmalla alku" -> "FROM_LEFT",
     "Osoitenumerot vasemmalla loppu" -> "TO_LEFT",
     "Linkin tila" -> "CONSTRUCTIONTYPE"
+  )
+  private val codeValueFielddMappings = Map(
+    "Tasosijainti" -> "VERTICALLEVEL"
   )
 
   private val fieldInOTH = List("link_Type", "functional_Class")
@@ -74,7 +76,7 @@ trait RoadLinkCsvImporter {
   private val mandatoryFields = "Linkin ID"
 
 
-  val mappings = textFieldMappings ++ intFieldMappings
+  val mappings = textFieldMappings ++ intFieldMappings ++ codeValueFielddMappings
 
   val MandatoryParameters: Set[String] = mappings.keySet + mandatoryFields
 
@@ -121,6 +123,14 @@ trait RoadLinkCsvImporter {
     }
   }
 
+  private def verifyValueCode(parameterName: String, parameterValue: String): ParsedLinkRow = {
+    val autorizedValues = List(-11, -1, 0, 1, 2, 3, 4, 5, 10)
+    if (autorizedValues.contains(parameterValue.toInt)) {
+      (Nil, List(LinkProperty(columnName = codeValueFielddMappings(parameterName), value = parameterValue.toString)))
+    } else
+    (List(parameterName), Nil)
+  }
+
   private def linkRowToProperties(csvRowWithHeaders: Map[String, Any]): ParsedLinkRow = {
     csvRowWithHeaders.foldLeft(Nil: MalformedParameters, Nil: ParsedProperties) { (result, parameter) =>
       val (key, value) = parameter
@@ -135,6 +145,9 @@ trait RoadLinkCsvImporter {
           result.copy(_2 = LinkProperty(columnName = textFieldMappings(key), value = value) :: result._2)
         } else if (intFieldMappings.contains(key)) {
           val (malformedParameters, properties) = verifyValueType(key, value.toString)
+          result.copy(_1 = malformedParameters ::: result._1, _2 = properties ::: result._2)
+        } else if (codeValueFielddMappings.contains(key)) {
+          val (malformedParameters, properties) = verifyValueCode(key, value.toString)
           result.copy(_1 = malformedParameters ::: result._1, _2 = properties ::: result._2)
         } else if (mandatoryFields.contains(key) && !value.toString.forall(_.isDigit)) {
           result.copy(_1 = List(mandatoryFields) ::: result._1 , _2 = result._2)
