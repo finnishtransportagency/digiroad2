58147dd389d1a7e55f8ce6ea82827c019635fb97
Lourenço <pedro.m.lourenco@cgi.com>
2017-07-12 14:42:25 +0300

    Uusi validation change and input prefill validation

--- viite-UI/src/view/RoadAddressProjectEditForm.js
+++ viite-UI/src/view/RoadAddressProjectEditForm.js
@@ -97,7 +97,7 @@
         '<select class="form-control" id="dropDown" size="1">'+
         '<option selected disabled hidden>Valitse</option>'+
         '<option value="lakkautus"' + (status == 1 ? ' selected' : '') + '>Lakkautus</option>'+
-        '<option value="uusi"' + (selected[0].roadNumber === 0 ? ' ' : ' disabled')+'>Uusi</option>'+
+        '<option value="uusi"' + (selected[0].status !== 0 && selected[0].status !== 1 ? ' ' : ' disabled')+'>Uusi</option>'+
         '<option value="action4" disabled>Numeroinnin muutos</option>'+
         '<option value="action5" disabled>Ennallaan</option>'+
         '<option value="action6" disabled>Kalibrointiarvon muutos</option>'+
@@ -120,8 +120,7 @@
         '<div><label></label></div><div><label style = "margin-top: 50px">TIEOSOITTEEN TIEDOT</label></div>' +
         addSmallLabel('TIE') + addSmallLabel('OSA') + addSmallLabel('AJR')+ addSmallLabel('ELY')  + addSmallLabel('JATKUU')+
         '</div>' +
-        '<div class="form-group new-road-address" hidden>' +
-        addSmallInputNumber('tie',selectedProjectLink[0].roadNumber) + addSmallInputNumber('osa',selectedProjectLink[0].roadPartNumber) + addSmallInputNumber('ajr', selectedProjectLink[0].trackCode)+  addSmallInputNumberDisabled('ely', selectedProjectLink[0].elyCode) +addSelect()+
+        '<div class="form-group new-road-address" hidden>'+ addSmallInputNumber('tie',(selectedProjectLink[0].roadNumber !== 0 ? selectedProjectLink[0].roadNumber : '')) + addSmallInputNumber('osa',(selectedProjectLink[0].roadPartNumber !== 0 ? selectedProjectLink[0].roadPartNumber : '')) + addSmallInputNumber('ajr',(selectedProjectLink[0].trackCode !== 99 ? selectedProjectLink[0].trackCode : '')) + addSmallInputNumberDisabled('ely', selectedProjectLink[0].elyCode) +addSelect()+
         '</div>';
     };
 
