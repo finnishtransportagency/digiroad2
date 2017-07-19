37fae5678d3deadf09a9d9faf87d7a4bd008ee13
Lourenço <pedro.m.lourenco@cgi.com>
2017-07-19 21:18:17 +0300

    Fix in determining mValues for new road addresses

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/ProjectDeltaCalculatorSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/ProjectDeltaCalculatorSpec.scala
@@ -20,48 +20,6 @@ class ProjectDeltaCalculatorSpec  extends FunSuite with Matchers{
       roadAddress.sideCode, roadAddress.calibrationPoints, floating=false, roadAddress.geom, project.id, LinkStatus.NotHandled, RoadType.PublicRoad, roadAddress.linkGeomSource, GeometryUtils.geometryLength(roadAddress.geom))
   }
 
-  test("validate correct mValuesCreation") {
-    val idRoad0 = 0L
-    val idRoad1 =1L
-    val idRoad2 = 2L
-    val projectId = 1
-    val baseLength = 100L
-    val rap = RoadAddressProject(projectId, ProjectState.apply(1), "TestProject", "TestUser", DateTime.parse("2700-01-01"), "TestUser", DateTime.parse("1972-03-03"), DateTime.parse("2700-01-01"), "Some additional info", List.empty[ReservedRoadPart], None)
-    val projectLink0 = toProjectLink(rap)(RoadAddress(idRoad0, 5, 1, RoadType.Unknown, Track.Combined, Discontinuous, 0L, 10L, Some(DateTime.parse("1901-01-01")), Some(DateTime.parse("1902-01-01")), Option("tester"), 0, 12345L, 0.0, 9.8, SideCode.TowardsDigitizing, 0, (None, None), false,
-      Seq(Point(0.0, 0.0), Point(0.0, 9.8)), LinkGeomSource.NormalLinkInterface))
-    val projectLink1 = toProjectLink(rap)(RoadAddress(idRoad1, 5, 1, RoadType.Unknown, Track.Combined, Discontinuous, 0L, 10L, Some(DateTime.parse("1901-01-01")), Some(DateTime.parse("1902-01-01")), Option("tester"), 0, 12345L, 0.0, 9.8, SideCode.TowardsDigitizing, 0, (None, None), false,
-      Seq(Point(0.0, 0.0), Point(0.0, 9.8)), LinkGeomSource.NormalLinkInterface))
-    val projectLink2 = toProjectLink(rap)(RoadAddress(idRoad2, 5, 1, RoadType.Unknown, Track.Combined, Discontinuous, 0L, 10L, Some(DateTime.parse("1901-01-01")), Some(DateTime.parse("1902-01-01")), Option("tester"), 0, 12345L, 0.0, 9.8, SideCode.TowardsDigitizing, 0, (None, None), false,
-      Seq(Point(0.0, 0.0), Point(0.0, 9.8)), LinkGeomSource.NormalLinkInterface))
-
-    val projectLinkSeq = Seq(projectLink0, projectLink1, projectLink2)
-    var linkLengths: Map[RoadPartBasis, Seq[RoadPartLengths]] = Map.empty
-    projectLinkSeq.foreach(pl => {
-      val index = new RoadPartBasis(pl.roadNumber, pl.roadPartNumber)
-      linkLengths = linkLengths + ( index -> Seq(RoadPartLengths(pl.linkId, baseLength)).++(linkLengths.getOrElse(index,Seq.empty)))
-    })
-
-    val output = ProjectDeltaCalculator.determineMValues(projectLinkSeq, linkLengths)
-    output.length should be(3)
-	
-    output(0).id should be(idRoad0)
-    output(0).startMValue should be(0.0)
-    output(0).endMValue should be(baseLength)
-    output(0).startAddrMValue should be(Math.round(output(0).startMValue))
-    output(0).endAddrMValue should be(Math.round(output(0).endMValue))
-
-    output(1).id should be(idRoad1)
-    output(1).startMValue should be(0.0)
-    output(1).endMValue should be(baseLength)
-    output(1).startAddrMValue should be(Math.round(baseLength))
-    output(1).endAddrMValue should be(Math.round(baseLength*2))
-
-    output(2).id should be(idRoad2)
-    output(2).startMValue should be(0.0)
-    output(2).endMValue should be(baseLength)
-    output(2).startAddrMValue should be(Math.round(baseLength*2))
-    output(2).endAddrMValue should be(Math.round(baseLength*3))
-  }
 
   test("addressMValues N MValues calculation for new road addresses") {
     val idRoad0 = 0L
