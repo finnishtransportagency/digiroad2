a02edf5c35b1ad74ded8bcf4db40fc93ecbea230
marquesfm <francisco.marques@cgi.com>
2017-07-24 14:35:21 +0300

    DROTH-781 (US 750) Extend PointAssetOperations for the traffic sign asset - Add New Unit Tests for Traffic Sign Service methods.

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/TrafficSignServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/TrafficSignServiceSpec.scala
@@ -1,5 +1,6 @@
 package fi.liikennevirasto.digiroad2
 
+import fi.liikennevirasto.digiroad2.asset.LinkGeomSource.NormalLinkInterface
 import fi.liikennevirasto.digiroad2.asset._
 import fi.liikennevirasto.digiroad2.linearasset.RoadLink
 import fi.liikennevirasto.digiroad2.user.{Configuration, User}
@@ -49,6 +50,80 @@ class TrafficSignServiceSpec extends FunSuite with Matchers {
       result.lon should equal(374467)
       result.lat should equal(6677347)
       result.mValue should equal(103)
+    }
+  }
+
+  test("Can fetch by municipality") {
+    when(mockRoadLinkService.getRoadLinksWithComplementaryAndChangesFromVVH(235)).thenReturn((Seq(
+      VVHRoadlink(388553074, 235, Seq(Point(0.0, 0.0), Point(200.0, 0.0)), Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink), Nil))
+
+    runWithRollback {
+      val result = service.getByMunicipality(235).find(_.id == 600073).get
+
+      result.id should equal(600073)
+      result.linkId should equal(1611317)
+      result.lon should equal(374467)
+      result.lat should equal(6677347)
+      result.mValue should equal(103)
+    }
+  }
+
+  test("Expire Traffic Sign") {
+    runWithRollback {
+      service.getPersistedAssetsByIds(Set(600073)).length should be(1)
+      service.expire(600073, testUser.username)
+      service.getPersistedAssetsByIds(Set(600073)) should be(Nil)
+    }
+  }
+
+  test("Create new Traffic Sign") {
+    runWithRollback {
+      val properties = Set(
+        SimpleProperty("liikennemerkki_tyyppi", List(PropertyValue("1"))),
+        SimpleProperty("liikennemerkki_arvo", List(PropertyValue("80"))),
+        SimpleProperty("liikennemerkki_lisatieto", List(PropertyValue("Additional Info for test"))))
+      val id = service.create(IncomingTrafficSign(2.0, 0.0, 388553075, properties), testUser.username, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 235, linkSource = NormalLinkInterface)
+
+      val assets = service.getPersistedAssetsByIds(Set(id))
+
+      assets.size should be(1)
+
+      val asset = assets.head
+
+      asset.id should be(id)
+      asset.linkId should be(388553075)
+      asset.lon should be(2)
+      asset.lat should be(0)
+      asset.mValue should be(2)
+      asset.floating should be(false)
+      asset.municipalityCode should be(235)
+      asset.propertyData.find(p => p.publicId == "liikennemerkki_tyyppi").get.values.head.propertyValue should be ("1")
+      asset.propertyData.find(p => p.publicId == "liikennemerkki_arvo").get.values.head.propertyValue should be ("80")
+      asset.propertyData.find(p => p.publicId == "liikennemerkki_lisatieto").get.values.head.propertyValue should be ("Additional Info for test")
+      asset.createdBy should be(Some(testUser.username))
+      asset.createdAt shouldBe defined
+    }
+  }
+
+  test("Update Traffic Sign") {
+    runWithRollback {
+      val trafficSign = service.getById(600073).get
+
+      val updatedProperties = Set(
+        SimpleProperty("liikennemerkki_tyyppi", List(PropertyValue("2"))),
+        SimpleProperty("liikennemerkki_arvo", List(PropertyValue("90"))),
+        SimpleProperty("liikennemerkki_lisatieto", List(PropertyValue("Updated Additional Info for test"))))
+      val updated = IncomingTrafficSign(trafficSign.lon, trafficSign.lat, trafficSign.linkId, updatedProperties)
+
+      service.update(trafficSign.id, updated, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 235, "unit_test", linkSource = NormalLinkInterface)
+      val updatedTrafficSign = service.getById(600073).get
+
+      updatedTrafficSign.propertyData.find(p => p.publicId == "liikennemerkki_tyyppi").get.values.head.propertyValue should be ("2")
+      updatedTrafficSign.propertyData.find(p => p.publicId == "liikennemerkki_arvo").get.values.head.propertyValue should be ("90")
+      updatedTrafficSign.propertyData.find(p => p.publicId == "liikennemerkki_lisatieto").get.values.head.propertyValue should be ("Updated Additional Info for test")
+      updatedTrafficSign.id should equal(updatedTrafficSign.id)
+      updatedTrafficSign.modifiedBy should equal(Some("unit_test"))
+      updatedTrafficSign.modifiedAt shouldBe defined
     }
   }
 }
