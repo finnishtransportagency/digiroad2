e381b89728fecaa405b2f155779dbf238902af3a
marquesfm <francisco.marques@cgi.com>
2017-08-07 13:35:56 +0300

    Merge of DROTH-585 - Fix of LRM calculation on Road Address test on RoadAddressDAOSpec.

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/roadaddress/oracle/RoadAddressDAOSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/roadaddress/oracle/RoadAddressDAOSpec.scala
@@ -7,8 +7,8 @@ import org.scalatest.{FunSuite, Matchers}
 class RoadAddressDAOSpec extends FunSuite with Matchers {
 
   test("LRM calculation on Road Address") {
-    val towards = RoadAddress(1L, 1L, 1L, Track.RightSide, 5, 100, 110, None, None, 1L, 123L, 1.5, 11.4, SideCode.TowardsDigitizing, false, Seq())
-    val against = RoadAddress(1L, 1L, 1L, Track.RightSide, 5, 100, 110, None, None, 1L, 123L, 1.5, 11.4, SideCode.AgainstDigitizing, false, Seq())
+    val towards = RoadAddress(1L, 1L, 1L, Track.RightSide, 5, 100, 110, None, None, 1L, 123L, 1.5, 11.4, SideCode.TowardsDigitizing, false, Seq(), false, None, None, None)
+    val against = RoadAddress(1L, 1L, 1L, Track.RightSide, 5, 100, 110, None, None, 1L, 123L, 1.5, 11.4, SideCode.AgainstDigitizing, false, Seq(), false, None, None, None)
     towards.addressMValueToLRM(100L).get should be (1.5 +- .001)
     against.addressMValueToLRM(100L).get should be (11.4 +- .001)
     towards.addressMValueToLRM(110L).get should be (11.4 +- .001)
