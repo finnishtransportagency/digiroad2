e7503c8dd9cf9fa559584b490fafc715cd6a5ae7
Campanha <ricardo.campanha.marques@cgi.com>
2017-08-24 11:00:42 +0300

    At least one of the new road address should connect the existing part

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
@@ -214,6 +214,12 @@ class ProjectService(roadAddressService: RoadAddressService, roadLinkService: Ro
         if (linksInProject.nonEmpty) {
           ProjectDAO.removeProjectLinksByProjectAndRoadNumber(roadAddressProjectID, newRoadNumber, newRoadPartNumber)
         }
+        val isNewConnectingToPart = projectAddressLinks.map{
+          pal =>
+            linksInProject.find(l => GeometryUtils.areAdjacent(l.geometry, pal.geometry))
+        }
+        if(isNewConnectingToPart.isEmpty)
+          throw new ProjectValidationException(s"Some new roads are not adjacent")
         val newValidAddresses = projectAddressLinks.foldLeft(linksInProject)((withAddedLinks, next) => continuousToPart(next, (projectAddressLinks ++ linksInProject).filterNot(l => l.linkId == next.linkId), withAddedLinks))
 
         val randomSideCode =
