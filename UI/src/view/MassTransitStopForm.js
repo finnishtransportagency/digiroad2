dc39a1c70c49a0996b5aa8c28449371785508d81
marquesfm <francisco.marques@cgi.com>
2017-09-01 17:37:30 +0300

    DROTH-873 (US 836) UI: Add new field in client side and validate max size - Create New Function to Validate PlatformNumber MaxSize.

--- UI/src/view/MassTransitStopForm.js
+++ UI/src/view/MassTransitStopForm.js
@@ -49,6 +49,13 @@
     };
   };
 
+  var ValidatePlatformNumberMaxSize = function (target, propertyPublicId) {
+    var propertyValue = target.currentTarget.value;
+    if (propertyPublicId === 'laiturinumero' && propertyValue.length > 3) {
+      target.currentTarget.value = propertyValue.substring(0, 3);
+    }
+  };
+
   var SaveButton = function() {
     var element = $('<button />').addClass('save btn btn-primary').text('Tallenna').click(function () {
       if (poistaSelected) {
@@ -263,6 +270,7 @@
           elementType = property.propertyType === 'long_text' ?
             $('<textarea />').addClass('form-control') : $('<input type="text"/>').addClass('form-control');
           element = elementType.bind('input', function(target){
+            ValidatePlatformNumberMaxSize(target, property.publicId);
             selectedMassTransitStopModel.setProperty(property.publicId, [{ propertyValue: target.currentTarget.value, propertyDisplayValue: target.currentTarget.value  }], property.propertyType, property.required);
           });
 
@@ -728,38 +736,6 @@
               {type: 'alert'});
         }
       });
-
-      //eventbus.on('assetPropertyValue:changed', function (event) {
-      //  var property = event.propertyData;
-      //
-      //  if (property.publicId === 'laiturinumero' && (_.find(property.values, function (value) {return value.propertyValue.length > 3;}))){
-      //    //var rootElement = $('#feature-attributes');
-      //    //rootElement.find('input[type="text"]').on('input change', function (event) {
-      //    //  var eventTarget = $(event.currentTarget);
-      //    //  selectedAsset.set({name: eventTarget.val()});
-      //    //});
-      //
-      //    var lastValueAdded = property.values[0].propertyValue.substring(0, 3);
-      //
-      //    var rootElement = $('#feature-attributes');
-      //    rootElement.find('input[type="text"]').on('input change', function (event) {
-      //      var eventTarget = $(event.currentTarget);
-      //      var propertyValue = $(event.currentTarget).val();
-      //    });
-      //
-      //    //property.values[0] = {propertyValue: lastValueAdded, propertyDisplayValue: lastValueAdded};
-      //
-      //    //_.map(property.values, function (value) {
-      //    //  value.propertyValue = lastValueAdded;
-      //    //  value.propertyDisplayValue = lastValueAdded
-      //    //});
-      //
-      //    //selectedMassTransitStopModel.setProperty(property.publicId, property.values, property.propertyType);
-      //
-      //    var t = 'teste 1';
-      //  }
-      //});
-
       backend.getEnumeratedPropertyValues();
     }
   };
