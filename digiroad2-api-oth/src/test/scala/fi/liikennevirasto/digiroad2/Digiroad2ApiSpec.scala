cf8fd1de4a3265d96eb97e303d149dcabdae5cf2
coelhojf <joana.f.coelho@cgi.com>
2017-09-05 17:26:05 +0300

    DROTH-877 Visualization: - Add "PlataformCode" element to StopChange.xsd - Correction of number of properties on test

--- digiroad2-api-oth/src/test/scala/fi/liikennevirasto/digiroad2/Digiroad2ApiSpec.scala
+++ digiroad2-api-oth/src/test/scala/fi/liikennevirasto/digiroad2/Digiroad2ApiSpec.scala
@@ -218,7 +218,7 @@ class Digiroad2ApiSpec extends AuthenticatedApiSpec with BeforeAndAfter {
     getWithUserAuth("/assetTypeProperties/10") {
       status should equal(200)
       val ps = parse(body).extract[List[Property]]
-      ps.size should equal(38)
+      ps.size should equal(39)
       val p1 = ps.find(_.publicId == TestPropertyId).get
       p1.publicId should be ("katos")
       p1.propertyType should be ("single_choice")
