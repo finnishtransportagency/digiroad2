ffc6156f61790f6627913a14d4c8b7564ce2c00e
leirasf <faustino.leiras@cgi.com>
2017-10-04 10:47:47 +0300

    Change db migration version after merge

--- digiroad2-oracle/src/main/resources/db/migration/V126__modifications_related_with_platform_number_property.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V126__modifications_related_with_platform_number_property.sql
@@ -3,7 +3,7 @@ ALTER TABLE PROPERTY ADD MAX_VALUE_LENGTH NUMBER;
 
 --Add property Laiturinumero
 INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
-VALUES (primary_key_seq.nextval,'Laiturinumero','db_migration_v122', sysdate);
+VALUES (primary_key_seq.nextval,'Laiturinumero','db_migration_v126', sysdate);
 
 INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID, MAX_VALUE_LENGTH)
-VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'text', 0, 'db_migration_v122', 'laiturinumero', (select id from LOCALIZED_STRING where VALUE_FI = 'Laiturinumero'), 3);
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Bussipysäkit'), 'text', 0, 'db_migration_v126', 'laiturinumero', (select id from LOCALIZED_STRING where VALUE_FI = 'Laiturinumero'), 3);
