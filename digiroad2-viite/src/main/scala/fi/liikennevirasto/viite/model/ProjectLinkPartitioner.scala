a32be4d62cdab7dd5314baf16fa83e231d70ca50
estrompah <hugo.estrompa@cgi.com>
2017-10-06 18:48:17 +0300

    VIITE-749 fix in projectLink Partitioner in order to filter by roadNumber, roadPartNumber and Track

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/ProjectLinkPartitioner.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/ProjectLinkPartitioner.scala
@@ -7,13 +7,14 @@ import fi.liikennevirasto.digiroad2.linearasset.GraphPartitioner
 object ProjectLinkPartitioner extends GraphPartitioner {
 
   def partition[T <: ProjectAddressLinkLike](links: Seq[T]): Seq[Seq[T]] = {
-    val linkGroups = links.groupBy { link => (
-      link.anomaly, link.roadNumber, link.roadPartNumber, link.trackCode,
+    val (roadsWithoutNumber, roadsWithNumber ) = links.partition(link => link.roadNumber == 0)
+    val linkGroupWithNumber = roadsWithNumber.groupBy { link => (link.roadNumber, link.roadPartNumber, link.trackCode, link.status)}
+    val linkGroupWithoutNumber = roadsWithoutNumber.groupBy{link => (link.anomaly,
       link.roadLinkType.equals(RoadLinkType.FloatingRoadLinkType), link.roadLinkSource.equals(LinkGeomSource.ComplimentaryLinkInterface), link.roadLinkSource.equals(LinkGeomSource.SuravageLinkInterface),
-      link.roadName, link.municipalityCode, link.status
-      )
+      link.roadName, link.municipalityCode, link.status)
+
     }
-    val clusters = for (linkGroup <- linkGroups.values.toSeq;
+    val clusters = for (linkGroup <- linkGroupWithNumber.values.toSeq ++ linkGroupWithoutNumber.values.toSeq;
                         cluster <- clusterLinks(linkGroup)) yield cluster
     clusters.map(linksFromCluster)
   }
