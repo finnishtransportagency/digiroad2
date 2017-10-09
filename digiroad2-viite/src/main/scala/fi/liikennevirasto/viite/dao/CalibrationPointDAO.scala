51eacb23e4157031585763a599e18ece3e1ede18
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2017-10-09 17:49:25 +0300

    *Small DAO fixes relative of creation of DAOSpec class *Creation of DAO unit tests

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/CalibrationPointDAO.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/CalibrationPointDAO.scala
@@ -2,7 +2,7 @@ package fi.liikennevirasto.viite.dao
 
 import slick.driver.JdbcDriver.backend.Database.dynamicSession
 import slick.jdbc.StaticQuery.interpolation
-import slick.jdbc.{StaticQuery => Q}
+import slick.jdbc.{GetResult, PositionedResult, StaticQuery => Q}
 
 /**
   * Created by pedrosag on 04-10-2017.
@@ -16,15 +16,32 @@ object CalibrationPointDAO {
 
   case class UserDefineCalibrationPoint(id: Long, projectLinkId: Long, projectId: Long, segmentMValue: Double, addressMValue: Long) extends CalibrationPointMValues
 
-  def findCalibrationPointById(id: Long): UserDefineCalibrationPoint = {
+  implicit val getCalibrationPoint = new GetResult[UserDefineCalibrationPoint] {
+    def apply(r: PositionedResult) = {
+
+      val id = r.nextLong()
+      val projectLinkId = r.nextLong()
+      val projectId = r.nextLong()
+      val segmentMValue = r.nextDouble()
+      val addressMValue = r.nextLong()
+
+      UserDefineCalibrationPoint(id, projectLinkId, projectId, segmentMValue, addressMValue)
+    }
+
+  }
+
+
+  def findCalibrationPointById(id: Long): Option[UserDefineCalibrationPoint] = {
     val baseQuery =
       s"""
          Select * From CALIBRATION_POINT Where ID = ${id}
        """
 
-    Q.queryNA[(Long, Long, Long, Double, Long)](baseQuery).list.map{
-      case (id,projectLinkId, projectId, linkM, addressM) => UserDefineCalibrationPoint(id,projectLinkId, projectId, linkM, addressM)
-    }.head
+    val tuples = Q.queryNA[UserDefineCalibrationPoint](baseQuery).list
+    tuples.groupBy(_.id).map{
+      case (id, calibrationPointList) =>
+        calibrationPointList.head
+    }.toList.headOption
   }
 
   def findCalibrationPointByRemainingValues(projectLinkId: Long, projectId: Long, segmentMValue: Double, addressMValue: Long): Seq[UserDefineCalibrationPoint] = {
@@ -38,7 +55,7 @@ object CalibrationPointDAO {
     }
   }
 
-  def findCalibrationPointsOfRoad(projectId: Long, linkId: Long): UserDefineCalibrationPoint = {
+  def findCalibrationPointsOfRoad(projectId: Long, linkId: Long): Seq[UserDefineCalibrationPoint] = {
     val baseQuery =
       s"""
          Select * From CALIBRATION_POINT Where PROJECT_LINK_ID = ${linkId} And PROJECT_ID = ${projectId}
@@ -46,29 +63,31 @@ object CalibrationPointDAO {
 
     Q.queryNA[(Long, Long, Long, Double, Long)](baseQuery).list.map{
       case (id,projectLinkId, projectId, linkM, addressM) => UserDefineCalibrationPoint(id,projectLinkId, projectId, linkM, addressM)
-    }.head
+    }
   }
 
-  def createCalibrationPoint(calibrationPoint: UserDefineCalibrationPoint) = {
+  def createCalibrationPoint(calibrationPoint: UserDefineCalibrationPoint): Long = {
     val nextCalibrationPointId = sql"""select CALIBRATION_POINT_ID_SEQ.nextval from dual""".as[Long].first
     sqlu"""
       Insert Into CALIBRATION_POINT (ID, PROJECT_LINK_ID, PROJECT_ID, LINK_M, ADDRESS_M) Values
       (${nextCalibrationPointId}, ${calibrationPoint.projectLinkId}, ${calibrationPoint.projectId}, ${calibrationPoint.segmentMValue}, ${calibrationPoint.addressMValue})
       """.execute
+    nextCalibrationPointId
   }
 
-  def createCalibrationPoint(projectLinkId: Long, projectId: Long, segmentMValue: Double, addressMValue: Long) = {
+  def createCalibrationPoint(projectLinkId: Long, projectId: Long, segmentMValue: Double, addressMValue: Long): Long = {
     val nextCalibrationPointId = sql"""select CALIBRATION_POINT_ID_SEQ.nextval from dual""".as[Long].first
     sqlu"""
       Insert Into CALIBRATION_POINT (ID, PROJECT_LINK_ID, PROJECT_ID, LINK_M, ADDRESS_M) Values
       (${nextCalibrationPointId}, ${projectLinkId}, ${projectId}, ${segmentMValue}, ${addressMValue})
       """.execute
+    nextCalibrationPointId
   }
 
   def updateSpecificCalibrationPointMeasures(id: Long, segmentMValue: Double, addressMValue: Long) = {
     sqlu"""
-        Update CALIBRATION_POINT Where ID = ${id} SET LINK_M = ${segmentMValue}, ADDRESS_M = ${addressMValue}
-      """
+        Update CALIBRATION_POINT Set LINK_M = ${segmentMValue}, ADDRESS_M = ${addressMValue} Where ID = ${id}
+      """.execute
   }
 
   def removeSpecificCalibrationPoint(id: Long) = {
