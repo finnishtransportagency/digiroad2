27f39988be683bbc6419a7ef4c750ec081d18c8d
Lourenço <pedro.m.lourenco@cgi.com>
2017-10-18 13:35:24 +0300

    VIITE-776 added missing ely column in select statement

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadAddressDAO.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadAddressDAO.scala
@@ -254,7 +254,7 @@ object RoadAddressDAO {
       val x2 = r.nextDouble()
       val y2 = r.nextDouble()
       val geomSource = LinkGeomSource.apply(r.nextInt)
-      val ely = r.nextLong();
+      val ely = r.nextLong()
 
       RoadAddress(id, roadNumber, roadPartNumber, RoadType.Unknown, Track.apply(trackCode), Discontinuity.apply(discontinuity),
         startAddrMValue, endAddrMValue, startDate, endDate, createdBy, lrmPositionId, linkId, startMValue, endMValue,
@@ -434,7 +434,7 @@ object RoadAddressDAO {
         select ra.id, ra.road_number, ra.road_part_number, ra.track_code,
         ra.discontinuity, ra.start_addr_m, ra.end_addr_m, ra.lrm_position_id, pos.link_id, pos.start_measure, pos.end_measure,
         pos.side_code, pos.adjusted_timestamp,
-        ra.start_date, ra.end_date, ra.created_by, ra.valid_from, ra.CALIBRATION_POINTS, ra.floating, t.X, t.Y, t2.X, t2.Y, link_source
+        ra.start_date, ra.end_date, ra.created_by, ra.valid_from, ra.CALIBRATION_POINTS, ra.floating, t.X, t.Y, t2.X, t2.Y, link_source, ra.ely
         from road_address ra cross join
         TABLE(SDO_UTIL.GETVERTICES(ra.geometry)) t cross join
         TABLE(SDO_UTIL.GETVERTICES(ra.geometry)) t2
