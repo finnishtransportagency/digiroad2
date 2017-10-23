4107851500241e56de6ea89ad04b985d1def0b6e
oskarmalmstrom <oskar.malmstrom@gmail.com>
2017-10-23 20:12:05 +0300

    DROTH-962: -new pictures of signs in collection

--- UI/src/model/TrafficSignsCollection.js
+++ UI/src/model/TrafficSignsCollection.js
@@ -9,7 +9,10 @@
       pedestrianCrossings: false, //[7]
       maximumLengths: false, //[8]
       generalWarnings: false, //[9]
-      turningRestrictions: false //[10, 11, 12]
+      turningRestrictions: false, //[10, 11, 12]
+      prohibitionsAndRestrictions: false,  //[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+      maximumRestrictions: false, //[30, 31, 32, 33, 34, 35]
+      generalWarningSigns: false //[36, 37, 38, 39, 40, 41, 42, 43]
     };
 
     var trafficSignValues = {
@@ -17,7 +20,10 @@
       pedestrianCrossings: [7],
       maximumLengths: [8],
       generalWarnings: [9],
-      turningRestrictions: [10, 11, 12]
+      turningRestrictions: [10, 11, 12],
+      prohibitionsAndRestrictions: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+      maximumRestrictions: [30, 31, 32, 33, 34, 35],
+      generalWarningSigns: [36, 37, 38, 39, 40, 41, 42, 43]
     };
 
     var filterTrafficSigns = function (asset) {
