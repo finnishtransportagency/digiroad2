18fc8892fb3a6fbe1ebe23e81a82d3a17273c502
tvenhola <tuomas.venhola@cgi.com>
2017-10-27 11:09:49 +0300

    SQL performance fix for fetch

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/masstransitstop/oracle/MassTransitStopDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/masstransitstop/oracle/MassTransitStopDao.scala
@@ -71,7 +71,9 @@ class MassTransitStopDao {
 
     terminalIdOption match {
       case Some(terminalId) =>
-        val filter = s"where a.asset_type_id = $typeId and (a.valid_to is null or a.valid_to > sysdate) and (($boundingBoxFilter ) or tbs.terminal_asset_id = $terminalId)"
+        val filter = s"    where a.asset_type_id = $typeId and (a.valid_to is null or a.valid_to > sysdate) and " +
+          s"a.id in (SELECT id from asset a WHERE ($boundingBoxFilter ) UNION " +
+          s"select bus_stop_asset_id FROM terminal_bus_stop_link where terminal_asset_id = $terminalId)"
         fetchPointAssets(withFilter(filter))
           .filter(r => GeometryUtils.geometryLength(Seq(position, Point(r.lon, r.lat))) <= meters || r.terminalId.contains(terminalId))
       case _ =>
