8519876a9ff15d05247d68ef56868aeaeb41474f
marquesfm <francisco.marques@cgi.com>
2017-11-09 17:05:06 +0200

    DROTH-1012 - Enable PlatformCode from vallu-xml for now - Remove PlatformCode from vallu-xml.

--- digiroad2-api-common/src/test/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessageSpec.scala
+++ digiroad2-api-common/src/test/scala/fi/liikennevirasto/digiroad2/vallu/ValluStoreStopChangeMessageSpec.scala
@@ -44,7 +44,6 @@ class ValluStoreStopChangeMessageSpec extends FlatSpec with MustMatchers {
     (xml \ "Names" ) must be ('empty)
     (xml \ "SpecialNeeds").text must equal("")
     (xml \ "Comments").text must equal("")
-    (xml \ "PlatformCode").text must equal("")
   }
 
   it must "specify external id" in {
