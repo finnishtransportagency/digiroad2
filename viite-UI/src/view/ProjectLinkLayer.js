08097ab1da6a6185eb35d9968b40dade8204f5c8
Lourenço <pedro.m.lourenco@cgi.com>
2017-11-09 14:31:15 +0200

    VIITE-483 added the form refresh for split links

--- viite-UI/src/view/ProjectLinkLayer.js
+++ viite-UI/src/view/ProjectLinkLayer.js
@@ -728,7 +728,10 @@
     eventbus.on('changeProjectDirection:clicked', function () {
       projectCollection.fetch(map.getView().calculateExtent(map.getSize()).join(','), currentZoom + 1, undefined, projectCollection.getPublishableStatus());
       eventbus.once('roadAddressProject:fetched', function () {
-        if (selectedProjectLinkProperty.get().length > 1)
+        if (selectedProjectLinkProperty.get().length > 1 && !_.isUndefined(selectedProjectLinkProperty.get()[0].connectedLinkId) {
+          selectedProjectLinkProperty.openSplit(selectedProjectLinkProperty.get()[0].linkId, true);
+        } else
+        if (selectedProjectLinkProperty.get().length > 1 && _.isUndefined(selectedProjectLinkProperty.get()[0].connectedLinkId))
           selectedProjectLinkProperty.open(selectedProjectLinkProperty.get()[0].linkId, true);
         else
           selectedProjectLinkProperty.open(selectedProjectLinkProperty.get()[0].linkId, false);
