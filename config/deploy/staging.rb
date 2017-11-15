1e1fef8757f90c4b7158a944451bd3bcc056bb59
leirasf <faustino.leiras@cgi.com>
2017-11-15 19:16:58 +0200

    Change vallu execution folder

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,8 +5,9 @@ server 'gateway', user: 'web', roles: %w{web app}
 namespace :deploy do
   task :start_vallu_server do
     on roles(:all) do
+      execute "killall -q node; exit 0"
       # Capistrano kills the vallu server before it gets to start up if sleep 1 is not defined
-      execute "cd #{release_path} && (nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 &) && sleep 100"
+      execute "cd /home/web/digiroad2/current && (nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 &) && sleep 100"
     end
   end
 
