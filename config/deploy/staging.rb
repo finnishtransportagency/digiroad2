ed21dc456991fa11bf333aac0a63c44f93f7fd11
leirasf <faustino.leiras@cgi.com>
2017-11-16 19:32:34 +0200

    Capistrano change vallu execution order

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,9 +7,9 @@ namespace :deploy do
     on roles(:all) do
       execute "killall -q node; exit 0"
       # Capistrano kills the vallu server before it gets to start up if sleep 1 is not defined
-      execute "cd /home/web/digiroad2/current/; (nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 &); sleep 2"
+      execute "cd #{release_path} && (nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 &) && sleep 200"
     end
   end
 
-  after :publishing, :start_vallu_server
+  before :start, :start_vallu_server
 end
