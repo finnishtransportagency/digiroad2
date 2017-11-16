43852fa7b2454bf3aa9189186178665ee703be57
leirasf <faustino.leiras@cgi.com>
2017-11-16 18:30:48 +0200

    Replace && by ; in capistrano execute task

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ namespace :deploy do
     on roles(:all) do
       execute "killall -q node; exit 0"
       # Capistrano kills the vallu server before it gets to start up if sleep 1 is not defined
-      execute "cd /home/web/digiroad2/current/; nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 & sleep 2"
+      execute "cd /home/web/digiroad2/current/; (nohup grunt vallu-test-server >> ./src/main/webapp/vallu-server.log 2>&1 &); sleep 2"
     end
   end
 
