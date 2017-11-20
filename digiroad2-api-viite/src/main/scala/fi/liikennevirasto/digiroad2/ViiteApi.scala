9f222784b446229fec4c1e0f949382aadebb5ee4
Lourenço <pedro.m.lourenco@cgi.com>
2017-11-20 13:09:03 +0200

    VIITE-862 fixed variable name

--- digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
+++ digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
@@ -471,12 +471,12 @@ class ViiteApi(val roadLinkService: RoadLinkService, val vVHClient: VVHClient,
           val writableProject = projectWritable(data("projectId").asInstanceOf[Long])
           val splitLinks = writableProject.preSplitSuravageLink(data("linkId").asInstanceOf[Long], data("x").asInstanceOf[Double],
             data("y").asInstanceOf[Double],data("projectId").asInstanceOf[Long],user.username)
-          val splitToApi = Map(
+          val split = Map(
             "roadNumber" -> splitLinks.head.roadNumber,
             "roadPartNumber" -> splitLinks.head.roadPartNumber,
             "trackCode" -> splitLinks.head.track
           ) ++ splitLinks.map(splitToApi)
-          Map("success" -> splitLinks.nonEmpty, "split" -> splitToApi)
+          Map("success" -> splitLinks.nonEmpty, "split" -> split)
         } catch {
           case e: IllegalStateException => Map("success" -> false, "errorMessage" -> e.getMessage)
           case _: NumberFormatException => BadRequest("Missing mandatory data")
