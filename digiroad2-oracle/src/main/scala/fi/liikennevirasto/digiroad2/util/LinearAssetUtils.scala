961745c3c6ac3006bd12787d253be43a3fe8faa3
leirasf <faustino.leiras@cgi.com>
2017-12-04 21:02:58 +0200

    Add logs to road width server and simplify deleted road links

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/LinearAssetUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/LinearAssetUtils.scala
@@ -41,8 +41,7 @@ object LinearAssetUtils {
      Used by LinearAssetService and SpeedLimitService
    */
   def deletedRoadLinkIds(change: Seq[ChangeInfo], current: Seq[RoadLink]): Seq[Long] = {
-    change.filter(_.oldId.nonEmpty).flatMap(_.oldId).filterNot(id => current.exists(rl => rl.linkId == id)).
-      filterNot(id => change.exists(ci => ci.newId.getOrElse(0) == id))
+    change.filter(_.oldId.nonEmpty).filterNot(change => current.exists(rl => rl.linkId == change.oldId.getOrElse(0))).flatMap(_.oldId)
   }
 
   private def persistedLinearAssetToLinearAsset(persisted: PersistedLinearAsset) = {
