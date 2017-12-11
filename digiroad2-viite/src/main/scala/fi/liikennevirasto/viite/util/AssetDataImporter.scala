33c3050e85825a0b4a427ae0135e1c4954a90786
Lourenço <pedro.m.lourenco@cgi.com>
2017-12-11 16:20:03 +0200

    VIITE-936 batch changes

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/util/AssetDataImporter.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/util/AssetDataImporter.scala
@@ -280,96 +280,36 @@ class AssetDataImporter {
     addressPS.close()
   }
 
-  private def importRoadAddressHistoryData(conversionDatabase: DatabaseDef, vvhClient: VVHClient, ely: Int, importOptions: ImportOptions,
-                                    vvhClientProd: Option[VVHClient]): Unit = {
-    def printRow(r: (Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, String, Option[String], String, String, Long, Double, Double, Double, Double)): String ={
-      s"""linkid: %d, alku: %d, loppu: %d, tie: %d, aosa: %d, ajr: %d, ely: %d, tietyyppi: %d, jatkuu: %d, aet: %d, let: %d, alkupvm: %s, loppupvm: %s, kayttaja: %s, muutospvm or rekisterointipvm: %s""".
-        format(r._1, r._2, r._3, r._4, r._5, r._6, r._7, r._8, r._9, r._10, r._11, r._12, r._13, r._14, r._15)
-    }
-    // Adjust the LRM Positions so that the link is filled with full data
-    def adjust(lrmPos: Seq[LRMPos], length: Double): Seq[LRMPos] = {
-      val coefficient: Double = length / (lrmPos.map(_.endM).max - lrmPos.map(_.startM).min)
-      lrmPos.map(lrm => lrm.copy(startM = lrm.startM * coefficient, endM = lrm.endM * coefficient))
-    }
-    val roads = conversionDatabase.withDynSession {
-        sql"""select linkid, alku, loppu,
+  private def importRoadAddressHistoryData(conversionDatabase: DatabaseDef, elyData: (Long, List[RoadPart]), importOptions: ImportOptions): Unit = {
+
+    print(s"\n${DateTime.now()} - ")
+    val ely = elyData._1
+    val existingRoads = elyData._2
+    println("Read %d rows from conversion database for ELY %d".format(existingRoads.size, ely))
+
+    val roadHistory = conversionDatabase.withDynSession {
+      sql"""select linkid, alku, loppu,
             tie, aosa, ajr,
             ely, tietyyppi,
             jatkuu, aet, let,
             TO_CHAR(alkupvm, 'YYYY-MM-DD'), TO_CHAR(loppupvm, 'YYYY-MM-DD'),
             kayttaja, TO_CHAR(COALESCE(muutospvm, rekisterointipvm), 'YYYY-MM-DD'), linkid * 10000 + ajr*1000 + aet as id,
             alkux, alkuy, loppux, loppuy
-            from VVH_TIEHISTORIA_HEINA2017 WHERE ely=$ely and TO_CHAR(loppupvm, 'YYYY-MM-DD') <= '2017-11-19'""".as[(Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, String, Option[String], String, String, Long, Double, Double, Double, Double)].list
+            from VVH_TIEHISTORIA_HEINA2017 WHERE ely=${elyData._1} AND TO_CHAR(loppupvm, 'YYYY-MM-DD') <= '2017-11-19'""".
+        as[(Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, Long, String, Option[String], String, String, Long, Double, Double, Double, Double)].list
     }
 
     print(s"\n${DateTime.now()} - ")
-    println("Read %d rows from conversion database for ELY %d".format(roads.size, ely))
-    val lrmList = roads.map(r => LRMPos(r._16, r._1, r._2.toDouble, r._3.toDouble)).groupBy(_.linkId) // linkId -> (id, linkId, startM, endM)
-    val addressList = roads.map(r => r._16 -> (r._4, r._5, r._6, r._7, r._8, r._9, r._10, r._11, r._12, r._13, r._14, r._15, r._17, r._18, r._19, r._20)).toMap
+    println("Read %d rows from conversion database for ELY %d".format(roadHistory.size, ely))
+    val lrmList = roadHistory.map(r => LRMPos(r._16, r._1, r._2.toDouble, r._3.toDouble)) // linkId -> (id, linkId, startM, endM)
+    val addressList = roadHistory.map(r => r._16 -> (r._4, r._5, r._6, r._7, r._8, r._9, r._10, r._11, r._12, r._13, r._14, r._15, r._17, r._18, r._19, r._20)).toMap
 
     print(s"${DateTime.now()} - ")
-    println("Total of %d link ids".format(lrmList.keys.size))
-    val linkIdSet = lrmList.keys.toSet // Mapping LinkId -> Id
-
-    //val vvhRoadLinkClient = if (importOptions.useFrozenLinkService) vvhClient.frozenTimeRoadLinkData else vvhClient.roadLinkData
-    val roadLinks = linkIdSet.grouped(4000).flatMap(group =>
-      // DEV complementary link load
-      if (importOptions.onlyComplementaryLinks)
-      // Import only complementary links
-        vvhClientProd.getOrElse(vvhClient).complementaryData.fetchByLinkIds(group)
-      else {
-        // If in production or QA environment -> load complementary links, too
-        if (vvhClientProd.isEmpty)
-          vvhClient.roadLinkData.fetchByLinkIds(group) ++ vvhClient.complementaryData.fetchByLinkIds(group)
-        else
-        // If in DEV environment -> don't load complementary links at this stage (no data for them)
-          vvhClientProd.get.roadLinkData.fetchByLinkIds(group) ++ vvhClientProd.get.complementaryData.fetchByLinkIds(group)
-      }
-    ).toSeq
-
-    val linkLengths = roadLinks.map{
-      roadLink =>
-        roadLink.linkId -> GeometryUtils.geometryLength(roadLink.geometry)
-    }.toMap
 
     print(s"${DateTime.now()} - ")
-    println("Read %d road links from vvh".format(linkLengths.size))
-
-    val floatingLinks = vvhClient.historyData.fetchVVHRoadLinkByLinkIds(roads.filterNot(r => linkLengths.get(r._1).isDefined).map(_._1).toSet).groupBy(_.linkId).mapValues(_.maxBy(_.endDate))
-    print(s"${DateTime.now()} - ")
-    println(floatingLinks.size + " links can be saved as floating addresses")
-
-    roads.filterNot(r => linkLengths.get(r._1).isDefined || floatingLinks.get(r._1).nonEmpty).foreach{
-      row => println("Suppressed row ID %d with reason 1: 'LINK-ID is not found in the VVH Interface' %s".format(row._16, printRow(row)))
-    }
-
-    val allLinkLengths = linkLengths ++ floatingLinks.mapValues(x => GeometryUtils.geometryLength(x.geometry))
-
-    val lrmPositions = allLinkLengths.flatMap {
-      case (linkId, length) => adjust(lrmList.getOrElse(linkId, List()), length)
-    }
 
     print(s"${DateTime.now()} - ")
-    println("%d segments with invalid link id removed".format(roads.filterNot(r => linkLengths.get(r._1).isDefined).size))
-
-    val linkIdMapping: Map[Long,Long] = if (vvhClientProd.nonEmpty) {
-      print(s"${DateTime.now()} - ")
-      println("Converting link ids to DEV link ids")
-      val mmlIdMaps = roadLinks.filter(_.attributes.get("MTKID").nonEmpty).map(rl => rl.attributes("MTKID").asInstanceOf[BigInt].longValue() -> rl.linkId).toMap
-      val links = mmlIdMaps.keys.toSet.grouped(4000).flatMap(grp => vvhClient.roadLinkData.fetchByMmlIds(grp)).toSeq
-      val fromMmlIdMap = links.map(rl => rl.attributes("MTKID").asInstanceOf[BigInt].longValue() -> rl.linkId).toMap
-      val (differ, same) = fromMmlIdMap.map { case (mmlId, devLinkId) =>
-        mmlIdMaps(mmlId) -> devLinkId
-      }.partition { case (pid, did) => pid != did }
-      print(s"${DateTime.now()} - ")
-      println("Removing the non-differing mmlId+linkId combinations from mapping: %d road links".format(same.size))
-      differ
-    } else {
-      Map()
-    }
-
-    print(s"${DateTime.now()} - ")
-    println("Link mapping contains %d entries".format(linkIdMapping.size))
+    //println("Link mapping contains %d entries".format(linkIdMapping.size))
 
     val lrmPositionPS = dynamicSession.prepareStatement("insert into lrm_position (ID, link_id, SIDE_CODE, start_measure, end_measure) values (?, ?, ?, ?, ?)")
     val addressPS = dynamicSession.prepareStatement("insert into ROAD_ADDRESS (id, lrm_position_id, road_number, road_part_number, " +
@@ -377,10 +317,10 @@ class AssetDataImporter {
       "VALID_FROM, geometry, floating, road_type, ely) values (viite_general_seq.nextval, ?, ?, ?, ?, ?, ?, ?, TO_DATE(?, 'YYYY-MM-DD'), " +
       "TO_DATE(?, 'YYYY-MM-DD'), ?, TO_DATE(?, 'YYYY-MM-DD'), MDSYS.SDO_GEOMETRY(4002, 3067, NULL, MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1), MDSYS.SDO_ORDINATE_ARRAY(" +
       "?,?,0.0,0.0,?,?,0.0,?)), ?, ?, ?)")
-    val ids = sql"""SELECT lrm_position_primary_key_seq.nextval FROM dual connect by level <= ${lrmPositions.size}""".as[Long].list
+    val ids = sql"""SELECT lrm_position_primary_key_seq.nextval FROM dual connect by level <= ${lrmList.size}""".as[Long].list
     val df = new DecimalFormat("#.###")
-    assert(ids.size == lrmPositions.size || lrmPositions.isEmpty)
-    lrmPositions.zip(ids).foreach { case ((pos), (lrmId)) =>
+    assert(ids.size == lrmList.size || lrmList.isEmpty)
+    lrmList.zip(ids).foreach { case ((pos), (lrmId)) =>
       assert(addressList.get(pos.id).size == 1)
       val address = addressList.get(pos.id).head
       val (startAddrM, endAddrM, sideCode) = if (address._7 < address._8) {
@@ -393,10 +333,10 @@ class AssetDataImporter {
       else
         (address._15, address._16, address._13, address._14)
 
-      println(s"insert into lrm_position (ID, link_id, SIDE_CODE, start_measure, end_measure) values ($lrmId, ${linkIdMapping.getOrElse(pos.linkId, pos.linkId)}, $sideCode, ${df.format(pos.startM).toDouble}, ${df.format(pos.endM).toDouble});")
+      println(s"insert into lrm_position (ID, link_id, SIDE_CODE, start_measure, end_measure) values ($lrmId, ${pos.linkId}, $sideCode, ${df.format(pos.startM).toDouble}, ${df.format(pos.endM).toDouble});")
 
       lrmPositionPS.setLong(1, lrmId)
-      lrmPositionPS.setLong(2, linkIdMapping.getOrElse(pos.linkId, pos.linkId))
+      lrmPositionPS.setLong(2, pos.linkId)
       lrmPositionPS.setLong(3, sideCode)
       lrmPositionPS.setDouble(4, df.format(pos.startM).toDouble)
       lrmPositionPS.setDouble(5, df.format(pos.endM).toDouble)
@@ -417,7 +357,7 @@ class AssetDataImporter {
       addressPS.setDouble(14, x2)
       addressPS.setDouble(15, y2)
       addressPS.setDouble(16, endAddrM - startAddrM)
-      addressPS.setInt(17, if (floatingLinks.contains(pos.linkId)) 1 else 0)
+      addressPS.setInt(17, 0)
       addressPS.setLong(18, address._5)
       addressPS.setLong(19, address._4)
       addressPS.addBatch()
@@ -482,9 +422,26 @@ class AssetDataImporter {
 
   def importRoadAddressHistory(conversionDatabase: DatabaseDef, vvhClient: VVHClient, vvhClientProd: Option[VVHClient],
                             importOptions: ImportOptions): Unit = {
-    val roadMaintainerElys = Seq(0, 1, 2, 3, 4, 8, 9, 10, 12, 14)
+    //val roadMaintainerElys = Seq(0, 1, 2, 3, 4, 8, 9, 10, 12, 14)
 
-    OracleDatabase.withDynTransaction {
+    //Get all current roads
+    val currentRoadsForEly = OracleDatabase.withDynTransaction {
+      sql"""SELECT ROAD_NUMBER, ROAD_PART_NUMBER, ELY FROM ROAD_ADDRESS WHERE END_DATE IS NULL""".as[(Long, Long, Long)].list.map{
+        case (roadNumber, roadPart, ely) => RoadPart(roadNumber, roadPart, ely)
+      }.groupBy(_.ely)
+    }
+
+    //Delete all the previous road history
+    OracleDatabase.withDynTransaction{
+      sqlu"""ALTER TABLE ROAD_ADDRESS DISABLE ALL TRIGGERS""".execute
+      sqlu"""DELETE FROM ROAD_ADDRESS WHERE END_DATE IS NOT NULL""".execute
+      sqlu"""DELETE FROM LRM_POSITION WHERE
+            NOT EXISTS (SELECT LRM_POSITION_ID FROM PROJECT_LINK WHERE LRM_POSITION_ID=LRM_POSITION.ID) AND
+            NOT EXISTS (SELECT LRM_POSITION_ID FROM PROJECT_LINK_HISTORY WHERE LRM_POSITION_ID=LRM_POSITION.ID) AND
+            NOT EXISTS (SELECT POSITION_ID FROM ASSET_LINK WHERE POSITION_ID=LRM_POSITION.ID)""".execute
+    }
+
+    /*OracleDatabase.withDynTransaction {
       sqlu"""ALTER TABLE ROAD_ADDRESS DISABLE ALL TRIGGERS""".execute
       sqlu"""DELETE FROM ROAD_ADDRESS""".execute
       sqlu"""DELETE FROM LRM_POSITION WHERE
@@ -492,17 +449,18 @@ class AssetDataImporter {
             NOT EXISTS (SELECT LRM_POSITION_ID FROM PROJECT_LINK_HISTORY WHERE LRM_POSITION_ID=LRM_POSITION.ID) AND
             NOT EXISTS (SELECT POSITION_ID FROM ASSET_LINK WHERE POSITION_ID=LRM_POSITION.ID)""".execute
       println (s"${DateTime.now ()} - Old address data removed")
-
-      roadMaintainerElys.foreach(ely => importRoadAddressHistoryData(conversionDatabase, vvhClient, ely, importOptions, vvhClientProd))
+*/
+    currentRoadsForEly.map(elyData => importRoadAddressHistoryData(conversionDatabase, elyData, importOptions))
+      //roadMaintainerElys.foreach(ely => importRoadAddressHistoryData(conversionDatabase, currentRoads, importOptions, vvhClientProd))
       // If running in DEV environment then include some testing complementary links
-      if (vvhClientProd.nonEmpty)
+      /*if (vvhClientProd.nonEmpty)
         roadMaintainerElys.foreach(ely => importRoadAddressHistoryData(conversionDatabase, vvhClient, ely, importOptions.copy(onlyComplementaryLinks = true), None))
+*/
+  //    println(s"${DateTime.now()} - Updating geometry adjustment timestamp to ${importOptions.geometryAdjustedTimeStamp}")
+  //    sqlu"""UPDATE LRM_POSITION
+  //      SET ADJUSTED_TIMESTAMP = ${importOptions.geometryAdjustedTimeStamp} WHERE ID IN (SELECT LRM_POSITION_ID FROM ROAD_ADDRESS)""".execute
 
-      println(s"${DateTime.now()} - Updating geometry adjustment timestamp to ${importOptions.geometryAdjustedTimeStamp}")
-      sqlu"""UPDATE LRM_POSITION
-        SET ADJUSTED_TIMESTAMP = ${importOptions.geometryAdjustedTimeStamp} WHERE ID IN (SELECT LRM_POSITION_ID FROM ROAD_ADDRESS)""".execute
-
-      println(s"${DateTime.now()} - Updating calibration point information")
+      //println(s"${DateTime.now()} - Updating calibration point information")
       // both dates are open-ended or there is overlap (checked with inverse logic)
       sqlu"""UPDATE ROAD_ADDRESS
         SET CALIBRATION_POINTS = 1
@@ -527,7 +485,6 @@ class AssetDataImporter {
               )
             )""".execute
       sqlu"""ALTER TABLE ROAD_ADDRESS ENABLE ALL TRIGGERS""".execute
-    }
   }
 
   def updateRoadAddressesValues(conversionDatabase: DatabaseDef, vvhClient: VVHClient) = {
@@ -710,4 +667,5 @@ class AssetDataImporter {
 }
 
 case class ImportOptions(onlyComplementaryLinks: Boolean, useFrozenLinkService: Boolean, geometryAdjustedTimeStamp: Long)
+case class RoadPart(roadNumber: Long, roadPart: Long, ely: Long)
 
