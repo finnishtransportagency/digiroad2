a64faf88b41cf344a77a420acd314e8d3729274c
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2017-12-12 18:18:24 +0200

    Added the related messages.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
@@ -1516,28 +1516,36 @@ object ProjectValidator {
     val values = Set(minorDiscontinuityFound, majorDiscontinuityFound, insufficientTrackCoverage, discontinuousAddressScheme,
       sharedLinkIdsExist, noContinuityCodesAtEnd, unsuccessfulRecalculation,missingEndOfRoad)
     //Viite-942
-    case object missingEndOfRoad extends ValidationError {def value = 0; def message = "<TO_BE_DETERMINED>"}
+    case object missingEndOfRoad extends ValidationError {def value = 0;
+      def message = "Tieosalle ei ole määritelty jatkuvuuskoodia, 1 - Tien loppu, tieosan viimeiselle linkille."}
     //Viite-453
     //There must be a minor discontinuity if the jump is longer than 0.1 m (10 cm) between road links
-    case object minorDiscontinuityFound extends ValidationError {def value = 1; def message = "<TO_BE_DETERMINED>"}
+    case object minorDiscontinuityFound extends ValidationError {def value = 1;
+      def message = "Tieosalla on lievä epäjatkuvuus. Määrittele jatkuvuuskoodi oikein kyseiselle linkille."}
     //Viite-453
     //There must be a major discontinuity if the jump is longer than 50 meters
-    case object majorDiscontinuityFound extends ValidationError {def value = 2;def message = "<TO_BE_DETERMINED>"}
+    case object majorDiscontinuityFound extends ValidationError {def value = 2;
+      def message = "Tieosalla on epäjatkuvuus. Määrittele jatkuvuuskoodi oikein kyseiselle linkille."}
     //Viite-453
     //For every track 1 there must exist track 2 that covers the same address span and vice versa
-    case object insufficientTrackCoverage extends ValidationError {def value = 3;def message = "<TO_BE_DETERMINED>"}
+    case object insufficientTrackCoverage extends ValidationError {def value = 3;
+      def message = "Tieosalta puuttuu toinen ajorata. Numeroi molemmat ajoradat."}
     //Viite-453
     //There must be a continuous road addressing scheme so that all values from 0 to the highest number are covered
-    case object discontinuousAddressScheme extends ValidationError {def value = 4;def message = "<TO_BE_DETERMINED>"}
+    case object discontinuousAddressScheme extends ValidationError {def value = 4;
+      def message = "Tieosoitteessa ei voi olla puuttuvia etäisyyksiä alku- ja loppuetäisyyden välillä."}
     //Viite-453
     //There are no link ids shared between the project and the current road address + lrm_position tables at the project date (start_date, end_date)
-    case object sharedLinkIdsExist extends ValidationError {def value = 5;def message = "<TO_BE_DETERMINED>"}
+    case object sharedLinkIdsExist extends ValidationError {def value = 5;
+      def message = "Linkillä on voimassa oleva tieosoite tämän projektin alkupäivämäärällä."}
     //Viite-453
     //Continuity codes are given for end of road
-    case object noContinuityCodesAtEnd extends ValidationError {def value = 6;def message = "<TO_BE_DETERMINED>"}
+    case object noContinuityCodesAtEnd extends ValidationError {def value = 6;
+      def message = "Tieosan lopusta puuttuu jatkuvuuskoodi."}
     //Viite-453
     //Recalculation of M values and delta calculation are both successful for every road part in project
-    case object unsuccessfulRecalculation extends ValidationError {def value = 7;def message = "<TO_BE_DETERMINED>"}
+    case object unsuccessfulRecalculation extends ValidationError {def value = 7;
+      def message = "Etäisyysarvojen laskenta epäonnistui."}
     def apply(intValue: Int): ValidationError = {
       values.find(_.value == intValue).get
     }
