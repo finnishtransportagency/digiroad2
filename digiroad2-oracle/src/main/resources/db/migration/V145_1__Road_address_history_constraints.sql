97f90e754a5890ae8f6b6236557a6db80ecd65fd
Lourenço <pedro.m.lourenco@cgi.com>
2017-12-15 13:29:50 +0200

    VIITE-937 added ely to index

--- digiroad2-oracle/src/main/resources/db/migration/V145_1__Road_address_history_constraints.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V145_1__Road_address_history_constraints.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX ROAD_ADDRESS_DUPLICATE_CHECK ON ROAD_ADDRESS (road_number, road_part_number, track_code, start_date, end_date, start_addr_m, end_addr_m, valid_from, valid_to, terminated, discontinuity, road_type);
+CREATE UNIQUE INDEX ROAD_ADDRESS_DUPLICATE_CHECK ON ROAD_ADDRESS (road_number, road_part_number, track_code, start_date, end_date, start_addr_m, end_addr_m, valid_from, valid_to, terminated, discontinuity, road_type, ely);
