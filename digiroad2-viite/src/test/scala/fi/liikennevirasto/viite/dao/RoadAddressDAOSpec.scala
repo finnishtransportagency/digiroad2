3d8fe0d0de6bc13dc84fb01108d9e438eefb84e0
Lourenço <pedro.m.lourenco@cgi.com>
2017-12-15 16:22:33 +0200

    VIITE-937 interception for error code in test

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/RoadAddressDAOSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/RoadAddressDAOSpec.scala
@@ -1,5 +1,7 @@
 package fi.liikennevirasto.viite.dao
 
+import java.sql.SQLException
+
 import com.github.tototoshi.slick.MySQLJodaSupport._
 import fi.liikennevirasto.digiroad2.asset.LinkGeomSource.ComplimentaryLinkInterface
 import fi.liikennevirasto.digiroad2.asset.{BoundingRectangle, LinkGeomSource, SideCode}
@@ -33,21 +35,17 @@ class RoadAddressDAOSpec extends FunSuite with Matchers {
   }
 
   test("insert roadaddress duplicate info check") {
-    var errored: Option[Boolean] = None
-    try {
       runWithRollback {
+        val error = intercept[SQLException] {
         sqlu"""
       Insert into LRM_POSITION (ID,LANE_CODE,SIDE_CODE,START_MEASURE,END_MEASURE,MML_ID,LINK_ID,ADJUSTED_TIMESTAMP,MODIFIED_DATE,LINK_SOURCE) values (lrm_position_primary_key_seq.nextval,null,'2','0','21,021',null,'1111102483','1476392565000',to_timestamp('17.09.15 19:39:30,365338000','RR.MM.DD HH24:MI:SS,FF'),'1')""".execute
         sqlu""" Insert into ROAD_ADDRESS (ID,ROAD_NUMBER,ROAD_PART_NUMBER,TRACK_CODE,DISCONTINUITY,START_ADDR_M,END_ADDR_M,LRM_POSITION_ID,START_DATE,END_DATE,CREATED_BY,VALID_FROM,CALIBRATION_POINTS,FLOATING,GEOMETRY,VALID_TO, ROAD_TYPE, ELY) values (viite_general_seq.nextval,'1010','1','0','5','627','648',lrm_position_primary_key_seq.currval,to_date('63.01.01','RR.MM.DD'),null,'tr',to_date('98.10.16','RR.MM.DD'),'0','0',MDSYS.SDO_GEOMETRY(4002,3067,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(288781.428,6825565.909,0,0,288763.118,6825576.235,0,21)),null, 1, 4)""".execute
-        errored = Some(true)
         sqlu"""
       Insert into LRM_POSITION (ID,LANE_CODE,SIDE_CODE,START_MEASURE,END_MEASURE,MML_ID,LINK_ID,ADJUSTED_TIMESTAMP,MODIFIED_DATE,LINK_SOURCE) values (lrm_position_primary_key_seq.nextval,null,'2','0','21,021',null,'1111102483','1476392565000',to_timestamp('17.09.15 19:39:30,365338000','RR.MM.DD HH24:MI:SS,FF'),'1')""".execute
         sqlu""" Insert into ROAD_ADDRESS (ID,ROAD_NUMBER,ROAD_PART_NUMBER,TRACK_CODE,DISCONTINUITY,START_ADDR_M,END_ADDR_M,LRM_POSITION_ID,START_DATE,END_DATE,CREATED_BY,VALID_FROM,CALIBRATION_POINTS,FLOATING,GEOMETRY,VALID_TO, ROAD_TYPE, ELY) values (viite_general_seq.nextval,'1010','1','0','5','627','648',lrm_position_primary_key_seq.currval,to_date('63.01.01','RR.MM.DD'),null,'tr',to_date('98.10.16','RR.MM.DD'),'0','0',MDSYS.SDO_GEOMETRY(4002,3067,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(288781.428,6825565.909,0,0,288763.118,6825576.235,0,21)),null, 1, 4)""".execute
-        errored = Some(false)
+        }
+        error.getErrorCode should be (20002)
       }
-    } catch {
-      case NonFatal(e) => errored should be(Some(true))
-    }
   }
 
 
