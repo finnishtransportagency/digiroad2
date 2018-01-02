bf557fde904ad08b23aeabfc350d6082add3f033
estrompah <hugo.estrompa@cgi.com>
2018-01-02 16:07:35 +0200

    changing the way of filter lrm_position id

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/util/AssetDataImporter.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/util/AssetDataImporter.scala
@@ -355,7 +355,7 @@ class AssetDataImporter {
     val df = new DecimalFormat("#.###")
     assert(ids.size == lrmAddresses.size || lrmAddresses.isEmpty)
     lrmAddresses.zip(ids).foreach { case ((pos), (lrmId)) =>
-      val address = roadList.find(r => r.lrmId == pos.id && r.startM == pos.startM && r.endM == pos.endM).get
+      val address = roadList.find(r => lrmId == pos.id).get
       val (startAddrM, endAddrM, sideCode) = if (address.startAddrM < address.endAddrM) {
         (address.startAddrM, address.endAddrM, SideCode.TowardsDigitizing.value)
       } else {
