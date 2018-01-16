0261882c5d1d9fcba95e49aa7a27f90da114dabd
Campanha <ricardo.campanha.marques@cgi.com>
2018-01-16 14:46:43 +0200

    VIITE-472 1. Added new method for geometry length calculation. 2. Throw warning if new endAddrM value for roadPart is minor or bigger than 5% of the real road part length (by geometry)

--- viite-UI/src/model/ProjectCollection.js
+++ viite-UI/src/model/ProjectCollection.js
@@ -20,6 +20,7 @@
     var UNAUTHORIZED_401 = 401;
     var PRECONDITION_FAILED_412 = 412;
     var INTERNAL_SERVER_ERROR_500 = 500;
+    var allowedAddrMValuePercentage = 0.05;
 
     var projectLinks = function() {
       return _.flatten(fetchedProjectLinks);
@@ -230,6 +231,21 @@
     };
 
     this.saveProjectLinks = function(changedLinks, statusCode) {
+      var thisone = this;
+      var validUserGivenAddrMValues = function(linkId, userEndAddr){
+        if(_.isUndefined(userEndAddr))
+          return false;
+          var roadPartIds = self.getMultiSelectIds(linkId);
+          var roadPartLinks = self.getByLinkId(roadPartIds);
+          var sortedGeometries = _.map(_.sortBy((roadPartLinks), function(link){return link.getData().endAddressM;}), function(roadPart){
+            return roadPart.getData().points;
+          });
+          var roadPartLength = _.reduce((sortedGeometries), function(length, geom){
+            return GeometryUtils.geometryLength(geom) + length;
+          }, 0.0);
+        return (userEndAddr >= (roadPartLength * (1 - allowedAddrMValuePercentage))) && (userEndAddr <= (roadPartLength * (1 + allowedAddrMValuePercentage)));
+      };
+
       applicationModel.addSpinner();
       var linkIds = _.unique(_.map(changedLinks,function (t){
         if(!_.isUndefined(t.linkId)){
@@ -261,37 +277,52 @@
       if(!isNaN(endDistance) && !isNaN(originalEndDistance) && originalEndDistance !== endDistance){
         dataJson.userDefinedEndAddressM = endDistance;
       }
-      if(!_.isEmpty(linkIds) && typeof projectId !== 'undefined' && projectId !== 0){
-        var ids = _.chain(changedLinks).map(function (cl) {
-          return cl.id;
-        }).uniq().value();
-        if(statusCode == LinkStatus.New.value && ids.length === 1 && ids[0] === 0){
-          backend.createProjectLinks(dataJson, function(successObject) {
-            if (!successObject.success) {
-              new ModalConfirm(successObject.errormessage);
-              applicationModel.removeSpinner();
-            } else {
-              publishableProject = successObject.publishable;
-              eventbus.trigger('projectLink:projectLinksCreateSuccess');
-              eventbus.trigger('roadAddress:projectLinksCreateSuccess');
-            }
-          });
-        }
-        else {
-          backend.updateProjectLinks(dataJson, function (successObject) {
-            if (!successObject.success) {
-              new ModalConfirm(successObject.errormessage);
-              applicationModel.removeSpinner();
-            } else {
-              publishableProject = successObject.publishable;
-              projectErrors = successObject.projectErrors;
-              eventbus.trigger('roadAddress:projectLinksUpdated', successObject);
-            }
-          });
-        }
-      } else {
-        eventbus.trigger('roadAddress:projectLinksUpdateFailed', PRECONDITION_FAILED_412);
+
+      if(!validUserGivenAddrMValues(_.first(dataJson.linkIds), dataJson.userDefinedEndAddressM)){
+        new GenericConfirmPopup("Antamasi pituus eroaa yli 5% prosenttia geometrian pituudesta, haluatko varmasti tallentaa tämän pituuden?", {
+          successCallback: function () {
+            thisone.createOrUpdate();
+          },
+          closeCallback: function () {
+            applicationModel.removeSpinner();
+          }
+        });
+      } else{
+        thisone.createOrUpdate();
       }
+      this.createOrUpdate = function(){
+        if(!_.isEmpty(linkIds) && typeof projectId !== 'undefined' && projectId !== 0){
+          var ids = _.chain(changedLinks).map(function (cl) {
+            return cl.id;
+          }).uniq().value();
+          if(statusCode == LinkStatus.New.value && ids.length === 1 && ids[0] === 0){
+            backend.createProjectLinks(dataJson, function(successObject) {
+              if (!successObject.success) {
+                new ModalConfirm(successObject.errormessage);
+                applicationModel.removeSpinner();
+              } else {
+                publishableProject = successObject.publishable;
+                eventbus.trigger('projectLink:projectLinksCreateSuccess');
+                eventbus.trigger('roadAddress:projectLinksCreateSuccess');
+              }
+            });
+          }
+          else {
+            backend.updateProjectLinks(dataJson, function (successObject) {
+              if (!successObject.success) {
+                new ModalConfirm(successObject.errormessage);
+                applicationModel.removeSpinner();
+              } else {
+                publishableProject = successObject.publishable;
+                projectErrors = successObject.projectErrors;
+                eventbus.trigger('roadAddress:projectLinksUpdated', successObject);
+              }
+            });
+          }
+        } else {
+          eventbus.trigger('roadAddress:projectLinksUpdateFailed', PRECONDITION_FAILED_412);
+        }
+      };
     };
 
     this.preSplitProjectLinks = function(suravage, nearestPoint){
