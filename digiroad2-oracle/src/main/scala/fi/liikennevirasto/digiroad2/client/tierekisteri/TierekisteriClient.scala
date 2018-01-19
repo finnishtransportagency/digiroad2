dd4e945440fcec3b61646689e164e582870caa74
leirasf <faustino.leiras@cgi.com>
2018-01-19 14:28:06 +0200

    DROTH-1128 Change TierekisteriTrafficVolumeAssetClient to a new scala file

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/tierekisteri/TierekisteriClient.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/tierekisteri/TierekisteriClient.scala
@@ -136,9 +136,6 @@ object TRLaneArrangementType {
   case object Unknown extends TRLaneArrangementType { def value = 99; }
 }
 
-case class TierekisteriTrafficData(roadNumber: Long, startRoadPartNumber: Long, endRoadPartNumber: Long,
-                                   track: Track, startAddressMValue: Long, endAddressMValue: Long, assetValue: Int) extends TierekisteriAssetData
-
 case class TierekisteriRoadWidthData(roadNumber: Long, startRoadPartNumber: Long, endRoadPartNumber: Long,
                                      track: Track, startAddressMValue: Long, endAddressMValue: Long, assetValue: Int) extends TierekisteriAssetData
 
@@ -336,29 +333,6 @@ trait TierekisteriClient{
     if (fieldValue.isEmpty)
       throw new TierekisteriClientException("Missing mandatory field in response '%s'".format(field))
     fieldValue
-  }
-}
-
-class TierekisteriTrafficVolumeAssetClient(trEndPoint: String, trEnable: Boolean, httpClient: CloseableHttpClient) extends TierekisteriAssetDataClient {
-  override def tierekisteriRestApiEndPoint: String = trEndPoint
-  override def tierekisteriEnabled: Boolean = trEnable
-  override def client: CloseableHttpClient = httpClient
-  type TierekisteriType = TierekisteriTrafficData
-
-  override val trAssetType = "tl201"
-  private val trKVL = "KVL"
-
-  override def mapFields(data: Map[String, Any]): Option[TierekisteriTrafficData] = {
-    //Mandatory field
-    val assetValue = convertToInt(getMandatoryFieldValue(data, trKVL)).get
-    val roadNumber = convertToLong(getMandatoryFieldValue(data, trRoadNumber)).get
-    val roadPartNumber = convertToLong(getMandatoryFieldValue(data, trRoadPartNumber)).get
-    val endRoadPartNumber = convertToLong(getMandatoryFieldValue(data, trEndRoadPartNumber)).getOrElse(roadPartNumber)
-    val startMValue = convertToLong(getMandatoryFieldValue(data, trStartMValue)).get
-    val endMValue = convertToLong(getMandatoryFieldValue(data, trEndMValue)).get
-    val track = convertToInt(getMandatoryFieldValue(data, trTrackCode)).map(Track.apply).getOrElse(Track.Unknown)
-
-    Some(TierekisteriTrafficData(roadNumber, roadPartNumber, endRoadPartNumber, track, startMValue, endMValue, assetValue))
   }
 }
 
