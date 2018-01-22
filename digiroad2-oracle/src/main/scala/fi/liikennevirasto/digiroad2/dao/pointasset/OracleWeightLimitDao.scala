4d2f039f14375c858791ebd0c884d02c463b1083
marquesfm <francisco.marques@cgi.com>
2018-01-22 13:16:06 +0200

    DROTH-1120 Service: Extend PointAssetOperations for new point weight limit - Created WeightLimit Service with new assets (Weight limit, Axel weight limit,  Bogie weight limit, Trailer truck weight limit) extending the PointAssetOperations; - Created WeightLimit DAO with fetchByFilter method.

---
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/pointasset/OracleWeightLimitDao.scala
@@ -0,0 +1,58 @@
+package fi.liikennevirasto.digiroad2.dao.pointasset
+
+import fi.liikennevirasto.digiroad2.dao.Queries._
+import fi.liikennevirasto.digiroad2.{PersistedPointAsset, Point}
+import org.joda.time.DateTime
+import slick.driver.JdbcDriver.backend.Database
+import Database.dynamicSession
+import fi.liikennevirasto.digiroad2.asset.LinkGeomSource
+import fi.liikennevirasto.digiroad2.dao.Sequences
+import slick.jdbc.StaticQuery.interpolation
+import slick.jdbc.{GetResult, PositionedResult, StaticQuery}
+import com.github.tototoshi.slick.MySQLJodaSupport._
+
+case class WeightLimit(id: Long, linkId: Long,
+                              lon: Double, lat: Double,
+                              mValue: Double, floating: Boolean,
+                              vvhTimeStamp: Long,
+                              municipalityCode: Int,
+                              createdBy: Option[String] = None,
+                              createdAt: Option[DateTime] = None,
+                              modifiedBy: Option[String] = None,
+                              modifiedAt: Option[DateTime] = None,
+                              linkSource: LinkGeomSource) extends PersistedPointAsset
+
+object OracleWeightLimitDao {
+  def fetchByFilter(queryFilter: String => String): Seq[WeightLimit] = {
+
+    val query =
+      """
+        select a.id, pos.link_id, a.geometry, pos.start_measure, a.floating, pos.adjusted_timestamp, a.municipality_code, a.created_by, a.created_date, a.modified_by, a.modified_date,
+        pos.link_source
+        from asset a
+        join asset_link al on a.id = al.asset_id
+        join lrm_position pos on al.position_id = pos.id
+      """
+    val queryWithFilter = queryFilter(query) + " and (a.valid_to > sysdate or a.valid_to is null)"
+    StaticQuery.queryNA[WeightLimit](queryWithFilter).iterator.toSeq
+  }
+
+  implicit val getPointAsset = new GetResult[WeightLimit] {
+    def apply(r: PositionedResult) = {
+      val id = r.nextLong()
+      val linkId = r.nextLong()
+      val point = r.nextBytesOption().map(bytesToPoint).get
+      val mValue = r.nextDouble()
+      val floating = r.nextBoolean()
+      val vvhTimeStamp = r.nextLong()
+      val municipalityCode = r.nextInt()
+      val createdBy = r.nextStringOption()
+      val createdDateTime = r.nextTimestampOption().map(timestamp => new DateTime(timestamp))
+      val modifiedBy = r.nextStringOption()
+      val modifiedDateTime = r.nextTimestampOption().map(timestamp => new DateTime(timestamp))
+      val linkSource = r.nextInt()
+
+      WeightLimit(id, linkId, point.x, point.y, mValue, floating, vvhTimeStamp, municipalityCode, createdBy, createdDateTime, modifiedBy, modifiedDateTime, linkSource = LinkGeomSource(linkSource))
+    }
+  }
+}
---
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/pointasset/WeightLimitService.scala
@@ -0,0 +1,118 @@
+package fi.liikennevirasto.digiroad2.service.pointasset
+
+import fi.liikennevirasto.digiroad2._
+import fi.liikennevirasto.digiroad2.asset.LinkGeomSource
+import fi.liikennevirasto.digiroad2.dao.pointasset.{WeightLimit, OracleWeightLimitDao}
+import fi.liikennevirasto.digiroad2.linearasset.{RoadLink, RoadLinkLike}
+import fi.liikennevirasto.digiroad2.service.RoadLinkService
+
+case class IncomingWeightLimit(lon: Double, lat: Double, linkId: Long) extends IncomingPointAsset
+case class IncomingAxelWeightLimit(lon: Double, lat: Double, linkId: Long) extends IncomingPointAsset
+case class IncomingBogieWeightLimit(lon: Double, lat: Double, linkId: Long) extends IncomingPointAsset
+case class IncomingTrailerTruckWeightLimit(lon: Double, lat: Double, linkId: Long) extends IncomingPointAsset
+
+
+class WeightLimitService(val roadLinkService: RoadLinkService) extends PointAssetOperations {
+  type IncomingAsset = IncomingWeightLimit
+  type PersistedAsset = WeightLimit
+
+  override def typeId: Int = 320
+
+  override def setAssetPosition(asset: IncomingWeightLimit, geometry: Seq[Point], mValue: Double) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def update(id: Long, updatedAsset: IncomingWeightLimit, geometry: Seq[Point], municipality: Int, username: String, linkSource: LinkGeomSource) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def setFloating(persistedAsset: WeightLimit, floating: Boolean) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def fetchPointAssets(queryFilter: (String) => String, roadLinks: Seq[RoadLinkLike]): Seq[WeightLimit] = {
+    OracleWeightLimitDao.fetchByFilter(queryFilter)
+  }
+
+  override def create(asset: IncomingWeightLimit, username: String, roadLink: RoadLink) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def toIncomingAsset(asset: IncomePointAsset, link: RoadLink): Option[IncomingWeightLimit] = {
+    GeometryUtils.calculatePointFromLinearReference(link.geometry, asset.mValue).map {
+      point => IncomingWeightLimit(point.x, point.y, link.linkId)
+    }
+  }
+}
+
+
+class AxelWeightLimitService(val roadLinkService: RoadLinkService) extends PointAssetOperations {
+  type IncomingAsset = IncomingAxelWeightLimit
+  type PersistedAsset = WeightLimit
+
+  override def typeId: Int = 340
+
+  override def setAssetPosition(asset: IncomingAxelWeightLimit, geometry: Seq[Point], mValue: Double) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def update(id: Long, updatedAsset: IncomingAxelWeightLimit, geometry: Seq[Point], municipality: Int, username: String, linkSource: LinkGeomSource) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def setFloating(persistedAsset: WeightLimit, floating: Boolean) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def fetchPointAssets(queryFilter: (String) => String, roadLinks: Seq[RoadLinkLike]): Seq[WeightLimit] = {
+    OracleWeightLimitDao.fetchByFilter(queryFilter)
+  }
+
+  override def create(asset: IncomingAxelWeightLimit, username: String, roadLink: RoadLink) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def toIncomingAsset(asset: IncomePointAsset, link: RoadLink): Option[IncomingAxelWeightLimit] = {
+    GeometryUtils.calculatePointFromLinearReference(link.geometry, asset.mValue).map {
+      point => IncomingAxelWeightLimit(point.x, point.y, link.linkId)
+    }
+  }
+}
+
+
+class BogieWeightLimitService(val roadLinkService: RoadLinkService) extends PointAssetOperations {
+  type IncomingAsset = IncomingBogieWeightLimit
+  type PersistedAsset = WeightLimit
+
+  override def typeId: Int = 350
+
+  override def setAssetPosition(asset: IncomingBogieWeightLimit, geometry: Seq[Point], mValue: Double) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def update(id: Long, updatedAsset: IncomingBogieWeightLimit, geometry: Seq[Point], municipality: Int, username: String, linkSource: LinkGeomSource) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def setFloating(persistedAsset: WeightLimit, floating: Boolean) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def fetchPointAssets(queryFilter: (String) => String, roadLinks: Seq[RoadLinkLike]): Seq[WeightLimit] = {
+    OracleWeightLimitDao.fetchByFilter(queryFilter)
+  }
+
+  override def create(asset: IncomingBogieWeightLimit, username: String, roadLink: RoadLink) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def toIncomingAsset(asset: IncomePointAsset, link: RoadLink): Option[IncomingBogieWeightLimit] = {
+    GeometryUtils.calculatePointFromLinearReference(link.geometry, asset.mValue).map {
+      point => IncomingBogieWeightLimit(point.x, point.y, link.linkId)
+    }
+  }
+}
+
+
+class TrailerTruckWeightLimitService(val roadLinkService: RoadLinkService) extends PointAssetOperations {
+  type IncomingAsset = IncomingTrailerTruckWeightLimit
+  type PersistedAsset = WeightLimit
+
+  override def typeId: Int = 330
+
+  override def setAssetPosition(asset: IncomingTrailerTruckWeightLimit, geometry: Seq[Point], mValue: Double) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def update(id: Long, updatedAsset: IncomingTrailerTruckWeightLimit, geometry: Seq[Point], municipality: Int, username: String, linkSource: LinkGeomSource) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def setFloating(persistedAsset: WeightLimit, floating: Boolean) = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def fetchPointAssets(queryFilter: (String) => String, roadLinks: Seq[RoadLinkLike]): Seq[WeightLimit] = {
+    OracleWeightLimitDao.fetchByFilter(queryFilter)
+  }
+
+  override def create(asset: IncomingTrailerTruckWeightLimit, username: String, roadLink: RoadLink): Long = throw new UnsupportedOperationException("Not Supported Method")
+
+  override def toIncomingAsset(asset: IncomePointAsset, link: RoadLink): Option[IncomingTrailerTruckWeightLimit] = {
+    GeometryUtils.calculatePointFromLinearReference(link.geometry, asset.mValue).map {
+      point => IncomingTrailerTruckWeightLimit(point.x, point.y, link.linkId)
+    }
+  }
+}
+
+
