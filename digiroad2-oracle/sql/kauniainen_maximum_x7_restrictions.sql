0b3d348351e706534d9bcdf88b2a784ca906f109
marquesfm <francisco.marques@cgi.com>
2018-01-22 18:25:59 +0200

    DROTH-1121 Service: Extend PointAssetOperations for new point weight limit - Created Test Class for Height Limit Service; - Add some data do execute the Unit Tests created on Test Class for Height Limit Service.

---
+++ digiroad2-oracle/sql/kauniainen_maximum_x7_restrictions.sql
@@ -0,0 +1,11 @@
+insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600074,360,'dr2_test_data',235);
+INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000680, 1611387, 388553548, 16.592, 16.592, 1);
+insert into asset_link (ASSET_ID, POSITION_ID) values (600074, 70000680);
+UPDATE asset
+  SET geometry = MDSYS.SDO_GEOMETRY(4401,
+                                    3067,
+                                    NULL,
+                                    MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1),
+                                    MDSYS.SDO_ORDINATE_ARRAY(374101.60105163435, 6677437.872017591, 0, 0)
+                                   )
+  WHERE id = 600074;
---
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/HeightLimitServiceSpec.scala
@@ -0,0 +1,49 @@
+package fi.liikennevirasto.digiroad2.service.pointasset
+
+import fi.liikennevirasto.digiroad2.util.TestTransactions
+import fi.liikennevirasto.digiroad2.{Point, GeometryUtils}
+import fi.liikennevirasto.digiroad2.asset.{TrafficDirection, Municipality, BoundingRectangle, UnknownLinkType}
+import fi.liikennevirasto.digiroad2.client.vvh.{FeatureClass, VVHRoadlink}
+import fi.liikennevirasto.digiroad2.linearasset.RoadLink
+import fi.liikennevirasto.digiroad2.service.RoadLinkService
+import fi.liikennevirasto.digiroad2.user.{Configuration, User}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, FunSuite}
+
+class HeightLimitServiceSpec extends FunSuite with Matchers {
+  def toRoadLink(l: VVHRoadlink) = {
+    RoadLink(l.linkId, l.geometry, GeometryUtils.geometryLength(l.geometry),
+      l.administrativeClass, 1, l.trafficDirection, UnknownLinkType, None, None, l.attributes + ("MUNICIPALITYCODE" -> BigInt(l.municipalityCode)))
+  }
+  val testUser = User(
+    id = 1,
+    username = "Hannu",
+    configuration = Configuration(authorizedMunicipalities = Set(235)))
+  val mockRoadLinkService = MockitoSugar.mock[RoadLinkService]
+  when(mockRoadLinkService.getRoadLinksFromVVH(any[BoundingRectangle], any[Set[Int]])).thenReturn(Seq(
+    VVHRoadlink(1611317, 235, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), Municipality,
+      TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+  val service = new HeightLimitService(mockRoadLinkService) {
+    override def withDynTransaction[T](f: => T): T = f
+
+    override def withDynSession[T](f: => T): T = f
+  }
+
+  def runWithRollback(test: => Unit): Unit = TestTransactions.runWithRollback(service.dataSource)(test)
+
+  test("Can fetch by bounding box") {
+    when(mockRoadLinkService.getRoadLinksWithComplementaryAndChangesFromVVH(any[BoundingRectangle], any[Set[Int]], any[Boolean])).thenReturn((List(), Nil))
+
+    runWithRollback {
+      val result = service.getByBoundingBox(testUser, BoundingRectangle(Point(374101, 6677437), Point(374102, 6677438))).head
+      result.id should equal(600070)
+      result.linkId should equal(1611387)
+      result.lon should equal(374101.60105163435)
+      result.lat should equal(6677437.872017591)
+      result.mValue should equal(16.592)
+    }
+  }
+}
