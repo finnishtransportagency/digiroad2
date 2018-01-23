8600dc96fa3a498b4462f8a5af4f32c4340b5640
marquesfm <francisco.marques@cgi.com>
2018-01-23 19:12:40 +0200

    DROTH-1121 Service: Extend PointAssetOperations for new point Height limit - Unit tests of Height Limit Service Added.

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/HeightLimitServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/HeightLimitServiceSpec.scala
@@ -1,8 +1,9 @@
 package fi.liikennevirasto.digiroad2.service.pointasset
 
+import fi.liikennevirasto.digiroad2.asset.LinkGeomSource.NormalLinkInterface
 import fi.liikennevirasto.digiroad2.util.TestTransactions
 import fi.liikennevirasto.digiroad2.{Point, GeometryUtils}
-import fi.liikennevirasto.digiroad2.asset.{TrafficDirection, Municipality, BoundingRectangle, UnknownLinkType}
+import fi.liikennevirasto.digiroad2.asset._
 import fi.liikennevirasto.digiroad2.client.vvh.{FeatureClass, VVHRoadlink}
 import fi.liikennevirasto.digiroad2.linearasset.RoadLink
 import fi.liikennevirasto.digiroad2.service.RoadLinkService
@@ -35,7 +36,7 @@ class HeightLimitServiceSpec extends FunSuite with Matchers {
   def runWithRollback(test: => Unit): Unit = TestTransactions.runWithRollback(service.dataSource)(test)
 
   test("Can fetch by bounding box") {
-    when(mockRoadLinkService.getRoadLinksWithComplementaryAndChangesFromVVH(any[BoundingRectangle], any[Set[Int]], any[Boolean])).thenReturn((List(), Nil))
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(any[BoundingRectangle], any[Set[Int]], any[Boolean])).thenReturn(List())
 
     runWithRollback {
       val result = service.getByBoundingBox(testUser, BoundingRectangle(Point(374101, 6677437), Point(374102, 6677438))).head
@@ -48,8 +49,8 @@ class HeightLimitServiceSpec extends FunSuite with Matchers {
   }
 
   test("Can fetch by municipality") {
-    when(mockRoadLinkService.getRoadLinksWithComplementaryAndChangesFromVVH(235)).thenReturn((Seq(
-      VVHRoadlink(1611387, 235, Seq(Point(0.0, 0.0), Point(200.0, 0.0)), Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink), Nil))
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(235)).thenReturn(Seq(
+      VVHRoadlink(1611387, 235, Seq(Point(0.0, 0.0), Point(200.0, 0.0)), Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
 
     runWithRollback {
       val result = service.getByMunicipality(235).find(_.id == 600074).get
@@ -59,6 +60,44 @@ class HeightLimitServiceSpec extends FunSuite with Matchers {
       result.lon should equal(374101.60105163435)
       result.lat should equal(6677437.872017591)
       result.mValue should equal(16.592)
+    }
+  }
+
+  test("Expire Height Limit") {
+    runWithRollback {
+      service.getPersistedAssetsByIds(Set(600075)).length should be(1)
+    }
+    an[UnsupportedOperationException] should be thrownBy service.expire(600075, testUser.username)
+  }
+
+  test("Create new") {
+    val roadLink = RoadLink(388553075, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 10, Municipality, 1, TrafficDirection.BothDirections, Motorway, None, None, Map("MUNICIPALITYCODE" -> BigInt(235)))
+    an[UnsupportedOperationException] should be thrownBy service.create(IncomingHeightLimit(2, 0.0, 388553075, 10), "test", roadLink)
+  }
+
+  test("Update Height Limit") {
+    val linkGeometry = Seq(Point(0.0, 0.0), Point(200.0, 0.0))
+
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(235)).thenReturn(Seq(
+      VVHRoadlink(1611387, 235, linkGeometry, Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(91)).thenReturn(Seq(
+      VVHRoadlink(123, 91, linkGeometry, Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+    runWithRollback {
+      val beforeUpdate = service.getByMunicipality(235).find(_.id == 600074).get
+      beforeUpdate.id should equal(600074)
+      beforeUpdate.lon should equal(374101.60105163435)
+      beforeUpdate.lat should equal(6677437.872017591)
+      beforeUpdate.mValue should equal(16.592)
+      beforeUpdate.linkId should equal(1611387)
+      beforeUpdate.municipalityCode should equal(235)
+      beforeUpdate.createdBy should equal(Some("dr2_test_data"))
+      beforeUpdate.createdAt.isDefined should equal(true)
+      beforeUpdate.modifiedBy should equal(None)
+      beforeUpdate.modifiedAt should equal(None)
+
+      an[UnsupportedOperationException] should be thrownBy service.update(id = 600074, IncomingHeightLimit(100, 0, 123, 20), linkGeometry, 91, "test", linkSource = NormalLinkInterface)
     }
   }
 }
