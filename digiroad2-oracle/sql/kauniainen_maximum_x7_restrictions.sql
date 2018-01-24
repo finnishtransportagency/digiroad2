aa2cf757fdf5574926d80d37bd8e82366b80426b
marquesfm <francisco.marques@cgi.com>
2018-01-24 16:32:26 +0200

    DROTH-1121 Service: Extend PointAssetOperations for new point Height limit - Add some data do execute the Unit Tests created on Test Class for Weight Limit Service; - Unit tests of BogieWeight Limit Service Added.

--- digiroad2-oracle/sql/kauniainen_maximum_x7_restrictions.sql
+++ digiroad2-oracle/sql/kauniainen_maximum_x7_restrictions.sql
@@ -53,3 +53,33 @@ UPDATE asset
                                     MDSYS.SDO_ORDINATE_ARRAY(374101.60105163435, 6677437.872017591, 0, 0)
                                    )
   WHERE id = 600077;
+
+
+
+-- Assets Created for Unit Test of BogieWeightLimit Service (Tierekisterin suurin sallittu telimassa)
+insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600078,350,'dr2_test_data',235);
+INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000685, 1611387, 388553548, 16.592, 16.592, 1);
+insert into asset_link (ASSET_ID, POSITION_ID) values (600078, 70000685);
+UPDATE asset
+  SET geometry = MDSYS.SDO_GEOMETRY(4401,
+                                    3067,
+                                    NULL,
+                                    MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1),
+                                    MDSYS.SDO_ORDINATE_ARRAY(374101.60105163435, 6677437.872017591, 0, 0)
+                                   )
+  WHERE id = 600078;
+
+
+
+-- Assets Created for Unit Test of TrailerTruckWeightLimit Service (Tierekisterin yhdistelmän suurin sallittu massa)
+insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600079,330,'dr2_test_data',235);
+INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000686, 1611387, 388553548, 16.592, 16.592, 1);
+insert into asset_link (ASSET_ID, POSITION_ID) values (600079, 70000686);
+UPDATE asset
+  SET geometry = MDSYS.SDO_GEOMETRY(4401,
+                                    3067,
+                                    NULL,
+                                    MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1),
+                                    MDSYS.SDO_ORDINATE_ARRAY(374101.60105163435, 6677437.872017591, 0, 0)
+                                   )
+  WHERE id = 600079;
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/WeightLimitServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/WeightLimitServiceSpec.scala
@@ -35,9 +35,14 @@ class WeightLimitServiceSpec extends FunSuite with Matchers {
     override def withDynTransaction[T](f: => T): T = f
     override def withDynSession[T](f: => T): T = f
   }
+  val bogieWeightLimitService = new BogieWeightLimitService(mockRoadLinkService) {
+    override def withDynTransaction[T](f: => T): T = f
+    override def withDynSession[T](f: => T): T = f
+  }
 
   def runWithRollbackWeightLimit(test: => Unit): Unit = TestTransactions.runWithRollback(weightLimitService.dataSource)(test)
   def runWithRollbackAxleWeightLimit(test: => Unit): Unit = TestTransactions.runWithRollback(axleWeightLimitService.dataSource)(test)
+  def runWithRollbackBogieWeightLimit(test: => Unit): Unit = TestTransactions.runWithRollback(bogieWeightLimitService.dataSource)(test)
 
   test("Can fetch by bounding box WeightLimit Asset") {
     when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(any[BoundingRectangle], any[Set[Int]], any[Boolean])).thenReturn(List())
@@ -136,7 +141,7 @@ class WeightLimitServiceSpec extends FunSuite with Matchers {
 
   test("Expire AxleWeightLimit") {
     runWithRollbackAxleWeightLimit {
-      weightLimitService.getPersistedAssetsByIds(Set(600077)).length should be(1)
+      axleWeightLimitService.getPersistedAssetsByIds(Set(600077)).length should be(1)
     }
     an[UnsupportedOperationException] should be thrownBy axleWeightLimitService.expire(600077, testUser.username)
   }
@@ -169,6 +174,72 @@ class WeightLimitServiceSpec extends FunSuite with Matchers {
       beforeUpdate.modifiedAt should equal(None)
 
       an[UnsupportedOperationException] should be thrownBy axleWeightLimitService.update(id = 600077, IncomingAxleWeightLimit(100, 0, 123, 20, 0, Some(0)), linkGeometry, 91, "test", linkSource = NormalLinkInterface)
+    }
+  }
+
+  test("Can fetch by bounding box BogieWeightLimit Asset") {
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(any[BoundingRectangle], any[Set[Int]], any[Boolean])).thenReturn(List())
+
+    runWithRollbackBogieWeightLimit {
+      val result = bogieWeightLimitService.getByBoundingBox(testUser, BoundingRectangle(Point(374101, 6677437), Point(374102, 6677438))).head
+      result.id should equal(600078)
+      result.linkId should equal(1611387)
+      result.lon should equal(374101.60105163435)
+      result.lat should equal(6677437.872017591)
+      result.mValue should equal(16.592)
+    }
+  }
+
+  test("Can fetch by municipality BogieWeightLimit Asset") {
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(235)).thenReturn(Seq(
+      VVHRoadlink(1611387, 235, Seq(Point(0.0, 0.0), Point(200.0, 0.0)), Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+    runWithRollbackBogieWeightLimit {
+      val result = bogieWeightLimitService.getByMunicipality(235).find(_.id == 600078).get
+
+      result.id should equal(600078)
+      result.linkId should equal(1611387)
+      result.lon should equal(374101.60105163435)
+      result.lat should equal(6677437.872017591)
+      result.mValue should equal(16.592)
+    }
+  }
+
+  test("Expire BogieWeightLimit") {
+    runWithRollbackBogieWeightLimit {
+      bogieWeightLimitService.getPersistedAssetsByIds(Set(600078)).length should be(1)
+    }
+    an[UnsupportedOperationException] should be thrownBy bogieWeightLimitService.expire(600078, testUser.username)
+  }
+
+  test("Create new BogieWeightLimit Asset") {
+    val roadLink = RoadLink(388553075, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 10, Municipality, 1, TrafficDirection.BothDirections, Motorway, None, None, Map("MUNICIPALITYCODE" -> BigInt(235)))
+    an[UnsupportedOperationException] should be thrownBy bogieWeightLimitService.create(IncomingBogieWeightLimit(2, 0.0, 388553075, 10, 0, Some(0)), "test", roadLink)
+  }
+
+  test("Update BogieWeight Limit") {
+    val linkGeometry = Seq(Point(0.0, 0.0), Point(200.0, 0.0))
+
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(235)).thenReturn(Seq(
+      VVHRoadlink(1611387, 235, linkGeometry, Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+    when(mockRoadLinkService.getRoadLinksWithComplementaryFromVVH(91)).thenReturn(Seq(
+      VVHRoadlink(123, 91, linkGeometry, Municipality, TrafficDirection.BothDirections, FeatureClass.AllOthers)).map(toRoadLink))
+
+    runWithRollbackBogieWeightLimit {
+      val beforeUpdate = bogieWeightLimitService.getByMunicipality(235).find(_.id == 600078).get
+      beforeUpdate.id should equal(600078)
+      beforeUpdate.lon should equal(374101.60105163435)
+      beforeUpdate.lat should equal(6677437.872017591)
+      beforeUpdate.mValue should equal(16.592)
+      beforeUpdate.linkId should equal(1611387)
+      beforeUpdate.municipalityCode should equal(235)
+      beforeUpdate.createdBy should equal(Some("dr2_test_data"))
+      beforeUpdate.createdAt.isDefined should equal(true)
+      beforeUpdate.modifiedBy should equal(None)
+      beforeUpdate.modifiedAt should equal(None)
+
+      an[UnsupportedOperationException] should be thrownBy bogieWeightLimitService.update(id = 600078, IncomingBogieWeightLimit(100, 0, 123, 20, 0, Some(0)), linkGeometry, 91, "test", linkSource = NormalLinkInterface)
     }
   }
 }
