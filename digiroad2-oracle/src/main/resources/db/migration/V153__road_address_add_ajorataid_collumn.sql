c1891102d7113586db3ccb9387fa551beb468143
Campanha <ricardo.campanha.marques@cgi.com>
2018-01-25 16:51:22 +0200

    VIITE-1071 added sequence to common_history column

--- digiroad2-oracle/src/main/resources/db/migration/V153__road_address_add_ajorataid_collumn.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V153__road_address_add_ajorataid_collumn.sql
@@ -1,1 +1,9 @@
-Alter Table ROAD_ADDRESS Add COMMON_HISTORY_ID NUMBER(38) DEFAULT 0; --for now is 0. Should be not null
+ALTER TABLE ROAD_ADDRESS ADD COMMON_HISTORY_ID NUMBER(38);
+
+CREATE sequence common_history_seq
+  increment by 1
+  start with 187692978
+  minvalue 1
+  maxvalue 999999999999999999999999999
+  cache 100
+  cycle;
