2cc487b1e00d2f134dc04ce52f3559b23a1ae59f
coelhojf <joana.f.coelho@cgi.com>
2018-02-02 20:15:19 +0200

    (US 900) labeling correction

--- UI/src/view/HeightLimitLabel.js
+++ UI/src/view/HeightLimitLabel.js
@@ -1,10 +1,45 @@
 (function(root) {
 
   root.HeightLimitLabel = function(){
-    LimitationLabel.call(this);
+    AssetLabel.call(this);
     var me = this;
+    var IMAGE_HEIGHT = 23;
 
-    this.getImage = function () {
+    var backgroundStyle = function () {
+      return new ol.style.Style({
+        image: new ol.style.Icon(({
+          src: getImage(),
+          anchor : [0.5, 1.5]
+        }))
+      });
+    };
+
+    this.getStyle = function (value) {
+      return [backgroundStyle(),
+        new ol.style.Style({
+          text: new ol.style.Text({
+            text: textStyle(value),
+            fill: new ol.style.Fill({
+              color: '#ffffff'
+            }),
+            font: '14px sans-serif',
+            offsetY:  -IMAGE_HEIGHT
+          })
+        })];
+    };
+
+    this.getValue = function(asset) {
+      return asset.limit;
+    };
+
+    var textStyle = function (value) {
+      if (_.isUndefined(value))
+        return '';
+      return '' + value;
+    };
+
+
+    var getImage = function () {
       return 'images/greenLabeling.png';
     };
   };
--- UI/src/view/WeightLimitLabel.js
+++ UI/src/view/WeightLimitLabel.js
@@ -1,10 +1,57 @@
 (function(root) {
 
   root.WeightLimitLabel = function(){
-    LimitationLabel.call(this);
+    AssetLabel.call(this);
     var me = this;
+    var IMAGE_HEIGHT = 23;
 
-    this.getImage = function (typeId) {
+    var backgroundStyle = function (value, index) {
+      return new ol.style.Style({
+        image: new ol.style.Icon(({
+          src: getImage(value),
+          anchor : [0.5, 1.5 + index]
+        }))
+      });
+    };
+
+    this.getStyle = function(value){
+      return createMultiStyles(value);
+    };
+
+    var createMultiStyles = function(values){
+      var styles = [];
+      _.forEach(values, function(value, index){
+        styles.push(backgroundStyle(value.typeId, index), textStyle(value, index));
+      });
+      return styles;
+    };
+
+
+    this.getValue = function(weightAsset) {
+      return  _.sortBy(weightAsset.assets, function(asset) { return asset.typeId; });
+    };
+
+    var textStyle = function(asset, index) {
+      return new ol.style.Style({
+        text: new ol.style.Text({
+          text: getTextValue(asset.limit),
+          fill: new ol.style.Fill({
+            color: '#ffffff'
+          }),
+          font: '14px sans-serif',
+          offsetY:   -IMAGE_HEIGHT - (index * IMAGE_HEIGHT)
+        })
+      });
+    };
+
+
+    var getTextValue = function (value) {
+      if (_.isUndefined(value))
+        return '';
+      return '' + value;
+    };
+
+    var getImage = function (typeId) {
       var images = {
         320: 'images/blueLabeling.png'   ,
         330: 'images/greenLabeling.png',
