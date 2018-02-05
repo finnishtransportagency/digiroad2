c60e80fcc6b38f2e177a7929845e258f3e8f53fc
leirasf <faustino.leiras@cgi.com>
2018-02-05 21:42:43 +0200

    DROTH-1025 European road disapear

--- digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
+++ digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
@@ -1332,6 +1332,7 @@ class Digiroad2Api(val roadLinkService: RoadLinkService,
       case RoadWidth.typeId => roadWidthService
       case Prohibition.typeId => prohibitionService
       case HazmatTransportProhibition.typeId => prohibitionService
+      case EuropeanRoads.typeId => textValueLinearAssetService
       case _ => linearAssetService
     }
   }
