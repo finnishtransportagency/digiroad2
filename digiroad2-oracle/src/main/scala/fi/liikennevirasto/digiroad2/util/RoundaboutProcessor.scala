cc2472e103174875e8b371fda0ca4bff65f2d618
joanacoelho <joana.f.coelho@cgi.com>
2018-03-26 12:54:35 +0300

    DROTH-1191 (US 1152) Batch process to run through all municipalities: test

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/RoundaboutProcessor.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/RoundaboutProcessor.scala
@@ -12,7 +12,7 @@ object RoundaboutProcessor {
     val (adjacents, rest) = roadLinks.partition(r => GeometryUtils.areAdjacent(r.geometry, nextLink.geometry))
     adjacents match{
       case Seq() if acc.size < 3 && !withIncomplete => {
-        println("findRoundaboutRecursive: Seq() &&  acc.size < 3 && !withIncomplete")
+        println(s"findRoundaboutRecursive: Seq() &&  acc.size < 3 && !withIncomplete firstLink =  ${firstLink.linkId} nextLink = ${nextLink.linkId}")
         (Seq.empty, rest)
       }
       case Seq() if GeometryUtils.areAdjacent(firstLink.geometry, nextLink.geometry) =>
