3fed021c9bddb07c3eb55ca14e3459f94474308b
oskarmalmstrom <oskar.malmstrom@gmail.com>
2018-05-04 20:40:45 +0300

    DROTH-1309 Database: scripts to add new asset type and properties -Corrected value_fi for Viherhoitoluokat

--- digiroad2-oracle/src/main/resources/db/migration/V165__add_Hoitoluokat_asset_type.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V165__add_Hoitoluokat_asset_type.sql
@@ -62,7 +62,7 @@ INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
 VALUES (primary_key_seq.nextval,'Viherhoitoluokka','db_migration_v165', sysdate);
 
 INSERT INTO PROPERTY (ID,ASSET_TYPE_ID,PROPERTY_TYPE,REQUIRED, CREATED_BY, NAME_LOCALIZED_STRING_ID, PUBLIC_ID)
-VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Hoitoluokat'),'single_choice',1, 'db_migration_v165', (SELECT ID FROM LOCALIZED_STRING WHERE VALUE_FI = 'Talvihoitoluokka' AND CREATED_BY='db_migration_v165'),'hoitoluokat_viherhoitoluokka');
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Hoitoluokat'),'single_choice',1, 'db_migration_v165', (SELECT ID FROM LOCALIZED_STRING WHERE VALUE_FI = 'Viherhoitoluokka' AND CREATED_BY='db_migration_v165'),'hoitoluokat_viherhoitoluokka');
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
 VALUES (primary_key_seq.nextval, 1, '(N1) 2-ajorataiset tiet', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_viherhoitoluokka'));
