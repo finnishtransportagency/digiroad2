a0e5f45fa3dcdedd4ef35c93dbfc191c1f188c2f
oskarmalmstrom <oskar.malmstrom@gmail.com>
2018-05-24 20:42:48 +0300

    DROTH-1350: -new stylerule for linear assets -changed form behaviour for text value fields

--- UI/src/model/selectedLinearAssetFactory.js
+++ UI/src/model/selectedLinearAssetFactory.js
@@ -20,10 +20,12 @@
   }
 
   function euroAndExitValidator(val) {
-    var values = val.replace(/[ \t\f\v]/g,'').split(/[\n,]+/);
-    return _.every(values, function(value){
-      return value.match(/^[0-9|Ee][0-9|Bb]{0,2}/) && value.toString().length < 4;
-    });
+    if(!_.isUndefined(val)){
+      var values = val.replace(/[ \t\f\v]/g,'').split(/[\n,]+/);
+      return _.every(values, function(value){
+        return value.match(/^[0-9|Ee][0-9|Bb]{0,2}/) && value.toString().length < 4;
+      });
+    }
   }
 
   function construct(backend, collection, asset) {
