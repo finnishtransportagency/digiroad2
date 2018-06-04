0ff34e1478498e9d3f4cd83f20ae44483f0464cc
cardadorm <miguel.cardador@cgi.com>
2018-06-04 10:09:01 +0100

    Increase the winter care class values once more

--- digiroad2-oracle/src/main/resources/db/migration/V165__add_Hoitoluokat_asset_type.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V165__add_Hoitoluokat_asset_type.sql
@@ -9,37 +9,37 @@ INSERT INTO PROPERTY (ID,ASSET_TYPE_ID,PROPERTY_TYPE, CREATED_BY, NAME_LOCALIZED
 VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Hoitoluokat'),'single_choice', 'db_migration_v165', (SELECT ID FROM LOCALIZED_STRING WHERE VALUE_FI = 'Talvihoitoluokka' AND CREATED_BY='db_migration_v165'),'hoitoluokat_talvihoitoluokka');
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 0, '(IsE) Liukkaudentorjunta ilman toimenpideaikaa', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 1, '(IsE) Liukkaudentorjunta ilman toimenpideaikaa', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 1, '(Is) Normaalisti aina paljaana', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 2, '(Is) Normaalisti aina paljaana', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 2, '(I) Normaalisti paljaana', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 3, '(I) Normaalisti paljaana', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 3, '(Ib) Pääosin suolattava, ajoittain hieman liukas', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 4, '(Ib) Pääosin suolattava, ajoittain hieman liukas', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 4, '(Ic) Pääosin hiekoitettava, ohut lumipolanne sallittu', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 5, '(Ic) Pääosin hiekoitettava, ohut lumipolanne sallittu', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 5, '(II) Pääosin lumipintainen', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 6, '(II) Pääosin lumipintainen', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 6, '(III) Pääosin lumipintainen, pisin toimenpideaika', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 7, '(III) Pääosin lumipintainen, pisin toimenpideaika', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 7, '(L) Kevyen liikenteen laatukäytävät', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 8, '(L) Kevyen liikenteen laatukäytävät', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 8, '(K1) Melko vilkkaat kevyen liikenteen väylät', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 9, '(K1) Melko vilkkaat kevyen liikenteen väylät', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 9, '(K2) Kevyen liikenteen väylien perus talvihoitotaso', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 10, '(K2) Kevyen liikenteen väylien perus talvihoitotaso', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 10, '(ei talvih.) Kevyen liikenteen väylät, joilla ei talvihoitoa', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
+VALUES (primary_key_seq.nextval, 11, '(ei talvih.) Kevyen liikenteen väylät, joilla ei talvihoitoa', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
 VALUES (primary_key_seq.nextval, 20, 'Pääkadut ja vilkkaat väylät', ' ', 'db_migration_v165', (select id from property where public_ID = 'hoitoluokat_talvihoitoluokka'));
