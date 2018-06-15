c901864b2d82df0dc1ebbc3bcff95f9ce2963d56
cardadorm <miguel.cardador@cgi.com>
2018-06-15 09:23:57 +0100

    DROTH-1386 Service: Add new sevice and DAO methods to show all notifications

--- digiroad2-oracle/src/main/resources/db/migration/V172__create_notification_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V172__create_notification_table.sql
@@ -1,7 +1,7 @@
 create table notification
 (
   id number primary key,
-  created_date timestamp default current_timestamp not null,
+  created_date DATE default current_date not null,
   heading varchar2(128),
   content varchar2(4000)
 );
