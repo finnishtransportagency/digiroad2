509d8a382c3616ad339685705762b4602dd216f8
cardadorm <miguel.cardador@cgi.com>
2018-06-27 18:59:52 +0100

    DROTH-1407 TR: Extend tierekisteri importer for pedestrian

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/TierekisteriDataImporter.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/TierekisteriDataImporter.scala
@@ -120,6 +120,10 @@ object TierekisteriDataImporter {
     new CareClassTierekisteriImporter()
   }
 
+  lazy val pedestrianCrossingTierekisteriImporter: PedestrianCrossingTierekisteriImporter = {
+    new PedestrianCrossingTierekisteriImporter()
+  }
+
   def getLastExecutionDate(tierekisteriAssetImporter: TierekisteriImporterOperations): Option[DateTime] = {
     OracleDatabase.withDynSession{
       val assetId = tierekisteriAssetImporter.getAssetTypeId
@@ -224,7 +228,8 @@ object TierekisteriDataImporter {
     "bogieWeightLimit" -> bogieWeightLimitTierekisteriImporter,
     "heightLimit" -> heightLimitTierekisteriImporter,
     "widthLimit" -> widthLimitTierekisteriImporter,
-    "careClass" -> careClassTierekisteriImporter
+    "careClass" -> careClassTierekisteriImporter,
+    "pedestrianCrossing" -> pedestrianCrossingTierekisteriImporter
   )
 
   private def importAssets(tierekisteriAssetImporter: TierekisteriImporterOperations): Unit = {
