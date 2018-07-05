827b3c2e4450f20952b197fbfd2b9ef51dff604d
marquesfm <francisco.marques@cgi.com>
2018-07-05 19:38:30 +0300

    After DROTH-1153_Library_upgrades merge - FlywayException fixed because we have found more than one migration file with the same name

--- UI/src/view/defaultLocationButton.js
+++ UI/src/view/defaultLocationButton.js
@@ -10,7 +10,7 @@
         var actualLonLat = {lon: 0, lat: 0};
         eventbus.on('map:moved', function (event) {
             if (!_.isUndefined(event.center))
-                actualLonLat = {lon: event.center[0], lat: event.center[1]}
+                actualLonLat = {lon: event.center[0], lat: event.center[1]};
         });
 
         $('#default-location-btn').on('click', function () {
