2b76ccadc5bf7b30dbd34ff1c6c2af49a4b1c65c
marquesfm <francisco.marques@cgi.com>
2018-07-18 17:13:09 +0300

    DROTH-1453 (US 1414) UI: Create new Label for Non-Paved road type - Delete exception for PavedRoad at deleteLinearAssets method; - Added Non Paved Road at list of properties of the asset.

--- UI/src/utils/backend-utils.js
+++ UI/src/utils/backend-utils.js
@@ -288,28 +288,16 @@
       });
     }, 1000);
 
-    this.deleteLinearAssets = _.throttle(function(data, success, failure) {
-      if (data.typeId == 110) { // Pavement must be set to null value, not deleted
-        $.ajax({
-          contentType: "application/json",
-          type: "POST",
-          url: "api/linearassets",
-          data: JSON.stringify(data),
-          dataType: "json",
-          success: success,
-          error: failure
-        });
-      } else {
-        $.ajax({
-          contentType: "application/json",
-          type: "DELETE",
-          url: "api/linearassets",
-          data: JSON.stringify(data),
-          dataType: "json",
-          success: success,
-          error: failure
-        });
-      }
+    this.deleteLinearAssets = _.throttle(function (data, success, failure) {
+      $.ajax({
+        contentType: "application/json",
+        type: "DELETE",
+        url: "api/linearassets",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: success,
+        error: failure
+      });
     }, 1000);
 
     this.splitLinearAssets = function(typeId, id, splitMeasure, createdValue, existingValue, success, failure) {
