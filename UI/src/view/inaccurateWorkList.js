6b1c8d9b3fa63539bc44a617b784ef2a1de17309
joanacoelho <joana.f.coelho@cgi.com>
2018-09-07 17:29:33 +0300

    DROTH-1517 (US 1492) Inaccurate Work List

--- UI/src/view/inaccurateWorkList.js
+++ UI/src/view/inaccurateWorkList.js
@@ -8,32 +8,27 @@
 
     this.workListItemTable = function(layerName, workListItems, municipalityName) {
 
-      var municipalityHeader = function(municipalityName, totalCount) {
-        var countString = totalCount ? ' (yhteensä ' + totalCount + ' kpl)' : '';
-        return $('<h2/>').html(municipalityName + countString);
+      var municipalityHeader = function(municipalityName) {
+        return $('<h2/>').html(municipalityName);
       };
       var tableHeaderRow = function(headerName) {
         return $('<caption/>').html(headerName);
       };
       var tableContentRows = function(Ids) {
         return _.map(Ids, function(item) {
-          return $('<tr/>').append($('<td/>').append(typeof item.id !== 'undefined' ? assetLink(item) : idLink(item)));
+          return $('<tr/>').append($('<td/>').append(item.assetId ? assetLink(item) : idLink(item)));
         });
       };
-      var idLink = function(id) {
-        var link = '#' + layerName + '/' + id;
+      var idLink = function(item) {
+        var link = '#' + layerName + '/' + item.linkId;
         return $('<a class="work-list-item"/>').attr('href', link).html(link);
       };
-      var floatingValidator = function() {
-        return $('<span class="work-list-item"> &nbsp; *</span>');
+
+      var assetLink = function(item) {
+        var link = '#' + layerName + '/' + item.assetId;
+        return $('<a class="work-list-item"/>').attr('href', link).html(link);
       };
-      var assetLink = function(asset) {
-        var link = '#' + layerName + '/' + asset.id;
-        var workListItem = $('<a class="work-list-item"/>').attr('href', link).html(link);
-        if(asset.floatingReason === 1) //floating reason equal to RoadOwnerChanged
-          workListItem.append(floatingValidator);
-        return workListItem;
-      };
+
       var tableForGroupingValues = function(values, Ids) {
         if (!Ids || Ids.length === 0) return '';
         return $('<table/>').addClass('table')
@@ -41,7 +36,7 @@
           .append(tableContentRows(Ids));
       };
 
-      return $('<div/>').append(municipalityHeader(municipalityName, workListItems.totalCount))
+      return $('<div/>').append(municipalityHeader(municipalityName))
           .append(tableForGroupingValues('Kunnan omistama', workListItems.Municipality))
           .append(tableForGroupingValues('Valtion omistama', workListItems.State))
           .append(tableForGroupingValues('Yksityisen omistama', workListItems.Private))
