02be9f2d67db86420fd422035d3c5805b3d9087c
marquesfm <francisco.marques@cgi.com>
2018-09-19 12:39:54 +0300

    DROTH-1534 Traffic signs: corrections to dropdown texts - FIx Sql file added at Flyway to correct dropdown texts to Traffic signs.

--- digiroad2-oracle/src/main/resources/db/migration/V183__update_traffic_sign_enumerated_values.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V183__update_traffic_sign_enumerated_values.sql
@@ -12,58 +12,58 @@ WHERE
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'Taajama Päättyy',
+	NAME_FI = 'Taajama päättyy',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Taajama päättyy');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Taajama Päättyy');
 
 
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'Suurin Sallittu Pituus',
+	NAME_FI = 'Suurin sallittu pituus',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Suurin sallittu pituus');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Suurin Sallittu Pituus');
 
 
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'Vasemmalle Kääntyminen Kielletty',
+	NAME_FI = 'Vasemmalle kääntyminen kielletty',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Vasemmalle kääntyminen kielletty');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Vasemmalle Kääntyminen Kielletty');
 
 
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'Oikealle Kääntyminen Kielletty',
+	NAME_FI = 'Oikealle kääntyminen kielletty',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Oikealle kääntyminen kielletty');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Oikealle Kääntyminen Kielletty');
 
 
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'U-Käännös Kielletty',
+	NAME_FI = 'U-käännös kielletty',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'U-käännös kielletty');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'U-Käännös Kielletty');
 
 
 UPDATE
 	ENUMERATED_VALUE
 SET
-	NAME_FI = 'Moottorikäyttöisellä ajoveuvolla ajo kielletty',
+	NAME_FI = 'Moottorikäyttöisellä ajoneuvolla ajo kielletty',
 	MODIFIED_DATE = SYSDATE,
 	MODIFIED_BY = 'db_migration_v183'
 WHERE
-	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Moottorikäyttöisellä ajoneuvolla ajo kielletty');
+	id = (SELECT ID FROM ENUMERATED_VALUE WHERE NAME_FI = 'Moottorikäyttöisellä ajoveuvolla ajo kielletty');
