ea16ae19e87d40583c99859f0cd2f89faa4a724d
joanacoelho <joana.f.coelho@cgi.com>
2018-09-25 18:35:53 +0300

    (US 1396) UI: Add labeling for type Extensions

--- UI/src/view/point_asset/servicePointLabel.js
+++ UI/src/view/point_asset/servicePointLabel.js
@@ -10,13 +10,14 @@
       return sign.type;
     };
 
+    var getSignExtensionType = function (sign) {
+      return sign.typeExtension;
+    };
+
     me.getPropertiesConfiguration = function () {
       return [
         {signValue: [15], image: 'images/service_points/parkingGarage.png'},
         {signValue: [12], image: 'images/service_points/parking.png'},
-        {signValue: [11], image: 'images/service_points/railwayStation2.png'},
-        {signValue: [11], image: 'images/service_points/railwayStation.png'},
-        {signValue: [1], image: 'images/service_points/subwayStation.png'},
         {signValue: [16], image: 'images/service_points/busStation.png'},
         {signValue: [8], image: 'images/service_points/airport.png'},
         {signValue: [9], image: 'images/service_points/ferry.png'},
@@ -26,8 +27,71 @@
         {signValue: [5], image: 'images/service_points/borderCrossingPoint.png', validation: validateText, height: 25},
         {signValue: [13], image: 'images/service_points/loadingTerminalForCars.png', validation: validateText, height: 25},
         {signValue: [14], image: 'images/service_points/parkingAreaBusesAndTrucks.png', validation: validateText, height: 25},
-        {signValue: [17], image: 'images/service_points/chargingPointElectricCars.png', validation: validateText, height: 25}
+        {signValue: [17], image: 'images/service_points/chargingPointElectricCars.png', validation: validateText, height: 25},
+        {signValue: [11], typeExtension: 5, image: 'images/service_points/railwayStation2.png'},
+        {signValue: [11], typeExtension: 6, image: 'images/service_points/railwayStation.png'},
+        {signValue: [11], typeExtension: 7, image: 'images/service_points/subwayStation.png'}
       ];
+    };
+
+    me.getLabelProperty = function (sign, counter) {
+
+      var labelProperty = _.find(me.getPropertiesConfiguration(), function(properties) {
+        var includesSign = _.includes(properties.signValue, me.getSignType(sign));
+        if(properties.typeExtension)
+          return includesSign && properties.typeExtension === getSignExtensionType(sign);
+        else
+          return includesSign;
+      });
+
+      function findImage() {
+        return labelProperty && labelProperty.image ? labelProperty.image : 'images/traffic-signs/badValue.png';
+      }
+
+      function getTextOffsetX(){
+        return labelProperty && labelProperty.offsetX ? labelProperty.offsetX :  0;
+      }
+
+      function getTextOffsetY(){
+        var offsetY =  labelProperty && labelProperty.offsetY ? labelProperty.offsetY :  1;
+        return parseInt(''+ getHeight() / 2) + offsetY;
+      }
+
+      function getValidation(){
+        return labelProperty && labelProperty.validation ? labelProperty.validation.call(sign) : false ;
+      }
+
+      function getValue(){
+        return labelProperty && labelProperty.convertion ? labelProperty.convertion.call(sign) : sign.value;
+      }
+
+      function getAdditionalInfo(){
+        return labelProperty && labelProperty.additionalInfo ? labelProperty.additionalInfo.call(sign) : '';
+      }
+
+      function getUnit() {
+        return labelProperty && labelProperty.unit ? labelProperty.unit.call(sign) : '';
+      }
+
+      function getMaxLength() {
+        return labelProperty && labelProperty.maxLabelLength ? labelProperty.maxLabelLength : 20;
+      }
+
+      function getHeight() {
+        return labelProperty && labelProperty.height ? labelProperty.height : 35;
+      }
+
+      return {
+        findImage: findImage,
+        getTextOffsetX: getTextOffsetX,
+        getTextOffsetY: getTextOffsetY,
+        getValidation: getValidation,
+        getValue : getValue,
+        getUnit : getUnit,
+        getAdditionalInfo: getAdditionalInfo,
+        getMaxLength: getMaxLength,
+        getHeight: getHeight
+      };
     };
 
     var validateText = function () { return true;};
@@ -39,8 +103,6 @@
       return _.flatten(_.chain(groupedAssets).map(function(assets){
         var imgPosition = {x: 0 , y: me.stickPosition.y};
         return _.map(assets, function(asset){
-          // styles = styles.concat(me.getStickStyle());
-          // imgPosition.y += me.getLabelProperty(value).getHeight();
           var values = me.getValue(asset);
           if (!(_.isUndefined(values) || _.isEmpty(values))) {
             var styles = [];
@@ -49,7 +111,6 @@
               imgPosition.y += me.getLabelProperty(value).getHeight();
               return me.getStyle(value, imgPosition );
             })));
-            // styles = styles.concat(me.getStyle(value, imgPosition));
             var feature = me.createFeature(getPoint(asset));
             feature.setStyle(styles);
             feature.setProperties(_.omit(asset, 'geometry'));
@@ -61,7 +122,7 @@
 
     function getDefaultText(type) {
       var defaultTextValues =
-        [{type: 5, text: 'Rajanylityspaikka'},
+        [ {type: 5, text: 'Rajanylityspaikka'},
           {type: 13, text: 'Lastausterminaali'},
           {type: 14, text: 'Pysäköintialue'},
           {type: 17, text: 'Latauspiste'}];
@@ -72,7 +133,7 @@
 
     me.getValue = function (asset) {
       return _.map(asset.services, function (service) {
-        return {value: getDefaultText(service.serviceType), type: service.serviceType};
+        return {value: getDefaultText(service.serviceType), type: service.serviceType, typeExtension: service.typeExtension};
       });
     };
   };
