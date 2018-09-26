034479257f2de6de4d664583b41e2d4682c3b7f3
cardadorm <miguel.cardador@cgi.com>
2018-09-26 12:46:49 +0100

    remove commented code

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetFiller.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetFiller.scala
@@ -3,7 +3,6 @@ package fi.liikennevirasto.digiroad2.linearasset
 import fi.liikennevirasto.digiroad2.asset.{SideCode}
 
 object LinearAssetFiller {
-//  case class AdjustedSpeedLimitSegment(speedLimitSegment: SpeedLimit, adjustedMValue: Option[Double])
   case class MValueAdjustment(assetId: Long, linkId: Long, startMeasure: Double, endMeasure: Double)
   case class VVHChangesAdjustment(assetId: Long, linkId: Long, startMeasure: Double, endMeasure: Double, vvhTimestamp: Long)
   case class SideCodeAdjustment(assetId: Long, sideCode: SideCode, typeId: Int)
