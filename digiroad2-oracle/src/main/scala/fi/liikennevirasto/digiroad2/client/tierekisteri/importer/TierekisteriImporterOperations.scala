c770df277d7258f08f6452a855fad13b635954c1
oskarmalmstrom <oskar.malmstrom@gmail.com>
2018-10-09 19:15:02 +0300

    DROTH-1595 typo

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/tierekisteri/importer/TierekisteriImporterOperations.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/tierekisteri/importer/TierekisteriImporterOperations.scala
@@ -263,14 +263,14 @@ trait TierekisteriAssetImporterOperations extends TierekisteriImporterOperations
 
   def importAssets(): Unit = {
     //Expire all asset in state roads in all the municipalities
-//    val municipalities = getAllMunicipalities
-//    municipalities.foreach { municipality =>
-//      withDynTransaction{
-//        expireAssets(municipality, Some(State))
-//      }
-//    }
+    val municipalities = getAllMunicipalities
+    municipalities.foreach { municipality =>
+      withDynTransaction{
+        expireAssets(municipality, Some(State))
+      }
+    }
 
-    val roadNumbers = Seq(29)/*getAllViiteRoadNumbers*/
+    val roadNumbers = getAllViiteRoadNumbers
 
     roadNumbers.foreach {
       roadNumber =>
