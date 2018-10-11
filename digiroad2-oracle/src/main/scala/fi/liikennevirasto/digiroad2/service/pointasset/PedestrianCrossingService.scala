0f32e3c9dde5360319ae143cc15ba35ea72099d3
cardadorm <miguel.cardador@cgi.com>
2018-10-11 15:36:56 +0100

    filter out the expired asset

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/pointasset/PedestrianCrossingService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/pointasset/PedestrianCrossingService.scala
@@ -73,7 +73,7 @@ class PedestrianCrossingService(val roadLinkService: RoadLinkService) extends Po
 
   override def getByBoundingBox(user: User, bounds: BoundingRectangle) : Seq[PersistedAsset] = {
     val (roadLinks, changeInfo) = roadLinkService.getRoadLinksWithComplementaryAndChangesFromVVH(bounds)
-    super.getByBoundingBox(user, bounds, roadLinks, changeInfo, floatingAdjustment(adjustmentOperation, createOperation))
+    super.getByBoundingBox(user, bounds, roadLinks, changeInfo, floatingAdjustment(adjustmentOperation, createOperation)).filterNot(_.expired)
   }
 
   private def createOperation(asset: PersistedAsset, adjustment: AssetAdjustment): PersistedAsset = {
