2f60a50a370f83e206c37c3fb41281ee62d9c444
joanacoelho <joana.f.coelho@cgi.com>
2018-11-02 12:26:38 +0200

    DROTH-1617 Changes after code review

--- UI/src/application.js
+++ UI/src/application.js
@@ -414,8 +414,6 @@
   };
 
   var setupProjections = function() {
-    // proj4.defs('EPSG:3067', '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs');
-
     proj4.defs('EPSG:3067', '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs');
     ol.proj.proj4.register(proj4);
   };
