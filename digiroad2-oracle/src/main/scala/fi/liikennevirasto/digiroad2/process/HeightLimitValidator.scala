683826f20f9c4b763642c1c39851be5f5aea80f7
cardadorm <miguel.cardador@cgi.com>
2018-12-03 19:41:13 +0200

    change events on router change the radiu for 500m on massRestriction

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/HeightLimitValidator.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/HeightLimitValidator.scala
@@ -8,6 +8,7 @@ import fi.liikennevirasto.digiroad2.service.pointasset.TrafficSignType
 class HeightLimitValidator extends SevenRestrictionsLimitationValidator {
   override def assetTypeInfo: AssetTypeInfo = HeightLimit
   override val allowedTrafficSign: Set[TrafficSignType] = Set(TrafficSignType.FreeHeight, TrafficSignType.MaxHeightExceeding)
+  override val radiusDistance: Int = 50
 
   override def comparingAssetAndTrafficValue(asset: PersistedLinearAsset, trafficSign: PersistedTrafficSign): Boolean = {
     TrafficSignType.apply(trafficSignService.getTrafficSignsProperties(trafficSign, "trafficSigns_type").get.propertyValue.toInt) match {
