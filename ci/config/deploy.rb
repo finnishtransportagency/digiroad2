221e37dd99be5a0117f44402149f984d5e31dc4d
marquesfm <francisco.marques@cgi.com>
2019-03-12 12:22:00 +0200

    DROTH-1899 (US 1638) Migrate fixture reset and run tests jobs to the new CI serve - Try to print digiroad2.properties env used.

--- ci/config/deploy.rb
+++ ci/config/deploy.rb
@@ -28,6 +28,7 @@ namespace :deploy do
       execute "cd #{deploy_path} && cp conversion.bonecp.properties #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp authentication.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp digiroad2.properties #{release_path}/conf/#{fetch(:stage)}/."
+      "echo cd #{deploy_path} && cp digiroad2.properties #{release_path}/conf/#{fetch(:stage)}/",
       execute "cd #{deploy_path} && cp smtp.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/digiroad2-oracle/src/test/resources/."
