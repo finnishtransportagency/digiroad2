3c46ebbc315d58abe0c117f0361e5688c1b1ba7e
marquesfm <francisco.marques@cgi.com>
2019-04-10 13:12:38 +0300

    DROTH-1889 Migrate fixture reset and run tests jobs to the new CI server - Commits copied until Apr 9, 2019.

--- ci/config/deploy.rb
+++ ci/config/deploy.rb
@@ -1,15 +1,11 @@
-lock '3.4.0'
-set :application, 'ci-test'
+lock '3.1.0'
+set :application, 'oth'
 set :repo_url, 'https://github.com/finnishtransportagency/digiroad2.git'
-set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'DROTH-1638_New_CI_Server'
-set :deploy_to, "/home/web/ci-test"
+set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
+set :deploy_to, "/home/web/oth"
 set :pty, true
-set :log_level, :debug
+set :log_level, :info
 set :grunt_target, ENV['GRUNT_TARGET'] || ''
-set :ssh_options, {
-  forward_agent: false,
-  keys: ["~/.ssh/id_rsa"]
-}
 
 namespace :deploy do
   task :start do
@@ -17,13 +13,13 @@ namespace :deploy do
       execute "cp #{deploy_to}/newrelic/* #{release_path}/."
       execute "cd #{release_path} && chmod 700 start.sh"
       execute "cd #{release_path} && nohup ./start.sh"
-      execute "cd #{release_path} && tmux new -s 'ci-test' -d"
+      execute "cd #{release_path} && tmux new -s 'oth' -d"
     end
   end
 
   task :prepare_release do
     on roles(:all) do |host|
-      execute "tmux kill-session -t 'ci-test' || true"
+      execute "tmux kill-session -t 'oth' || true"
       execute "mkdir -p #{release_path}/tmp"
       execute "cd #{release_path} && npm install && export TMPDIR=#{release_path}/tmp && yarn install && grunt deploy --target=#{fetch(:grunt_target)}"
       execute "cd #{deploy_path} && mkdir #{release_path}/digiroad2-oracle/lib && cp oracle/* #{release_path}/digiroad2-oracle/lib/."
@@ -31,11 +27,10 @@ namespace :deploy do
       execute "cd #{deploy_path} && cp bonecp.properties #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp conversion.bonecp.properties #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp authentication.properties #{release_path}/conf/#{fetch(:stage)}/."
-      execute "cd #{deploy_path} && cp smtp.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/digiroad2-oracle/src/test/resources/."
       execute "cd #{release_path} && cp revision.properties #{release_path}/conf/#{fetch(:stage)}/. || echo 'SKIP: No revision information available'"
-      execute "cd #{release_path} && ln -s /data1/logs/ci-test logs"
+      execute "cd #{release_path} && ln -s /data1/logs/oth logs"
       execute "cd #{release_path} && ./sbt -Ddigiroad2.env=#{fetch(:stage)} assembly"
       execute "cd #{release_path} && rsync -a dist/ src/main/webapp/oth/"
       execute "cd #{release_path} && rsync -a --exclude-from 'copy_exclude.txt' oth-UI/ src/main/webapp/oth/"
