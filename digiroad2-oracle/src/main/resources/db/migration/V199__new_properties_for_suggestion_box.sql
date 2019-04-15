594559916ffceef2b518355a22d37d3537da75ec
moraispedro <pedro.morais@cgi.com>
2019-04-15 17:22:10 +0300

    DROTH-1955 (US 1891) DataBase: Add New properties to have new checkbox value on the database for all Point assets

--- digiroad2-oracle/src/main/resources/db/migration/V199__new_properties_for_suggestion_box.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V199__new_properties_for_suggestion_box.sql
@@ -1,3 +1,4 @@
+-- PROPERTIES FOR SPECIAL ASSETS
 --Add property Ajokielto Vihjetieto
 INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
 VALUES (primary_key_seq.nextval,'Ajokielto Vihjetieto','db_migration_v199', sysdate);
@@ -62,3 +63,96 @@ VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (se
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
 VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'huoltotie_vihjetieto'));
+
+-- PROPERTIES FOR POINT ASSETS
+--Add property Esterakennelma Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Esterakennelma tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'esterakennelma'), 'checkbox', 0, 'db_migration_v199', 'obstacle_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Esterakennelma tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'obstacle_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'obstacle_suggest_box'));
+
+--Add property Rautatien tasoristeys Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Rautatien tasoristeys tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Rautatien tasoristeys'), 'checkbox', 0, 'db_migration_v199', 'railway_crossing_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Rautatien tasoristeys tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'railway_crossing_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'railway_crossing_suggest_box'));
+
+--Add property Opastustaulu Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Opastustaulu tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Opastustaulu ja sen informaatio'), 'checkbox', 0, 'db_migration_v199', 'directional_traffic_signs_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Opastustaulu tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'directional_traffic_signs_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'directional_traffic_signs_suggest_box'));
+
+--Add property Suojatie Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Suojatie tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Suojatie'), 'checkbox', 0, 'db_migration_v199', 'pedestrian_crossings_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Suojatie tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'pedestrian_crossings_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'pedestrian_crossings_suggest_box'));
+
+--Add property Liikennevalo Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Liikennevalo tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Liikennevalo'), 'checkbox', 0, 'db_migration_v199', 'traffic_lights_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Liikennevalo tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'traffic_lights_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'traffic_lights_suggest_box'));
+
+--Add property Liikennemerkit Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Liikennemerkki tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Liikennemerkki'), 'checkbox', 0, 'db_migration_v199', 'traffic_signs_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Liikennemerkki tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'traffic_signs_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'traffic_signs_suggest_box'));
+
+--Add property Palvelupiste Tarkistettu
+INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
+VALUES (primary_key_seq.nextval,'Palvelupiste tarkistettu','db_migration_v199', sysdate);
+
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+VALUES (primary_key_seq.nextval, (select id from asset_type where name = 'Palvelupiste'), 'checkbox', 0, 'db_migration_v199', 'service_points_suggest_box', (select id from LOCALIZED_STRING where VALUE_FI = 'Palvelupiste tarkistettu'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 0, 'Tarkistettu', ' ', 'db_migration_v199', (select id from property where public_ID = 'service_points_suggest_box'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+VALUES (primary_key_seq.nextval, 1, 'Vihjetieto', ' ', 'db_migration_v199', (select id from property where public_ID = 'service_points_suggest_box'));
+
