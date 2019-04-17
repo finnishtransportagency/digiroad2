d67908a66f3efd36b8d528130ba19453b076c032
oskarmalmstrom <oskar.malmstrom@gmail.com>
2019-04-17 16:57:48 +0300

    DROTH-1936 Update: flyway with inserts for new information signs

--- digiroad2-oracle/src/main/resources/db/migration/V198__information_signs_to_enumerated_values.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V198__information_signs_to_enumerated_values.sql
@@ -122,4 +122,4 @@ INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_
 VALUES (primary_key_seq.nextval, 190, 'Poistumisreitti', '' , 'db_migration_v198',  (select id from property where public_ID = 'trafficSigns_type'));
 
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-VALUES (primary_key_seq.nextval, 191, 'Suunnistustaulu', '' , 'db_migration_v198',  (select id from property where public_ID = 'trafficSigns_type'));
+VALUES (primary_key_seq.nextval, 191, 'Suunnistustaulu, pieni', '' , 'db_migration_v198',  (select id from property where public_ID = 'trafficSigns_type'));
