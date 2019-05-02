fcf5cb766f663e06cae26f776edf91f110470104
cardadorm <miguel.cardador@cgi.com>
2019-05-02 14:11:24 +0100

    Change flyway queries

--- digiroad2-oracle/src/main/resources/db/migration/V199_2__add_new_properties_to_convert_to_dynamic.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V199_2__add_new_properties_to_convert_to_dynamic.sql
@@ -3,7 +3,7 @@
 
 -- totalWeightLimit --
     INSERT INTO LOCALIZED_STRING (ID,VALUE_FI, CREATED_BY, CREATED_DATE)
-    VALUES (primary_key_seq.nextval,'kokonaispainorajoitus'','db_migration_v199', sysdate);
+    VALUES (primary_key_seq.nextval,'kokonaispainorajoitus','db_migration_v199', sysdate);
 
     INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
     VALUES (primary_key_seq.nextval, (SELECT id FROM asset_type WHERE name = 'Kokonaispainorajoitukset'), 'integer', 1, 'db_migration_v199', 'total_weight', (select id from LOCALIZED_STRING where VALUE_FI = 'kokonaispaino rajoitus'));
