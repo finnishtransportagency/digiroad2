ea4286ff891399fcf532bb179afe01e46cf4df41
marquesfm <francisco.marques@cgi.com>
2019-05-03 18:42:55 +0300

    DROTH-1863 New asset: Roadworks (Code Review) - flyway doesn't allow update, code reverted.

--- digiroad2-oracle/src/main/resources/db/migration/V196__date_period_table_and_properties.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V196__date_period_table_and_properties.sql
@@ -7,19 +7,19 @@ create table date_period_value (
 );
 
 insert into localized_string (id, value_fi, value_sv, created_by, created_date)
-values (primary_key_seq.nextval, 'Kelirikkokausi', NULL, 'db_migration_v196', sysdate);
+values (primary_key_seq.nextval, 'Kelirikkokausi', NULL, 'db_migration_v193', sysdate);
 
 insert into property(id, asset_type_id, property_type, required, created_by, public_id, name_localized_string_id)
-values (primary_key_seq.nextval, 130, 'date_period', '0', 'db_migration_v196', 'spring_thaw_period', (select id from localized_string where value_fi = 'Kelirikkokausi' and created_by = 'db_migration_v196'));
+values (primary_key_seq.nextval, 130, 'date_period', '0', 'db_migration_v193', 'spring_thaw_period', (select id from localized_string where value_fi = 'Kelirikkokausi' and created_by = 'db_migration_v193'));
 
 insert into localized_string (id, value_fi, value_sv, created_by, created_date)
-values (primary_key_seq.nextval, 'Jokavuotinen', NULL, 'db_migration_v196', sysdate);
+values (primary_key_seq.nextval, 'Jokavuotinen', NULL, 'db_migration_v193', sysdate);
 
 insert into property(id, asset_type_id, property_type, required, created_by, public_id, name_localized_string_id)
-values (primary_key_seq.nextval, 130, 'checkbox', '0', 'db_migration_v196', 'annual_repetition', (select id from localized_string where value_fi = 'Jokavuotinen' and created_by = 'db_migration_v196'));
+values (primary_key_seq.nextval, 130, 'checkbox', '0', 'db_migration_v193', 'annual_repetition', (select id from localized_string where value_fi = 'Jokavuotinen' and created_by = 'db_migration_v193'));
 
 insert into enumerated_value (id, value, name_fi, name_sv, created_by, property_id)
-values (primary_key_seq.nextval, 1, 'Jokavuotinen', ' ', 'db_migration_v196', (select id from property where public_id = 'annual_repetition'));
+values (primary_key_seq.nextval, 1, 'Jokavuotinen', ' ', 'db_migration_v193', (select id from property where public_id = 'annual_repetition'));
 
 insert into enumerated_value (id, value, name_fi, name_sv, created_by, property_id)
-values (primary_key_seq.nextval, 0, 'Ei toistu', ' ', 'db_migration_v196', (select id from property where public_id = 'annual_repetition'));
+values (primary_key_seq.nextval, 0, 'Ei toistu', ' ', 'db_migration_v193', (select id from property where public_id = 'annual_repetition'));
