beb56bc91f073b0e596fa22cdcf84e9fa0d9f466
marquesfm <francisco.marques@cgi.com>
2019-06-26 23:34:31 +0300

    DROTH-1638 Changing the CI-server - Revert some modification on deploy multiple files done on the branch.

--- ci/config/deploy/staging.rb
+++ ci/config/deploy/staging.rb
@@ -1,3 +1,15 @@
 role :app, %w{web@gateway}
 role :web, %w{web@gateway}
 server 'gateway', user: 'web', roles: %w{web app}
+
+ namespace :deploy do
+  task :start_vallu_server do
+    on roles(:all) do
+      execute "killall -q node; exit 0"
+      execute "cd #{release_path} && chmod 700 start_vallu_server.sh"
+      execute "cd #{release_path} && nohup ./start_vallu_server.sh"
+    end
+  end
+
+   after :publishing, :start_vallu_server
+end
