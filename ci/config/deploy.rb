7c451659c695d530138d24547a1f9bd4e7c2236a
marquesfm <francisco.marques@cgi.com>
2019-06-26 18:59:14 +0300

    DROTH-1638 Changing the CI-server - Fiz server

--- ci/config/deploy.rb
+++ ci/config/deploy.rb
@@ -17,30 +17,32 @@ namespace :deploy do
       execute "cp #{deploy_to}/newrelic/* #{release_path}/."
       execute "cd #{release_path} && chmod 700 start.sh"
       execute "cd #{release_path} && nohup ./start.sh"
-      execute "cd #{release_path} && tmux new -s 'digiroad2' -d"
+      execute "cd #{release_path} && tmux new -s 'waiting for your commands' -d"
     end
   end
 
   task :prepare_release do
     on roles(:all) do |host|
-      execute "tmux kill-session -t 'digiroad2' || true"
+      execute "tmux kill-session -t 'waiting for your commands' || true"
       execute "mkdir -p #{release_path}/tmp"
       execute "cd #{release_path} && npm install && export TMPDIR=#{release_path}/tmp && yarn install && grunt deploy --target=#{fetch(:grunt_target)}"
       execute "cd #{deploy_path} && mkdir #{release_path}/digiroad2-oracle/lib && cp oracle/* #{release_path}/digiroad2-oracle/lib/."
       execute "mkdir -p #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}"
+      execute "cd #{deploy_path} && cp digiroad2.properties #{release_path}/conf/staging/."
       execute "cd #{deploy_path} && cp bonecp.properties #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp conversion.bonecp.properties #{release_path}/digiroad2-oracle/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp authentication.properties #{release_path}/conf/#{fetch(:stage)}/."
+      execute "cd #{deploy_path} && cp smtp.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/conf/#{fetch(:stage)}/."
       execute "cd #{deploy_path} && cp keys.properties #{release_path}/digiroad2-oracle/src/test/resources/."
       execute "cd #{release_path} && cp revision.properties #{release_path}/conf/#{fetch(:stage)}/. || echo 'SKIP: No revision information available'"
       execute "cd #{release_path} && ln -s /data1/logs/digiroad2 logs"
       execute "cd #{release_path} && ./sbt -Ddigiroad2.env=#{fetch(:stage)} assembly"
-      execute "cd #{release_path} && rsync -a dist/ src/main/webapp/digiroad2/"
-      execute "cd #{release_path} && rsync -a --exclude-from 'copy_exclude.txt' UI/ src/main/webapp/digiroad2/"
-      execute "cd #{release_path} && rsync -a node_modules src/main/webapp/digiroad2/"
+      execute "cd #{release_path} && rsync -a dist/ src/main/webapp/"
+      execute "cd #{release_path} && rsync -a --exclude-from 'copy_exclude.txt' UI/ src/main/webapp/"
+      execute "cd #{release_path} && rsync -a node_modules src/main/webapp/"
       execute "cd #{release_path} && chmod 700 stop.sh"
-      execute "cd #{release_path} && ./stop.sh; exit 0"
+      execute "cd #{release_path} && ./stop.sh"
       execute "cd #{release_path} && ./sbt -Ddigiroad2.env=#{fetch(:stage)} 'project digiroad2-oracle' 'test:run-main fi.liikennevirasto.digiroad2.util.DatabaseMigration'"
     end
   end
