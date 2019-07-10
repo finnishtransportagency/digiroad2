1ff1210d5738d3e18caea95c9a6134b223251d53
marquesfm <francisco.marques@cgi.com>
2019-07-10 16:47:21 +0300

    DROTH-1638 Changing the CI-server - Apply Integration tag to perform QA deploy.

--- gruntfile.js
+++ gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function(grunt) {
       staging: {
         NODE_ENV: 'STAGING'
       },
+      integration: {
+        NODE_ENV: 'INTEGRATION'
+      },
       production: {
         NODE_ENV: 'PRODUCTION'
       }
