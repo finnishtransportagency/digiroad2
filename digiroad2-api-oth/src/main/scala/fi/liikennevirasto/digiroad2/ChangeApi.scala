910f46eb84a309a365cb7840d3eeaffc48cacfd4
marquesfm <francisco.marques@cgi.com>
2019-07-11 20:20:24 +0300

    DROTH-1860 Publishing the XML for bus stop change-info (Changes after Code Review) - Fix DateParser Import on ChangeApi and MassTransitStop.

--- digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/ChangeApi.scala
+++ digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/ChangeApi.scala
@@ -1,6 +1,7 @@
 package fi.liikennevirasto.digiroad2
 
 import fi.liikennevirasto.digiroad2.Digiroad2Context._
+import fi.liikennevirasto.digiroad2.asset.DateParser.DateTimePropertyFormat
 import fi.liikennevirasto.digiroad2.asset._
 import fi.liikennevirasto.digiroad2.linearasset.DynamicValue
 import fi.liikennevirasto.digiroad2.service.ChangedVVHRoadlink
