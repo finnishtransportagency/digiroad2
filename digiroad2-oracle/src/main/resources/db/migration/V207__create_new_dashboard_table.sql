c708b7c7ed8fd0953538d3202cff8a0374e3b83f
moraispedro <pedro.morais@cgi.com>
2019-07-12 17:56:09 +0300

    Minor commit on Flyway name of column

--- digiroad2-oracle/src/main/resources/db/migration/V207__create_new_dashboard_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V207__create_new_dashboard_table.sql
@@ -2,6 +2,6 @@ create table dashboard_info (
 	municipality_id number(22),
 	asset_type_id number(22),
 	modified_by varchar2(128),
-	last_modified_by timestamp,
+	last_modified_date timestamp,
 	foreign key (municipality_id) references municipality(id)
 );
