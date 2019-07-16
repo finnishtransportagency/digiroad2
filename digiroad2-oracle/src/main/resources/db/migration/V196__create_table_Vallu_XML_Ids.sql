5dc7b02f6a001177f35a37922ed62528dedb6dac
moraispedro <pedro.morais@cgi.com>
2019-07-16 19:15:15 +0300

    Flyway version fix

--- digiroad2-oracle/src/main/resources/db/migration/V196__create_table_Vallu_XML_Ids.sql
+++
@@ -1,7 +0,0 @@
-
-create table vallu_xml_ids (
-  id number primary key,
-  asset_id number references asset not null,
-  created_date DATE default sysdate,
- constraint assetId_createDate UNIQUE (asset_id, created_date)
-);
