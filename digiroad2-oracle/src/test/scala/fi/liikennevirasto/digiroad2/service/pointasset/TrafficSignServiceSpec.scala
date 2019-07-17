18e62c35d85c86699b8a43d09fcc8e2ba264adbf
marquesfm <francisco.marques@cgi.com>
2019-07-17 14:04:51 +0300

    DROTH-1936 Information board asset to traffic signs (Changes After merge master) - Fix Unit test relevant to InformationSigns type.

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/TrafficSignServiceSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/service/pointasset/TrafficSignServiceSpec.scala
@@ -673,7 +673,9 @@ class TrafficSignServiceSpec extends FunSuite with Matchers with BeforeAndAfter 
     val groupType = TrafficSignTypeGroup.InformationSigns
     val assetsIncluded = TrafficSignType.apply(groupType)
 
-    assetsIncluded.toSeq.sorted should be (Seq(113, 114, 115, 116, 117, 118, 119))
+    assetsIncluded.toSeq.sorted should be(Seq(113, 114, 115, 116, 117, 118, 119, 152, 153, 154, 155, 156, 157, 158,
+      159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181,
+      182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193))
   }
 
   test("Get by municipality and group"){
