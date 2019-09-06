a06eed10382a81b39cedeababf15271b8d24fbac
FilPinto <filipe.pinto@cgi.com>
2019-09-06 12:12:08 +0100

    DROTH-2014 (US 2048) DataBase: Update dummy data related with user information

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/user/UserConfigurationApi.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/user/UserConfigurationApi.scala
@@ -25,7 +25,7 @@ class UserConfigurationApi extends ScalatraServlet with JacksonJsonSupport
 
   get("/user/:username") {
     params.get("username").flatMap {
-      userProvider.getUser
+      userProvider.getUser(_)
     } match {
       case Some(u) => u
       case None => NotFound("User not found: " + params.get("username").getOrElse(""))
@@ -47,7 +47,7 @@ class UserConfigurationApi extends ScalatraServlet with JacksonJsonSupport
     // TODO: when implementing UI, use municipalities of client to determine authorization, only modify (add/remove) authorized municipalities
     val municipalities = parsedBody.extract[List[Int]].toSet
     params.get("username").flatMap {
-      userProvider.getUser
+      userProvider.getUser(_)
     }.map { u =>
       val updatedUser = u.copy(configuration = u.configuration.copy(authorizedMunicipalities = municipalities))
       userProvider.saveUser(updatedUser)
@@ -61,7 +61,7 @@ class UserConfigurationApi extends ScalatraServlet with JacksonJsonSupport
   put("/user/:username/name") {
     val name = parsedBody.extract[String]
     params.get("username").flatMap {
-      userProvider.getUser
+      userProvider.getUser(_)
     }.map { u =>
       val updatedUser = u.copy(name = Some(name))
       userProvider.saveUser(updatedUser)
@@ -149,7 +149,7 @@ class UserConfigurationApi extends ScalatraServlet with JacksonJsonSupport
   put("/user/:username/roles") {
     val newRoles = parsedBody.extract[List[String]].toSet
     params.get("username").flatMap {
-      userProvider.getUser
+      userProvider.getUser(_)
     }.map { u =>
       val updatedUser = u.copy(configuration = u.configuration.copy(roles = newRoles))
       userProvider.saveUser(updatedUser)
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/OracleUserProviderSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/OracleUserProviderSpec.scala
@@ -4,86 +4,76 @@ import java.time.LocalDate
 
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
 import fi.liikennevirasto.digiroad2.user.Configuration
-import fi.liikennevirasto.digiroad2.util.SqlScriptRunner._
-import fi.liikennevirasto.digiroad2.util.TestTransactions
 import org.scalatest.{FunSuite, Matchers}
+import slick.driver.JdbcDriver.backend.Database.dynamicSession
 
 class OracleUserProviderSpec extends FunSuite with Matchers {
-  def runWithRollback(test: => Unit): Unit = TestTransactions.runWithRollback()(test)
 
-    val TestUserName = "Oracleuserprovidertest"
+    val testUserName = "Oracleuserprovidertest"
     val north = 1000
     val east = 3000
     val newNorth = 2000
     val newEast = 6000
     val municipalityNumber = 235
 
-    test("create and get user") {
-      runWithRollback {
-        val provider = new OracleUserProvider
-        provider.deleteUser(TestUserName)
+  val provider = new OracleUserProvider
 
-      provider.getUser(TestUserName) shouldBe (None)
-      provider.createUser(TestUserName, Configuration(north = Some(1000)))
-      val user = provider.getUser(TestUserName).get
-      user.username should be(TestUserName.toLowerCase)
+  test("create and get user") {
+    OracleDatabase.withDynTransaction {
+      provider.getUser(testUserName, false) shouldBe (None)
+      provider.createUser(testUserName, Configuration(north = Some(1000)), newTransaction = false)
+      val user = provider.getUser(testUserName, false).get
+      user.username should be(testUserName.toLowerCase)
       user.configuration.north should be(Some(north))
-      }
+      dynamicSession.rollback()
     }
+  }
 
     test("update user last notification date field without update modified_date") {
-      runWithRollback {
-        val provider = new OracleUserProvider
-        provider.deleteUser(TestUserName)
-
-      provider.getUser(TestUserName) should be(None)
-      provider.createUser(TestUserName, Configuration(municipalityNumber = Some(municipalityNumber)))
-      val user = provider.getUser(TestUserName).get
+      OracleDatabase.withDynTransaction {
+     provider.getUser(testUserName, false) should be(None)
+      provider.createUser(testUserName, Configuration(municipalityNumber = Some(municipalityNumber)), newTransaction = false)
+      val user = provider.getUser(testUserName, false).get
 
       val updatedUser = user.copy(configuration = user.configuration.copy(lastNotificationDate = Some(LocalDate.now.toString)))
-      provider.updateUserConfiguration(updatedUser)
-      val userInfo = provider.getUser(TestUserName).get
-      userInfo.username should be(TestUserName.toLowerCase)
+      provider.updateUserConfiguration(updatedUser, false)
+      val userInfo = provider.getUser(testUserName, false).get
+      userInfo.username should be(testUserName.toLowerCase)
       userInfo.configuration.lastNotificationDate should be(Some(LocalDate.now.toString))
       userInfo.configuration.municipalityNumber should be(Some(municipalityNumber))
+        dynamicSession.rollback()
       }
     }
 
     test("update current user default map location") {
-      runWithRollback {
-        val provider = new OracleUserProvider
-        provider.deleteUser(TestUserName)
-
-      provider.getUser(TestUserName) should be(None)
-      provider.createUser(TestUserName, Configuration(east = Some(east), north = Some(north)))
-      val user = provider.getUser(TestUserName).get
+      OracleDatabase.withDynTransaction {
+      provider.getUser(testUserName, false) should be(None)
+      provider.createUser(testUserName, Configuration(east = Some(east), north = Some(north)), newTransaction = false)
+      val user = provider.getUser(testUserName, false).get
 
       val updatedUser = user.copy(configuration = user.configuration.copy(east = Some(newEast), north = Some(newNorth)))
-      provider.updateUserConfiguration(updatedUser)
-      val userInfo = provider.getUser(TestUserName).get
-      userInfo.username should be(TestUserName.toLowerCase)
+      provider.updateUserConfiguration(updatedUser, false)
+      val userInfo = provider.getUser(testUserName, false).get
+      userInfo.username should be(testUserName.toLowerCase)
       userInfo.configuration.east should be(Some(newEast))
       userInfo.configuration.north should be(Some(newNorth))
+        dynamicSession.rollback()
       }
     }
 
-
     test("update user last login date field") {
-      runWithRollback {
-        val provider = new OracleUserProvider
-        provider.deleteUser(TestUserName)
-
-      provider.getUser(TestUserName) should be(None)
-      provider.createUser(TestUserName, Configuration(municipalityNumber = Some(municipalityNumber)))
-      val user = provider.getUser(TestUserName).get
+      OracleDatabase.withDynTransaction {
+      provider.getUser(testUserName, false) should be(None)
+      provider.createUser(testUserName, Configuration(municipalityNumber = Some(municipalityNumber)), newTransaction = false)
+      val user = provider.getUser(testUserName, false).get
 
       val updatedUser = user.copy(configuration = user.configuration.copy(lastLoginDate = Some(LocalDate.now().toString())))
-      provider.updateUserConfiguration(updatedUser)
-      val userInfo = provider.getUser(TestUserName).get
-      userInfo.username should be(TestUserName.toLowerCase)
+      provider.updateUserConfiguration(updatedUser, false)
+      val userInfo = provider.getUser(testUserName, false).get
+      userInfo.username should be(testUserName.toLowerCase)
       userInfo.configuration.lastLoginDate should be(Some(LocalDate.now.toString))
       userInfo.configuration.municipalityNumber should be(Some(municipalityNumber))
-        provider.deleteUser(TestUserName)
+        dynamicSession.rollback()
       }
     }
 }
