cff28e17d1847690c38833e3db337121ea64ad46
oskarmalmstrom <oskar.malmstrom@cgi.com>
2019-09-11 12:50:03 +0300

    Add new property for the signs instead of adding a column on asset

--- digiroad2-oracle/src/main/resources/db/migration/V210__add_new_road_sign_side_column.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V210__add_new_road_sign_side_column.sql
@@ -1,1 +1,14 @@
-alter table asset add sign_road_side char(1) default '0';
+INSERT INTO LOCALIZED_STRING (ID, VALUE_FI, CREATED_BY, CREATED_DATE)
+	VALUES (primary_key_seq.nextval, 'Liikenteenvastainen', 'db_migration_v208', sysdate);
+
+-- New Properties for the new asset
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED, CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+	VALUES (primary_key_seq.nextval, 300, 'single_choice', 0, 'db_migration_v210', 'opposite_side_sign', (select max(id) from LOCALIZED_STRING where VALUE_FI = 'Liikenteenvastainen'));
+
+-- Create default values for the field
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+  VALUES (primary_key_seq.nextval, 1, 'Kyllä', ' ', 'db_migration_v210', (select id from property where public_ID = 'opposite_side_sign'));
+
+INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
+  VALUES (primary_key_seq.nextval, 0, 'Ei', ' ', 'db_migration_v210', (select id from property where public_ID = 'opposite_side_sign'));
+
