222d14e0297fb5ea557937c048a11c7f727cde09
FilPinto <53299933+FilPinto@users.noreply.github.com>
2019-09-23 11:47:20 +0100

    DROTH-2099 Unknown speed limit list disappears some times (#1427)
    * DROTH-2099 Unknown speed limit list disappears some times
    -Fixed form loading first than verification
    
    * DROTH-2099 Unknown speed limit list disappears some times
    
    * DROTH-2099 Unknown speed limit list disappears some times

--- UI/src/application.js
+++ UI/src/application.js
@@ -128,7 +128,7 @@
 
     RoadAddressInfoDataInitializer.initialize(isExperimental);
     MassTransitStopForm.initialize(backend, new FeedbackModel(backend, assetConfiguration, selectedMassTransitStopModel));
-    SpeedLimitForm.initialize(selectedSpeedLimit, new FeedbackModel(backend, assetConfiguration, selectedSpeedLimit));
+    SpeedLimitForm.initialize(selectedSpeedLimit, new FeedbackModel(backend, assetConfiguration, selectedSpeedLimit), backend);
 
     new WorkListView().initialize(backend);
     new VerificationWorkList().initialize();
