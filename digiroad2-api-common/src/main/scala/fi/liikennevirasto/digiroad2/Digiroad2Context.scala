51ea3e379aab262c8a5842e7f9d54ce338cf8b9d
cardadorm <miguel.cardador@cgi.com>
2019-11-14 17:03:40 +0200

    DROTH-217 No new parking prohibitions are created from some traffic signs
    (cherry picked from commit c512958986a235bb4b3775ea42a138871fe87d66)

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
@@ -283,12 +283,8 @@ class TrafficSignExpireAssets(trafficSignService: TrafficSignService, trafficSig
 class TrafficSignUpdateAssets(trafficSignService: TrafficSignService, trafficSignManager: TrafficSignManager) extends Actor {
   def receive = {
     case x: TrafficSignInfoUpdate =>
-       trafficSignService.getPersistedAssetsByIdsWithExpire(Set(x.expireId)).headOption match {
-      case Some(trafficType) => trafficSignManager.deleteAssets(Seq(trafficType))
-                                trafficSignManager.createAssets(x.newSign)
-      case _ => println("Nonexistent traffic Sign Type")
-    }
-      trafficSignManager.createAssets(x.newSign)
+            trafficSignManager.deleteAssets(Seq(x.oldSign))
+            trafficSignManager.createAssets(x.newSign)
     case _ => println("trafficSignUpdateAssets: Received unknown message")
   }
 }
