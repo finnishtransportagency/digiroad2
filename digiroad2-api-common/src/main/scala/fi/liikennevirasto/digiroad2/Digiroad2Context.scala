0e4684c8b72d0c62bfe5ec3941dffcd149341e27
cardadorm <miguel.cardador@cgi.com>
2019-11-29 17:37:13 +0200

    DROTH-2173 turning restrictions links dont show  - remove parameter from municipalityService
    (cherry picked from commit 283a5c6f753aaae5592a6c166ffa9a9cc76a04e4)

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
@@ -479,7 +479,7 @@ object Digiroad2Context {
   }
 
   lazy val municipalityService: MunicipalityService = {
-    new MunicipalityService(eventbus, roadLinkService)
+    new MunicipalityService
   }
 
   lazy val dynamicLinearAssetService: DynamicLinearAssetService = {
