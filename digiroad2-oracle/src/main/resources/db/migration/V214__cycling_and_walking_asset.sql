86118ca428af4f79808485da524b822dd88887d0
simoesri <ricardo.simoes@cgi.com>
2019-12-12 15:54:35 +0000

    Task DROTH-2219 (US: 2190) Fix the sql to flyway

--- digiroad2-oracle/src/main/resources/db/migration/V214__cycling_and_walking_asset.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V214__cycling_and_walking_asset.sql
@@ -18,40 +18,40 @@ INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED,CREATED_BY, PUB
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
 	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,2,'Pyöräily kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,3,'Jalankulun ja pyöräilyn väylä ','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,4,'Maantie tai yksityistie','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,5,'Katu','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,6,'Pyöräkatu','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,7,'Kylätie','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,8,'Kävelykatu','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,9,'Pihakatu','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,10,'Pyöräkaista','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,11,'Pyörätie','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,12,'Kaksisuuntainen pyörätie','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,13,'Yhdistetty pyörätie ja jalkakäytävä, yksisuuntainen pyörille','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,14,'Yhdistetty pyörätie ja jalkakäytävä, kaksisuuntainen pyörille','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,15,'Jalkakäytävä','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,16,'Puistokäytävä','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,17,'Pururata','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,18,'Ajopolku','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,19,'Polku','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
 INSERT INTO ENUMERATED_VALUE (ID, VALUE, NAME_FI, NAME_SV, CREATED_BY, PROPERTY_ID)
-	VALUES (primary_key_seq.nextval,1,'Pyöräily ja kävely kielletty','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
+	VALUES (primary_key_seq.nextval,20,'Lossi tai lautta ','','db_migration_v214',(SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'cyclingAndWalking_type'));
