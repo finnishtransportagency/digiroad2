eb772de5b76aa6e28564a080c6fddcde68000439
simoesri <ricardo.simoes@cgi.com>
2019-12-12 15:10:45 +0000

    Task DROTH-2219 (US: 2190) Fix the sql to flyway

--- digiroad2-oracle/src/main/resources/db/migration/V214_Cycling_and_walking_asset.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V214_Cycling_and_walking_asset.sql
@@ -1,17 +1,17 @@
 
 -- ASSET TYPE VALUE
-INSERT INTO ASSET_TYPE (ID, NAME, GEOMETRY_TYPE, CREATED_DATE,CREATED_BY)
-    VALUES ( 440, 'Käpy tietolaji', 'linear', SYSDATE, 'db_migration_v214');
+INSERT INTO ASSET_TYPE (ID, NAME, GEOMETRY_TYPE, CREATED_BY)
+    VALUES ( 440, 'Käpy tietolaji', 'linear', 'db_migration_v214');
 
 
 -- LOCALIZED_STRING VALUE
-INSERT INTO LOCALIZED_STRING (ID, VALUE_FI, CREATED_DATE, CREATED_BY)
-	VALUES( primary_key_seq.nextval, 'Käpy tietolaji', SYSDATE, 'db_migration_v214' );
+INSERT INTO LOCALIZED_STRING (ID, VALUE_FI, CREATED_BY)
+	VALUES( primary_key_seq.nextval, 'Käpy tietolaji', 'db_migration_v214' );
 
 
 -- PROPERTY VALUE
-INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED,CREATED_DATE,CREATED_BY, NAME_LOCALIZED_STRING_ID, PUBLIC_ID)
-	VALUES (primary_key_seq.nextval, 440, 'single_choice',1,SYSDATE,'db_migration_v214', (SELECT ID FROM  LOCALIZED_STRING WHERE VALUE_FI = 'Käpy tietolaji'), 'cyclingAndWalking_type');
+INSERT INTO PROPERTY (ID, ASSET_TYPE_ID, PROPERTY_TYPE, REQUIRED,CREATED_BY, PUBLIC_ID, NAME_LOCALIZED_STRING_ID)
+	VALUES (primary_key_seq.nextval, 440, 'single_choice',1,'db_migration_v214', 'cyclingAndWalking_type', (SELECT ID FROM  LOCALIZED_STRING WHERE VALUE_FI = 'Käpy tietolaji'));
 
 
 -- ENUMERATED VALUES
