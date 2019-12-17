dbb5f80d7c846a10b1c6fbc5c135e0ab7d373cf1
FilPinto <filipe.pinto@cgi.com>
2019-12-17 14:45:41 +0200

    DROTH-2228 (US 2224) Kalpa-API: Add new fields -Updated the "query" fetch with the dates properties -Added new fields for the dates

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/pointasset/OracleTrafficSignDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/pointasset/OracleTrafficSignDao.scala
@@ -55,6 +55,7 @@ object OracleTrafficSignDao {
                case
                 when ev.name_fi is not null then ev.name_fi
                 when tpv.value_fi is not null then tpv.value_fi
+                when dpv.date_time is not null then to_char(dpv.date_time, 'DD.MM.YYYY')
                 else null
                end as display_value, a.created_by, a.created_date, a.modified_by, a.modified_date, lp.link_source, a.bearing,
                lp.side_code, ap.additional_sign_type, ap.additional_sign_value, ap.additional_sign_info, ap.form_position, case when a.valid_to <= sysdate then 1 else 0 end as expired
@@ -64,6 +65,7 @@ object OracleTrafficSignDao {
         join property p on a.asset_type_id = p.asset_type_id
         left join single_choice_value scv on scv.asset_id = a.id and scv.property_id = p.id and p.property_type = 'single_choice'
         left join text_property_value tpv on tpv.asset_id = a.id and tpv.property_id = p.id and p.property_type = 'text'
+        left join date_property_value dpv on dpv.asset_id = a.id and dpv.property_id = p.id and p.property_type = 'date'
         left join enumerated_value ev on scv.enumerated_value_id = ev.id
         left join additional_panel ap ON ap.asset_id = a.id AND p.PROPERTY_TYPE = 'additional_panel_type'
       """
