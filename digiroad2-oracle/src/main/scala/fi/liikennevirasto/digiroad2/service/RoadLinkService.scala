fad73dbfaf66214d68567880c7d3a1ee0f7fe5e1
oskar.malmstrom@gmail.com <oskar.malmstrom@cgi.com>
2019-12-18 13:48:58 +0200

    removed commented code

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/RoadLinkService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/RoadLinkService.scala
@@ -925,22 +925,6 @@ class RoadLinkService(val vvhClient: VVHClient, val eventbus: DigiroadEventBus, 
       Some(roadLinks.minBy(roadlink => minimumDistance(point, roadlink.geometry)))
   }
 
-//  def getClosestRoadlinkForCarTrafficFromVVH(user: User, point: Point, forCarTraffic: Boolean = true): Seq[VVHRoadlink] = {
-//    val diagonal = Vector3d(10, 10, 0)
-//
-//    val roadLinks = user.isOperator() match {
-//        case true =>  getVVHRoadLinks(BoundingRectangle(point - diagonal, point + diagonal))
-//        case false => getVVHRoadLinks(BoundingRectangle(point - diagonal, point + diagonal), user.configuration.authorizedMunicipalities)
-//      }
-//
-//    val closestRoadLinks = roadLinks.isEmpty match {
-//      case true => Seq.empty[VVHRoadlink]
-//      case false => roadLinks.filter(rl => GeometryUtils.minimumDistance(point, rl.geometry) <= 10.0)
-//    }
-//
-//    if(forCarTraffic) closestRoadLinks.filter(_.featureClass != FeatureClass.CycleOrPedestrianPath) else closestRoadLinks.filter(_.featureClass == FeatureClass.CycleOrPedestrianPath)
-//  }
-
   def getClosestRoadlinkForCarTrafficFromVVH(user: User, point: Point, forCarTraffic: Boolean = true): Seq[RoadLink] = {
     val diagonal = Vector3d(10, 10, 0)
 
