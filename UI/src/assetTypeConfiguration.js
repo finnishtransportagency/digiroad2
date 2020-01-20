be67d1e6d4e28a1b5531b63cc89e82c66042c72e
simoesri <ricardo.simoes@cgi.com>
2020-01-20 14:08:34 +0000

    Bug DROTH-2232 - Fix when move the sign and save.

--- UI/src/assetTypeConfiguration.js
+++ UI/src/assetTypeConfiguration.js
@@ -963,6 +963,7 @@
         title: 'Liikennemerkit',
         allowComplementaryLinks: true,
         newAsset: { validityDirection: 2, propertyData: [
+          {'name': 'Liikenteenvastainen', 'propertyType': 'single_choice', 'publicId': "opposite_side_sign", values: [] },
           {'name': 'Tyyppi', 'propertyType': 'single_choice', 'publicId': "trafficSigns_type", values: [ {propertyValue: 1} ] },
           {'name': "Arvo", 'propertyType': 'text', 'publicId': "trafficSigns_value", values: []},
           {'name': "Lisatieto", 'propertyType': 'text', 'publicId': "trafficSigns_info", values: []},
@@ -987,6 +988,11 @@
             { types: [1, 2, 3, 4], validate: function (someValue) { return /^\d+$/.test(someValue) && _.includes(possibleSpeedLimitsValues, parseInt(someValue)); }},
             { types: [8, 30, 31, 32, 33, 34, 35], validate: function (someValue) { return /^\d*\.?\d+$/.test(someValue) ; }}
           ];
+
+          var opposite_side_sign =  selectedAsset.get().propertyData.find( function(prop) { if (prop.publicId === "opposite_side_sign") return prop; }) ;
+          if (_.isUndefined(opposite_side_sign) || _.isUndefined(opposite_side_sign.values[0]) || opposite_side_sign.values[0].propertyValue == "") {
+            selectedAsset.setPropertyByPublicId('opposite_side_sign', '0');
+          }
 
           var functionFn = _.find(validations, function(validation){ return _.includes(validation.types, parseInt(Property.getPropertyValue('Tyyppi', selectedAsset.get())));});
           return functionFn ?  functionFn.validate(Property.getPropertyValue('Arvo', selectedAsset.get())) : true;
