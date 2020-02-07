d12644b0a14ee5a25353d1cd31c4a9fb31786468
FilPinto <filipe.pinto@cgi.com>
2020-02-07 11:21:50 +0200

    DROTH-2252 (US 2240) Create all APIs at BackendUtil -Fixed wrong fetch

--- UI/src/utils/backend-utils.js
+++ UI/src/utils/backend-utils.js
@@ -274,6 +274,12 @@
       };
     });
 
+    this.getLanesWithComplementaryByBoundigBox = latestResponseRequestor(function(boundingBox, zoom) {
+      return {
+        url: 'api/lanes/complementary?bbox=' + boundingBox + '&zoom=' + zoom
+      };
+    });
+
     this.getReadOnlyLinearAssets = latestResponseRequestor(function(boundingBox, typeId, withRoadAddress) {
       return {
         url: 'api/linearassets/massLimitation?bbox=' + boundingBox + '&typeId=' + typeId + '&withRoadAddress=' + withRoadAddress
