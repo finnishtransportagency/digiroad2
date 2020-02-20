a8b2efe99f578900eb238311d15c6a8f409d6e03
marquesfm <francisco.marques@cgi.com>
2020-02-20 20:44:57 +0200

    DROTH-2212 Municipality merger -Added batch to task runners of all environments -No need to update center geometry so removed the comment with it -Removed comments -Semicoma removed from update and delete queries.
    (cherry picked from commit 06e4301)
    (cherry picked from commit e4be4ad91b0bc8c13eef17fda3ee392954d60b04)
    
    # Conflicts:
    #	ci/pipeline/dev_task_runner
    #	ci/pipeline/prod_task_runner
    #	ci/pipeline/qa_task_runner
    #	ci/pipeline/staging_task_runner

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/queries.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/queries.scala
@@ -370,19 +370,12 @@ object Queries {
   }
 
   def mergeMunicipalities(municipalityToDelete: Int, municipalityToMerge: Int): Unit = {
-    sqlu"""UPDATE ASSET SET MUNICIPALITY_CODE = $municipalityToMerge, MODIFIED_DATE = SYSDATE, MODIFIED_BY = 'batch_process_municipality_merge' WHERE MUNICIPALITY_CODE = $municipalityToDelete;""".execute
-    sqlu"""UPDATE UNKNOWN_SPEED_LIMIT SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete;""".execute
-    sqlu"""UPDATE INACCURATE_ASSET SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete;""".execute
-    sqlu"""UPDATE INCOMPLETE_LINK SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete;""".execute
-
-    // should be automatic, so maybe this query is not needed
-    // confirm this
-    sqlu"""UPDATE TEMP_ROAD_ADDRESS_INFO SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete; """.execute
-
-    // Update on municipalityToMerge should be in daily batch so no need to update that here only delete the municipalityToDelete rows
-    // confirm this
-    sqlu"""DELETE FROM MUNICIPALITY_VERIFICATION WHERE MUNICIPALITY_ID = $municipalityToDelete;""".execute
-
-    sqlu"""DELETE FROM MUNICIPALITY WHERE ID = $municipalityToDelete;""".execute
+    sqlu"""UPDATE ASSET SET MUNICIPALITY_CODE = $municipalityToMerge, MODIFIED_DATE = SYSDATE, MODIFIED_BY = 'batch_process_municipality_merge' WHERE MUNICIPALITY_CODE = $municipalityToDelete""".execute
+    sqlu"""UPDATE UNKNOWN_SPEED_LIMIT SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete""".execute
+    sqlu"""UPDATE INACCURATE_ASSET SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete""".execute
+    sqlu"""UPDATE INCOMPLETE_LINK SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete""".execute
+    sqlu"""UPDATE TEMP_ROAD_ADDRESS_INFO SET MUNICIPALITY_CODE = $municipalityToMerge WHERE MUNICIPALITY_CODE = $municipalityToDelete""".execute
+    sqlu"""DELETE FROM MUNICIPALITY_VERIFICATION WHERE MUNICIPALITY_ID = $municipalityToDelete""".execute
+    sqlu"""DELETE FROM MUNICIPALITY WHERE ID = $municipalityToDelete""".execute
   }
 }
