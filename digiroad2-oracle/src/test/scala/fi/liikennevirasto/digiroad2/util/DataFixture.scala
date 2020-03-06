16a515e6f9ef26289d43afa36c88d917abd17289
marquesfm <francisco.marques@cgi.com>
2020-03-06 17:15:10 +0200

    DROTH-2307 OTH crashes on both production servers - Created new batch to fill new roadlinks with previous speedlimit information (Only Prints on this version).
    (cherry picked from commit bc455ce)

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/DataFixture.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/DataFixture.scala
@@ -2285,6 +2285,55 @@ object DataFixture {
     println("Complete at time: " + DateTime.now())
   }
 
+  def fillNewRoadLinksWithPreviousInfo(): Unit = {
+    println("\nStart process to fill new road links with previous data, only for change type 12")
+    println(DateTime.now())
+    println("\n")
+
+    //Get All Municipalities
+    val municipalities: Seq[Int] = Seq(49, 734, 286) /*OracleDatabase.withDynSession {
+      Queries.getMunicipalities
+    }*/
+
+    OracleDatabase.withDynTransaction {
+      municipalities.foreach { municipality =>
+        println("\nWorking at Municipailty: " + municipality)
+        val (roadLinks, changes) = roadLinkService.getRoadLinksWithComplementaryAndChangesFromVVHByMunicipality(municipality, newTransaction = false)
+
+        val filteredRoadLinks = roadLinks.filter(r => r.isCarRoadOrCyclePedestrianPath)
+
+        val speedLimits =
+          filteredRoadLinks.flatMap { r =>
+            speedLimitService.getExistingAssetByRoadLink(r)
+          }
+
+        val newSpeedLimits = speedLimitService.newChangeAsset(roadLinks, speedLimits, changes).groupBy(_.linkId)
+
+        newSpeedLimits.foreach { case (speedLimitLinkId, speedLimitsToCreate) =>
+          val speedLimitRoadLink = filteredRoadLinks.find(_.linkId == speedLimitLinkId).get
+
+          //Create new SpeedLimits on gaps
+          speedLimitsToCreate.foreach { sl =>
+//            speedLimitDao.createSpeedLimit(sl.createdBy.getOrElse(LinearAssetTypes.VvhGenerated), sl.linkId, Measures(sl.startMeasure, sl.endMeasure),
+//              sl.sideCode, sl.value.get.value, Some(sl.vvhTimeStamp), sl.createdDateTime, sl.modifiedBy,
+//              sl.modifiedDateTime, sl.linkSource)
+            println("\nNew SpeedLimit created at Link Id: " + sl.linkId)
+
+            //Remove linkIds from Unknown Speed Limits working list after speedLimit creation
+//            speedLimitDao.purgeFromUnknownSpeedLimits(speedLimitLinkId, GeometryUtils.geometryLength(speedLimitRoadLink.geometry))
+            println("Removed linkId " + speedLimitLinkId + "UnknownSpeedLimits from working list")
+            println("\n")
+          }
+        }
+      }
+
+      println("\n")
+      println("Complete at time: ")
+      println(DateTime.now())
+      println("\n")
+    }
+  }
+
   private val trafficSignGroup = Map[String, TrafficSignTypeGroup] (
     "SpeedLimits" -> TrafficSignTypeGroup.SpeedLimits,
     "RegulatorySigns" ->  TrafficSignTypeGroup.RegulatorySigns,
@@ -2438,6 +2487,8 @@ object DataFixture {
         addObstaclesShapefile()
       case Some("merge_municipalities") =>
         mergeMunicipalities()
+      case Some("fill_new_roadLinks_info") =>
+        fillNewRoadLinksWithPreviousInfo()
       case _ => println("Usage: DataFixture test | import_roadlink_data |" +
         " split_speedlimitchains | split_linear_asset_chains | dropped_assets_csv | dropped_manoeuvres_csv |" +
         " unfloat_linear_assets | expire_split_assets_without_mml | generate_values_for_lit_roads | get_addresses_to_masstransitstops_from_vvh |" +
@@ -2452,7 +2503,7 @@ object DataFixture {
         " merge_additional_panels_to_trafficSigns | create_traffic_signs_using_linear_assets | create_prohibition_using_traffic_signs | " +
         " create_hazmat_transport_prohibition_using_traffic_signs  | create_parking_prohibition_using_traffic_signs | load_municipalities_verification_info |" +
         " resolving_Frozen_Links| import_private_road_info | normalize_user_roles | get_state_roads_with_overridden_functional_class | get_state_roads_with_undefined_functional_class |" +
-        " add_obstacles_shapefile | merge_municipalities")
+        " add_obstacles_shapefile | merge_municipalities | fill_new_roadLinks_info")
     }
   }
 }
