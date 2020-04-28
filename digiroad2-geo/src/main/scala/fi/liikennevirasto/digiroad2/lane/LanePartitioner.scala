686e6650701447787e4dc235e42ec94c1f805158
FilPinto <filipe.pinto@cgi.com>
2020-04-28 16:52:58 +0300

    DROTH-2244 Changes after code review -Fixed grouping

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/lane/LanePartitioner.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/lane/LanePartitioner.scala
@@ -5,22 +5,31 @@ import fi.liikennevirasto.digiroad2.linearasset.{GraphPartitioner, RoadLink}
 
 object LanePartitioner extends GraphPartitioner {
 
-  def partition[T <: Lane](links: Seq[T], roadLinksForSpeedLimits: Map[Long, RoadLink]): Seq[Seq[T]] = {
-    val (twoWayLinks, oneWayLinks) = links.partition(_.sideCode == SideCode.BothDirections.value)
-    val linkGroups = oneWayLinks.groupBy { link =>
-      val roadLink = roadLinksForSpeedLimits.get(link.linkId)
-      val roadIdentifier = roadLink.flatMap(_.roadIdentifier)
-      (roadIdentifier, roadLink.map(_.administrativeClass), link.laneAttributes, link.id == 0)
+  def partition[T <: Lane](lanes: Seq[T], roadLinks: Map[Long, RoadLink]): Seq[Seq[T]] = {
+    def groupLanes(lanes: Seq[T]): Seq[Seq[T]] = {
+      val groupedLanesByRoadLinkAndSideCode = lanes.groupBy(lane => (lane.linkId, lane.sideCode))
+
+      val linkGroups = groupedLanesByRoadLinkAndSideCode.groupBy { case ((roadLinkId, _), lanes) =>
+        val allLanesAttributes = lanes.flatMap(_.laneAttributes).sortBy(laneProp => (laneProp.publicId, laneProp.values.head.value.toString))
+        val roadLink = roadLinks.get(roadLinkId)
+        val roadIdentifier = roadLink.flatMap(_.roadIdentifier)
+
+        (roadIdentifier, roadLink.map(_.administrativeClass), allLanesAttributes, roadLinkId == 0)
+      }
+
+      val (linksToPartition, linksToPass) = linkGroups.partition { case ((roadIdentifier, _, _, _), _) => roadIdentifier.isDefined }
+      val clustersAux = linksToPartition.values.map(_.values.flatten.filter(lane =>
+        LaneNumber.isMainLane(lane.laneAttributes.find(_.publicId == "lane_code").get.values.head.value.asInstanceOf[Int]))
+      )
+
+      val clusters = for (linkGroup <- clustersAux.asInstanceOf[Seq[Seq[T]]];
+                          cluster <- clusterLinks(linkGroup)) yield cluster
+
+      clusters.map(linksFromCluster) ++ linksToPass.values.flatMap(_.values).toSeq
     }
 
-    val (linksToPartition, linksToPass) = linkGroups.partition { case ((roadIdentifier, _, _, _), _) => roadIdentifier.isDefined }
-
-    val clusters = for (linkGroup <- linksToPartition.values.toSeq;
-                        cluster <- clusterLinks(linkGroup)) yield cluster
-
-    val linkPartitions = clusters.map(linksFromCluster)
-
-    linkPartitions ++ linksToPass.values.flatten.map(x => Seq(x)) ++ twoWayLinks.map(x => Seq(x))
+    val (lanesOnOneDirectionRoad, lanesOnTwoDirectionRoad) = lanes.partition(_.sideCode == SideCode.BothDirections.value)
+    groupLanes(lanesOnOneDirectionRoad) ++ groupLanes(lanesOnTwoDirectionRoad)
   }
 
 }
