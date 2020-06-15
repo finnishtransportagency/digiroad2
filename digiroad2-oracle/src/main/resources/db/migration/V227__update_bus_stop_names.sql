47d255229a6915887373160b7ad2b4e38d36d650
FilPinto <filipe.pinto@cgi.com>
2020-06-15 17:59:30 +0300

    DROTH-2434 Change bus stop names
    (cherry picked from commit 48dcaac)

---
+++ digiroad2-oracle/src/main/resources/db/migration/V227__update_bus_stop_names.sql
@@ -0,0 +1,3 @@
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Linja-autopysäkki' WHERE NAME_FI = 'Linja-autojen paikallisliikenne';
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Kaukoliikenne (vanha tll)' WHERE NAME_FI = 'Linja-autojen kaukoliikenne';
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Pikavuoro (vanha tll)' WHERE NAME_FI = 'Linja-autojen pikavuoro';
