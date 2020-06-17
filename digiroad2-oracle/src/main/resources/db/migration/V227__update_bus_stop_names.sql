4d8230203520497b7eb9caf93e619eca75dd7785
FilPinto <filipe.pinto@cgi.com>
2020-06-17 13:01:32 +0300

    DROTH-2434 Change bus stop names -Alter sql updates to be more clear on what are we updating
    (cherry picked from commit efeb3d2)

--- digiroad2-oracle/src/main/resources/db/migration/V227__update_bus_stop_names.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V227__update_bus_stop_names.sql
@@ -1,3 +1,8 @@
-UPDATE ENUMERATED_VALUE SET NAME_FI = 'Linja-autopysäkki' WHERE NAME_FI = 'Linja-autojen paikallisliikenne';
-UPDATE ENUMERATED_VALUE SET NAME_FI = 'Kaukoliikenne (vanha tll)' WHERE NAME_FI = 'Linja-autojen kaukoliikenne';
-UPDATE ENUMERATED_VALUE SET NAME_FI = 'Pikavuoro (vanha tll)' WHERE NAME_FI = 'Linja-autojen pikavuoro';
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Linja-autopysäkki'
+WHERE NAME_FI = 'Linja-autojen paikallisliikenne' AND PROPERTY_ID = (SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'pysakin_tyyppi');
+
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Kaukoliikenne (vanha tll)'
+WHERE NAME_FI = 'Linja-autojen kaukoliikenne' AND PROPERTY_ID = (SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'pysakin_tyyppi');
+
+UPDATE ENUMERATED_VALUE SET NAME_FI = 'Pikavuoro (vanha tll)'
+WHERE NAME_FI = 'Linja-autojen pikavuoro' AND PROPERTY_ID = (SELECT ID FROM PROPERTY WHERE PUBLIC_ID = 'pysakin_tyyppi');
