d3fe78b2d9ccc12aba22f2d7b29e710502ac6736
Robert Visan <robert.visan@cgi.com>
2020-08-25 19:48:18 +0300

    Droth-2519 Color codes to the traffic lights when there are many devices in the same spot

--- UI/src/assetTypeConfiguration.js
+++ UI/src/assetTypeConfiguration.js
@@ -1286,7 +1286,8 @@
           ],
           newValues: [
             {symbolUrl: 'src/resources/digiroad2/bundle/assetlayer/images/direction-arrow.svg', label: 'Opastinlaite'},
-            {symbolUrl: 'src/resources/digiroad2/bundle/assetlayer/images/no-direction.svg', label: 'Useita erisuuntaisia opastinlaitteita'}
+            {symbolUrl: 'src/resources/digiroad2/bundle/assetlayer/images/direction-arrow-for-multiple.svg', label: 'label'}, //TODO: label to be decided
+            {symbolUrl: 'src/resources/digiroad2/bundle/assetlayer/images/no-direction-for-multiple.svg', label: 'Useita erisuuntaisia opastinlaitteita'}
           ]
         },
         formLabels: {
