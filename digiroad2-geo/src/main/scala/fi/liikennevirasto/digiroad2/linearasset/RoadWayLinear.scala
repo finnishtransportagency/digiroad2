6e85434821d43c4ef3599b267d224be1054c68a9
sasuolander <sasu.olander@sitowise.com>
2020-09-23 17:25:02 +0300

    DROTH-2541 add type to asses files, possible create factory method to create dynamicvariabe

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/RoadWayLinear.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/RoadWayLinear.scala
@@ -2,6 +2,7 @@ package fi.liikennevirasto.digiroad2.linearasset
 
 import java.util.Optional
 
+import fi.liikennevirasto.digiroad2.asset.{DynamicProperty, DynamicPropertyValue}
 import org.joda.time.DateTime
 
 case class LengthOfRoadAxisModel(
@@ -21,6 +22,47 @@ case class LengthOfRoadAxisModel(
                           start_date: DateTime,
                           end_date: DateTime
                         )
+case class LengthOfRoadAxisModelDynamic(
+                                  regulatoryNumber: String,
+                                  laneNumber: Int,
+                                  laneType: Optional[Int],
+                                  locationRelativeToLane: String,
+                                  municipalId: String,
+                                  condition: Optional[Int],
+                                  material: Optional[Int],
+                                  length: Optional[Int],
+                                  width: Optional[Int],
+                                  raised: Optional[Int],
+                                  milled: Optional[Int],
+                                  additional_info: Int,
+                                  state: Int,
+                                  start_date: DateTime,
+                                  end_date: DateTime
+                                )
+
+
+
+class LengthOfRoadAxisMethod{
+  private def createDynamicField()={
+    DynamicValue(DynamicAssetValue(Seq(
+      DynamicProperty("regulatory_number", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("lane_number", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("lane_type", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("lane_location", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("material", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("length", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("width", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("profile_mark", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("additional_information", "string", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("state", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("end_date", "date", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("start_date", "date", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("milled", "single_choice", required = false, Seq(DynamicPropertyValue(""))),
+      DynamicProperty("condition", "single_choice", required = false, Seq(DynamicPropertyValue("")))
+    )))
+  }
+}
+
 
 case class LengthOfRoadAxisCreate(typeId: Int, assetSequence: Seq[NewLinearAsset]) {}
 
