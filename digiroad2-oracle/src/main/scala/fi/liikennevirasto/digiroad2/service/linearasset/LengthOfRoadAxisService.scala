b564c8538cc8b77efa010e7a489f1419a019bfb7
sasuolander <sasu.olander@sitowise.com>
2020-09-29 19:11:11 +0300

    DROTH-2541 remove eventbuss, copy createWithoutTrancaction method, rethink whole thing

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/LengthOfRoadAxisService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/LengthOfRoadAxisService.scala
@@ -4,31 +4,42 @@ import fi.liikennevirasto.digiroad2.DigiroadEventBus
 import fi.liikennevirasto.digiroad2.client.vvh.VVHClient
 import fi.liikennevirasto.digiroad2.dao.{DynamicLinearAssetDao, MunicipalityDao, OracleAssetDao}
 import fi.liikennevirasto.digiroad2.dao.linearasset.OracleLinearAssetDao
-import fi.liikennevirasto.digiroad2.linearasset.{NewLinearAsset, Value, LengthOfRoadAxisCreate, LengthOfRoadAxisUpdate}
+import fi.liikennevirasto.digiroad2.linearasset.{DynamicValue, LengthOfRoadAxisCreate, LengthOfRoadAxisUpdate, NewLinearAsset, RoadLinkLike, Value}
 import fi.liikennevirasto.digiroad2.service.RoadLinkService
 import fi.liikennevirasto.digiroad2.util.PolygonTools
 import org.apache.commons.lang3.NotImplementedException
+import org.joda.time.DateTime
 
 
 
 class LengthOfRoadAxisService(roadLinkServiceImpl: RoadLinkService,
                               eventBusImpl: DigiroadEventBus) extends DynamicLinearAssetService(roadLinkServiceImpl: RoadLinkService, eventBusImpl: DigiroadEventBus) {
-  override def roadLinkService: RoadLinkService = roadLinkServiceImpl
 
-  override def dao: OracleLinearAssetDao = new OracleLinearAssetDao(roadLinkServiceImpl.vvhClient, roadLinkServiceImpl)
-
-  override def municipalityDao: MunicipalityDao = new MunicipalityDao
-
-  override def eventBus: DigiroadEventBus = eventBusImpl
-
-  override def vvhClient: VVHClient = roadLinkServiceImpl.vvhClient
-
-  override def polygonTools: PolygonTools = new PolygonTools()
-
-  override def assetDao: OracleAssetDao = new OracleAssetDao
-
-  override def dynamicLinearAssetDao: DynamicLinearAssetDao = new DynamicLinearAssetDao
   //crud
+
+  override def createWithoutTransaction(typeId: Int, linkId: Long, value: Value, sideCode: Int, measures: Measures, username: String,
+                                        vvhTimeStamp: Long = vvhClient.roadLinkData.createVVHTimeStamp(),
+                                        roadLink: Option[RoadLinkLike], fromUpdate: Boolean = false,
+                                        createdByFromUpdate: Option[String] = Some(""),
+                                        createdDateTimeFromUpdate: Option[DateTime] = Some(DateTime.now()),
+                                        verifiedBy: Option[String] = None, informationSource: Option[Int] = None): Long = {
+
+    val id = dao.createLinearAsset(typeId, linkId, expired = false, sideCode, measures, username,
+      vvhTimeStamp, getLinkSource(roadLink), fromUpdate, createdByFromUpdate,
+      createdDateTimeFromUpdate, verifiedBy, informationSource = informationSource)
+
+    value match {
+      case DynamicValue(multiTypeProps) =>
+        val properties = setPropertiesDefaultValues(multiTypeProps.properties, roadLink)
+        val defaultValues = dynamicLinearAssetDao.propertyDefaultValues(typeId).filterNot(
+                            defaultValue => properties.exists(_.publicId == defaultValue.publicId))
+        val props = properties ++ defaultValues.toSet
+        validateRequiredProperties(typeId, props)
+        dynamicLinearAssetDao.updateAssetProperties(id, props, typeId)
+      case _ => None
+    }
+    id
+  }
 
   /// create
   def createRoadwayLinear
@@ -36,8 +47,7 @@ class LengthOfRoadAxisService(roadLinkServiceImpl: RoadLinkService,
    username: String,
    vvhTimeStamp: Long = vvhClient.roadLinkData.
      createVVHTimeStamp()) = {
-    //TODO validate link
-    val addedElementId = withDynTransaction {
+    withDynTransaction {
       for (item <- listOfAsset) yield {
         item.assetSequence.map{asset=>
           val roadlink = roadLinkService.getRoadLinksAndComplementariesFromVVH(Set(asset.linkId), false).
@@ -49,19 +59,15 @@ class LengthOfRoadAxisService(roadLinkServiceImpl: RoadLinkService,
         }
       }
     }
-    eventBusImpl.publish("LengthOfRoadAxisService:create",addedElementId.toSet)
-    addedElementId
   }
   //TODO unit test
   def expireRoadwayLinear
   (listOfAsset: List[LengthOfRoadAxisUpdate],
    username: String
   ) = {
-    val expiredElement=  withDynTransaction{
+   withDynTransaction{
       for (item <- listOfAsset) yield {super.expire(item.ids, username)}
     }
-    eventBusImpl.publish("LengthOfRoadAxisService:expire",expiredElement.toSet)
-    expiredElement
   }
 
   /// Update
@@ -69,11 +75,8 @@ class LengthOfRoadAxisService(roadLinkServiceImpl: RoadLinkService,
   (listOfAsset: List[LengthOfRoadAxisUpdate],
    username: String
   ) = {
-   val modifiedElement=
      withDynTransaction{
        for (item <- listOfAsset) yield {super.updateWithoutTransaction(item.ids, item.value, username)}
      }
-    eventBusImpl.publish("LengthOfRoadAxisService:update",modifiedElement.toSet)
-    modifiedElement
   }
 }
