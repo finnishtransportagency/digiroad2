7020f4d76b8e86edb98825e72c1773bad2368bac
sasuolander <sasu.olander@sitowise.com>
2020-10-15 09:23:59 +0300

    DROTH-2541 formating

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/LengthOfRoadAxisService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/LengthOfRoadAxisService.scala
@@ -10,28 +10,30 @@ class LengthOfRoadAxisService(roadLinkServiceImpl: RoadLinkService,
   extends DynamicLinearAssetService(roadLinkServiceImpl: RoadLinkService,
     eventBusImpl: DigiroadEventBus) {
 
-    override def validateCondition(asset: NewLinearAsset): Unit = {
-        val value = asset.value.asInstanceOf[DynamicValue].value
-        val PT_regulatory_number = value.properties.filter(_.publicId == "PT_regulatory_number")
+  override def validateCondition(asset: NewLinearAsset): Unit = {
+    val value = asset.value.asInstanceOf[DynamicValue].value
+    val PT_regulatory_number = value.properties.filter(_.publicId == "PT_regulatory_number")
 
-        val validateEnumurationExist =(item:Seq[DynamicProperty])=>{
-            if (item.isEmpty) throw new Exception("mandatory")
-            else {
-                val innerValue = item.head.values
-                if (innerValue.isEmpty) throw new Exception("Empty values")
-                else
-                    innerValue.head.value
-            }
-        }
-        val assestsInLink= super.getPersistedAssetsByLinkIds(460,Seq(asset.linkId))
-
-        val PT_regulatory_numberFromLink=assestsInLink
-          .map(item => item.value.getOrElse("error").asInstanceOf[DynamicValue]
-            .value.properties.filter(_.publicId == "PT_regulatory_number")).flatMap(item => item.map(item => item.values.map(item => item.value.toString)))
-      var expresion =PT_regulatory_numberFromLink.contains(Seq(validateEnumurationExist(PT_regulatory_number).toString))
-        if (validateEnumurationExist(PT_regulatory_number).toString.toInt == 99) {} else{
-          if(expresion) {
-              throw new Exception("This regulatory number already exist in this roadlink.")}
+    val validateEnumurationExist = (item: Seq[DynamicProperty]) => {
+      if (item.isEmpty) throw new Exception("mandatory")
+      else {
+        val innerValue = item.head.values
+        if (innerValue.isEmpty) throw new Exception("Empty values")
+        else
+          innerValue.head.value.toString
+      }
     }
-        }
+    val assestsInLink = super.getPersistedAssetsByLinkIds(460, Seq(asset.linkId))
+
+    val PT_regulatory_numberFromLink = assestsInLink
+      .map(item => item.value.getOrElse("error").asInstanceOf[DynamicValue]
+        .value.properties.filter(_.publicId == "PT_regulatory_number"))
+      .flatMap(item => item.map(item => item.values.map(item => item.value.toString)))
+    val expresion = PT_regulatory_numberFromLink.contains(Seq(validateEnumurationExist(PT_regulatory_number)))
+    if (validateEnumurationExist(PT_regulatory_number).toInt == 99) {} else {
+      if (expresion) {
+        throw new Exception("This regulatory number already exist in this roadlink.")
+      }
+    }
+  }
 }
