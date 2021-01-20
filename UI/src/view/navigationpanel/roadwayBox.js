5d538485cec429616173a08c3bf38f8c2b83381e
sasuolander <sasu.olander@sitowise.com>
2021-01-20 13:23:29 +0200

    DROTH-2552 add base versio of other roadmark

--- UI/src/view/navigationpanel/roadwayBox.js
+++ UI/src/view/navigationpanel/roadwayBox.js
@@ -14,41 +14,60 @@
 
     this.labeling = function () {
 
-      var roadwayLabel =
-        '<div class="legend-entry">' +
-        '<div class="fat-label"> test</div>' +
-        '</div>';
-
-      var roadwayLines = [
-        [
-          0, 'Keskiviiva'
-        ],
-        [
-          1, 'Keskiviiva ja reunaviiva'
-        ],
-        [
-          2, 'Reunaviiva,ajokaistaviiva ja varoitusviiva'
-        ], [
-          3, 'Reunaviiva'
-        ], [
-          4, 'Ajokaistaviiva'
-        ]
+      var linearMarkingEntries = [
+        {index: 0, text: 'Keskiviiva'},
+        {index: 1, text: 'Keskiviiva ja reunaviiva'},
+        {index: 2, text: 'Reunaviiva,ajokaistaviiva ja varoitusviiva'},
+        {index: 3, text: 'Reunaviiva'},
+        {index: 4, text: 'Ajokaistaviiva'},
       ];
-      var roadwayLengthLegend = roadwayLegend(roadwayLabel, 'roadwayLength', roadwayLines);
 
-      return '<div class="panel-section panel-legend '+ me.legendName() + '-legend">' + roadwayLengthLegend + '</div>';
-    };
+      var widthOfRoadAxisEntries = [
+        {symbolUrl: 'images/mass-transit-stops/1.png', label: 'test'},
+        {symbolUrl: 'images/mass-transit-stops/1.png', label: 'test'},
+      ];
+      var otherMarkingEntries = [
+        {symbolUrl: 'images/mass-transit-stops/1.png', label: 'test'},
+        {symbolUrl: 'images/mass-transit-stops/1.png', label: 'test'},
+      ];
 
-    var roadwayLegend = function(label, className, values, additionalClass) {
-      var _additionalClass = additionalClass ? additionalClass : '';
-      return '<div class="' + className + '-legend ' + _additionalClass + '">' + label + _.map(values, function(value) {
-        return '<div class="legend-entry">' +
-          '<div class="label">' + value[1] + '</div>' +
-          '<div class="symbol linear roadway-' + value[0] + '" />' +
+      var roadwayLabel =
+          '<div class="legend-entry">' +
+          '<div class="fat-label"> test</div>' +
           '</div>';
-      }).join('') + '</div>';
+
+      var linearLegend = legendDiv('roadwayLength', roadwayLinearLegend(linearMarkingEntries), roadwayLabel);
+      var widthOfRoadAxisLegend = legendDiv('roadwayWidth', roadwayPointLikeLegend(widthOfRoadAxisEntries), roadwayLabel);
+      var otherMarkingLegend = legendDiv('roadwayOther', roadwayPointLikeLegend(otherMarkingEntries), roadwayLabel);
+
+      return '<div class="panel-section panel-legend '+ me.legendName() + '-legend">' +
+          linearLegend +
+          widthOfRoadAxisLegend +
+          otherMarkingLegend + '</div>';
+    };
+      var legendDiv =function(className,entries,label){
+          return '<div class="' + className + '-legend'+'">'+label+ entries+'</div>';
+      };
+
+    var roadwayPointLikeLegend = function(values){
+      return _.map(values, function(value) {
+            return '<div class="legend-entry">' +
+                '  <div class="label">' +
+                '    <span>' + value.label + '</span> ' +
+                '    <img class="symbol-to-right" src="' + value.symbolUrl + '"/>' +
+                '  </div>' +
+                '</div>';
+        }).join('');
     };
 
+    var roadwayLinearLegend = function(values) {
+      return _.map(values, function(value) {
+        return '<div class="legend-entry">' +
+          '<div class="label">' + value.text + '</div>' +
+          '<div class="symbol linear roadway-' + value.index + '" />' +
+          '</div>';
+      }).join('');
+    };
 
     this.template = function () {
       this.expanded = me.elements().expanded;
@@ -56,18 +75,29 @@
         if(applicationModel.isDirty()){
           $(me.expanded).find('input[type=radio][name=labelRadio][value=length-of-road-axis]').prop("checked", true);
           $('.roadwayLength-legend').show();
-         // eventbus.trigger('careClass:winterCare', true);
+          $('.roadwayWidth-legend').hide();
+          $('.roadwayOther-legend').hide();
           new Confirm();
         } else {
           console.log(this.value);
           if (this.value === 'length-of-road-axis') {
             $('.roadwayLength-legend').show();
-            //eventbus.trigger('careClass:winterCare', true);
+              $('.roadwayWidth-legend').hide();
+              $('.roadwayOther-legend').hide();
+              //event buss trigger layer where asset are
             me.showEditModeButton();
-          } else {
-            $('.roadwayLength-legend').hide();
-           // eventbus.trigger('careClass:winterCare', false);
+          } else if (this.value === 'width-of-road-axis') {
+              $('.roadwayLength-legend').hide();
+              $('.roadwayWidth-legend').show();
+              $('.roadwayOther-legend').hide();
+            //event buss trigger layer where asset are
             applicationModel.setReadOnly(true);
+          } else if (this.value === 'other-roadway') {
+              $('.roadwayLength-legend').hide();
+              $('.roadwayWidth-legend').hide();
+              $('.roadwayOther-legend').show();
+              //event buss trigger layer where asset are
+              applicationModel.setReadOnly(true);
           }
         }
       });
