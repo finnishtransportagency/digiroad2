578812bb023f6c13dc8056fd55d1367c5fb6f18d
sasuolander <sasu.olander@sitowise.com>
2021-01-29 12:04:25 +0200

    remove heapdump and increase memory max
    (cherry picked from commit e664ed45f492b1669b3f0824eac2aa7488997ee8)

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 if [[ -e ~/.java.opts ]]; then
   source ~/.java.opts
 else
-  javaopts="-Xms2048m -Xmx4096m"
+  javaopts="-Xms2048m -Xmx12288m"
 fi
 jarfile="target/scala-2.11/digiroad2-assembly-0.1.0-SNAPSHOT.jar"
 javaopts="$javaopts -Dfile.encoding=UTF8 -Djava.security.egd=file:///dev/urandom -jar $jarfile"
 logfile="digiroad2.boot.log"
 jmxmonitoring="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.rmi.port=9010"
 
-heapdumpparameter="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/web/digiroad2/current/logs"
-nohup java $heapdumpparameter $jmxmonitoring $javaopts > $logfile &
+nohup java $jmxmonitoring $javaopts > $logfile &
