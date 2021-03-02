13c814ffdcba32cefb088b1e42a90419c9b463d4
sasuolander <sasu.olander@sitowise.com>
2021-03-02 09:58:16 +0200

    DROTH-2702 add new migration, some editing is till needed

---
+++ digiroad2-oracle/src/main/resources/db/migration/V7_ELY_data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (0,E'Ahvenanmaa',E'Aland',ST_GeomFromText('POINTZM (117869.307159741 6695233.48910223 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (1,E'Lappi',E'Lappland',ST_GeomFromText('POINTZM (470092.69926 7504895.42551454 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (2,E'Pohjois-Pohjanmaa',E'Norra Osterbotten',ST_GeomFromText('POINTZM (502448.375683011 7185083.81896492 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (3,E'Etela-Pohjanmaa',E'Sodra Osterbotten',ST_GeomFromText('POINTZM (273178.19125461 6999709.7974981 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (4,E'Keski-Suomi',E'Mellersta Finland',ST_GeomFromText('POINTZM (418850.209302328 6932071.78237523 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (5,E'Pohjois-Savo',E'Norra Savolax',ST_GeomFromText('POINTZM (574999.408689495 6950155.809661 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (6,E'Pirkanmaa',E'Birkaland',ST_GeomFromText('POINTZM (323996.151419635 6844239.172157 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (7,E'Kaakkois-Suomi',E'Sydvastra Finland',ST_GeomFromText('POINTZM (230120.084611288 6748148.87306083 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (8,E'Uusimaa',E'Nyland',ST_GeomFromText('POINTZM (533880.796720066 6756886.74130609 0.0 0.0)', 3067),3);
+INSERT INTO ely (id,name_fi,name_sv,geometry,zoom)  VALUES (9,E'Varsinais-Suomi',E'Egentliga Finland',ST_GeomFromText('POINTZM (383467.095615344 6718181.18728251 0.0 0.0)', 3067),3);
