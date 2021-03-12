12e69568e569c104c486006169491719e7ef6015
sasuolander <sasu.olander@sitowise.com>
2021-03-12 12:27:04 +0200

    DROTH-2702 add missing currval()

--- digiroad2-oracle/sql/kauniainen_traffic_signs.sql
+++ digiroad2-oracle/sql/kauniainen_traffic_signs.sql
@@ -1,6 +1,6 @@
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600073,300,'dr2_test_data',235);
 INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (nextval('lrm_position_primary_key_seq'), 1611317, 388553074, 103.000, 103.000, 1);
-insert into asset_link (ASSET_ID, POSITION_ID) values (600073, lrm_position_primary_key_seq.currval);
+insert into asset_link (ASSET_ID, POSITION_ID) values (600073, currval(lrm_position_primary_key_seq));
 insert into text_property_value(id, asset_id, property_id, value_fi, created_date, created_by) values (621599, 600073, (select id from property where public_id='trafficSigns_info'), 'Add Info For Test', current_timestamp, 'dr2_test_data');
 insert into text_property_value(id, asset_id, property_id, value_fi, created_date, created_by) values (621600, 600073, (select id from property where public_id='trafficSigns_value'), '80', current_timestamp, 'dr2_test_data');
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600073, (select id from enumerated_value where name_fi='C32 Nopeusrajoitus'), (select id from property where public_id='trafficSigns_type'));
--- digiroad2-oracle/sql/siilinjarvi_verificationService_test_data.sql
+++ digiroad2-oracle/sql/siilinjarvi_verificationService_test_data.sql
@@ -1,24 +1,24 @@
   -- Data used on UnitTests "get assets Latests Modifications with one municipality" and "get assets Latests Modifications for Ely user with two municipalities"
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 100, CURRENT_TIMESTAMP - 3,'testuser');;
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 1000, null, 0.000, 25.000);;
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);;
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));;
 
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 30, CURRENT_TIMESTAMP - 2,'testuser');;
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 2000, null, 0.000, 25.000);;
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);;
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));;
 
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 50, (CURRENT_TIMESTAMP - 1) - interval '4' hour,'testuser');;
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 3000, null, 0.000, 25.000);;
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);;
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));;
 
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 70, (CURRENT_TIMESTAMP - 1) - interval '2' hour,'testuser');;
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 4000, null, 0.000, 25.000);;
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);;
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));;
 
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 70, CURRENT_TIMESTAMP - 1,'testuser');;
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 5000, null, 0.000, 25.000);;
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);;
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));;
 
   INSERT INTO ASSET (ID,ASSET_TYPE_ID,MODIFIED_DATE, MODIFIED_BY) values (nextval('primary_key_seq'), 90, CURRENT_TIMESTAMP,'testuser');
   INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE) VALUES (nextval('LRM_POSITION_PRIMARY_KEY_SEQ'), 6000, null, 0.000, 25.000);
-  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,lrm_position_primary_key_seq.CURRVAL);
+  INSERT INTO ASSET_LINK (ASSET_ID,POSITION_ID) values (primary_key_seq.CURRVAL,currval(lrm_position_primary_key_seq));
