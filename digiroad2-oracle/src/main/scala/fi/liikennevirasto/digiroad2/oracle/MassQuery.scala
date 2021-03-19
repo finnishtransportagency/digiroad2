687f4d9100c638e68aabdceb02143854cd911d22
sasuolander <sasu.olander@sitowise.com>
2021-03-19 10:34:51 +0200

    DROTH-2706 create temporary table when using MassQuery in session

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/oracle/MassQuery.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/oracle/MassQuery.scala
@@ -5,6 +5,12 @@ import slick.jdbc.StaticQuery.interpolation
 
 object MassQuery {
   def withIds[T](ids: Set[Long])(f: String => T): T = {
+    sqlu"""
+      CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_ID (
+        ID BIGINT NOT NULL,
+	      CONSTRAINT TEMP_ID_PK PRIMARY KEY (ID)
+      ) ON COMMIT DELETE ROWS
+    """.execute
     val insertLinkIdPS = dynamicSession.prepareStatement("insert into temp_id (id) values (?)")
     try {
       ids.foreach { id =>
@@ -13,7 +19,7 @@ object MassQuery {
       }
       insertLinkIdPS.executeBatch()
       val ret = f("temp_id")
-      sqlu"""DELETE FROM temp_id""".execute
+      sqlu"TRUNCATE TABLE TEMP_ID".execute
       ret
     } finally {
       insertLinkIdPS.close()
