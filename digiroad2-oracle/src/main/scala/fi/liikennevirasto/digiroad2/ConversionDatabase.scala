ad173b58848a3812d9ecce9f9e249a0f67a64916
sasuolander <sasu.olander@sitowise.com>
2021-04-12 11:31:30 +0300

    DROTH-2706 change if else structure

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ConversionDatabase.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/ConversionDatabase.scala
@@ -15,21 +15,22 @@ object ConversionDatabase {
 
   private def toPoints(bytes: Object): Seq[Point] = {
     val geometry = bytes.asInstanceOf[PGobject]
-    if (geometry == null) Nil
-    else if(geometry.getValue.nonEmpty) {
+    if (geometry == null) Nil else{
       val geom = PGgeometry.geomFromString(geometry.getValue)
-      val point = geom.getFirstPoint
-      List(Point(point.x, point.y))
-    }
-    else {
-      val geom = PGgeometry.geomFromString(geometry.getValue)
-      val listOfPoint=ListBuffer[Point]()
-      for (i <- 0 until geom.numPoints() ){
-        val point =geom.getPoint(i)
-        listOfPoint += Point(point.x,point.y)
+      if(geom.numPoints()==1 ) {
+        val point = geom.getFirstPoint
+        List(Point(point.x, point.y))
       }
-      listOfPoint.toList
+      else {
+        val listOfPoint=ListBuffer[Point]()
+        for (i <- 0 until geom.numPoints() ){
+          val point =geom.getPoint(i)
+          listOfPoint += Point(point.x,point.y)
+        }
+        listOfPoint.toList
+      }
     }
+
   }
 
   lazy val dataSource = {
