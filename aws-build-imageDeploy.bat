247af231a3725bf93f07110b82f94b0561fdf406
sasuolander <sasu.olander@sitowise.com>
2021-04-16 14:42:32 +0300

    DROTH-2739 remove reference of viite and update comment

--- aws-build-imageDeploy.bat
+++ aws-build-imageDeploy.bat
@@ -2,12 +2,12 @@ grunt  && ^
 sbt assembly && ^
 docker build -f aws\fargate\Dockerfile -t digiroad2:latest .
 
-REM for testing docker image in AWS
+REM for testing docker image in AWS, all real deployment through AWS CodeBuild
 echo "Building Docker image"
 ./aws-build-image.sh
 echo "Logging in to AWS ECR with Docker"
-aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin 783354560127.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin "ecr storage here"
 echo "Tagging image"
-docker tag viite:latest 783354560127.dkr.ecr.eu-west-1.amazonaws.com/digiroad2:latest
+docker tag digiroad2:latest "ecr storage here"/digiroad2:latest
 echo "Pushing image"
 docker push 783354560127.dkr.ecr.eu-west-1.amazonaws.com/digiroad2:latest
