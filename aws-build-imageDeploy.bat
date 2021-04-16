75f8c7c5187cb91333c678818ed26398d38012e4
sasuolander <sasu.olander@sitowise.com>
2021-04-16 14:43:40 +0300

    DROTH-2739 remove build script

--- aws-build-imageDeploy.bat
+++ aws-build-imageDeploy.bat
@@ -3,8 +3,6 @@ sbt assembly && ^
 docker build -f aws\fargate\Dockerfile -t digiroad2:latest .
 
 REM for testing docker image in AWS, all real deployment through AWS CodeBuild
-echo "Building Docker image"
-./aws-build-image.sh
 echo "Logging in to AWS ECR with Docker"
 aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin "ecr storage here"
 echo "Tagging image"
