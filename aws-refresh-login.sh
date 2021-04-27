fd3145ebbdb99743f8e0ff31628341a4e428688d
rene-sitowise <rene-sitowise@sitowise.com>
2021-04-27 14:01:47 +0300

    Changed aws login and refresh scripts according to digiroad2 equivalent ones.

--- aws-refresh-login.sh
+++ aws-refresh-login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Before running this script you must authenticate through Väylä SAML:
 #
-# python3 aws/login/vaylaAssumeRoleAWSCLI.py --username <Your Väylä username> --account 783354560127 --role ViiteAdmin --region eu-west-1
+# python3 aws/login/vaylaAssumeRoleAWSCLI.py --username <Your Väylä username> --account 475079312496 --role DigiroadOthAdmin --region eu-west-1
 #
-python3 aws/login/vaylaAssumeRoleAWSCLI.py --refresh true
+python aws/login/vaylaAssumeRoleAWSCLI.py --refresh true
