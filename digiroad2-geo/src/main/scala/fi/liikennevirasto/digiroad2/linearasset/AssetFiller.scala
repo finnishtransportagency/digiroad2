aff06f8e7c77e7adf8cf5c2893918bb25a6af9e2
sasuolander <sasu.olander@sitowise.com>
2021-05-12 23:57:25 +0300

    DROTH-2777 debug logging

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/AssetFiller.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/AssetFiller.scala
@@ -526,7 +526,12 @@ class AssetFiller {
       }
       logger.warn("adjustedAssets 7478 "+adjustedAssets.count(a=>a.linkId==7478).toString)
       logger.warn("adjustedAssets 1611374 "+adjustedAssets.count(a=>a.linkId==1611374).toString)
-      logger.warn("assetAdjustments "+assetAdjustments.toString)
+      if(adjustedAssets.count(a=>a.linkId==1611374)==2){
+        logger.warn("adjustedAssets 1611374 "+adjustedAssets(0).toString)
+        logger.warn("adjustedAssets 1611374 "+adjustedAssets(1).toString)
+        logger.warn("assetAdjustments "+assetAdjustments.toString)
+      }
+
       (existingAssets ++ toLinearAsset(adjustedAssets, roadLink), assetAdjustments)
     }
   }
