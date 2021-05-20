205cdb8fbb0a769e714d20540e09fbcff38bef01
rene-sitowise <rene-sitowise@sitowise.com>
2021-05-20 17:57:29 +0300

    DROTH-2755 small fix

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
@@ -443,7 +443,7 @@ class PostGISSpeedLimitDao(val vvhClient: VVHClient, val roadLinkService: RoadLi
 //    val losAngelesTime = timestamp.atZone(ZoneId.of("Europe/Helsinki"))
 //    println(losAngelesTime)
     val creationDate = createdDate match {
-      case datetime => s"""TO_TIMESTAMP('$datetime', 'YYYY-MM-DD"T"HH24:MI:SS.FF3TZH:TZM')"""
+      case Some(datetime) => s"""TO_TIMESTAMP('$datetime', 'YYYY-MM-DD"T"HH24:MI:SS.FF3TZH:TZM')"""
       case None => "current_timestamp"
     }
 
