74ecd76e64098e74d21f9fed5bb3430b90902bb9
sasuolander <sasu.olander@sitowise.com>
2021-05-24 15:27:30 +0300

    DROTH-2786 let us still use old cookie authentication in local development, improve logging

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/authentication/JWTAuthentication.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/authentication/JWTAuthentication.scala
@@ -38,29 +38,14 @@ trait JWTAuthentication extends Authentication {
   val dataHeader = "X-Iam-Data"
 
   def authenticate(request: HttpServletRequest)(implicit userProvider: UserProvider): User = {
-
-    val username: String =
-      if (!Digiroad2Properties.authenticationTestMode) {
-
-        // Example value:
-        // eyJ0eXAiOiJKV1QiLCJraWQiOiJlMzQ0NGNhOS0wNThiLTRmN2YtODFiYi1mMmU2ZTRlZTE3NTYiLCJhbGciOiJFUzI1NiIsImlzcyI6Imh0d
-        // HBzOi8vY29nbml0by1pZHAuZXUtd2VzdC0xLmFtYXpvbmF3cy5jb20vZXUtd2VzdC0xX29OelBzaVhFSiIsImNsaWVudCI6IjNjdGMyMGQzaT
-        // RnaHY5NGtzMHNlbXQ0ZTE1Iiwic2lnbmVyIjoiYXJuOmF3czplbGFzdGljbG9hZGJhbGFuY2luZzpldS13ZXN0LTE6MDgzNTg5MjgyOTE3Omx
-        // vYWRiYWxhbmNlci9hcHAvVmF5bGEtRE1aLUFMQi85ZmM2YzA5OTJiNzRhZjA3IiwiZXhwIjoxNTkwNzU5ODA5fQ==.eyJjdXN0b206cm9vbGk
-        // iOiJpbnRfa2F5dHRhamF0LEV4dHJhbmV0X0theXR0YWphLGFybjphd3M6aWFtOjoxMTc1MzEyMjMyMjE6cm9sZS9WaWl0ZUFkbWluXFwsYXJu
-        // OmF3czppYW06OjExNzUzMTIyMzIyMTpzYW1sLXByb3ZpZGVyL1ZheWxhVGVzdE9BTSIsInN1YiI6IjJiNWEyYjY1LWNhMDYtNDZlMi04YTUyL
-        // WE1MTkwYjQ5NWQxMiIsImVtYWlsX3ZlcmlmaWVkIjoiZmFsc2UiLCJjdXN0b206dWlkIjoiSzU2Nzk5NyIsImVtYWlsIjoic2FtaS5rb3Nvbm
-        // VuQGNnaS5jb20iLCJ1c2VybmFtZSI6InZheWxhdGVzdG9hbV9zYW1pLmtvc29uZW5AY2dpLmNvbSIsImV4cCI6MTU5MDc1OTgwOSwiaXNzIjo
-        // iaHR0cHM6Ly9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbS9ldS13ZXN0LTFfb056UHNpWEVKIn0=.V98ZvUxOi5LvC_CxoVt
-        // 628pO2ZBGkTSXXTdDaQ5DtjEj2SOC0LuSFzEV56rNkbmIvJ7elYayOTUBZlTZmVAqQw==
-        val tokenHeaderValue = request.getHeader(dataHeader)
-
-        JWTReader.getUsername(tokenHeaderValue)
-
+    val username: String = {
+      if (request.getCookies != null && request.getCookies.exists(p => p.getName == "testusername") && Digiroad2Properties.authenticationTestMode) {
+        request.getCookies.find(p => p.getName == "testusername").orNull.getValue
       } else {
-        jwtLogger.info("Using authentication test mode.")
-        Digiroad2Properties.authenticationTestUser
+        val tokenHeaderValue = request.getHeader(dataHeader)
+        JWTReader.getUsername(tokenHeaderValue)
       }
+    }
 
     if (username.isEmpty) {
       jwtLogger.warn(s"Authentication failed. Missing username in JWT payload.")
@@ -68,7 +53,7 @@ trait JWTAuthentication extends Authentication {
     }
 
     val user = userProvider.getUser(username).getOrElse(viewerUser)
-    jwtLogger.info(s"Authenticate request, remote user = $user in Digiroad.${if (user.isNotInDigiroad()) " (User not added in Digiroad.)" else ""}")
+    jwtLogger.info(s"Authenticate request, remote user = $username.${if (user.isNotInDigiroad()) " (User not added in Digiroad.)" else ""}")
     user
 
   }
