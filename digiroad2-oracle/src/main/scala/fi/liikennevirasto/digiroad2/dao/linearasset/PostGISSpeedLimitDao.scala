a136a0bc598c00295ed3e24a956d5d3ac18ffe90
rene-sitowise <rene-sitowise@sitowise.com>
2021-05-24 13:55:00 +0300

    DROTH-2755 Test why build failed

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
@@ -446,7 +446,7 @@ class PostGISSpeedLimitDao(val vvhClient: VVHClient, val roadLinkService: RoadLi
     val sourceFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     val targetFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 
-    val dateTime = LocalDateTime.parse(createdDate, sourceFormat)
+    val dateTime = LocalDateTime.parse(sourceDateTime, sourceFormat)
     val formatedDateTime = dateTime.atZone(ZoneId.of("UTC")).format(targetFormat)
     println(formatedDateTime)
     val createdDate2 = Option(formatedDateTime)
