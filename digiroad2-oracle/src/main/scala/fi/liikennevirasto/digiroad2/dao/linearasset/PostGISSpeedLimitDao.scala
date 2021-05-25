67826b47debe5e67af8a703d0ff730754b6ee57a
rene-sitowise <rene-sitowise@sitowise.com>
2021-05-25 12:35:08 +0300

    DROTH-2775 Test if removing timezone marker helps.

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/linearasset/PostGISSpeedLimitDao.scala
@@ -442,21 +442,21 @@ class PostGISSpeedLimitDao(val vvhClient: VVHClient, val roadLinkService: RoadLi
 //    val losAngelesTime = timestamp.atZone(ZoneId.of("Europe/Helsinki"))
 //    println(losAngelesTime)
 
-    val sourceDateTime = "2021-05-20T13:07:38.343Z"
-    val sourceFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    val targetFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-
-    val dateTime = LocalDateTime.parse(createdDate.get.toString(), sourceFormat)
-    val formatedDateTime = dateTime.atZone(ZoneId.of("UTC")).format(targetFormat)
-    println(formatedDateTime)
-    val createdDate2 = Option(formatedDateTime)
-    val creationDate = createdDate2 match {
-      case Some(datetime) => s"""TO_TIMESTAMP('${datetime.format(new Date())}', 'YYYY-MM-DD"T"HH24:MI:SS.FF3TZH:TZM')"""
+//    val sourceDateTime = "2021-05-20T13:07:38.343Z"
+//    val sourceFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+//    val targetFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+//
+//    val dateTime = LocalDateTime.parse(createdDate.get.toString(), sourceFormat)
+//    val formatedDateTime = dateTime.atZone(ZoneId.of("UTC")).format(targetFormat)
+//    println(formatedDateTime)
+//    val createdDate2 = Option(formatedDateTime)
+    val creationDate = createdDate match {
+      case Some(datetime) => s"""TO_TIMESTAMP('${datetime}', 'YYYY-MM-DD"T"HH24:MI:SS.FF3)"""
       case None => "current_timestamp"
     }
 
     val modifiedDate = modifiedAt match {
-      case Some(datetime) => s"""TO_TIMESTAMP('$datetime', 'YYYY-MM-DD"T"HH24:MI:SS.FF3TZH:TZM')"""
+      case Some(datetime) => s"""TO_TIMESTAMP('$datetime', 'YYYY-MM-DD"T"HH24:MI:SS.FF3')"""
       case None => "NULL"
     }
 
