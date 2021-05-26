d1076e8b42fd600312827d132d889a201b4459f9
Anna Arikainen <anna.arikainen@sitowise.com>
2021-05-26 11:04:18 +0300

    DROTH-2757 delete save_deploy_info reference

--- gruntfile.js
+++ gruntfile.js
@@ -231,7 +231,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', [ 'jshint', 'env:production', 'configureProxies:oth', 'preprocess:production', 'connect:oth', 'mocha:unit', 'mocha:integration', 'clean', 'less:production', 'concat', 'uglify', 'cachebreaker']);
 
-  grunt.registerTask('deploy', ['clean', 'env:' + target, 'preprocess:production', 'less:production', 'concat', 'uglify', 'cachebreaker', 'save_deploy_info']);
+  grunt.registerTask('deploy', ['clean', 'env:' + target, 'preprocess:production', 'less:production', 'concat', 'uglify', 'cachebreaker']);
 
   grunt.registerTask('integration-test', [ 'jshint', 'env:development', 'configureProxies:oth', 'preprocess:development', 'connect:oth', 'mocha:integration']);
 
