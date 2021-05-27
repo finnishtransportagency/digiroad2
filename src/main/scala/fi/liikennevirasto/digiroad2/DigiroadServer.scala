50d60e1103e09bb110994a128b2e60c8cdef3add
sasuolander <sasu.olander@sitowise.com>
2021-05-27 11:34:07 +0300

    DROTH-2749 add logging

--- src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
@@ -59,6 +59,7 @@ class OAGProxyServlet extends ProxyServlet {
 
   override def rewriteURI(req: HttpServletRequest): java.net.URI = {
     val url = Digiroad2Properties.rasterServiceUrl +  regex.replaceFirstIn(req.getRequestURI, "/wmts/maasto")
+    logger.info(url)
     java.net.URI.create(url)
   }
 
