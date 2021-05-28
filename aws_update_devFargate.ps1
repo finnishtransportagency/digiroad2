6b9156151413a010bd46566d788abb646ad3e0bb
sasuolander <sasu.olander@sitowise.com>
2021-05-28 16:06:40 +0300

    add region

--- aws_update_devFargate.ps1
+++ aws_update_devFargate.ps1
@@ -1,4 +1,4 @@
 # create
-# aws cloudformation create-stack --stack-name devFargate --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --parameters file://aws/cloud-formation/fargateService/parameter.json --profile vaylaapp
+# aws cloudformation create-stack --region eu-west-1 --stack-name devFargate --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --parameters file://aws/cloud-formation/fargateService/parameter.json --profile vaylaapp
 # update devFargate
 aws cloudformation update-stack --stack-name devFargate --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --parameters file://aws/cloud-formation/fargateService/parameter.json --profile vaylaapp
--- aws_update_devpipeline.ps1
+++ aws_update_devpipeline.ps1
@@ -1,5 +1,5 @@
 # create
-# aws cloudformation create-stack --stack-name devpipeline --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/cicd/cicd-github.yaml --parameters file://aws/cloud-formation/cicd/parameter.json --profile vaylaapp
+# aws cloudformation create-stack  --region eu-west-1 --stack-name devpipeline --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/cicd/cicd-github.yaml --parameters file://aws/cloud-formation/cicd/parameter.json --profile vaylaapp
 
 # update devpipeline
 aws cloudformation update-stack --stack-name devpipeline --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/cicd/cicd-github.yaml --parameters file://aws/cloud-formation/cicd/parameter.json --profile vaylaapp
