543b050be6197a86188a60a8a3cde17122bc88e7
sasuolander <sasu.olander@sitowise.com>
2021-06-04 16:45:28 +0300

    DROTH-2793 add env validation and some space for readability

--- aws/fargate/startup.sh
+++ aws/fargate/startup.sh
@@ -1,26 +1,51 @@
 #!/bin/sh
 
-if ! [ $batchMode = "true" ]; then
+if ! [ "$batchMode" = "true" ]; then
   echo "Server mode"
-  java $javaParameter -jar /digiroad2.jar
+  java "$javaParameter" -jar /digiroad2.jar
   else
     echo "Batch mode"
-    if [ $batchRunType = "UpdateIncompleteLinkList" ]; then
-      echo "UpdateIncompleteLinkList"
-      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.UpdateIncompleteLinkList
-    elif [ $batchRunType = "DataFixture" ] &&  [[ ! -z $trafficSignGroup ]]; then
-      echo "DataFixture with trafficSignGroup"
-      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction $trafficSignGroup
-    elif [ $batchRunType = "DataFixture" ]; then
-      echo "DataFixture"
-      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction
-    elif [ $batchRunType = "AssetValidatorProcess" ]; then
-      echo "AssetValidatorProcess"
-      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.AssetValidatorProcess $assetForValidation
-    elif [ $batchRunType = "TierekisteriDataImporter" ]; then
-      echo "TierekisteriDataImporter"
-      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.TierekisteriDataImporter $tierekisteriAction $tierekisteriAsset $trafficSignGroup
-    else
-      echo "Wrong mode"
+      if  [[ -z "$batchRunType" ]]; then
+        echo "batchRunType env is not defined"
+      else
+        if [ "$batchRunType" = "UpdateIncompleteLinkList" ]; then
+
+          echo "UpdateIncompleteLinkList"
+          java "$javaParameter" -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.UpdateIncompleteLinkList
+        elif [ "$batchRunType" = "DataFixture" ] &&  [[ ! -z "$trafficSignGroup" ]]; then
+
+          echo "DataFixture with trafficSignGroup"
+           if  [[ ! -z "$batcAction" ]]; then
+            java "$javaParameter" -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture "$batcAction" "$trafficSignGroup"
+          else
+           echo "batcAction env is not defined"
+          fi
+        elif [ "$batchRunType" = "DataFixture" ]; then
+
+          echo "DataFixture"
+          if [[ ! -z "$batcAction" ]]; then
+             java "$javaParameter" -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture "$batcAction"
+          else
+           echo "batcAction env is not defined"
+          fi
+        elif [ "$batchRunType" = "AssetValidatorProcess" ]; then
+
+          echo "AssetValidatorProcess"
+          if [[ ! -z "$assetForValidation" ]]; then
+            java "$javaParameter" -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.AssetValidatorProcess "$assetForValidation"
+          else
+           echo "assetForValidation env is not defined"
+          fi
+        elif [ "$batchRunType" = "TierekisteriDataImporter" ]; then
+
+          echo "TierekisteriDataImporter"
+          if [[ ! -z "$tierekisteriAction" ]] && [[ ! -z "$tierekisteriAsset" ]] && [[ ! -z "$trafficSignGroup" ]]; then
+            java "$javaParameter" -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.TierekisteriDataImporter "$tierekisteriAction" "$tierekisteriAsset" "$trafficSignGroup"
+          else
+            echo "tierekisteriAction or tierekisteriAsset or trafficSignGroup env is not defined"
+          fi
+        else
+          echo "Wrong mode"
+    fi
   fi
 fi
