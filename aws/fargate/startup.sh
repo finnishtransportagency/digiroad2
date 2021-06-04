2d9bced612dafad2a24b4b6a5a288a425821b4b5
sasuolander <sasu.olander@sitowise.com>
2021-06-04 10:24:59 +0300

    DROTH-2793 remove yest and formating

--- aws/fargate/startup.sh
+++ aws/fargate/startup.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-if ! [ $batchEnv = "true" ]
-  then
-    echo "Server mode"
-    java $javaParameter -jar /digiroad2.jar
+if ! [ $batchEnv = "true" ]; then
+  echo "Server mode"
+  java $javaParameter -jar /digiroad2.jar
   else
     echo "Batch mode"
     if [ $batchMode = "UpdateIncompleteLinkList" ]; then
       echo "UpdateIncompleteLinkList"
-      printf YES | java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.UpdateIncompleteLinkList
+      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.UpdateIncompleteLinkList
     elif [ $batchMode = "DataFixture" ] &&  [[ ! -z $trafficSignGroup ]]; then
       echo "DataFixture with trafficSignGroup"
-      printf YES | java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction $trafficSignGroup
+      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction $trafficSignGroup
     elif [ $batchMode = "DataFixture" ]; then
       echo "DataFixture"
-      printf YES | java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction
+      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.DataFixture $batcAction
     elif [ $batchMode = "AssetValidatorProcess" ]; then
       echo "AssetValidatorProcess"
-      printf YES | java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.AssetValidatorProcess $assetForValidation
+      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.AssetValidatorProcess $assetForValidation
     elif [ $batchMode = "TierekisteriDataImporter" ]; then
       echo "TierekisteriDataImporter"
-      printf YES | java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.TierekisteriDataImporter $tierekisteriAction $tierekisteriAsset $trafficSignGroup
+      java $javaParameter -cp /digiroad2.jar fi.liikennevirasto.digiroad2.util.TierekisteriDataImporter $tierekisteriAction $tierekisteriAsset $trafficSignGroup
     else
       echo "Wrong mode"
   fi
