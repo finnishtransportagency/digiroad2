9abee3dc1fa07a61e645f52a9962ea59ab4fb0ce
sasuolander <sasu.olander@sitowise.com>
2021-08-16 14:34:55 +0300

    DROTH-2656 add basic skeleton for line creation prosess

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/DataFixture.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/DataFixture.scala
@@ -9,7 +9,7 @@ import java.util.{Date, NoSuchElementException, Properties}
 import com.googlecode.flyway.core.Flyway
 import fi.liikennevirasto.digiroad2.asset.{HeightLimit, _}
 import fi.liikennevirasto.digiroad2.client.tierekisteri._
-import fi.liikennevirasto.digiroad2.client.viite.SearchViiteClient
+import fi.liikennevirasto.digiroad2.client.viite.{IntegrationViiteClient, SearchViiteClient}
 import fi.liikennevirasto.digiroad2.client.vvh.ChangeType.New
 import fi.liikennevirasto.digiroad2.client.vvh.{VVHClient, VVHRoadlink}
 import fi.liikennevirasto.digiroad2.dao.RoadLinkDAO.{AdministrativeClassDao, FunctionalClassDao, LinkAttributes, LinkAttributesDao}
@@ -2416,6 +2416,10 @@ object DataFixture {
     println("\n")
   }
 
+  def newRoadAddressFromViite(): Unit ={
+    AutomaticLaneCreationProcess.process()
+  }
+  
   private val trafficSignGroup = Map[String, TrafficSignTypeGroup] (
     "SpeedLimits" -> TrafficSignTypeGroup.SpeedLimits,
     "RegulatorySigns" ->  TrafficSignTypeGroup.RegulatorySigns,
@@ -2646,6 +2650,8 @@ object DataFixture {
         restoreExpiredAssetsFromTRImport()
       case Some("move_old_expired_assets") =>
         moveOldExpiredAssets()
+      case Some("new_road_address_from_viite") =>
+        newRoadAddressFromViite()
       case _ => println("Usage: DataFixture test | import_roadlink_data |" +
         " split_speedlimitchains | split_linear_asset_chains | dropped_assets_csv | dropped_manoeuvres_csv |" +
         " unfloat_linear_assets | expire_split_assets_without_mml | generate_values_for_lit_roads | get_addresses_to_masstransitstops_from_vvh |" +
