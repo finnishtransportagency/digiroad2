ee8e84488f0c7487b1a2c1e26c5e6b91c10cc50a
sasuolander <sasu.olander@sitowise.com>
2021-09-28 08:14:53 +0300

    let just delete old bat and sh files

--- data-validator.bat
+++
@@ -1,2 +0,0 @@
-if [%2]==[] sbt "project digiroad2-oracle" "test:run-main fi.liikennevirasto.digiroad2.util.AssetValidatorProcess %1"
-if [%2]<>[] sbt %2 "project digiroad2-oracle" "test:run-main fi.liikennevirasto.digiroad2.util.AssetValidatorProcess %1"
