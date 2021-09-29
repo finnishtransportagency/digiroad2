846642f3faa973223d8dc519e62c2f1e1937afdd
sasuolander <sasu.olander@sitowise.com>
2021-09-29 17:22:41 +0300

    DROTH-2972 add example commands

---
+++ aws_update_qaFargate.ps1
@@ -0,0 +1,12 @@
+# create
+#aws cloudformation create-stack --region eu-west-1 --stack-name qaTaskDefinitionRight --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/ECSTaskExecutionRole.yaml --profile vaylaapp
+aws cloudformation create-stack --region eu-west-1 --stack-name qaFargate --on-failure DELETE --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --parameters file://aws/cloud-formation/qa/parameter.json --profile vaylaapp
+
+aws cloudformation validate-template --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --profile vaylaapp
+
+# update devFargate
+aws cloudformation update-stack --stack-name qaFargate  --capabilities CAPABILITY_NAMED_IAM --template-body file://aws/cloud-formation/fargateService/alb_ecs.yaml --parameters file://aws/cloud-formation/qa/parameter.json --profile vaylaapp
+
+
+
+
