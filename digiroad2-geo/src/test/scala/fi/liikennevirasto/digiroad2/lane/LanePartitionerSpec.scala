d108145c9cd54cb01ecbca4a7abed5d96f4096d7
antti.ahopelto <antti.ahopelto@sitowise.com>
2021-10-22 14:08:36 +0300

    DROTH-2982 Improved tests

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/lane/LanePartitionerSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/lane/LanePartitionerSpec.scala
@@ -7,31 +7,28 @@ import org.scalatest._
 
 class LanePartitionerSpec extends FunSuite with Matchers {
 
-  test("Lanes should be partitioned by side code and lanecode") {
-
-    val mainLaneBothDirectionsA = PieceWiseLane(1l, 0l, 1, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
-      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(1)))))
-
-    val mainLaneBothDirectionsB = PieceWiseLane(2l, 0l, 1, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
-      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(1)))))
-
-    val mainLaneTowards = PieceWiseLane(3l, 0l, 2, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
-      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(1)))))
-
-    val firstLeftAdditionalTowards = PieceWiseLane(4l, 0l, 2, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
-      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(2)))))
-
-    val firstRightAdditionalTowards = PieceWiseLane(5l, 0l, 2, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
-      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(3)))))
-
-    val lanes = Seq(mainLaneBothDirectionsA, mainLaneBothDirectionsB, mainLaneTowards, firstLeftAdditionalTowards, firstRightAdditionalTowards)
-    val lanesPartitioned = partitionBySideCodeAndLaneCode(lanes)
-
-    lanesPartitioned should have size 6
-    lanesPartitioned(0) should be (List(mainLaneBothDirectionsA, mainLaneBothDirectionsB))
-    lanesPartitioned(1) should be (List(mainLaneTowards))
-    lanesPartitioned(4) should be (Stream(firstLeftAdditionalTowards, firstRightAdditionalTowards))
-
+  def createPieceWiseLane(id: Long, sideCode: Int, laneCode: Int): PieceWiseLane = {
+    PieceWiseLane(id, 0l, sideCode, false, Seq(Point(0.0, 0.0), Point(10.0, 0.0)), 0d, 10d, Set(Point(0.0, 0.0),
+      Point(10.0, 0.0)), None, None, None, None, 0l, None, AdministrativeClass(1), Seq(LaneProperty("lane_code", Seq(LanePropertyValue(laneCode)))))
   }
 
+  def createLanes(): Seq[PieceWiseLane] = {
+    val mainLaneBothDirectionsA = createPieceWiseLane(1l, 1, 1)
+    val mainLaneBothDirectionsB = createPieceWiseLane(2l, 1, 1)
+    val mainLaneTowards = createPieceWiseLane(3l, 2, 1)
+    val firstLeftAdditionalTowards = createPieceWiseLane(4l, 2, 2)
+    val firstRightAdditionalTowards = createPieceWiseLane(5l, 2, 3)
+
+    Seq(mainLaneBothDirectionsA, mainLaneBothDirectionsB, mainLaneTowards, firstLeftAdditionalTowards, firstRightAdditionalTowards)
+  }
+
+  test("Lanes should be partitioned by side code and lanecode") {
+    val lanes = createLanes()
+    val lanesPartitioned = partitionBySideCodeAndLaneCode(lanes)
+
+    lanesPartitioned.size should equal(6)
+    lanesPartitioned(0).size should equal(2)
+    lanesPartitioned(1).size should equal(1)
+    lanesPartitioned(4).size should equal(2)
+  }
 }
