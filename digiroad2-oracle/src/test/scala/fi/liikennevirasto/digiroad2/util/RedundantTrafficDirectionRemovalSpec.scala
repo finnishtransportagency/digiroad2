e53bb0f9486dac0feacb394e2f8d79d4effede8f
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2021-12-17 12:06:52 +0200

    DROTH-3061 test improved so that it uses real vvh value instead of hard coded

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/RedundantTrafficDirectionRemovalSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/RedundantTrafficDirectionRemovalSpec.scala
@@ -19,22 +19,28 @@ class RedundantTrafficDirectionRemovalSpec extends FunSuite with Matchers {
   def withDynTransaction(f: => Unit): Unit = PostGISDatabase.withDynTransaction(f)
 
   test("A redundant traffic direction is removed, but a valid is not") {
+    val linkSet = PostGISDatabase.withDynTransaction(roadLinkService.fetchVVHRoadlinks(Set(1681765,1611347,1611181,1611380,1611265,1610928,1611183,1611675,1610953,1611480)))
+    if (linkSet.isEmpty) {
+      cancel("No roadlinks found, so test canceled.")
+    }
+    val linkWithRedundantTrafficDirection = linkSet.head
     withDynTransaction {
-      RoadLinkDAO.insert("traffic_direction", 1681765, None, 2)
-      // the second traffic direction is set so that it can't incidentally match with VVH traffic direction value
+      // put the vvh value of the first received link to the traffic direction table, so that it's certainly redundant
+      RoadLinkDAO.insert("traffic_direction", linkWithRedundantTrafficDirection.linkId, None, linkWithRedundantTrafficDirection.trafficDirection.value)
+      // the second traffic direction is set as 10 that it can't incidentally match with any VVH traffic direction value
       RoadLinkDAO.insert("traffic_direction", 6510465, None, 10)
       val linkIdsBeforeRemoval = RoadLinkDAO.TrafficDirectionDao.getLinkIds()
-      linkIdsBeforeRemoval should contain(1681765)
+      linkIdsBeforeRemoval should contain(linkWithRedundantTrafficDirection.linkId)
       linkIdsBeforeRemoval should contain(6510465)
     }
     RedundantTrafficDirectionRemoval.deleteRedundantTrafficDirectionFromDB()
     withDynTransaction {
       val linkIdsAfterRemoval = RoadLinkDAO.TrafficDirectionDao.getLinkIds()
-      linkIdsAfterRemoval should not contain(1681765)
+      linkIdsAfterRemoval should not contain  linkWithRedundantTrafficDirection.linkId
       linkIdsAfterRemoval should contain(6510465)
       RoadLinkDAO.delete("traffic_direction", 6510465)
       val linkIdsAfterCleanUp = RoadLinkDAO.TrafficDirectionDao.getLinkIds()
-      linkIdsAfterCleanUp should not contain(6510465)
+      linkIdsAfterCleanUp should not contain 6510465
     }
   }
 }
