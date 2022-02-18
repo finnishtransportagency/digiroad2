f3bffd1efa1aa4621c09e4eefc0b2708f3a2cfd2
antti.ahopelto <antti.ahopelto@sitowise.com>
2022-02-18 13:38:10 +0200

    DROTH-3092 added logging to roadlinks and lanes

--- digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
+++ digiroad2-api-oth/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Api.scala
@@ -659,15 +659,15 @@ class Digiroad2Api(val roadLinkService: RoadLinkService,
   }
 
   private def getRoadLinksFromVVH(municipalities: Set[Int], withRoadAddress: Boolean = true)(bbox: String): Seq[Seq[Map[String, Any]]] = {
-    val boundingRectangle = constructBoundingRectangle(bbox)
+    val boundingRectangle = LogUtils.time(logger, "TEST LOG Constructing boundingBox")(constructBoundingRectangle(bbox))
     validateBoundingBox(boundingRectangle)
-    val roadLinkSeq = roadLinkService.getRoadLinksFromVVH(boundingRectangle, municipalities)
+    val roadLinkSeq = LogUtils.time(logger, "TEST LOG Get and enrich RoadLinks from VVH")(roadLinkService.getRoadLinksFromVVH(boundingRectangle, municipalities))
     val roadLinks = if(withRoadAddress) {
-      val viiteInformation = roadAddressService.roadLinkWithRoadAddress(roadLinkSeq)
-      val vkmInformation = roadAddressService.roadLinkWithRoadAddressTemp(viiteInformation.filterNot(_.attributes.contains("VIITE_ROAD_NUMBER")))
+      val viiteInformation = LogUtils.time(logger, "TEST LOG Get Viite road address for links")(roadAddressService.roadLinkWithRoadAddress(roadLinkSeq))
+      val vkmInformation = LogUtils.time(logger, "TEST LOG Get Temp road address for links")(roadAddressService.roadLinkWithRoadAddressTemp(viiteInformation.filterNot(_.attributes.contains("VIITE_ROAD_NUMBER"))))
       viiteInformation.filter(_.attributes.contains("VIITE_ROAD_NUMBER")) ++ vkmInformation
     } else roadLinkSeq
-    partitionRoadLinks(roadLinks)
+    LogUtils.time(logger, "TEST LOG Partition roadLinks")(partitionRoadLinks(roadLinks))
   }
 
   private def getRoadlinksWithComplementaryFromVVH(municipalities: Set[Int], withRoadAddress: Boolean = true)(bbox: String): Seq[Seq[Map[String, Any]]] = {
