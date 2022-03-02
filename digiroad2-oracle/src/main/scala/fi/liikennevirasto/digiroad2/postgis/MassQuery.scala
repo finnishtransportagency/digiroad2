7f9ecc3f64b840b368f0a8b1df6b76701533eef7
sasuolander <sasu.olander@sitowise.com>
2022-03-02 13:27:16 +0200

    DROTH-3092 add logging into mass queries

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/postgis/MassQuery.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/postgis/MassQuery.scala
@@ -1,28 +1,45 @@
 package fi.liikennevirasto.digiroad2.postgis
 
+import fi.liikennevirasto.digiroad2.util.LogUtils
+import org.slf4j.LoggerFactory
 import slick.driver.JdbcDriver.backend.Database.dynamicSession
 import slick.jdbc.StaticQuery.interpolation
 
 object MassQuery {
   def withIds[T](ids: Set[Long])(f: String => T): T = {
-    sqlu"""
+    
+    val logger = LoggerFactory.getLogger(getClass)
+    LogUtils.time(logger, "TEST LOG MassQuery withIds"){
+
+      if (ids.size > 1000) {
+        logger.info("over thousand "+ids.size)
+      }else{
+        logger.info("under thousand "+ids.size)
+      }
+      LogUtils.time(logger, "TEST LOG create TEMP_ID table"){
+        sqlu"""
       CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_ID (
         ID BIGINT NOT NULL,
 	      CONSTRAINT TEMP_ID_PK PRIMARY KEY (ID)
       ) ON COMMIT DELETE ROWS
     """.execute
-    val insertLinkIdPS = dynamicSession.prepareStatement("insert into temp_id (id) values (?)")
-    try {
-      ids.foreach { id =>
-        insertLinkIdPS.setLong(1, id)
-        insertLinkIdPS.addBatch()
       }
-      insertLinkIdPS.executeBatch()
-      val ret = f("temp_id")
-      sqlu"TRUNCATE TABLE TEMP_ID".execute
-      ret
-    } finally {
-      insertLinkIdPS.close()
+      val insertLinkIdPS = dynamicSession.prepareStatement("insert into temp_id (id) values (?)")
+
+      LogUtils.time(logger, "TEST LOG insert into TEMP_ID"){
+        try {
+          ids.foreach { id =>
+            insertLinkIdPS.setLong(1, id)
+            insertLinkIdPS.addBatch()
+          }
+          insertLinkIdPS.executeBatch()
+          val ret = f("temp_id")
+          sqlu"TRUNCATE TABLE TEMP_ID".execute
+          ret
+        } finally {
+          insertLinkIdPS.close()
+        }
+      }
     }
   }
 }
