7bb7802b05c2e4994a4eb2968b3dd55afe118615
sasuolander <sasu.olander@sitowise.com>
2022-03-02 18:39:01 +0200

    DROTH-3092  DROTH-3092 improve logging

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/postgis/MassQuery.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/postgis/MassQuery.scala
@@ -7,16 +7,9 @@ import slick.jdbc.StaticQuery.interpolation
 import java.sql.PreparedStatement
 
 object MassQuery {
+  val logger = LoggerFactory.getLogger(getClass)
   def withIds[T](ids: Set[Long])(f: String => T): T = {
-    
-    val logger = LoggerFactory.getLogger(getClass)
     LogUtils.time(logger, "TEST LOG MassQuery withIds"){
-
-      if (ids.size > 1000) {
-        logger.info("over thousand "+ids.size)
-      }else{
-        logger.info("under thousand "+ids.size)
-      }
       LogUtils.time(logger, "TEST LOG create TEMP_ID table"){
         sqlu"""
       CREATE TEMPORARY TABLE IF NOT EXISTS TEMP_ID (
@@ -27,7 +20,7 @@ object MassQuery {
       }
       val insertLinkIdPS = dynamicSession.prepareStatement("insert into temp_id (id) values (?)")
 
-      LogUtils.time(logger, "TEST LOG insert into TEMP_ID"){
+      LogUtils.time(logger, s"TEST LOG insert into TEMP_ID ${ids.size}"){
         try {
           ids.foreach { id =>
             insertLinkIdPS.setLong(1, id)
