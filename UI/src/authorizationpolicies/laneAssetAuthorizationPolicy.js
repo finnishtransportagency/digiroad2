9cc43e23cb2a863acabfe16add3937f7015ef6b1
sasuolander <sasu.olander@sitowise.com>
2022-04-20 12:47:04 +0300

    DROTH-3156 remove unneeded handleSuggestedAsset

--- UI/src/authorizationpolicies/laneAssetAuthorizationPolicy.js
+++ UI/src/authorizationpolicies/laneAssetAuthorizationPolicy.js
@@ -13,8 +13,5 @@
       return (!me.isUser('viewer') && me.isLaneMaintainer() || me.isOperator());
     };
     
-    this.handleSuggestedAsset = function(selectedAsset) {
-      return (_.isNull(selectedAsset.getId()) && me.isOperator()) || (selectedAsset.isSuggested() && (me.isOperator() || me.isMunicipalityMaintainer()));
-    };
   };
 })(this);
