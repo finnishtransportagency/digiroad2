f1ea40eeed077aaa3d360e66c43ee22c4d10a31a
reetta-kivisto <reetta.kivisto@sitowise.com>
2022-06-28 12:12:20 +0300

    DROTH-3293 New RoadLinkDAO

---
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/RoadLinkDAO.scala
@@ -0,0 +1,198 @@
+package fi.liikennevirasto.digiroad2.dao
+
+import slick.driver.JdbcDriver.backend.Database
+import Database.dynamicSession
+import com.vividsolutions.jts.geom.Polygon
+import fi.liikennevirasto.digiroad2.Point
+import fi.liikennevirasto.digiroad2.asset.{AdministrativeClass, BoundingRectangle, ConstructionType, LinkGeomSource, TrafficDirection}
+import fi.liikennevirasto.digiroad2.client.vvh.{FeatureClass, VVHRoadlink}
+import fi.liikennevirasto.digiroad2.postgis.PostGISDatabase
+import org.joda.time.DateTime
+import org.postgis.PGgeometry
+import org.postgresql.util.PGobject
+import slick.jdbc.{GetResult, PositionedResult}
+import slick.jdbc.StaticQuery.interpolation
+
+import scala.collection.mutable.ListBuffer
+
+class RoadLinkDAO {
+  private val geometryColumn: String = "shape"
+
+  implicit val getRoadLink: GetResult[VVHRoadlink] = new GetResult[VVHRoadlink] {
+    def apply(r: PositionedResult): VVHRoadlink = {
+      val linkId = r.nextLong()
+      val mtkId = r.nextLong()
+      val mtkHereFlip = r.nextInt()
+      val municipality = r.nextInt()
+      val path = r.nextObjectOption().map(extractGeometry).get
+      val administrativeClass = r.nextInt()
+      val directionType = r.nextIntOption()
+      val mtkClass = r.nextInt()
+      val roadNameFi = r.nextStringOption()
+      val roadNameSe = r.nextStringOption()
+      val roadNameSm = r.nextStringOption()
+      val roadNumber = r.nextLongOption()
+      val roadPart = r.nextIntOption()
+      val constructionType = r.nextInt()
+      val verticalLevel = r.nextInt()
+      val horizontalAccuracy = r.nextLong()
+      val verticalAccuracy = r.nextLong()
+      val createdDate = r.nextTimestampOption().map(new DateTime(_))
+      val lastEditedDate = r.nextTimestampOption().map(new DateTime(_))
+      val fromLeft = r.nextLongOption()
+      val toLeft = r.nextLongOption()
+      val fromRight = r.nextLongOption()
+      val toRight = r.nextLongOption()
+      val validFrom = r.nextTimestampOption().map(new DateTime(_))
+      val geometryEdited = r.nextTimestampOption().map(new DateTime(_))
+      val surfaceType = r.nextInt()
+      val subType = r.nextInt()
+      val objectId = r.nextLong()
+      val startNode = r.nextLong()
+      val endNode = r.nextLong()
+      val sourceInfo = r.nextInt()
+      val length  = r.nextDouble()
+
+      val geometry = path.map(point => Point(point(0), point(1), point(2)))
+      val geometryForApi = path.map(point => Map("x" -> point(0), "y" -> point(1), "z" -> point(2), "m" -> point(3)))
+      val geometryWKT = "LINESTRING ZM (" + path.map(point => s"${point(0)} ${point(1)} ${point(2)} ${point(3)}").mkString(", ") + ")"
+      val featureClass = extractFeatureClass(mtkClass)
+      val modifiedAt = extractModifiedDate(validFrom, lastEditedDate, geometryEdited)
+
+      val attributes = Map(
+        "MTKID" -> mtkId,
+        "MTKCLASS" -> mtkClass,
+        "HORIZONTALACCURACY" -> horizontalAccuracy,
+        "VERTICALACCURACY" -> verticalAccuracy,
+        "VERTICALLEVEL" -> BigInt(verticalLevel),
+        "CONSTRUCTIONTYPE" -> constructionType,
+        "ROADNAME_FI" -> roadNameFi,
+        "ROADNAME_SE" -> roadNameSe,
+        "ROADNAME_SM" -> roadNameSm,
+        "ROADNUMBER" -> roadNumber,
+        "ROADPARTNUMBER" -> roadPart,
+        "FROM_LEFT" -> fromLeft,
+        "TO_LEFT" -> toLeft,
+        "FROM_RIGHT" -> fromRight,
+        "TO_RIGHT" -> toRight,
+        "MUNICIPALITYCODE" -> BigInt(municipality),
+        "MTKHEREFLIP" -> mtkHereFlip,
+        "VALIDFROM" -> validFrom.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
+        "GEOMETRY_EDITED_DATE" -> geometryEdited.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
+        "CREATED_DATE" -> createdDate.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
+        "LAST_EDITED_DATE" -> lastEditedDate.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
+        "SURFACETYPE" -> BigInt(surfaceType),
+        "SUBTYPE" -> subType,
+        "OBJECTID" -> objectId,
+        "STARTNODE" -> startNode,
+        "ENDNODE" -> endNode,
+        "points" -> geometryForApi,
+        "geometryWKT" -> geometryWKT
+      ).collect {
+        case (key, Some(value)) => key -> value
+        case (key, value) if value != None => key -> value
+      }
+
+      VVHRoadlink(linkId, municipality, geometry, AdministrativeClass.apply(administrativeClass),
+        extractTrafficDirection(directionType), featureClass, modifiedAt, attributes,
+        ConstructionType.apply(constructionType), LinkGeomSource.apply(sourceInfo), length)
+    }
+  }
+  def getLinksWithFilter(filter: String): Seq[VVHRoadlink] = {
+    sql"""select linkid, mtkid, mtkhereflip, municipalitycode, shape, adminclass, directiontype, mtkclass, roadname_fi,
+                 roadname_se, roadname_sm, roadnumber, roadpartnumber, constructiontype, verticallevel, horizontalaccuracy,
+                 verticalaccuracy, created_date, last_edited_date, from_left, to_left, from_right, to_right, validfrom,
+                 geometry_edited_date, surfacetype, subtype, objectid, startnode, endnode, sourceinfo, geometrylength
+          from roadlink
+          where #$filter
+          """.as[VVHRoadlink].list
+  }
+
+  def getByMunicipalitiesAndBounds(bounds: BoundingRectangle, municipalities: Set[Int],
+                                   filter: Option[String]): Seq[VVHRoadlink] = {
+    val bboxFilter = PostGISDatabase.boundingBoxFilter(bounds, geometryColumn)
+    val withFilter = (municipalities.nonEmpty, filter.nonEmpty) match {
+      case (true, true) =>  s"and municipalitycode in (${municipalities.mkString(",")}) and ${filter.get}"
+      case (true, false) => s"and municipalitycode in (${municipalities.mkString(",")})"
+      case (false, true) => s"and ${filter.get}"
+      case _ => ""
+    }
+
+    getLinksWithFilter(s"$bboxFilter $withFilter")
+  }
+
+  def getByMunicipality(municipality: Int, filter: Option[String] = None): Seq[VVHRoadlink] = {
+    val queryFilter =
+      if (filter.nonEmpty) s"and ${filter.get}"
+      else ""
+
+    getLinksWithFilter(s"municipalitycode = $municipality $queryFilter")
+  }
+
+  def getByPolygon(polygon: Polygon): Seq[VVHRoadlink] = {
+    val polygonFilter = PostGISDatabase.polygonFilter(polygon, geometryColumn)
+
+    getLinksWithFilter(polygonFilter)
+  }
+
+  def getLinksIdByPolygons(polygon: Polygon): Seq[Long] = {
+    val polygonFilter = PostGISDatabase.polygonFilter(polygon, geometryColumn)
+
+    sql"""select linkid
+          from roadlink
+          where #$polygonFilter
+       """.as[Long].list
+  }
+
+  protected def extractFeatureClass(code: Int): FeatureClass = {
+    code match {
+      case 12316 => FeatureClass.TractorRoad
+      case 12141 => FeatureClass.DrivePath
+      case 12314 => FeatureClass.CycleOrPedestrianPath
+      case 12312 => FeatureClass.WinterRoads
+      case 12153 => FeatureClass.SpecialTransportWithoutGate
+      case 12154 => FeatureClass.SpecialTransportWithGate
+      case 12131 => FeatureClass.CarRoad_IIIa
+      case 12132 => FeatureClass.CarRoad_IIIb
+      case _ => FeatureClass.AllOthers
+    }
+  }
+
+  protected def extractTrafficDirection(code: Option[Int]): TrafficDirection = {
+    code match {
+      case Some(0) => TrafficDirection.BothDirections
+      case Some(1) => TrafficDirection.TowardsDigitizing
+      case Some(2) => TrafficDirection.AgainstDigitizing
+      case _ => TrafficDirection.UnknownDirection
+    }
+  }
+
+  protected def extractModifiedDate(validFrom: Option[DateTime], lastEdited: Option[DateTime],
+                                    geometryEdited: Option[DateTime]): Option[DateTime] = {
+    val validFromTime = if (validFrom.nonEmpty) validFrom.get.getMillis else 0
+    val lastEditedTime = if (lastEdited.nonEmpty) lastEdited.get.getMillis else 0
+    val geometryEditedTime = if (geometryEdited.nonEmpty) geometryEdited.get.getMillis else 0
+
+    val lastModification = {
+      if (lastEditedTime > geometryEditedTime) Some(lastEditedTime)
+      else if (geometryEditedTime > 0) Some(geometryEditedTime)
+      else None
+    }
+    lastModification.orElse(Option(validFromTime)).map(modified => new DateTime(modified))
+  }
+
+  protected def extractGeometry(data: Object): List[List[Double]] = {
+    val geometry = data.asInstanceOf[PGobject]
+    if (geometry == null) Nil
+    else {
+      val geomValue = geometry.getValue
+      val geom = PGgeometry.geomFromString(geomValue)
+      val listOfPoint= ListBuffer[List[Double]]()
+      for (i <- 0 until geom.numPoints() ){
+        val point =geom.getPoint(i)
+        listOfPoint += List(point.x, point.y, point.z, point.m)
+      }
+      listOfPoint.toList
+    }
+  }
+}
