6e1a57efeed5ca869cd016b6b4c16696304b8ffd
sasuolander <sasu.olander@sitowise.com>
2022-07-25 15:58:42 +0300

    DROTH-3250 change primary key

--- digiroad2-oracle/src/main/resources/db/migration/V1_6__add_column_complimentary_id.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_6__add_column_complimentary_id.sql
@@ -1,3 +1,6 @@
 
 ALTER TABLE roadlinkex ADD COLUMN linkid_UUID TYPE VARCHAR(40);
 create index linkid_UUID_index on roadlinkex (linkid_UUID);
+
+ALTER TABLE roadlinkex DROP CONSTRAINT roadlinkex_pkey;
+alter table roadlinkex ADD primary key (linkid_UUID);
