a6397ce9914c7a1b07f884ad3569f19f9ede1a6e
sasuolander <sasu.olander@sitowise.com>
2022-07-27 16:27:18 +0300

    DROTH-3242 initial version of batch

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/LinkIdImporter.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/LinkIdImporter.scala
@@ -1,99 +1,94 @@
 package fi.liikennevirasto.digiroad2.util
 
-import java.sql.{BatchUpdateException, SQLSyntaxErrorException}
 import fi.liikennevirasto.digiroad2.postgis.{MassQuery, PostGISDatabase}
 import org.joda.time.DateTime
-import slick.jdbc.StaticQuery.interpolation
-import slick.jdbc._
-import slick.driver.JdbcDriver.backend.{Database, DatabaseDef}
+import slick.driver.JdbcDriver.backend.Database
 import Database.dynamicSession
-import fi.liikennevirasto.digiroad2.{DummyEventBus, DummySerializer}
-import fi.liikennevirasto.digiroad2.client.vvh.RoadLinkClient
-import fi.liikennevirasto.digiroad2.dao.Queries
-import fi.liikennevirasto.digiroad2.service.RoadLinkService
+import slick.jdbc.StaticQuery.interpolation
+
+import java.sql.PreparedStatement
+import scala.collection.parallel.ForkJoinTaskSupport
+import scala.concurrent.forkjoin.ForkJoinPool
 
 object LinkIdImporter {
   def withDynTransaction(f: => Unit): Unit = PostGISDatabase.withDynTransaction(f)
   def withDynSession[T](f: => T): T = PostGISDatabase.withDynSession(f)
 
-  case class TableSpec(name: String)
-
-  def importLinkIdsFromVVH(vvhHost: String): Unit = {
-    withDynTransaction {
-      try { sqlu"""drop table mml_id_to_link_id""".execute } catch {
-        case e: SQLSyntaxErrorException => // ok
-      }
-      sqlu"""create table mml_id_to_link_id (
-          mml_id number(38, 0),
-          link_id varchar(40),
-          primary key (mml_id)
-        )""".execute
+  def changeLinkIdIntoKMTKVersion(): Unit = {
+    val forkJoinPool = new ForkJoinPool(20)
+    // what todo when doing historia tables
+    val tableNames = Seq(
+      "lane_history_position", "lane_position", "lrm_position", "lrm_position_history",
+      "temp_road_address_info", "road_link_attributes", "administrative_class",
+      "traffic_direction", "monouvre_element_history", "monouvre_element", "inaccurate_asset"
+    )
+    val tableNamesPar = tableNames.par
+    tableNamesPar.tasksupport = new ForkJoinTaskSupport(forkJoinPool)
+    tableNames.par.foreach { table =>
+      updateTable(table)
     }
 
-    val tableNames = Seq(
-      "lrm_position",
-      "traffic_direction",
-      "incomplete_link",
-      "functional_class",
-      "link_type",
-      "manoeuvre_element",
-      "unknown_speed_limit")
+    updateTableRoadLink("roadlink")
+  }
 
-    tableNames.foreach { table =>
-      updateTable(table, vvhHost)
+  def page(tableName: String, min: Int, max: Int) = {
+    sql"""select * from (select a.*, row_number() OVER() rnum 
+            from (select distinct vvh_id from #$tableName) a limit $max ) 
+            derivedMml where rnum >= $min"""
+  }
+
+  def flipColumns(tableName: String, columnName: String): Unit = {
+    sqlu"""ALTER TABLE ${tableName} DROP COLUMN vvh_id""".execute
+    sqlu"""ALTER TABLE ${tableName} RENAME COLUMN ${columnName} to vvh_id""".execute
+    sqlu"""ALTER TABLE ${tableName} ADD COLUMN ${columnName} NUMERIC(38)""".execute
+  }
+
+  def prepare(tableName: String) = {
+    val count = sql"""select count(distinct vvh_id) from #$tableName""".as[Int].first
+    val batches = getBatchDrivers(0, count, 20000)
+    val total = batches.size
+    (batches, total)
+  }
+
+  def updateCommand(columnName: String): String = {
+    s"""update ? SET ${columnName} = (select id from frozenlinks_vastintaulu_csv 
+                where vvh_linkid = ? ) where vvh_id = ? """
+  }
+
+  def createRow(statement: PreparedStatement, id: Int, tableName: String): Unit = {
+    statement.setString(1, tableName)
+    statement.setInt(2, id)
+    statement.setInt(3, id)
+    statement.addBatch()
+  }
+
+  def updateTableRoadLink(tableName: String): Unit = {
+    withDynTransaction {
+      val startTime = DateTime.now()
+      val (batches, total) = prepare(tableName)
+
+      print(s"[${DateTime.now}] Table $tableName: Fetching $total batches of links… ")
+      flipColumns(tableName, "linkid")
+      batches.foreach { case (min, max) =>
+        val ids = page(tableName, min, max).as[Int].list
+        MassQuery.executeBatch(updateCommand("linkid")) {
+          statement => {ids.foreach(i => {createRow(statement, i, tableName)})}}
+      }
+      println(s"done in ${AssetDataImporter.humanReadableDurationSince(startTime)}.")
     }
   }
 
-  def updateTable(tableName: String, vvhHost: String): Unit = {
-    val roadLinkClient = new RoadLinkClient(vvhHost)
-    val roadLinkService = new RoadLinkService(roadLinkClient,new DummyEventBus,new DummySerializer)
-
+  def updateTable(tableName: String): Unit = {
     withDynTransaction {
-      sqlu"""delete from mml_id_to_link_id""".execute
-
-      val tempPS = dynamicSession.prepareStatement(
-        """
-        insert into mml_id_to_link_id (mml_id, link_id) values (?, ?)
-        """)
-
-      val count = sql"""select count(distinct mml_id) from #$tableName""".as[Int].first
-
-      val batches = getBatchDrivers(0, count, 20000)
-      val total = batches.size
-
-      print(s"[${DateTime.now}] Table $tableName: Fetching $total batches of links… ")
-
       val startTime = DateTime.now()
-
+      val (batches, total) = prepare(tableName)
+      print(s"[${DateTime.now}] Table $tableName: Fetching $total batches of links… ")
+      flipColumns(tableName, "link_id")
       batches.foreach { case (min, max) =>
-        val mmlIds =
-          sql"""
-               select *
-               from (select a.*, row_number() OVER() rnum
-                      from (select distinct mml_id from #$tableName) a
-                      limit $max
-                ) derivedMml where rnum >= $min
-          """.as[Long].list
-        val links = roadLinkService.fetchByMmlIds(mmlIds.toSet)
-        links.foreach { link =>
-          val mmlId = link.attributes("MTKID").asInstanceOf[BigInt].longValue()
-          tempPS.setLong(1, mmlId)
-          tempPS.setString(2, link.linkId)
-          tempPS.addBatch()
-        }
-        try {
-          tempPS.executeBatch()
-        }
-        catch {
-          case e: BatchUpdateException => // ok, assume it was a duplicate MML ID
-        }
+        val ids = page(tableName, min, max).as[Int].list
+        MassQuery.executeBatch(updateCommand("link_id")) { 
+          statement => {ids.foreach(i => {createRow(statement, i, tableName)})}}
       }
-
-      sqlu"""
-        update #$tableName pos
-               set pos.link_id = (select t.link_id from mml_id_to_link_id t where t.mml_id = pos.mml_id)
-      """.execute
-
       println(s"done in ${AssetDataImporter.humanReadableDurationSince(startTime)}.")
     }
   }
