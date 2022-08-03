1c552dd792489e7f3bc8429a6f9ae428cb50d1d1
antti.ahopelto <antti.ahopelto@sitowise.com>
2022-08-03 13:03:23 +0300

    DROTH-3104 Moved getObjectFromS3 to AwsService
    (cherry picked from commit d9e720f2c9c511f46391ebb0da362da12c9df3c8)

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/MainLaneStartDateImporter.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/MainLaneStartDateImporter.scala
@@ -3,20 +3,17 @@ package fi.liikennevirasto.digiroad2.util
 import fi.liikennevirasto.digiroad2.client.vvh.VVHClient
 import fi.liikennevirasto.digiroad2.csvDataImporter.LanesCsvImporter
 import fi.liikennevirasto.digiroad2.middleware.UpdateOnlyStartDates
-import fi.liikennevirasto.digiroad2.service.RoadLinkService
+import fi.liikennevirasto.digiroad2.service.{AwsService, RoadLinkService}
 import fi.liikennevirasto.digiroad2.user.{Configuration, User}
 import fi.liikennevirasto.digiroad2.{DummyEventBus, DummySerializer}
 import org.slf4j.LoggerFactory
-import software.amazon.awssdk.auth.credentials.{InstanceProfileCredentialsProvider, ProfileCredentialsProvider}
-import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
 
 object MainLaneStartDateImporter {
   lazy val vvhClient = new VVHClient(Digiroad2Properties.vvhRestApiEndPoint)
   lazy val roadLinkService = new RoadLinkService(vvhClient, new DummyEventBus, new DummySerializer)
   lazy val lanesCsvImporter: LanesCsvImporter = new LanesCsvImporter(roadLinkService, new DummyEventBus)
   val logger = LoggerFactory.getLogger(getClass)
-  val s3: S3Client = S3Client.create()
+  val awsService = new AwsService
 
   def main(args: Array[String]): Unit = {
 
@@ -24,16 +21,11 @@ object MainLaneStartDateImporter {
     val objectKey = args(1)
     processStartDates(s3bucket, objectKey)
 
-    def getObjectFromS3(s3bucket: String, key: String) = {
-      val getObjectRequest = GetObjectRequest.builder().bucket(s3bucket).key(key).build()
-      s3.getObject(getObjectRequest)
-    }
-
     def processStartDates(s3bucket: String, objectKey: String) = {
       val user = User(0, "start_date_importer", Configuration())
       val onlyStartDates = UpdateOnlyStartDates(true)
 
-      val s3Object = getObjectFromS3(s3bucket, objectKey)
+      val s3Object = awsService.S3.getObjectFromS3(s3bucket, objectKey)
       val fileName = objectKey
       val result = lanesCsvImporter.processing(s3Object, user, onlyStartDates, fileName)
 
