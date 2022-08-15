cd8bf6c53387989ad79034d8f801a17aa5b35a3f
reetta-kivisto <reetta.kivisto@sitowise.com>
2022-08-15 13:55:15 +0300

    DROTH-3244 Update test data

--- digiroad2-oracle/sql/kauniainen_railway_crossings.sql
+++ digiroad2-oracle/sql/kauniainen_railway_crossings.sql
@@ -1,5 +1,5 @@
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600049,230,'dr2_test_data',235);
-INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000012, 1611317, 388553074, 103.000, 103.000, 1);
+INSERT INTO LRM_POSITION (ID, LINK_ID, VVH_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000012, '52d58ce5-39e8-4ab4-8c43-d347a9945ab5:1', 1611317, 388553074, 103.000, 103.000, 1);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600049, 70000012);
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600049, (select id from enumerated_value where name_fi='Valo/äänimerkki'), (select id from property where public_id='turvavarustus'));
 insert into text_property_value(id, asset_id, property_id, value_fi, created_date, created_by) values (nextval('primary_key_seq'), 600049, (select id from property where public_id='tasoristeystunnus'), 'test_code', current_timestamp, 'dr2_test_data');
@@ -7,7 +7,7 @@ UPDATE asset SET geometry = ST_GeomFromText('POINT(374467 6677347 0 0)',3067)
   WHERE id = 600049;
 
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600050,230,'dr2_test_data',235);
-INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000013, 1611341, 388553080, 69.000, 69.000, 1);
+INSERT INTO LRM_POSITION (ID, LINK_ID, VVH_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000013, '44dc1e56-79fb-452b-b35c-7ccb2b17b8aa:1', 1611341, 388553080, 69.000, 69.000, 1);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600050, 70000013);
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600050, (select id from enumerated_value where name_fi='Puolipuomi'), (select id from property where public_id='turvavarustus'));
 insert into text_property_value(id, asset_id, property_id, value_fi, created_date, created_by) values (nextval('primary_key_seq'), 600050, (select id from property where public_id='tasoristeystunnus'), 'test_code', current_timestamp, 'dr2_test_data');
@@ -15,7 +15,7 @@ UPDATE asset SET geometry = ST_GeomFromText('POINT(374443.764141219 6677245.2833
   WHERE id = 600050;
 
 insert into ASSET (ID,ASSET_TYPE_ID,CREATED_BY,MUNICIPALITY_CODE) values (600051,230,'dr2_test_data',235);
-INSERT INTO LRM_POSITION (ID, LINK_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000014, 12345, 12345, 103.000, 103.000, 1);
+INSERT INTO LRM_POSITION (ID, LINK_ID, VVH_ID, MML_ID, START_MEASURE, END_MEASURE, SIDE_CODE) VALUES (70000014, 'abcdabcd-1234-5678-abcd-abcdabcdabcd:7', 12345, 12345, 103.000, 103.000, 1);
 insert into asset_link (ASSET_ID, POSITION_ID) values (600051, 70000014);
 insert into text_property_value(id, asset_id, property_id, value_fi, created_date, created_by) values (600052, 600051, (select id from property where public_id='rautatien_tasoristeyksen_nimi'), 'Hyvä nimi', current_timestamp, 'dr2_test_data');
 insert into single_choice_value(asset_id, enumerated_value_id, property_id) values (600051, (select id from enumerated_value where name_fi='Valo/äänimerkki'), (select id from property where public_id='turvavarustus'));
