8fce2152d88ac387a6a18f328ceeefd02743fcc1
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2022-09-09 13:54:06 +0300

    DROTH-3409 unique constraint for asset_id + change file to v1_7

--- digiroad2-oracle/src/main/resources/db/migration/V1_7__municipality_asset_id_mapping_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_7__municipality_asset_id_mapping_table.sql
@@ -4,5 +4,6 @@ CREATE TABLE municipality_asset_id_mapping
     municipality_asset_id   varchar(128) NOT NULL,
     municipality_code   bigint NOT NULL,
     CONSTRAINT fk_municipality_asset_id_mapping_asset FOREIGN KEY (asset_id) REFERENCES asset(id) ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT asset_id_unique UNIQUE (asset_id),
     CONSTRAINT pk_municipality_asset_id_code_pair PRIMARY KEY (municipality_asset_id, municipality_code)
 );
