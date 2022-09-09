238626a71371abbe2baca2b1ab662dd9c6e98153
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2022-09-09 13:09:38 +0300

    DROTH-3409 create mapping table for municipality asset id

---
+++ digiroad2-oracle/src/main/resources/db/migration/V1_6__municipality_asset_id_mapping_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE municipality_asset_id_mapping
+(
+    asset_id    bigint NOT NULL,
+    municipality_asset_id   varchar(128) NOT NULL,
+    municipality_code   bigint NOT NULL,
+    CONSTRAINT fk_municipality_asset_id_mapping_asset FOREIGN KEY (asset_id) REFERENCES asset(id) ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT pk_municipality_asset_id_code_pair PRIMARY KEY (municipality_asset_id, municipality_code)
+);
