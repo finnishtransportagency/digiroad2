86b2ceaf15244733da88b8012e5b40948fe9f196
sasuolander <sasu.olander@sitowise.com>
2022-09-20 09:25:26 +0300

    DROTH-3235 fix test

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/RoadLinkDAOSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/RoadLinkDAOSpec.scala
@@ -1,5 +1,6 @@
 package fi.liikennevirasto.digiroad2.dao
 
+import fi.liikennevirasto.digiroad2.asset.ConstructionType
 import fi.liikennevirasto.digiroad2.postgis.PostGISDatabase
 import fi.liikennevirasto.digiroad2.util.{LinkIdGenerator, TestTransactions}
 import org.joda.time.DateTime
@@ -15,8 +16,8 @@ class RoadLinkDAOSpec extends FunSuite {
     runWithRollback  {
       val dao = new RoadLinkDAO
       val (linkId1, linkId2) = (LinkIdGenerator.generateRandom(), LinkIdGenerator.generateRandom())
-      sqlu"""INSERT INTO kgv_roadlink (linkid, municipalitycode, roadname_fi, roadname_se,constructiontype,shape) VALUES($linkId1, 853, 'Tarkk''ampujankatu', 'Skarpskyttegatan',0,'SRID=3067;LINESTRING ZM(385935.666 6671107.833 19.858 0, 386028.217 6671112.363 20.596 92.661)'::geometry)""".execute
-      sqlu"""INSERT INTO kgv_roadlink (linkid, municipalitycode, roadname_fi, roadname_se,constructiontype,shape) VALUES($linkId2, 441, 'Sammalinen', NULL,0,'SRID=3067;LINESTRING ZM(385935.666 6671107.833 19.85 0, 386028.217 6671112.363 20.596 92.661)'::geometry)""".execute
+      sqlu"""INSERT INTO kgv_roadlink (linkid, municipalitycode, roadname_fi, roadname_se,constructiontype,shape) VALUES($linkId1, 853, 'Tarkk''ampujankatu', 'Skarpskyttegatan',${ConstructionType.InUse.value},'SRID=3067;LINESTRING ZM(385935.666 6671107.833 19.858 0, 386028.217 6671112.363 20.596 92.661)'::geometry)""".execute
+      sqlu"""INSERT INTO kgv_roadlink (linkid, municipalitycode, roadname_fi, roadname_se,constructiontype,shape) VALUES($linkId2, 441, 'Sammalinen', NULL,${ConstructionType.InUse.value},'SRID=3067;LINESTRING ZM(385935.666 6671107.833 19.85 0, 386028.217 6671112.363 20.596 92.661)'::geometry)""".execute
       dao.fetchByRoadNames("roadname_fi", Set("Tarkk'ampujankatu")).size should be (1)
       dao.fetchByRoadNames("roadname_fi", Set("Tarkk'ampujankatu","Sammalinen")).size should be (2)
     }
