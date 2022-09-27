3497c1f3803db28d5ff8ba71ad48f8937efbcb64
antti.ahopelto <antti.ahopelto@sitowise.com>
2022-09-27 12:00:21 +0300

    DROTH-3420 Use Enum, format code better

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/kgv/KgvOperation.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/kgv/KgvOperation.scala
@@ -3,6 +3,7 @@ package fi.liikennevirasto.digiroad2.client.kgv
 import com.vividsolutions.jts.geom.Polygon
 import fi.liikennevirasto.digiroad2.Point
 import fi.liikennevirasto.digiroad2.asset._
+import fi.liikennevirasto.digiroad2.client.kgv.KgvCollection.Changes
 import fi.liikennevirasto.digiroad2.client.{ClientException, FeatureClass, Filter, LinkOperationError, RoadLinkFetched}
 import fi.liikennevirasto.digiroad2.util.{Digiroad2Properties, KgvUtil, LogUtils, Parallel}
 import org.apache.http.HttpStatus
@@ -275,7 +276,7 @@ abstract class KgvOperation(extractor:ExtractorBase) extends LinkOperationsAbstr
   override protected implicit val jsonFormats = DefaultFormats.preservingEmptyValues
 
   private def convertToFeature(content: Map[String, Any]): Feature = {
-    val geometry = if( serviceName == "keskilinjavarasto:change" ) Geometry("", List())
+    val geometry = if( serviceName == Changes.value ) Geometry("", List())
     else{
       Geometry(`type` = content("geometry").asInstanceOf[Map[String, Any]]("type").toString,
         coordinates = content("geometry").asInstanceOf[Map[String, Any]]("coordinates").asInstanceOf[List[List[Double]]])
@@ -341,10 +342,14 @@ abstract class KgvOperation(extractor:ExtractorBase) extends LinkOperationsAbstr
 
               val nextLink = Try(links.find(_.title=="Next page").get.href).getOrElse("")
               val previousLink = Try(links.find(_.title=="Previous page").get.href).getOrElse("")
-              val features = if(serviceName == "keskilinjavarasto:change"){
+              val features = if(serviceName == Changes.value){
                 feature("features").asInstanceOf[List[Map[String, Any]]].map(convertToFeature)
               }
-              else feature("features").asInstanceOf[List[Map[String, Any]]] .filter(roadLinkStatusFilter).map(convertToFeature)
+              else {
+                feature("features").asInstanceOf[List[Map[String, Any]]]
+                  .filter(roadLinkStatusFilter)
+                  .map(convertToFeature)
+              }
               Some(FeatureCollection(
                 `type` = "FeatureCollection",
                 features = features,
