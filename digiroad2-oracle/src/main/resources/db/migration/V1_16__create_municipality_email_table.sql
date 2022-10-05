b7146b5fa7d81c8ab5ac4ddafe5095bd0a94bf9d
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2022-10-05 14:14:43 +0300

    DROTH-3439 change email to email_address for clarity

--- digiroad2-oracle/src/main/resources/db/migration/V1_16__create_municipality_email_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_16__create_municipality_email_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE municipality_email
 (
     id                  bigint    PRIMARY KEY NOT NULL,
-    email               varchar(255)    NOT NULL,
+    email_address       varchar(255)    NOT NULL,
     municipality_code   bigint    NOT NULL,
     created_date        timestamp NOT NULL DEFAULT current_timestamp
 );
