90326a599e9d31eec7d8a29fb1f42715c8c7be89
reetta-kivisto <reetta.kivisto@sitowise.com>
2022-10-14 14:58:25 +0300

    DROTH-3459 Get complementary links from qgis_roadlinkex table

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/ComplementaryLinkDAO.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/ComplementaryLinkDAO.scala
@@ -15,9 +15,26 @@ import slick.jdbc.{GetResult, PositionedResult}
 
 class ComplementaryLinkDAO extends RoadLinkDAO {
 
+  protected override def withFinNameFilter(roadNameSource: String)(roadNames: Set[String]): String = {
+    val fixedRoadNameSource = roadNameSource match {
+      case "ROADNAME_FI" => "ROADNAMEFIN"
+      case "ROADNAME_SE" => "ROADNAMESWE"
+      case _ => roadNameSource
+    }
+    withRoadNameFilter(fixedRoadNameSource, roadNames)
+  }
+
+  protected override def withMtkClassFilter(ids: Set[Long]): String = {
+    withFilter("ROADCLASS", ids)
+  }
+
+  protected override def withLastEditedDateFilter(lowerDate: DateTime, higherDate: DateTime): String = {
+    withDateLimitFilter("VERSIONSTARTTIME", lowerDate, higherDate)
+  }
+
   implicit override val getRoadLink: GetResult[RoadLinkFetched] = new GetResult[RoadLinkFetched] {
     def apply(r: PositionedResult): RoadLinkFetched = {
-      val linkId = r.nextLong()
+      val linkId = r.nextString()
       val municipality = r.nextInt()
       val path = r.nextObjectOption().map(extractGeometry).get
       val administrativeClass = r.nextInt()
@@ -25,24 +42,16 @@ class ComplementaryLinkDAO extends RoadLinkDAO {
       val mtkClass = r.nextInt()
       val roadNameFi = r.nextStringOption()
       val roadNameSe = r.nextStringOption()
-      val roadNameSm = r.nextStringOption()
+      val roadNameSme = r.nextStringOption()
+      val roadNameSmn = r.nextStringOption()
+      val roadNameSms = r.nextStringOption()
       val roadNumber = r.nextLongOption()
       val roadPart = r.nextIntOption()
       val constructionType = r.nextInt()
       val verticalLevel = r.nextInt()
-      val horizontalAccuracy = r.nextBigDecimalOption()
-      val verticalAccuracy = r.nextBigDecimalOption()
       val createdDate = r.nextTimestampOption().map(new DateTime(_))
       val lastEditedDate = r.nextTimestampOption().map(new DateTime(_))
-      val fromLeft = r.nextLongOption()
-      val toLeft = r.nextLongOption()
-      val fromRight = r.nextLongOption()
-      val toRight = r.nextLongOption()
-      val validFrom = r.nextTimestampOption().map(new DateTime(_))
-      val geometryEdited = r.nextTimestampOption().map(new DateTime(_))
       val surfaceType = r.nextInt()
-      val subType = r.nextInt()
-      val objectId = r.nextLong()
       val sourceInfo = r.nextInt()
       val length  = r.nextDouble()
       val custOwner = r.nextLongOption()
@@ -51,31 +60,23 @@ class ComplementaryLinkDAO extends RoadLinkDAO {
       val geometryForApi = path.map(point => Map("x" -> point(0), "y" -> point(1), "z" -> point(2), "m" -> point(3)))
       val geometryWKT = "LINESTRING ZM (" + path.map(point => s"${point(0)} ${point(1)} ${point(2)} ${point(3)}").mkString(", ") + ")"
       val featureClass = extractFeatureClass(mtkClass)
-      val modifiedAt = extractModifiedDate(validFrom, lastEditedDate, geometryEdited)
+      val modifiedAt = extractModifiedDate(createdDate.map(_.getMillis), lastEditedDate.map(_.getMillis))
 
       val attributes = Map(
         "MTKCLASS" -> mtkClass,
-        "HORIZONTALACCURACY" -> horizontalAccuracy,
-        "VERTICALACCURACY" -> verticalAccuracy,
         "VERTICALLEVEL" -> BigInt(verticalLevel),
         "CONSTRUCTIONTYPE" -> constructionType,
         "ROADNAME_FI" -> roadNameFi,
         "ROADNAME_SE" -> roadNameSe,
-        "ROADNAMESME" -> roadNameSm,
+        "ROADNAMESME" -> roadNameSme,
+        "ROADNAMESMN" -> roadNameSmn,
+        "ROADNAMESMS" -> roadNameSms,
         "ROADNUMBER" -> roadNumber,
         "ROADPARTNUMBER" -> roadPart,
-        "FROM_LEFT" -> fromLeft,
-        "TO_LEFT" -> toLeft,
-        "FROM_RIGHT" -> fromRight,
-        "TO_RIGHT" -> toRight,
         "MUNICIPALITYCODE" -> BigInt(municipality),
-        "VALIDFROM" -> validFrom.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
-        "GEOMETRY_EDITED_DATE" -> geometryEdited.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
         "CREATED_DATE" -> createdDate.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
         "LAST_EDITED_DATE" -> lastEditedDate.map(time => BigInt(time.toDateTime.getMillis)).getOrElse(None),
         "SURFACETYPE" -> BigInt(surfaceType),
-        "SUBTYPE" -> subType,
-        "OBJECTID" -> objectId,
         "points" -> geometryForApi,
         "geometryWKT" -> geometryWKT,
         "CUST_OWNER" -> custOwner
@@ -84,7 +85,7 @@ class ComplementaryLinkDAO extends RoadLinkDAO {
         case (key, value) if value != None => key -> value
       }
 
-      RoadLinkFetched(linkId.toString, municipality, geometry, AdministrativeClass.apply(administrativeClass),
+      RoadLinkFetched(linkId, municipality, geometry, AdministrativeClass.apply(administrativeClass),
         extractTrafficDirection(directionType), featureClass, modifiedAt, attributes,
         ConstructionType.apply(constructionType), ComplimentaryLinkInterface, length)
     }
@@ -92,35 +93,21 @@ class ComplementaryLinkDAO extends RoadLinkDAO {
   
   override def getLinksWithFilter(filter: String): Seq[RoadLinkFetched] = {
     LogUtils.time(logger,"TEST LOG Getting complementery roadlinks" ){
-      sql"""select linkid, municipalitycode, shape, adminclass, directiontype, mtkclass, roadname_fi, roadname_se,
-                 roadname_sm, roadnumber, roadpartnumber, constructiontype, verticallevel, horizontalaccuracy,
-                 verticalaccuracy, created_date, last_edited_date, from_left, to_left, from_right, to_right, validfrom,
-                 geometry_edited_date, surfacetype, subtype, objectid, sourceinfo, geometrylength, cust_owner
-          from roadlinkex
-          where subtype = 3 and #$filter
+      sql"""select linkid, municipalitycode, shape, adminclass, directiontype, roadclass, roadnamefin, roadnameswe,
+                 roadnamesme, roadnamesmn, roadnamesms, roadnumber, roadpartnumber, lifecyclestatus, surfacerelation,
+                 starttime, versionstarttime, surfacetype, datasource, horizontallength, cust_owner
+          from qgis_roadlinkex
+          where #$filter
           """.as[RoadLinkFetched].list
     }
-  }
-
-  protected def extractModifiedDate(createdDate: Option[DateTime], lastEdited: Option[DateTime], geometryEdited: Option[DateTime]): Option[DateTime] = {
-    val createdDateTime = if (createdDate.nonEmpty) createdDate.get.getMillis else 0
-    val lastEditedTime = if (lastEdited.nonEmpty) lastEdited.get.getMillis else 0
-    val geometryEditedTime = if (geometryEdited.nonEmpty) geometryEdited.get.getMillis else 0
-
-    val lastModification = {
-      if (lastEditedTime > geometryEditedTime) Some(lastEditedTime)
-      else if (geometryEditedTime > 0) Some(geometryEditedTime)
-      else None
-    }
-    lastModification.orElse(Option(createdDateTime)).map(modified => new DateTime(modified))
   }
   
   override def getLinksIdByPolygons(polygon: Polygon): Seq[String] = {
     val polygonFilter = PostGISDatabase.polygonFilter(polygon, geometryColumn)
     LogUtils.time(logger,"TEST LOG Getting complementery roadlinks by polygon" ){
       sql"""select linkid
-          from roadlinkex
-          where subtype = 3 and #$polygonFilter
+          from qgis_roadlinkex
+          where #$polygonFilter
        """.as[String].list
     }
   }
