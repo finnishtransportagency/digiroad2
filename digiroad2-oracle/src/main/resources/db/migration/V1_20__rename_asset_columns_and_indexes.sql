37e60aef072551e96c0493fd3cec04b369be4567
reetta-kivisto <reetta.kivisto@sitowise.com>
2022-11-28 09:41:53 +0200

    DROTH-3489 Add new external id column with indexes

--- digiroad2-oracle/src/main/resources/db/migration/V1_20__rename_asset_columns_and_indexes.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_20__rename_asset_columns_and_indexes.sql
@@ -3,3 +3,9 @@ ALTER INDEX external_id_index RENAME TO national_id_index;
 
 ALTER TABLE asset_history RENAME COLUMN external_id TO national_id;
 ALTER INDEX hist_external_id_index RENAME TO hist_national_id_index;
+
+ALTER TABLE asset ADD COLUMN external_id VARCHAR(128);
+CREATE INDEX external_id_idx ON asset (external_id);
+
+ALTER TABLE asset_history ADD COLUMN external_id VARCHAR(128);
+CREATE INDEX hist_external_id_idx ON asset_history (external_id);
