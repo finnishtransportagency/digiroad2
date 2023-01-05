4bc282f78ffdf33d1e601550c65611fc672a0caa
reetta-kivisto <reetta.kivisto@sitowise.com>
2023-01-05 12:18:02 +0200

    DROTH-3562 Log how many retry attempts are left

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
@@ -1,16 +1,18 @@
 package fi.liikennevirasto.digiroad2.util
 
+import org.slf4j.Logger
 import scala.annotation.tailrec
 
 object ClientUtils {
 
   @tailrec
-  def retry[T](retries: Int)(fn: => T): T = {
+  def retry[T](retries: Int, logger: Logger)(fn: => T): T = {
     try {
       fn
     } catch {
-      case e =>
-        if (retries > 1) retry(retries - 1)(fn)
+      case e: Throwable =>
+        logger.error(s"TEST LOG Query failed (retries left: ${retries - 1}). Error: ${e.getMessage}")
+        if (retries > 1) retry(retries - 1, logger)(fn)
         else throw e
     }
   }
