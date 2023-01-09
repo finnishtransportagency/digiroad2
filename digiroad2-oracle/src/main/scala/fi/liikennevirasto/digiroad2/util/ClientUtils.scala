1964aa599ec71564a3ebbe6a2735f8c99a126fb3
sasuolander <sasu.olander@sitowise.com>
2023-01-09 15:48:25 +0200

    DROTH-3564 fix backoff implementations

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
@@ -6,18 +6,20 @@ import scala.annotation.tailrec
 object ClientUtils {
 
   @tailrec
-  def retry[T](retries: Int, logger: Logger)(fn: => T): T = {
-    val exponent = 1
+  def retry[T](retries: Int, logger: Logger,exponentToLoop: Int =1)(fn: => T): T = {
+    var exponent = exponentToLoop
     try {
       fn
     } catch {
       case e: Throwable =>
         logger.error(s"TEST LOG Query failed (retries left: ${retries - 1}). Error: ${e.getMessage}")
         if (retries > 1){
-          Thread.sleep(500*(exponent + 1)) // wait before making new request
-          retry(retries - 1, logger)(fn)
-        } 
+          Thread.sleep(500*exponent) // wait before making new request
+          exponent+=1
+          retry(retries - 1, logger,exponent)(fn)
+        }
         else throw e
+      
     }
   }
 
