26712a8c81e777c3d331c8afbdd58ebf321fb91b
sasuolander <sasu.olander@sitowise.com>
2023-01-09 14:06:16 +0200

    DROTH-3564 add waiting time before trying again

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
@@ -7,12 +7,16 @@ object ClientUtils {
 
   @tailrec
   def retry[T](retries: Int, logger: Logger)(fn: => T): T = {
+    val exponent = 1
     try {
       fn
     } catch {
       case e: Throwable =>
         logger.error(s"TEST LOG Query failed (retries left: ${retries - 1}). Error: ${e.getMessage}")
-        if (retries > 1) retry(retries - 1, logger)(fn)
+        if (retries > 1){
+          Thread.sleep(500*(exponent + 1)) // wait before making new request
+          retry(retries - 1, logger)(fn)
+        } 
         else throw e
     }
   }
