0324513113556a48a108d8d6044e23d56d6f63eb
sasuolander <sasu.olander@sitowise.com>
2023-01-18 09:44:10 +0200

    DROTH-3564 add more logging to find better way to reproduce error

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/ClientUtils.scala
@@ -6,17 +6,22 @@ import scala.annotation.tailrec
 object ClientUtils {
 
   @tailrec
-  def retry[T](retries: Int, logger: Logger,exponentToLoop: Int =1)(fn: => T): T = {
+  def retry[T](retries: Int, logger: Logger,exponentToLoop: Int =1,firstRun : Boolean= true, commentForFailing : String = "")(fn: => T): T = {
     var exponent = exponentToLoop
     try {
       fn
     } catch {
       case e: Throwable =>
         logger.error(s"TEST LOG Query failed (retries left: ${retries - 1}). Error: ${e.getMessage}")
+        
+        if (firstRun && commentForFailing.nonEmpty) {
+          logger.info(commentForFailing)
+        }
+        
         if (retries > 1){
           Thread.sleep(500*exponent) // wait before making new request
           exponent+=1
-          retry(retries - 1, logger,exponent)(fn)
+          retry(retries - 1, logger,exponent,firstRun = false)(fn)
         }
         else throw e
       
