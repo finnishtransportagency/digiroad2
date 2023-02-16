700c29ab3d6595c948c1c7225ad63436bca71f02
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2023-02-16 16:46:37 +0200

    DROTH-3505 tests and improvements

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/RoadLinkChangeClient.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/client/RoadLinkChangeClient.scala
@@ -1,58 +1,44 @@
 package fi.liikennevirasto.digiroad2.client
 
 import fi.liikennevirasto.digiroad2.Point
+import fi.liikennevirasto.digiroad2.asset.{AdministrativeClass, TrafficDirection, Unknown}
 import fi.liikennevirasto.digiroad2.service.AwsService
 import fi.liikennevirasto.digiroad2.util.Digiroad2Properties
 import org.joda.time.DateTime
 import org.json4s.FieldSerializer.{renameFrom, renameTo}
-import org.json4s._
+import org.json4s.JsonAST.JString
 import org.json4s.jackson.parseJson
+import org.json4s.{CustomSerializer, _}
 import org.postgis.PGgeometry
 import org.slf4j.LoggerFactory
 import software.amazon.awssdk.services.s3.model._
-
-import scala.Seq
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
 import scala.io.Source.fromInputStream
 
+trait RoadLinkChangeType {
+  def value: String
+}
+object RoadLinkChangeType {
+  val values = Set(Add, Remove, Replace, Split, Unknown)
+
+  def apply(stringValue: String): RoadLinkChangeType = {
+    values.find(_.value == stringValue).getOrElse(Unknown)
+  }
+
+  case object Add extends RoadLinkChangeType { def value = "add" }
+  case object Remove extends RoadLinkChangeType { def value = "remove" }
+  case object Replace extends RoadLinkChangeType { def value = "replace"}
+  case object Split extends RoadLinkChangeType { def value = "split" }
+  case object Unknown extends RoadLinkChangeType { def value = "unknown" }
+}
+
 class RoadLinkChangeClient {
-  val awsService = new AwsService
-  val s3Service: awsService.S3.type = awsService.S3
-  val s3Client = s3Service.s3
-  val s3Bucket: String = Digiroad2Properties.roadLinkChangeS3BucketName
+  lazy val awsService = new AwsService
+  lazy val s3Service: awsService.S3.type = awsService.S3
+  lazy val s3Client = s3Service.s3
+  lazy val s3Bucket: String = Digiroad2Properties.roadLinkChangeS3BucketName
   val logger = LoggerFactory.getLogger(getClass)
-
-  sealed trait ChangeType2 {
-    def value: String
-  }
-  object ChangeType2 {
-    val values = Set(Add, Remove, Replace, Split, Unknown)
-
-    def apply(stringValue: String): ChangeType2 = {
-      values.find(_.value == stringValue).getOrElse(Unknown)
-    }
-
-    case object Add extends ChangeType2 {
-      def value = "add"
-    }
-
-    case object Remove extends ChangeType2 {
-      def value = "remove"
-    }
-
-    case object Replace extends ChangeType2 {
-      def value = "replace"
-    }
-
-    case object Split extends ChangeType2 {
-      def value = "split"
-    }
-
-    case object Unknown extends ChangeType2 {
-      def value = "unknown"
-    }
-  }
 
   private def lineStringToPoints(lineString: String): List[Point] = {
     val geometry = PGgeometry.geomFromString(lineString)
@@ -68,15 +54,48 @@ class RoadLinkChangeClient {
     renameTo("newLinks", "new") orElse renameTo("oldLink", "old"),
     renameFrom("new", "newLinks") orElse renameFrom("old", "oldLink"))
 
-  object ChangeTypeSerializer extends CustomSerializer[ChangeType2](_ => (
+  object RoadLinkChangeTypeSerializer extends CustomSerializer[RoadLinkChangeType](_ => (
     {
       case JString(stringValue) =>
-        ChangeType2(stringValue)
+        RoadLinkChangeType(stringValue)
     },
     {
-      case changeType: ChangeType2 =>
+      case changeType: RoadLinkChangeType =>
         JObject(JField("changeType", JString(changeType.value)))
     }
+  ))
+
+  object AdminClassSerializer extends CustomSerializer[AdministrativeClass](_ => (
+    {
+      case JInt(bigIntValue) =>
+        AdministrativeClass(bigIntValue.toInt)
+      case JNull => Unknown
+    },
+    {
+      case adminClass: AdministrativeClass =>
+        JObject(JField("adminClass", JInt(adminClass.value)))
+    }
+  ))
+
+  object TrafficDirectionSerializer extends CustomSerializer[TrafficDirection](_ => (
+  {
+    case JInt(directionValue) =>
+      directionValue.toInt match {
+        case 0 => TrafficDirection.BothDirections
+        case 1 => TrafficDirection.TowardsDigitizing
+        case 2 => TrafficDirection.AgainstDigitizing
+        case _ => TrafficDirection.UnknownDirection
+      }
+  },
+  {
+    case trafficDirection: TrafficDirection =>
+      trafficDirection match {
+        case TrafficDirection.BothDirections => JInt(0)
+        case TrafficDirection.TowardsDigitizing => JInt(1)
+        case TrafficDirection.AgainstDigitizing => JInt(2)
+        case _ => JNull
+      }
+  }
   ))
 
   object GeometrySerializer extends CustomSerializer[List[Point]](_ => (
@@ -86,46 +105,42 @@ class RoadLinkChangeClient {
     },
     {
       case points: List[Point] =>
-        JObject(JField("geometry", JString("")))
+        JObject(JField("geometry", JString(""))) // not implemented until reverse operation is needed
     }
   ))
 
+  implicit val formats = DefaultFormats + changeItemSerializer + RoadLinkChangeTypeSerializer + GeometrySerializer +
+    AdminClassSerializer + TrafficDirectionSerializer
 
-
-  implicit val formats = DefaultFormats + changeItemSerializer + ChangeTypeSerializer + GeometrySerializer
-
-
-  case class RoadLinkInfo(linkId: String, linkLength: Double, geometry: List[Point], roadClass: Int, adminClass: Option[Int], municipality: Int, trafficDirection: Int)
+  case class RoadLinkInfo(linkId: String, linkLength: Double, geometry: List[Point], roadClass: Int, adminClass: AdministrativeClass, municipality: Int, trafficDirection: TrafficDirection)
   case class ReplaceInfo(oldLinkId: String, newLinkId: String, oldFromMValue: Double, oldToMValue: Double, newFromMValue: Double, newToMValue: Double)
-  case class RoadLinkChange(changeType: ChangeType2, oldLink: Option[RoadLinkInfo], newLinks: Seq[RoadLinkInfo], replaceInfo: Seq[ReplaceInfo])
+  case class RoadLinkChange(changeType: RoadLinkChangeType, oldLink: Option[RoadLinkInfo], newLinks: Seq[RoadLinkInfo], replaceInfo: Seq[ReplaceInfo])
 
   def fetchLatestSuccessfulUpdateDate(): DateTime = {
-    // date of transfer to frozen road link network as a placeholder
+    // placeholder value as long as fetching this date from db is possible
     DateTime.parse("2022-05-10")
   }
 
   def listFilesAccordingToDates(since: DateTime, until: DateTime) = {
     def isValidKey(key: String): Boolean = {
       try {
-        val keySince = DateTime.parse(key.substring(0, 9))
-        val keyUntil = DateTime.parse(key.substring(10, 19))
-        if (keySince.isBefore(since) || keyUntil.isAfter(until)) false else true
+        val keyParts = key.replace(".json", "").split("_")
+        val keySince = DateTime.parse(keyParts.head)
+        val keyUntil = DateTime.parse(keyParts.last)
+        !(keySince.isBefore(since) || keyUntil.isAfter(until)) // get no changes before or after the requested period
       } catch {
-        case illegalArgument: IllegalArgumentException => {
+        case illegalArgument: IllegalArgumentException =>
           logger.error("Key provides no valid dates.")
           false
-        }
-        case e: Exception => {
+        case e: Exception =>
           logger.error(e.getMessage)
           false
-        }
       }
     }
 
     val request = ListObjectsV2Request.builder().bucket(s3Bucket).build()
     val objects = s3Client.listObjectsV2(request).contents().asScala.toList
-    val keys = objects.map(_.key()).filter(key => isValidKey(key))
-    keys
+    objects.map(_.key()).filter(key => isValidKey(key))
   }
 
   def fetchChangeSetFromS3(filename: String) = {
@@ -137,22 +152,17 @@ class RoadLinkChangeClient {
   def convertToRoadLinkChange(changeJson: String) : Seq[RoadLinkChange] = {
     val json = parseJson(changeJson)
     try {
-      val roadLinkChanges = json.extract[Seq[RoadLinkChange]]
-      roadLinkChanges
+      json.extract[Seq[RoadLinkChange]]
     } catch {
-      case e => {
+      case e =>
         logger.error(e.getMessage)
         Seq.empty[RoadLinkChange]
-      }
     }
-
   }
 
   def getRoadLinkChanges(since: DateTime = fetchLatestSuccessfulUpdateDate(), until: DateTime = DateTime.now()): Seq[RoadLinkChange] = {
     val keys = listFilesAccordingToDates(since, until)
     val changes = keys.map(key => fetchChangeSetFromS3(key))
-    val roadLinkChanges = changes.map(change => convertToRoadLinkChange(change)).flatten
-    roadLinkChanges
+    changes.map(change => convertToRoadLinkChange(change)).flatten
   }
-
 }
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/client/RoadLinkChangeClientSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/client/RoadLinkChangeClientSpec.scala
@@ -1,31 +1,171 @@
 package fi.liikennevirasto.digiroad2.client
 
+import fi.liikennevirasto.digiroad2.Point
+import fi.liikennevirasto.digiroad2.asset.{State, Unknown}
+import fi.liikennevirasto.digiroad2.asset.TrafficDirection.{BothDirections, TowardsDigitizing, AgainstDigitizing}
 import org.joda.time.DateTime
 import org.scalatest.{FunSuite, Matchers}
+import fi.liikennevirasto.digiroad2.client.RoadLinkChangeType.{Add, Remove, Replace, Split}
+import scala.io.Source
 
 class RoadLinkChangeClientSpec extends FunSuite with Matchers {
 
   val roadLinkChangeClient = new RoadLinkChangeClient
 
-  val changeJson = """[{"changeType":"split","old":{"linkId":"3ef8bd3a-6ded-4b41-8f9f-a380e549d4b8:1","linkLength":27.33015679,"geometry":"SRID=3067;LINESTRING ZM (445821.699 6782950.418 105.496 0,445819.279 6782968.184 104.937 17.93,445816.368 6782977.122 104.622 27.33)","roadClass":12314,"adminClass":1,"municipality":111,"trafficDirection":0},"new":[{"linkId":"0006ace4-6471-402d-8c3d-d64b79555540:2","linkLength":8.82657193,"geometry":"SRID=3067;LINESTRING ZM (445821.699 6782950.418 105.559 0,445820.9650000001 6782959.214 105.335 8.827)","roadClass":12314,"adminClass":1,"municipality":111,"trafficDirection":0},{"linkId":"ea0d4ac5-23ce-40fe-8c32-fa9d4f84d934:3","linkLength":18.52716877,"geometry":"SRID=3067;LINESTRING ZM (445820.9650000001 6782959.214 105.335 0,445819.279 6782968.184 104.968 9.127,445816.368 6782977.122 104.622 18.527)","roadClass":12314,"adminClass":1,"municipality":111,"trafficDirection":0}],"replaceInfo":[{"oldLinkId":"3ef8bd3a-6ded-4b41-8f9f-a380e549d4b8:1","newLinkId":"0006ace4-6471-402d-8c3d-d64b79555540:2","oldFromMValue":0,"oldToMValue":8.815,"newFromMValue":0,"newToMValue":8.827},{"oldLinkId":"3ef8bd3a-6ded-4b41-8f9f-a380e549d4b8:1","newLinkId":"ea0d4ac5-23ce-40fe-8c32-fa9d4f84d934:3","oldFromMValue":8.815,"oldToMValue":27.33,"newFromMValue":0,"newToMValue":18.527}]}]"""
+  val filePath = getClass.getClassLoader.getResource("smallChangeSet.json").getPath
+  val jsonFile = Source.fromFile(filePath).mkString
+  val changes = roadLinkChangeClient.convertToRoadLinkChange(jsonFile)
 
-
-  ignore("test S3 fetch") {
-    roadLinkChangeClient.fetchChangeSetFromS3("smallChangeSet.json")
+  test("test json convert with whole set") {
+    changes.size should be(24)
   }
 
-  test("test json convert") {
-    val change = roadLinkChangeClient.convertToRoadLinkChange(changeJson)
-    println(change.foreach(a => println(a)))
+  test("RoadLinkChange for 'add' contains correct info") {
+    val addChange = changes(0)
+    addChange.changeType should be(Add)
+    addChange.oldLink should be(None)
+    addChange.newLinks.size should be(1)
+    addChange.newLinks.head.linkId should be("f2eba575-f306-4c37-b49d-a4d27a3fc049:1")
+    addChange.newLinks.head.linkLength should be(226.20059703)
+    addChange.newLinks.head.geometry.size should be(9)
+    addChange.newLinks.head.geometry.head should be(Point(372164.164, 6680334.588, 39.069))
+    addChange.newLinks.head.geometry.last should be(Point(372330.456, 6680485.857, 41.831))
+    addChange.newLinks.head.roadClass should be(12314)
+    addChange.newLinks.head.adminClass should be(State)
+    addChange.newLinks.head.municipality should be(49)
+    addChange.newLinks.head.trafficDirection should be(BothDirections)
+    addChange.replaceInfo should be(Seq.empty)
   }
 
-  test("fetch changes from S3 and convert to RoadLinkChange") {
-    val roadLinkChanges = roadLinkChangeClient.getRoadLinkChanges()
-    roadLinkChanges.foreach(c => println(c))
+  test("RoadLinkChange for 'remove' contains correct info") {
+    val removeChange = changes(2)
+    removeChange.changeType should be(Remove)
+    removeChange.oldLink.get.linkId should be("7766bff4-5f02-4c30-af0b-42ad3c0296aa:1")
+    removeChange.oldLink.get.linkLength should be(30.92807173)
+    removeChange.oldLink.get.geometry.size should be(5)
+    removeChange.oldLink.get.geometry.head should be(Point(366408.515, 6674439.018, 3.933))
+    removeChange.oldLink.get.geometry.last should be(Point(366425.832, 6674457.102, 5.957))
+    removeChange.oldLink.get.roadClass should be(12314)
+    removeChange.oldLink.get.adminClass should be(Unknown)
+    removeChange.oldLink.get.municipality should be(49)
+    removeChange.oldLink.get.trafficDirection should be(BothDirections)
+    removeChange.replaceInfo should be(Seq.empty)
   }
 
-  ignore("filter files") {
-    roadLinkChangeClient.listFilesAccordingToDates(DateTime.parse("2022-5-10"), DateTime.parse("2022-5-17"))
+  test("RoadLinkChange for 'replace' contains correct info") {
+    val replaceChange = changes(4)
+    replaceChange.changeType should be(Replace)
+    replaceChange.oldLink.get.linkId should be("41cca8ff-4644-41aa-8de1-2702f1a57f80:1")
+    replaceChange.oldLink.get.linkLength should be(186.08235918)
+    replaceChange.oldLink.get.geometry.size should be(14)
+    replaceChange.oldLink.get.geometry.head should be(Point(361966.115, 6682342.526, 52.256))
+    replaceChange.oldLink.get.geometry.last should be(Point(361938.529, 6682516.719, 54.601))
+    replaceChange.oldLink.get.roadClass should be(12131)
+    replaceChange.oldLink.get.adminClass should be(State)
+    replaceChange.oldLink.get.municipality should be(49)
+    replaceChange.oldLink.get.trafficDirection should be(BothDirections)
+
+    replaceChange.newLinks.size should be(1)
+    replaceChange.newLinks.head.linkId should be("41cca8ff-4644-41aa-8de1-2702f1a57f80:2")
+    replaceChange.newLinks.head.linkLength should be(186.08235918)
+    replaceChange.newLinks.head.geometry.size should be(14)
+    replaceChange.newLinks.head.geometry.head should be(Point(361966.115, 6682342.526, 52.256))
+    replaceChange.newLinks.head.geometry.last should be(Point(361938.529, 6682516.719, 54.601))
+    replaceChange.newLinks.head.roadClass should be(12131)
+    replaceChange.newLinks.head.adminClass should be(State)
+    replaceChange.newLinks.head.municipality should be(49)
+    replaceChange.newLinks.head.trafficDirection should be(BothDirections)
+
+    replaceChange.replaceInfo.size should be(1)
+    replaceChange.replaceInfo.head.oldLinkId should be("41cca8ff-4644-41aa-8de1-2702f1a57f80:1")
+    replaceChange.replaceInfo.head.newLinkId should be("41cca8ff-4644-41aa-8de1-2702f1a57f80:2")
+    replaceChange.replaceInfo.head.oldFromMValue should be(0)
+    replaceChange.replaceInfo.head.oldToMValue should be(186.0823591774)
+    replaceChange.replaceInfo.head.newFromMValue should be(0)
+    replaceChange.replaceInfo.head.newToMValue should be(186.0823591774)
   }
 
+  test("RoadLinkChange for 'split' contains correct info") {
+    val splitChange = changes(10)
+    splitChange.changeType should be(Split)
+    splitChange.oldLink.get.linkId should be("c19bd6b4-9923-4ce9-a9cb-09779708913e:1")
+    splitChange.oldLink.get.linkLength should be(121.67349003)
+    splitChange.oldLink.get.geometry.size should be(10)
+    splitChange.oldLink.get.geometry.head should be(Point(370507.036, 6678446.659, 15.777))
+    splitChange.oldLink.get.geometry.last should be(Point(370624.618, 6678469.114, 14.312))
+    splitChange.oldLink.get.roadClass should be(12131)
+    splitChange.oldLink.get.adminClass should be(State)
+    splitChange.oldLink.get.municipality should be(49)
+    splitChange.oldLink.get.trafficDirection should be(TowardsDigitizing)
+
+    splitChange.newLinks.size should be(3)
+
+    splitChange.newLinks.head.linkId should be("0d92e4a4-51cf-4abe-8f0e-8ee22241c3ff:1")
+    splitChange.newLinks.head.linkLength should be(17.30470614)
+    splitChange.newLinks.head.geometry.size should be(3)
+    splitChange.newLinks.head.geometry.head should be(Point(370524.238, 6678444.954, 15.134))
+    splitChange.newLinks.head.geometry.last should be(Point(370507.036, 6678446.659, 15.777))
+    splitChange.newLinks.head.roadClass should be(12131)
+    splitChange.newLinks.head.adminClass should be(State)
+    splitChange.newLinks.head.municipality should be(49)
+    splitChange.newLinks.head.trafficDirection should be(AgainstDigitizing)
+
+    splitChange.newLinks(1).linkId should be("d59dd3a9-a94d-4f26-b311-6b9b8361c717:1")
+    splitChange.newLinks(1).linkLength should be(86.94114036)
+    splitChange.newLinks(1).geometry.size should be(6)
+    splitChange.newLinks(1).geometry.head should be(Point(370541.665, 6678445.318, 14.602))
+    splitChange.newLinks(1).geometry.last should be(Point(370624.618, 6678469.114, 14.312))
+    splitChange.newLinks(1).roadClass should be(12131)
+    splitChange.newLinks(1).adminClass should be(State)
+    splitChange.newLinks(1).municipality should be(49)
+    splitChange.newLinks(1).trafficDirection should be(TowardsDigitizing)
+
+    splitChange.newLinks.last.linkId should be("e92c98c9-5a62-4995-a9c0-e40ae0b65747:1")
+    splitChange.newLinks.last.linkLength should be(17.43162599)
+    splitChange.newLinks.last.geometry.size should be(4)
+    splitChange.newLinks.last.geometry.head should be(Point(370524.238, 6678444.954, 15.134))
+    splitChange.newLinks.last.geometry.last should be(Point(370541.665, 6678445.318, 14.602))
+    splitChange.newLinks.last.roadClass should be(12131)
+    splitChange.newLinks.last.adminClass should be(State)
+    splitChange.newLinks.last.municipality should be(49)
+    splitChange.newLinks.last.trafficDirection should be(TowardsDigitizing)
+
+    splitChange.replaceInfo.size should be(3)
+
+    splitChange.replaceInfo.head.oldLinkId should be("c19bd6b4-9923-4ce9-a9cb-09779708913e:1")
+    splitChange.replaceInfo.head.newLinkId should be("0d92e4a4-51cf-4abe-8f0e-8ee22241c3ff:1")
+    splitChange.replaceInfo.head.oldFromMValue should be(0)
+    splitChange.replaceInfo.head.oldToMValue should be(17.305)
+    splitChange.replaceInfo.head.newFromMValue should be(0)
+    splitChange.replaceInfo.head.newToMValue should be(17.305)
+
+    splitChange.replaceInfo(1).oldLinkId should be("c19bd6b4-9923-4ce9-a9cb-09779708913e:1")
+    splitChange.replaceInfo(1).newLinkId should be("d59dd3a9-a94d-4f26-b311-6b9b8361c717:1")
+    splitChange.replaceInfo(1).oldFromMValue should be(34.733)
+    splitChange.replaceInfo(1).oldToMValue should be(121.673)
+    splitChange.replaceInfo(1).newFromMValue should be(0)
+    splitChange.replaceInfo(1).newToMValue should be(86.941)
+
+    splitChange.replaceInfo.last.oldLinkId should be("c19bd6b4-9923-4ce9-a9cb-09779708913e:1")
+    splitChange.replaceInfo.last.newLinkId should be("e92c98c9-5a62-4995-a9c0-e40ae0b65747:1")
+    splitChange.replaceInfo.last.oldFromMValue should be(17.305)
+    splitChange.replaceInfo.last.oldToMValue should be(34.733)
+    splitChange.replaceInfo.last.newFromMValue should be(0)
+    splitChange.replaceInfo.last.newToMValue should be(17.432)
+  }
+
+  // ignored by default, used to test locally that the fetch and convert process works
+  ignore("fetch changes from S3 and convert to RoadLinkChange") {
+    val roadLinkChanges_2022_6_20_2022_6_30 = roadLinkChangeClient.getRoadLinkChanges(DateTime.parse("2022-6-20"), DateTime.parse("2022-6-30"))
+    roadLinkChanges_2022_6_20_2022_6_30.size should be(197)
+    roadLinkChanges_2022_6_20_2022_6_30.foreach(change => change.changeType.isInstanceOf[RoadLinkChangeType] should be(true))
+    val roadLinkChanges_2022_7_1_2022_7_15 = roadLinkChangeClient.getRoadLinkChanges(DateTime.parse("2022-7-1"), DateTime.parse("2022-7-15"))
+    roadLinkChanges_2022_7_1_2022_7_15.size should be(25)
+    roadLinkChanges_2022_7_1_2022_7_15.foreach(change => change.changeType.isInstanceOf[RoadLinkChangeType] should be(true))
+    val roadLinkChanges_2022_7_16_2022_7_29 = roadLinkChangeClient.getRoadLinkChanges(DateTime.parse("2022-7-16"), DateTime.parse("2022-7-29"))
+    roadLinkChanges_2022_7_16_2022_7_29.size should be(8)
+    roadLinkChanges_2022_7_16_2022_7_29.foreach(change => change.changeType.isInstanceOf[RoadLinkChangeType] should be(true))
+    val roadlinkChanges_all = roadLinkChangeClient.getRoadLinkChanges(DateTime.parse("2022-6-1"), DateTime.parse("2022-7-31"))
+    roadlinkChanges_all.size should be(230)
+  }
 }
