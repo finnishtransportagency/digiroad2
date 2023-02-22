17b32e614653e11ac4ab3edd2829503d165b92f5
antti.ahopelto <antti.ahopelto@sitowise.com>
2023-02-22 08:44:09 +0200

    DROTH-3514 Add LaneUpdater.scala, Add LaneUpdaterSpec.scala

---
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/LaneUpdater.scala
@@ -0,0 +1,10 @@
+package fi.liikennevirasto.digiroad2.util.assetUpdater
+
+import fi.liikennevirasto.digiroad2.client.RoadLinkChangeClient
+
+class LaneUpdater {
+
+  val roadLinkChangeClient = new RoadLinkChangeClient
+
+
+}
---
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/LaneUpdaterSpec.scala
@@ -0,0 +1,10 @@
+package fi.liikennevirasto.digiroad2.util.assetUpdater
+
+import org.scalatest.{FunSuite, Matchers}
+
+class LaneUpdaterSpec extends FunSuite with Matchers{
+  val laneUpdater = new LaneUpdater
+  test("Testailua") {
+
+  }
+}
