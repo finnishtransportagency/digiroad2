5224325acf990f77604ce0a2537911d97da17527
sasuolander <sasu.olander@sitowise.com>
2023-05-29 16:50:00 +0300

    DROTH-3696 change how changset are merged, replacedAssetID set for now, need to check if actually needed

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetFiller.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetFiller.scala
@@ -1,17 +1,17 @@
 package fi.liikennevirasto.digiroad2.linearasset
 
 import fi.liikennevirasto.digiroad2.asset.SideCode
-
 object LinearAssetFiller {
-  case class MValueAdjustment(assetId: Long, linkId: String, startMeasure: Double, endMeasure: Double,timeStamp: Long=0)
+  case class MValueAdjustment(assetId: Long, linkId: String, startMeasure: Double, endMeasure: Double,timeStamp: Long=0,oldLinkId:Option[String] = None)
   case class VVHChangesAdjustment(assetId: Long, linkId: String, startMeasure: Double, endMeasure: Double, timeStamp: Long=0)
-  case class SideCodeAdjustment(assetId: Long, sideCode: SideCode, typeId: Int)
+  case class SideCodeAdjustment(assetId: Long, sideCode: SideCode, typeId: Int,oldLinkId:Option[String] = None)
   case class ValueAdjustment(asset: PieceWiseLinearAsset)
   case class ChangeSet(droppedAssetIds: Set[Long],
                        adjustedMValues: Seq[MValueAdjustment],
                        adjustedVVHChanges: Seq[VVHChangesAdjustment],
                        adjustedSideCodes: Seq[SideCodeAdjustment],
                        expiredAssetIds: Set[Long],
+                       replacedAssetIds: Set[Long] = Set(),
                        valueAdjustments: Seq[ValueAdjustment]){
     def isEmpty: Boolean = {
       this.droppedAssetIds.isEmpty &&
@@ -28,6 +28,7 @@ object LinearAssetFiller {
         this.adjustedVVHChanges.filterNot(_.assetId <= 0),
         this.adjustedSideCodes.filterNot(_.assetId <= 0),
         this.expiredAssetIds.filterNot(_ <= 0),
+        this.replacedAssetIds.filterNot(_ <= 0),
         this.valueAdjustments.filterNot(_.asset.id <= 0))
     }
   }
@@ -38,5 +39,23 @@ object LinearAssetFiller {
       originalAsset.startMeasure == adjustment.startMeasure && originalAsset.endMeasure == adjustment.endMeasure
     })
     changeSet.copy(adjustedMValues = redundantFiltered)
+  }
+  
+  val emptyChangeSet = ChangeSet(droppedAssetIds = Set.empty[Long],
+    expiredAssetIds = Set.empty[Long],
+    adjustedMValues = Seq.empty[MValueAdjustment],
+    adjustedVVHChanges = Seq.empty[VVHChangesAdjustment],
+    adjustedSideCodes = Seq.empty[SideCodeAdjustment],
+    valueAdjustments = Seq.empty[ValueAdjustment])
+  def combineChangeSets: (ChangeSet, ChangeSet) => ChangeSet = (a, z) => {
+    a.copy(
+      droppedAssetIds = a.droppedAssetIds ++ z.droppedAssetIds,
+      expiredAssetIds = (a.expiredAssetIds ++ z.expiredAssetIds),
+      replacedAssetIds = (a.replacedAssetIds ++ z.replacedAssetIds),
+      adjustedMValues = (a.adjustedMValues ++ z.adjustedMValues).distinct,
+      adjustedVVHChanges = (a.adjustedVVHChanges ++ z.adjustedVVHChanges).distinct,
+      adjustedSideCodes = (a.adjustedSideCodes ++ z.adjustedSideCodes).distinct,
+      valueAdjustments = (a.valueAdjustments ++ z.valueAdjustments).distinct
+    );
   }
 }
