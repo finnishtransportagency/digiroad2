e60faa8c57cb057bd994899dc9b0ef78a6ff72e2
Sasu Ölander <sasu.olander@sitowise.com>
2023-08-29 12:52:13 +0300

    fix naming

--- aws/cloud-formation/task-definition/update-taskdefinition.ps1
+++ aws/cloud-formation/task-definition/update-taskdefinition.ps1
@@ -2,6 +2,6 @@
 #aws cloudformation create-stack --on-failure DELETE --stack-name dev-Taskdefinition --template-body file://aws/cloud-formation/taskdefinition/dev-create-taskdefinition.yaml --profile vaylaapp --parameters file://aws/cloud-formation/taskdefinition/dev-taskdefinition-parameter.json --capabilities CAPABILITY_IAM
 
 # DEV
-aws cloudformation update-stack --stack-name dev-Taskdefinition --template-body file://aws/cloud-formation/taskdefinition/dev-create-taskdefinition.yaml --profile vaylaapp --parameters file://aws/cloud-formation/taskdefinition/dev-taskdefinition-parameter.json --capabilities CAPABILITY_IAM
+aws cloudformation update-stack --stack-name dev-Taskdefinition --template-body file://aws/cloud-formation/task-definition/dev-create-taskdefinition.yaml --profile vaylaapp --parameters file://aws/cloud-formation/task-definition/dev-taskdefinition-parameter.json --capabilities CAPABILITY_IAM
 # QA
-aws cloudformation update-stack --stack-name qa-Taskdefinition --template-body file://aws/cloud-formation/taskdefinition/qa-create-taskdefinition.yaml --profile vaylaapp --parameters file://aws/cloud-formation/taskdefinition/qa-taskdefinition-parameter.json --capabilities CAPABILITY_IAM
+aws cloudformation update-stack --stack-name qa-Taskdefinition --template-body file://aws/cloud-formation/task-definition/qa-create-taskdefinition.yaml --profile vaylaapp --parameters file://aws/cloud-formation/task-definition/qa-taskdefinition-parameter.json --capabilities CAPABILITY_IAM
