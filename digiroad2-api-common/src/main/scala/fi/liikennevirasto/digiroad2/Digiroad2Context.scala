f69cbb80ec8404e42e3c493238a07cc10f949926
Sasu Ölander <sasu.olander@sitowise.com>
2023-09-13 14:19:54 +0300

    DROTH-3816 add typing

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
@@ -204,7 +204,7 @@ class LaneWorkListInsertItem(laneWorkListService: LaneWorkListService) extends A
 
 class AssetUpdater(linearAssetService: LinearAssetService) extends Actor {
   val logger = LoggerFactory.getLogger(getClass)
-  def receive = {
+  def receive: Receive = {
     case a: AssetUpdate =>
       lazy val eventbus = new DummyEventBus
       lazy val roadLinkService: RoadLinkService = {
