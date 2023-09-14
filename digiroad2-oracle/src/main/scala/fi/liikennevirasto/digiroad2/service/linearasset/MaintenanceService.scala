e5990c4cda62097a2d0ac617e48e983cdfaf2ef7
Sasu Ölander <sasu.olander@sitowise.com>
2023-09-14 13:51:11 +0300

    DROTH-3816 add missing unknown generation and adjuctments

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/MaintenanceService.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/service/linearasset/MaintenanceService.scala
@@ -211,7 +211,6 @@ class MaintenanceService(roadLinkServiceImpl: RoadLinkService, eventBusImpl: Dig
       val createdId = createdValue.map(createWithoutTransaction(linearAsset.typeId, linearAsset.linkId, _, linearAsset.sideCode, Measures(createdLinkMeasures._1, createdLinkMeasures._2), username, linearAsset.timeStamp, Some(roadLink), fromUpdate= true,  createdByFromUpdate = linearAsset.createdBy, createdDateTimeFromUpdate = linearAsset.createdDateTime))
       adjustLinearAssetsAction(Set(roadLink.linkId),linearAsset.typeId,newTransaction = false)
       Seq(existingId, createdId).flatten
-      
     }
   }
 }
