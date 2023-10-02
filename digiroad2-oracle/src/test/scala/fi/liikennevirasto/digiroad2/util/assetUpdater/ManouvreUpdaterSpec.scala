93c95e422ceb9ab8f53cb12abd5ccdb756ec76d6
Sasu Ölander <sasu.olander@sitowise.com>
2023-10-02 16:27:13 +0300

    DROTH-3522 test for version changes

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/ManouvreUpdaterSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/ManouvreUpdaterSpec.scala
@@ -1,12 +1,12 @@
 package fi.liikennevirasto.digiroad2.util.assetUpdater
 
-import fi.liikennevirasto.digiroad2.DigiroadEventBus
-import fi.liikennevirasto.digiroad2.asset.{AdministrativeClass, TrafficVolume}
+import fi.liikennevirasto.digiroad2.{DigiroadEventBus, Point}
+import fi.liikennevirasto.digiroad2.asset.{AdministrativeClass, Municipality, SingleCarriageway, TrafficDirection, TrafficVolume}
 import fi.liikennevirasto.digiroad2.client.RoadLinkClient
 import fi.liikennevirasto.digiroad2.dao.{DynamicLinearAssetDao, MunicipalityDao, PostGISAssetDao}
 import fi.liikennevirasto.digiroad2.dao.linearasset.{PostGISLinearAssetDao, PostGISMaintenanceDao}
-import fi.liikennevirasto.digiroad2.linearasset.{NumericValue, ValidityPeriod}
-import fi.liikennevirasto.digiroad2.service.linearasset.{MaintenanceService, ManoeuvreService, NewManoeuvre}
+import fi.liikennevirasto.digiroad2.linearasset.{NumericValue, RoadLink, ValidityPeriod}
+import fi.liikennevirasto.digiroad2.service.linearasset.{ElementTypes, MaintenanceService, ManoeuvreService, NewManoeuvre}
 import fi.liikennevirasto.digiroad2.util.PolygonTools
 import org.mockito.Mockito.when
 import org.scalatest.{FunSuite, Matchers}
@@ -42,39 +42,53 @@ class ManouvreUpdaterSpec extends FunSuite with Matchers with  UpdaterUtilsSuite
     val geometry = generateGeometry(0, 9)
     val oldRoadLink = createRoadLink(linkIdVersion1, generateGeometry(0, 9))
     val newLink = createRoadLink(linkIdVersion2, geometry)
+    val unchangedLink = createRoadLink(linkId7, generateGeometry(8, 26))
     val change = changeReplaceNewVersion(linkIdVersion1, linkIdVersion2)
 
     runWithRollback {
       when(mockRoadLinkService.getExistingOrExpiredRoadLinkByLinkId(linkIdVersion1, false)).thenReturn(Some(oldRoadLink))
       when(mockRoadLinkService.getExistingAndExpiredRoadLinksByLinkIds(Set(linkIdVersion2))).thenReturn(Seq(newLink))
-      val id1 = 1
+      when(mockRoadLinkService.getExistingAndExpiredRoadLinksByLinkIds(Set(linkIdVersion1,linkId7))).thenReturn(Seq(oldRoadLink,unchangedLink))
+      when(mockRoadLinkService.getRoadLinksByLinkIds(Set(linkIdVersion1,linkId7))).thenReturn(Seq(oldRoadLink,unchangedLink))
+      when(mockRoadLinkService.getRoadLinksByLinkIds(Set(linkIdVersion1,linkId7),false)).thenReturn(Seq(oldRoadLink,unchangedLink))
+      when(mockRoadLinkService.getExistingAndExpiredRoadLinksByLinkIds(Set(linkIdVersion2,linkId7))).thenReturn(Seq(newLink,unchangedLink))
+      when(mockRoadLinkService.getExistingAndExpiredRoadLinksByLinkIds(Set(linkId7))).thenReturn(Seq(unchangedLink))
+      val roadLink2 = Seq(
+        RoadLink(linkIdVersion1, List(Point(0.0, 0.0), Point(8.0, 0.0)), 8, Municipality, 1, TrafficDirection.BothDirections, SingleCarriageway, None, None),
+        RoadLink(linkId7, List(Point(8.0, 0.0), Point(25.0, 0.0)), 25.0, Municipality, 1, TrafficDirection.BothDirections, SingleCarriageway, None, None)
+      )
+
+      val roadLink3 = Seq(
+        RoadLink(linkIdVersion2, List(Point(0.0, 0.0), Point(8.0, 0.0)), 8, Municipality, 1, TrafficDirection.BothDirections, SingleCarriageway, None, None),
+        RoadLink(linkId7, List(Point(8.0, 0.0), Point(25.0, 0.0)), 25.0, Municipality, 1, TrafficDirection.BothDirections, SingleCarriageway, None, None)
+      )
+
       val newManoeuvres = Seq(NewManoeuvre(Set.empty[ValidityPeriod],
-        Seq(), None, linkIds = Seq(), trafficSignId = None, isSuggested = false
-
+        Seq(), None, linkIds = roadLink2.map(_.linkId), trafficSignId = None, isSuggested = false
       ))
-      Service.createManoeuvres(newManoeuvres,"test")
-      val assetsBefore = Service.getByRoadLinkId(Set(id1), false)
-      assetsBefore.size should be(1)
-
-
+      val newManoeuvres1 = NewManoeuvre(Set.empty[ValidityPeriod],
+        Seq(), None, linkIds = roadLink2.map(_.linkId), trafficSignId = None, isSuggested = false
+      )
+      val id = Service.createManoeuvre("test",newManoeuvres1,roadLink2,newTransaction = false)
+      val assetsBefore = Service.getByRoadLinkId(roadLink2.map(_.linkId).toSet, newTransaction = false).find(p=>p.id ==id).get
+      assetsBefore.id should be(id)
+      
       TestManouvreUpdater.updateByRoadLinks(TrafficVolume.typeId, Seq(change))
-      val assetsAfter = Service.getByRoadLinkId(Set(linkIdVersion2), false)
- /*     assetsAfter.size should be(1)
-      assetsAfter.head.linkId should be(linkIdVersion2)
-      val sorted = assetsAfter.sortBy(_.startMeasure)
-      sorted.head.startMeasure should be(0)
-      sorted.head.endMeasure should be(8)
-      assetsAfter.head.value.isEmpty should be(false)
-      assetsAfter.head.value.get should be(NumericValue(3))*/
+      val assetsAfter = Service.getByRoadLinkId(roadLink3.map(_.linkId).toSet, false).find(p=>p.id ==id)
 
-/*      val oldIds = Seq(id1)
-      val assets = TestLinearAssetUpdater.getReport().map(a => PairAsset(a.before, a.after.headOption, a.changeType))
-      assets.size should be(1)
-      assets.map(a => {
-        a.oldAsset.isDefined should be(true)
-        oldIds.contains(a.oldAsset.get.assetId) should be(true)
-        a.oldAsset.get.assetId should be(a.newAsset.get.assetId)
-      })*/
+      assetsAfter.get.elements.map(a=> {
+        val (source,destination )=  (a.sourceLinkId,a.destLinkId )
+        if (a.elementType== ElementTypes.FirstElement) {
+          source should  be(linkIdVersion2)
+          destination should  be(linkId7)
+          roadLink3.map(_.linkId).contains(source) should be(true)
+          roadLink3.map(_.linkId).contains(destination) should be(true)
+        }
+        if (a.elementType == ElementTypes.LastElement) {
+          source should be(linkId7)
+          roadLink3.map(_.linkId).contains(source) should be(true)
+        }
+      })
     }
   }
   
