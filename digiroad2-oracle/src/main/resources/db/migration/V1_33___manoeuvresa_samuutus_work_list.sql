774fa3486d2e082b6b3f24293e10758f2f1646e4
Sasu Ölander <sasu.olander@sitowise.com>
2023-10-16 08:40:17 +0300

    DROTH-3522 move position of drop table comman, fix syntax error

--- digiroad2-oracle/src/main/resources/db/migration/V1_33___manoeuvresa_samuutus_work_list.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_33___manoeuvresa_samuutus_work_list.sql
@@ -1,6 +1,6 @@
 CREATE TABLE manouvre_samuutus_work_list(
     assetId        numeric(38),
-    linkIds        varchar(400),
+    linkIds        varchar(400)
 );
 
 ALTER TABLE manouvre_samuutus_work_list ADD CONSTRAINT manouvre_samuutus_work_list_asset_unique UNIQUE(assetId);
