930003f54812265761c91058c6c86d047cb8b1a5
Sasu Ölander <sasu.olander@sitowise.com>
2023-10-27 15:06:08 +0300

    DROTH-3862 remove filtering

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/PavedRoadUpdater.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/PavedRoadUpdater.scala
@@ -57,15 +57,4 @@ class PavedRoadUpdater(service: PavedRoadService) extends DynamicLinearAssetUpda
     removePavement(operationStep,changes)
   }
 
-  override def filterChanges(changes: Seq[RoadLinkChange]): Seq[RoadLinkChange] = {
-    val (remove, other) = changes.partition(_.changeType == RoadLinkChangeType.Remove)
-    val linksOther = other.flatMap(_.newLinks.map(_.linkId)).toSet
-    val filterChanges = if (linksOther.nonEmpty) {
-      val links = roadLinkService.getExistingAndExpiredRoadLinksByLinkIds(linksOther,false)
-      val filteredLinks = links.filter(_.functionalClass > 4).map(_.linkId)
-      other.filter(p => filteredLinks.contains(p.newLinks.head.linkId))
-    } else Seq()
-    filterChanges ++ remove
-  }
-
 }
