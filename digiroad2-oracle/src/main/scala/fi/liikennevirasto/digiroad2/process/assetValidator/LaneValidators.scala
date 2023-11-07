68f5ccc8b4ca2f102e701dab10f3e1c74d495c5c
Sasu Ölander <sasu.olander@sitowise.com>
2023-11-07 16:15:56 +0200

    DROTH-3692 remove unneeded/unusefull comment

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/assetValidator/LaneValidators.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/assetValidator/LaneValidators.scala
@@ -9,27 +9,26 @@ object LaneValidators extends Validators {
   
   override def forSamuutus: Seq[ValidatorFunction] = Seq(laneFitIntoLink, lanesIsNotTooShort, lanesDoesNotOverlap, incoherentLaneCode)
   override def forTopology: Seq[ValidatorFunction] = Seq(laneFitIntoLink, lanesIsNotTooShort, lanesDoesNotOverlap, incoherentLaneCode)
-  //4. lanes
-  //a)
+
   private def laneFitIntoLink(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,"laneFitIntoLink"){
       Validators.returnValidationResult("laneWhichDoesNotFallInFullyToLink",LaneValidatorDao.laneWhichDoesNotFallInFullyToLink(linkIds))
     }
   }
-  //b)
+  
   private def lanesDoesNotOverlap(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,"lanesDoesNotOverlap"){
       Validators.returnValidationResult("overFlappingLanes",LaneValidatorDao.overFlappingLanes(linkIds))
     }
   }
-  //c)
+  
   private def lanesIsNotTooShort(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,"lanesIsNotTooShort"){
       Validators.returnValidationResult("laneTooShort",LaneValidatorDao.laneTooShort(linkIds)) 
     }
     
   }
-  //d)
+  
   private def incoherentLaneCode(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,"incoherentLaneCode"){
       Validators.returnValidationResult("incoherentLaneCode",LaneValidatorDao.incoherentLane(linkIds))
--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/assetValidator/LinearAssetValidators.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/process/assetValidator/LinearAssetValidators.scala
@@ -9,27 +9,25 @@ object LinearAssetValidators extends Validators{
   private val logger = LoggerFactory.getLogger(getClass)
   override def forSamuutus: Seq[ValidatorFunction] = Seq(assetFitIntoLink,  assetDoesNotOverlap, assetPartsFullLenthIsSameAsLink)
   override def forTopology: Seq[ValidatorFunction] = Seq(assetFitIntoLink, assetDoesNotOverlap, assetsIsNotTooShort, assetPartsFullLenthIsSameAsLink)
-  //1
-  //a)
+  
   private def assetFitIntoLink(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,s"assetFitIntoLink, asset type: $assetType"){
       Validators.returnValidationResult("doesNotFallInFullyToLink",ValidatorLinearDao.assetWhichDoesNotFallInFullyToLink(assetType,linkIds))
     }
   }
-  //b)
+  
   private def assetDoesNotOverlap(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,s"assetDoesNotOverlap, asset type: $assetType"){
       Validators.returnValidationResult("overFlappingAsset",ValidatorLinearDao.overFlappingAssets(assetType,linkIds))
     }
   }
-  //c)
+  
   private def assetsIsNotTooShort(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,s"assetsIsNotTooShort, asset type: $assetType"){
       Validators.returnValidationResult("assetTooShort",ValidatorLinearDao.assetTooShort(assetType,linkIds))
     }
   }
-  //2.
-  // a)
+  
   private def assetPartsFullLenthIsSameAsLink(assetType: Int, linkIds: Set[String]): returnResult = {
     LogUtils.time(logger,s"assetPartsFullLenthIsSameAsLink, asset type: $assetType"){
       if (AssetTypeInfo.roadLinkLongAssets.contains(assetType)) {
