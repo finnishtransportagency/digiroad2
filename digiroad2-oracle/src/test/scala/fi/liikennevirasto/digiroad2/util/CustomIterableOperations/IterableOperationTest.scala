ee3fc77342678ecc8100b3e95c4bc4b97f2cb868
Sasu Ölander <sasu.olander@sitowise.com>
2023-11-13 16:58:03 +0200

    DROTH-3855  add unit test grouping method

---
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/CustomIterableOperations/IterableOperationTest.scala
@@ -0,0 +1,43 @@
+package fi.liikennevirasto.digiroad2.util.CustomIterableOperations
+
+import fi.liikennevirasto.digiroad2.asset.{InformationSource, LinkGeomSource}
+import fi.liikennevirasto.digiroad2.linearasset.{NumericValue, PersistedLinearAsset}
+import org.joda.time.DateTime
+import org.scalatest.{FunSuite, Matchers}
+
+class IterableOperationTest extends FunSuite with Matchers {
+
+  val linkIdsRepeated = Seq.fill(5)("link1") ++ Seq.fill(3)("link2") ++ Seq.fill(2)("link3")
+  val dummyAssets: Seq[PersistedLinearAsset] = linkIdsRepeated.zipWithIndex.map { case (linkId, i) =>
+    PersistedLinearAsset(
+      id = i + 1,
+      linkId = linkId,
+      sideCode = i % 2, 
+      value = Some(NumericValue(0)),
+      startMeasure = 0,
+      endMeasure =10,
+      createdBy = Some(s"Creator_${i + 1}"),
+      createdDateTime = Some(DateTime.now()),
+      modifiedBy = Some(s"Modifier_${i + 1}"),
+      modifiedDateTime = Some(DateTime.now()),
+      expired = false,
+      typeId = (i + 1) % 3, 
+      timeStamp = System.currentTimeMillis(),
+      geomModifiedDate = Some(DateTime.now()),
+      linkSource = LinkGeomSource(0),
+      verifiedBy = Some(s"Verifier_${i + 1}"),
+      verifiedDate = Some(DateTime.now()),
+      informationSource = Some(InformationSource(0)),
+      oldId = (i + 1) * 100 
+    )
+  }
+
+
+  test("Make sure that grouping is actually working") {
+    val grouped = IterableOperation.groupByLinkIdHashMap(dummyAssets)
+    grouped("link1").size should be(5)
+    grouped("link2").size should be(3)
+    grouped("link3").size should be(2)
+  }
+
+}
