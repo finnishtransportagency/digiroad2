10664827ace98abc44c18052c6050069fce70be2
Sasu Ölander <sasu.olander@sitowise.com>
2023-11-21 16:13:08 +0200

    DROTH-3855  DROTH-3888 fix validation performance

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/ValidatorDao.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/ValidatorDao.scala
@@ -7,7 +7,8 @@ import slick.jdbc.{GetResult, PositionedResult, StaticQuery}
 sealed case class LinearReferenceAsset(assetId:Long,lrm:LinearReference,laneCode:Option[Int]=None)
 
 trait ValidatorDao {
-  val filterLink = (column: String, links: Set[String]) => if (links.nonEmpty) s" and ${column} in (${links.map(t => s"'${t}'").mkString(",")})" else ""
+  val filterLink = (column: String, links: Set[String]) => if (links.nonEmpty) s"JOIN ( VALUES ${links.map(t => s"('${t}')").mkString(",")} ) links(link) ON (${column} = link) " else ""
+  val filterLink2 = (column: String, links: Set[String]) => if (links.nonEmpty) s" and ${column} in (${links.map(t => s"'${t}'").mkString(",")})" else ""
 
   implicit val getResult: GetResult[LinearReferenceAsset] = new GetResult[LinearReferenceAsset] {
     def apply(r: PositionedResult): LinearReferenceAsset = {
@@ -43,11 +44,11 @@ object ValidatorLinearDao extends ValidatorDao{
                 JOIN asset_link al ON a.id = al.asset_id
                 JOIN lrm_position lp ON al.position_id = lp.id
                 JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+                ${filterLink("lp.link_id", links)}
                 WHERE aty.geometry_type = 'linear' and ${assetTypeFilter}
                 AND a.floating = FALSE
                 AND (a.valid_to > current_timestamp OR a.valid_to IS NULL)
                 AND (lp.start_measure < 0.0 OR lp.end_measure > kr.geometrylength)
-                ${filterLink("lp.link_id", links)}
                 """
 
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResult).iterator.toSeq
@@ -61,13 +62,15 @@ object ValidatorLinearDao extends ValidatorDao{
       FROM asset a
       JOIN asset_link al ON a.id = al.asset_id
       JOIN lrm_position lp ON al.position_id = lp.id
+      ${filterLink("lp.link_id", links)}
       WHERE ${assetTypeFilter}
       AND (a.valid_to > current_timestamp OR a.valid_to IS NULL)
-      AND a.floating = FALSE ${filterLink("lp.link_id", links)}
+      AND a.floating = FALSE 
       AND EXISTS (SELECT 1
           FROM asset a2
           JOIN asset_link al2 ON a2.id = al2.asset_id
           JOIN lrm_position lp2 ON al2.position_id = lp2.id
+          ${filterLink("lp2.link_id", links)}
           WHERE a2.asset_type_id = a.asset_type_id
           AND a2.id != a.id
           AND a2.floating = FALSE
@@ -92,10 +95,10 @@ object ValidatorLinearDao extends ValidatorDao{
       JOIN lrm_position lp ON al.position_id = lp.id
       JOIN asset_type aty ON a.asset_type_id = aty.id
       JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+      ${filterLink("lp.link_id", links)}
       WHERE ${assetTypeFilter} and aty.geometry_type = 'linear'
       AND kr.geometrylength >= 2.0
       AND (lp.end_measure - lp.start_measure) < 2.0
-      ${filterLink("lp.link_id", links)}
       """
 
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResult).iterator.toSeq
@@ -110,8 +113,9 @@ object ValidatorLinearDao extends ValidatorDao{
            FROM asset a2
            JOIN asset_link al2 ON a2.id = al2.asset_id
            JOIN lrm_position lp2 ON al2.position_id = lp2.id
+           ${filterLink("lp2.link_id", links)}
            WHERE a2.asset_type_id = a.asset_type_id
-           AND lp2.link_id = lp.link_id ${filterLink("lp2.link_id", links)}
+           AND lp2.link_id = lp.link_id 
            AND a2.floating = FALSE
            AND (a2.valid_to > current_timestamp OR a2.valid_to IS NULL)
            AND ((lp.side_code = 1 AND lp2.side_code = 1)
@@ -122,8 +126,9 @@ object ValidatorLinearDao extends ValidatorDao{
           FROM asset a3
           JOIN asset_link al3 ON a3.id = al3.asset_id
           JOIN lrm_position lp3 ON al3.position_id = lp3.id
+          ${filterLink("lp3.link_id", links)}
           WHERE a3.asset_type_id = a.asset_type_id
-          AND lp3.link_id = lp.link_id ${filterLink("lp3.link_id", links)}
+          AND lp3.link_id = lp.link_id 
           AND a3.floating = FALSE
           AND (a3.valid_to > current_timestamp OR a3.valid_to IS NULL)
           AND ((lp.side_code = 1 AND lp3.side_code = 1)
@@ -133,6 +138,7 @@ object ValidatorLinearDao extends ValidatorDao{
       JOIN asset_link al ON a.id = al.asset_id
       JOIN lrm_position lp ON al.position_id = lp.id
       JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+      ${filterLink("lp.link_id", links)}
       WHERE ${assetTypeFilter}
       AND a.floating = false
       AND (a.valid_to > current_timestamp OR a.valid_to IS NULL)
@@ -142,16 +148,17 @@ object ValidatorLinearDao extends ValidatorDao{
               SELECT 1 FROM asset a4
                 JOIN asset_link al4 ON a4.id = al4.asset_id
                 JOIN lrm_position lp4 ON al4.position_id = lp4.id
+                ${filterLink("lp4.link_id", links)}
                 WHERE a4.asset_type_id = a.asset_type_id
                 AND lp4.link_id = lp.link_id AND a4.id != a.id AND a4.floating = false
-                AND (a4.valid_to > current_timestamp OR a4.valid_to IS NULL) AND lp4.side_code != lp.side_code ${filterLink("lp4.link_id", links)}
+                AND (a4.valid_to > current_timestamp OR a4.valid_to IS NULL) AND lp4.side_code != lp.side_code
               )
            )
-        ) ${filterLink("lp.link_id", links)}
+        )
       )
       SELECT DISTINCT id, link_id,side_code, start_measure, end_measure
       FROM result_set
-      WHERE assets_total_lenght != geometrylength ${filterLink("link_id", links)}"""
+      WHERE assets_total_lenght != geometrylength"""
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResult).iterator.toSeq
   }
 
@@ -164,9 +171,9 @@ object LaneValidatorDao extends ValidatorDao{
                   JOIN lane_link ll ON l.id = ll.lane_id
                   JOIN lane_position lp ON ll.lane_position_id = lp.id
                   JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+                  ${filterLink("link_id", links)}
                   WHERE (l.valid_to > current_timestamp OR l.valid_to IS NULL)
                   AND (lp.start_measure < 0.0 OR lp.end_measure > kr.geometrylength)
-                  ${filterLink("link_id", links)}
                   """
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResultLane).iterator.toSeq
   }
@@ -176,20 +183,21 @@ object LaneValidatorDao extends ValidatorDao{
       FROM lane l
       JOIN lane_link ll ON l.id = ll.lane_id
       JOIN lane_position lp ON ll.lane_position_id = lp.id
+      ${filterLink("lp.link_id", links)}
       WHERE (l.valid_to > current_timestamp OR l.valid_to IS NULL)
       AND EXISTS (SELECT 1
           FROM lane l2
           JOIN lane_link ll2 ON l2.id = ll2.lane_id
           JOIN lane_position lp2 ON ll2.lane_position_id = lp2.id
+          ${filterLink("lp2.link_id", links)}
           WHERE (l2.valid_to > current_timestamp OR l2.valid_to IS NULL)
           AND l2.id != l.id
           AND lp2.link_id = lp.link_id
           AND lp2.side_code = lp.side_code
           AND l2.lane_code = l.lane_code
-          ${filterLink("lp2.link_id", links)}
           AND (((lp2.start_measure >= lp.start_measure AND lp2.start_measure < lp.end_measure) OR (lp2.end_measure > lp.start_measure AND lp2.end_measure <= lp.end_measure))        
                OR ((lp.start_measure >= lp2.start_measure AND lp.start_measure < lp2.end_measure) OR (lp.end_measure > lp2.start_measure AND lp.end_measure <= lp2.end_measure)))  
-      ) ${filterLink("lp.link_id", links)}
+      )
       """
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResultLane).iterator.toSeq
   }
@@ -202,9 +210,10 @@ object LaneValidatorDao extends ValidatorDao{
       JOIN lane_link ll ON l.id = ll.lane_id
       JOIN lane_position lp ON ll.lane_position_id = lp.id
       JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+      ${filterLink("lp.link_id", links)}
       WHERE (l.valid_to > current_timestamp OR l.valid_to IS NULL)
       AND kr.geometrylength >= 2.0
-      AND (lp.end_measure - lp.start_measure) < 2.0 ${filterLink("lp.link_id", links)}"""
+      AND (lp.end_measure - lp.start_measure) < 2.0"""
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResultLane).iterator.toSeq
   }
   
@@ -215,19 +224,21 @@ object LaneValidatorDao extends ValidatorDao{
       FROM lane l
       JOIN lane_link ll ON l.id = ll.lane_id
       JOIN lane_position lp ON ll.lane_position_id = lp.id
+      ${filterLink("lp.link_id", links)}
       WHERE l.lane_code != 1
       AND (l.valid_to > current_timestamp OR l.valid_to IS NULL)
       AND EXISTS(SELECT 1
           FROM lane l2
           JOIN lane_link ll2 ON l2.id = ll2.lane_id
           JOIN lane_position lp2 ON ll2.lane_position_id = lp2.id
+          ${filterLink("lp2.link_id", links)}
           WHERE lp2.link_id = lp.link_id
           AND (l2.valid_to > current_timestamp OR l2.valid_to IS NULL)
           AND lp2.side_code = lp.side_code
           AND MOD(l.lane_code, 2) = MOD(l2.lane_code, 2)
           AND l2.lane_code < l.lane_code
-          AND (lp2.start_measure > lp.start_measure OR lp2.end_measure < lp.end_measure) ${filterLink("lp2.link_id", links)}
-      ) ${filterLink("lp.link_id", links)}"""
+          AND (lp2.start_measure > lp.start_measure OR lp2.end_measure < lp.end_measure)
+      )"""
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResultLane).iterator.toSeq
   }
   
@@ -244,11 +255,11 @@ object PointAssetValidatorDao extends ValidatorDao{
                 JOIN asset_link al ON a.id = al.asset_id
                 JOIN lrm_position lp ON al.position_id = lp.id
                 JOIN kgv_roadlink kr ON lp.link_id = kr.linkid
+                ${filterLink("lp.link_id", links)}
                 WHERE ${assetTypeFilter} and aty.geometry_type = 'point' 
                 AND a.floating = FALSE
                 AND (a.valid_to > current_timestamp OR a.valid_to IS NULL)
                 AND (lp.start_measure < 0.0 OR lp.start_measure > kr.geometrylength)
-                ${filterLink("lp.link_id", links)}
                 """.stripMargin
     StaticQuery.queryNA[LinearReferenceAsset](sql)(getResult).iterator.toSeq
   }
