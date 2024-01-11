36f53646b0891fe64a8c3e34b6e9fc0e5683efcb
Sasu Ölander <sasu.olander@sitowise.com>
2024-01-11 08:58:42 +0200

    DROTH-3913 remove unused variable and fix comment

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/LinearAssetUpdater.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/LinearAssetUpdater.scala
@@ -619,7 +619,7 @@ class LinearAssetUpdater(service: LinearAssetOperations) {
     /**
       *
       * @param level default five is arbitrary, adjust as needed
-      *  [[groupSizeForParallelRun]] if we assume one link will be operated in ~ 43ms then 1500 links will be operated about in one minute.
+      * [[groupSizeForParallelRun]] if we assume one link will be operated in ~ 43ms then 1500 links will be operated about in one minute.
       * @return
       */
     def parallelLoop(level: Int = 5):List[(Seq[PieceWiseLinearAsset], ChangeSet)] = {
