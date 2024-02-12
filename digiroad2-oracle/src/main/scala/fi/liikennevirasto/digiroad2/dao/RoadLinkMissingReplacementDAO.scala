170cca08b222dbd7083526640f4a3532c12a1abe
antti.ahopelto <antti.ahopelto@sitowise.com>
2024-02-12 08:19:59 +0200

    DROTH-3937 Add WIP methods for finding geom matches

--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/RoadLinkMissingReplacementDAO.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/dao/RoadLinkMissingReplacementDAO.scala
@@ -1,10 +1,55 @@
 package fi.liikennevirasto.digiroad2.dao
 
+import fi.liikennevirasto.digiroad2.{GeometryUtils, Point}
 import fi.liikennevirasto.digiroad2.client.ReplaceInfoWithGeometry
+import fi.liikennevirasto.digiroad2.postgis.MassQuery
 
+case class ReplaceInfoRow(id: Long, oldLinkId: Option[String], oldGeometry: List[Point], newLinkId: Option[String],
+                          newGeometry: List[Point], oldFromMValue: Option[Double], oldToMValue: Option[Double],
+                          newFromMValue: Option[Double], newToMValue: Option[Double], digitizationChange: Boolean)
 class RoadLinkMissingReplacementDAO {
 
-  def insertReplaceInfos(replaceInfos: Seq[ReplaceInfoWithGeometry]) = {
+  def findPotentialMatches(): Unit = {
 
+  }
+
+  def insertReplaceInfos(replaceInfos: Seq[ReplaceInfoWithGeometry]) = {
+    val ids = Sequences.nextPrimaryKeySeqValues(replaceInfos.size)
+    val replaceInfosWithIds = replaceInfos.zipWithIndex.map { case (replaceInfo, index) =>
+      ReplaceInfoRow(ids(index), replaceInfo.oldLinkId, replaceInfo.oldGeometry, replaceInfo.newLinkId, replaceInfo.newGeometry,
+        replaceInfo.oldFromMValue, replaceInfo.oldToMValue, replaceInfo.newFromMValue, replaceInfo.newToMValue, replaceInfo.digitizationChange)
+    }
+
+    val insertReplaceInfoRow =
+      s"""insert into replace_info (id, old_link_id, old_geometry, new_link_id, new_geometry, old_from_m_value,
+         | old_to_m_value, new_from_m_value, new_to_m_value, digitization_change)
+         |values ((?), (?), (?), (?), (?), (?), (?), (?), (?), (?))""".stripMargin
+    MassQuery.executeBatch(insertReplaceInfoRow) { statement =>
+      replaceInfosWithIds.foreach { ri =>
+        statement.setLong(1, ri.id)
+        if(ri.oldLinkId.nonEmpty) statement.setString(2, ri.oldLinkId.get)
+        else statement.setNull(2, java.sql.Types.VARCHAR)
+        if(ri.oldGeometry.nonEmpty) {
+          val geomWKT = GeometryUtils.toWktLineString(ri.oldGeometry).string
+          statement.setObject(3, geomWKT, java.sql.Types.OTHER)
+        } else statement.setNull(3, java.sql.Types.OTHER)
+        if(ri.newLinkId.nonEmpty) statement.setString(4, ri.newLinkId.get)
+        else statement.setNull(4, java.sql.Types.VARCHAR)
+        if(ri.newGeometry.nonEmpty) {
+          val geomWKT = GeometryUtils.toWktLineString(ri.newGeometry).string
+          statement.setObject(5, geomWKT, java.sql.Types.OTHER)
+        } else statement.setNull(5, java.sql.Types.OTHER)
+        if(ri.oldFromMValue.nonEmpty) statement.setDouble(6, ri.oldFromMValue.get)
+        else statement.setNull(6, java.sql.Types.DOUBLE)
+        if(ri.oldToMValue.nonEmpty) statement.setDouble(7, ri.oldToMValue.get)
+        else statement.setNull(7, java.sql.Types.DOUBLE)
+        if(ri.newFromMValue.nonEmpty) statement.setDouble(8, ri.newFromMValue.get)
+        else statement.setNull(8, java.sql.Types.DOUBLE)
+        if(ri.newToMValue.nonEmpty) statement.setDouble(9, ri.newToMValue.get)
+        else statement.setNull(9, java.sql.Types.DOUBLE)
+        statement.setBoolean(10, ri.digitizationChange)
+        statement.addBatch()
+      }
+    }
   }
 }
--- digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/RoadLinkReplacementFinder.scala
+++ digiroad2-oracle/src/main/scala/fi/liikennevirasto/digiroad2/util/RoadLinkReplacementFinder.scala
@@ -1,18 +1,27 @@
 package fi.liikennevirasto.digiroad2.util
 
-import fi.liikennevirasto.digiroad2.client.{ReplaceInfo, ReplaceInfoWithGeometry, RoadLinkChangeClient, RoadLinkChangeSet}
+import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory
+import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory, Polygon}
+import fi.liikennevirasto.digiroad2.{GeometryUtils, Point}
+
+import fi.liikennevirasto.digiroad2.client.{ReplaceInfo, ReplaceInfoWithGeometry, RoadLinkChange, RoadLinkChangeClient}
+
 import fi.liikennevirasto.digiroad2.dao.Queries
 import fi.liikennevirasto.digiroad2.postgis.PostGISDatabase
 import fi.liikennevirasto.digiroad2.service.RoadLinkMissingReplacementService
+import org.geotools.geometry.jts.JTSFactoryFinder
+
+import scala.io.Source
 
 object RoadLinkReplacementFinder {
 
   lazy val roadLinkChangeClient: RoadLinkChangeClient = new RoadLinkChangeClient
-  lazy val bufferTolerance: Double = ???
+  lazy val bufferTolerance: Double = 15.0
+  lazy val parallelTolerance: Double = 5.0
   lazy val missingReplacementService: RoadLinkMissingReplacementService = new RoadLinkMissingReplacementService
+  lazy val geometryFactory: GeometryFactory = JTSFactoryFinder.getGeometryFactory(null)
 
-  def withLinkGeometry(replaceInfos: Seq[ReplaceInfo], changeSets: Seq[RoadLinkChangeSet]): Seq[ReplaceInfoWithGeometry] = {
-    val changes = changeSets.flatMap(_.changes)
+  def withLinkGeometry(replaceInfos: Seq[ReplaceInfo], changes: Seq[RoadLinkChange]): Seq[ReplaceInfoWithGeometry] = {
     val oldLinks = changes.flatMap(_.oldLink)
     val newLinks = changes.flatMap(_.newLinks)
     replaceInfos.map(ri => {
@@ -30,17 +39,66 @@ object RoadLinkReplacementFinder {
     //TODO Tälle oma assetTypeId latestSuccessfullTauluun, jotta voidaan hakea oikeat muutossetit
     val lastSuccess = PostGISDatabase.withDynSession(Queries.getLatestSuccessfulSamuutus(???))
     val changeSets = roadLinkChangeClient.getRoadLinkChanges(lastSuccess)
+    val changes = changeSets.flatMap(_.changes)
+  }
 
-    val replaceInfos = changeSets.flatMap(_.changes.flatMap(_.replaceInfo))
+  def testiFunktio() ={
+    val filePath: String = "C:\\Users\\antti.ahopelto\\IdeaProjects\\digiroad2\\digiroad2-oracle\\src\\test\\resources\\smallChangeSet.json"
+    val jsonFile: String = Source.fromFile(filePath).mkString
+    val testChanges: Seq[RoadLinkChange] = roadLinkChangeClient.convertToRoadLinkChange(jsonFile)
+//    findMissingReplacementsWithPostGis(testChanges)
+    findMissingReplacementsWithGeotools(testChanges)
+  }
+
+  def findMissingReplacementsWithPostGis(changes: Seq[RoadLinkChange]) = {
+    val replaceInfos = changes.flatMap(_.replaceInfo)
     val replaceInfosWithoutReplacement = replaceInfos.filter(ri => ri.newLinkId.isEmpty || ri.oldLinkId.isEmpty)
-    val replaceInfosWithGeometry = withLinkGeometry(replaceInfosWithoutReplacement, changeSets)
+    val replaceInfosWithGeometry = withLinkGeometry(replaceInfosWithoutReplacement, changes)
     missingReplacementService.insertReplaceInfos(replaceInfosWithGeometry)
-
-
   }
 
-  def findMissingReplacements(changeSet: RoadLinkChangeSet) = {
-  ???
+  def findMissingReplacementsWithGeotools(changes: Seq[RoadLinkChange]) = {
+    val replaceInfos = changes.flatMap(_.replaceInfo)
+    val replaceInfosWithoutReplacement = replaceInfos.filter(ri => ri.newLinkId.isEmpty || ri.oldLinkId.isEmpty)
+    val replaceInfosWithGeometry = withLinkGeometry(replaceInfosWithoutReplacement, changes)
+    matchRemovesAndAdds(replaceInfosWithGeometry)
   }
+
+  def matchRemovesAndAdds(replaceInfosWithGeometry: Seq[ReplaceInfoWithGeometry]) = {
+
+
+    def findMatchesWithParallel(addedLink: ReplaceInfoWithGeometry, removedLinks: Seq[ReplaceInfoWithGeometry]) = {
+
+    }
+
+    def polygonWithBuffer(replaceInfoWithGeometry: ReplaceInfoWithGeometry): Polygon = {
+      val geomToUse = if(replaceInfoWithGeometry.oldGeometry.nonEmpty) replaceInfoWithGeometry.oldGeometry
+      else replaceInfoWithGeometry.newGeometry
+
+      val coordinates = geomToUse.map(p => new Coordinate(p.x, p.y)).toArray
+      val lineString = geometryFactory.createLineString(coordinates)
+      lineString.buffer(bufferTolerance).asInstanceOf[Polygon]
+    }
+
+    def findMatchesWithBuffer(addedLink: ReplaceInfoWithGeometry, removedLinks: Seq[ReplaceInfoWithGeometry]) = {
+      removedLinks.filter(removedLink => {
+        val addedLinkPolygonWithBuffer = polygonWithBuffer(addedLink)
+        val removedLinkPolygonWithBuffer = polygonWithBuffer(removedLink)
+
+        // Check if polygons overlap
+        val intersection = addedLinkPolygonWithBuffer.intersection(removedLinkPolygonWithBuffer)
+        val addedLinkArea = addedLinkPolygonWithBuffer.getArea
+        val removedLinkArea = removedLinkPolygonWithBuffer.getArea
+        val overlapPercentage = (intersection.getArea / math.min(addedLinkArea, removedLinkArea)) * 100.0
+
+        overlapPercentage > 50.0
+      })
+    }
+
+    val (removed, added) = replaceInfosWithGeometry.partition(_.newLinkId.isEmpty)
+    val matchedLinksByBuffer = added.map(addedLink => (addedLink, findMatchesWithBuffer(addedLink,removed))).filter(_._2.nonEmpty)
+    val matchedLinksByParallel = added.map(addedLink => (addedLink, findMatchesWithParallel(addedLink, removed)))
+  }
+
 
 }
