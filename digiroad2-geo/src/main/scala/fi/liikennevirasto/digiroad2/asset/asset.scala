d2fd67cf29e262ffa65fdd41262ae85caf20b6ab
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2024-03-06 08:36:24 +0200

    DROTH-3985 fit hazmat to road link length

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/asset.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/asset/asset.scala
@@ -1013,8 +1013,8 @@ object AssetTypeInfo {
   val physicalObjectsWithValidityDirection = Seq(MassTransitLane.typeId, NumberOfLanes.typeId, CyclingAndWalking.typeId,
     ParkingProhibition.typeId)
 
-  val roadLinkLongAssets = Seq(SpeedLimitAsset.typeId, RoadWidth.typeId, EuropeanRoads.typeId,
-    CyclingAndWalking.typeId, CareClass.typeId, TrafficVolume.typeId, ExitNumbers.typeId, PavedRoad.typeId)
+  val roadLinkLongAssets = Seq(SpeedLimitAsset.typeId, RoadWidth.typeId, EuropeanRoads.typeId, CyclingAndWalking.typeId,
+    CareClass.typeId, TrafficVolume.typeId, ExitNumbers.typeId, PavedRoad.typeId, HazmatTransportProhibition.typeId)
 
   def apply(value: Int): AssetTypeInfo = {
     values.find(_.typeId == value).getOrElse(UnknownAssetTypeId)
--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/HazMatTransportProhibitionUpdaterSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/assetUpdater/HazMatTransportProhibitionUpdaterSpec.scala
@@ -62,4 +62,32 @@ class HazMatTransportProhibitionUpdaterSpec extends FunSuite with Matchers with 
       assetsAfter.map(v => v.value.get.equals(assetValues))
     }
   }
+85
+  test("a hazmat prohibition asset is made road link long") {
+    val oldLinkId = "875766ca-83b1-450b-baf1-db76d59176be:1"
+    val newLinkId = "6eec9a4a-bcac-4afb-afc8-f4e6d40ec571:1"
+    val changes = roadLinkChangeClient.convertToRoadLinkChange(source)
+
+    runWithRollback {
+      val oldRoadLink = roadLinkService.getExpiredRoadLinkByLinkId(oldLinkId).get
+      val oldRoadLinkRaw = roadLinkService.getExpiredRoadLinkByLinkIdNonEncrished(oldLinkId)
+      when(mockRoadLinkService.fetchRoadlinkAndComplementary(oldLinkId)).thenReturn(oldRoadLinkRaw)
+      when(mockRoadLinkService.fetchRoadlinksByIds(any[Set[String]])).thenReturn(Seq.empty[RoadLinkFetched])
+
+      val id = serviceHazmat.createWithoutTransaction(HazmatTransportProhibition.typeId, oldLinkId,
+        assetValues, SideCode.BothDirections.value, Measures(0, 10), "testuser", 0L, Some(oldRoadLink), false, None, None)
+      val assetsBefore = serviceHazmat.getPersistedAssetsByIds(HazmatTransportProhibition.typeId, Set(id), false)
+
+      assetsBefore.size should be(1)
+      assetsBefore.head.expired should be(false)
+
+      TestHazMatProhibitionUpdater.updateByRoadLinks(HazmatTransportProhibition.typeId, changes)
+      val assetsAfter = serviceHazmat.getPersistedAssetsByLinkIds(HazmatTransportProhibition.typeId, Seq(newLinkId), false)
+
+      assetsAfter.size should be(1)
+      assetsAfter.head.value.get.asInstanceOf[Prohibitions].prohibitions.head should be(assetValues.prohibitions.head)
+      assetsAfter.head.startMeasure should be(0.0)
+      assetsAfter.head.endMeasure should be(35.212)
+    }
+  }
 }
