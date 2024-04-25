a0fd9be34cb9935e05fc42c21848cc58fed5ce3f
Juha-Pekka Tulijoki <juha-pekka.tulijoki@sitowise.com>
2024-04-25 09:55:41 +0300

    DROTH-3955 fix disappearing arrow in traffic signs and lights

--- UI/src/view/point_asset/pointAssetLayer.js
+++ UI/src/view/point_asset/pointAssetLayer.js
@@ -382,15 +382,23 @@
 
     function handleChanged() {
       var asset = selectedAsset.get();
-      var newAsset = _.merge({}, asset, {rotation: determineRotation(asset), bearing: determineBearing(asset),
-        administrativeClass: obtainAdministrativeClass(asset), constructionType: obtainConstructionType(asset)});
-      _.find(me.vectorLayer.getSource().getFeatures(), {values_: {id: newAsset.id}}).values_= newAsset;
-      var featureRedraw = _.find(me.vectorLayer.getSource().getFeatures(), function(feature) {
-          return feature.getProperties().id === newAsset.id;
+      var featureRedraw = me.vectorLayer.getSource().getFeatures().filter(function (feature) {
+        return feature.getProperties().id === asset.id;
       });
-      featureRedraw.setProperties({'geometry': new ol.geom.Point([newAsset.lon, newAsset.lat])});
-      selectControl.addSelectionFeatures([featureRedraw]);
-
+      var newProperties = {
+        'rotation': determineRotation(asset),
+        'bearing': determineBearing(asset),
+        'administrativeClass': obtainAdministrativeClass(asset),
+        'constructionType': obtainConstructionType(asset),
+        'geometry': new ol.geom.Point([asset.lon, asset.lat])
+      };
+      featureRedraw.forEach(function (feature) {
+        feature.setProperties(newProperties);
+        if (layerName === 'trafficLights') {
+          feature.setProperties({'propertyData': asset.propertyData});
+        }
+      });
+      selectControl.addSelectionFeatures(featureRedraw);
     }
 
     this.createNewAsset =  function(coordinates) {
