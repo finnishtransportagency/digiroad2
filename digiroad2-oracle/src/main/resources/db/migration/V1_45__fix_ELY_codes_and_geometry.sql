5c7d633b258128788c7d524a26d8b712c729a613
antti.ahopelto <antti.ahopelto@sitowise.com>
2024-09-27 12:59:44 +0300

    DROTH-4163 Rename migration file and add updates for fixing current invalid geometry points

--- digiroad2-oracle/src/main/resources/db/migration/V1_45__fix_ELY_codes_and_geometry.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_45__fix_ELY_codes_and_geometry.sql
@@ -30,3 +30,15 @@ SET ely_nro = CASE
     WHEN ely_nro = 9 THEN 2
     ELSE ely_nro
 END;
+
+UPDATE ely
+SET geometry = ST_GeomFromText('POINT ZM(533880.796720066 6756886.74130609 0 0)', 3067)
+WHERE id = 6;
+
+UPDATE ely
+SET geometry = ST_GeomFromText('POINT ZM(383467.095615344 6718181.18728251 0 0)', 3067)
+WHERE id = 1;
+
+UPDATE ely
+SET geometry = ST_GeomFromText('POINT ZM(230120.084611288 6748148.87306083 0 0)', 3067)
+WHERE id = 2;
