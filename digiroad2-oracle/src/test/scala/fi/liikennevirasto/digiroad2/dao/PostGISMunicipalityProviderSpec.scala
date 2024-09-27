291fd23c0d12cda6898800d2b7f340626d0cb338
antti.ahopelto <antti.ahopelto@sitowise.com>
2024-09-27 13:24:02 +0300

    DROTH-4163 Fix unit test by using ELY enumerations

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/PostGISMunicipalityProviderSpec.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/dao/PostGISMunicipalityProviderSpec.scala
@@ -1,11 +1,12 @@
 package fi.liikennevirasto.digiroad2.dao
 
+import fi.liikennevirasto.digiroad2.asset.{AhvenanmaaEly, LappiEly}
 import org.scalatest.{FunSuite, Matchers, Tag}
 
 class PostGISMunicipalityProviderSpec extends FunSuite with Matchers {
   val provider = new PostGISMunicipalityProvider
-  val elyCodeForLappi = Set(1)
-  val elyCodeForAhvenanmaa = Set(0)
+  val elyCodeForLappi = Set(LappiEly.id)
+  val elyCodeForAhvenanmaa = Set(AhvenanmaaEly.id)
   val elyCodesForLappiAndAhvenanmaa = elyCodeForLappi ++ elyCodeForAhvenanmaa
   val municipalitiesForLappiEly = Set(47, 148, 240, 241, 261, 273, 320, 498, 583, 614, 683, 698, 732, 742, 751, 758, 845, 851, 854, 890, 976)
   val municipalitiesForAahvenanmaaEly = Set(35, 43, 60, 62, 65, 76, 170, 295, 318, 417, 438, 478, 736, 766, 771, 941)
