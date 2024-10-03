ac00378da10195461bc9f4aa1c2326fbe1c14e1f
matias.kaakinen <matias.kaakinen@sitowise.com>
2024-10-03 14:49:12 +0300

    DROTH-4153 Revert " DROTH-4153 removed tail part of the migration, modified tests to match"
    This reverts commit 01fa44bd41a05ca2ce41cd0ef4c4dbb904cc4785.

---
+++ digiroad2-oracle/src/main/resources/db/migration/V1_47__update_single_choice_value_and_delete_from_enumerated_value.sql
@@ -0,0 +1,62 @@
+-- Asfaltti
+
+CREATE TEMPORARY TABLE hard_and_soft_asphalt_id AS 
+SELECT ev.id 
+FROM enumerated_value ev 
+JOIN property p ON ev.property_id = p.id
+AND public_id = 'paallysteluokka' AND 
+ev.name_fi IN ('Kovat asfalttibetonit', 'Pehmeät asfalttibetonit');
+
+UPDATE single_choice_value  
+SET enumerated_value_id = (	
+	SELECT ev.id 
+	FROM enumerated_value ev 
+	WHERE ev.name_fi = 'Asfaltti')
+WHERE enumerated_value_id IN (SELECT id FROM hard_and_soft_asphalt_id);
+
+DELETE FROM enumerated_value ev
+WHERE ev.id IN (SELECT id FROM hard_and_soft_asphalt_id);
+
+DROP TABLE IF EXISTS hard_and_soft_asphalt_id;
+
+-- Sitomaton kulutuskerros
+
+CREATE TEMPORARY TABLE gravel_surface_and_gravel_wear_layer_id AS 
+SELECT ev.id 
+FROM enumerated_value ev 
+JOIN property p ON ev.property_id = p.id
+AND public_id = 'paallysteluokka' AND 
+ev.name_fi IN ('Soratien pintaus', 'Sorakulutuskerros');
+
+UPDATE single_choice_value  
+SET enumerated_value_id = (
+	SELECT ev.id 
+	FROM enumerated_value ev 
+	WHERE ev.name_fi = 'Sitomaton kulutuskerros')
+WHERE enumerated_value_id IN (SELECT id FROM gravel_surface_and_gravel_wear_layer_id);
+
+DELETE FROM enumerated_value ev
+WHERE ev.id IN (SELECT id FROM gravel_surface_and_gravel_wear_layer_id);
+
+DROP TABLE IF EXISTS gravel_surface_and_gravel_wear_layer_id;
+
+-- Muut päällysteluokat
+
+CREATE TEMPORARY TABLE concrete_and_other_coatings_id AS 
+SELECT ev.id 
+FROM enumerated_value ev 
+JOIN property p ON ev.property_id = p.id
+AND public_id = 'paallysteluokka' AND 
+ev.name_fi IN ('Betoni', 'Muut pinnoitteet');
+
+UPDATE single_choice_value  
+SET enumerated_value_id = (
+	SELECT ev.id 
+	FROM enumerated_value ev 
+	WHERE ev.name_fi = 'Muut päällysteluokat')
+WHERE enumerated_value_id IN (SELECT id FROM concrete_and_other_coatings_id);
+
+DELETE FROM enumerated_value ev
+WHERE ev.id IN (SELECT id FROM concrete_and_other_coatings_id);
+
+DROP TABLE IF EXISTS concrete_and_other_coatings_id;
