d3a5e24c95bdcdc4ff430be4ed20a4487c675b7e
antti.ahopelto <antti.ahopelto@sitowise.com>
2024-10-10 13:45:25 +0300

    DROTH-4168 remove unneeded map event listeners from popUpMapView.js

--- UI/src/view/pop_up_map/popUpMapView.js
+++ UI/src/view/pop_up_map/popUpMapView.js
@@ -13,37 +13,5 @@
             map.getView().setCenter([pos.x, pos.y]);
             map.getView().setZoom(12);
         }, this);
-
-        eventbus.on('coordinates:selected', function(position) {
-            if (geometrycalculator.isInBounds(map.getProperties().extent, position.lon, position.lat)) {
-                map.getView().setCenter([position.lon, position.lat]);
-                map.getView().setZoom(zoomlevels.getAssetZoomLevelIfNotCloser(zoomlevels.getViewZoom(map)));
-            } else {
-
-            }
-        }, this);
-
-        map.on('moveend', function(event) {
-            var target = document.getElementById(map.getTarget());
-            target.style.cursor = '';
-            applicationModel.moveMap(zoomlevels.getViewZoom(map), map.getView().calculateExtent(map.getSize()), map.getView().getCenter());
-        });
-
-        map.on('pointermove', function(event) {
-            var pixel = map.getEventPixel(event.originalEvent);
-            var hit = map.hasFeatureAtPixel(pixel);
-            var target = document.getElementById(map.getTarget());
-            target.style.cursor = hit ? 'pointer' : (target.style.cursor === 'move' ? target.style.cursor : '');
-            eventbus.trigger('map:mouseMoved', event);
-        }, true);
-
-        map.on('singleclick', function(event) {
-            eventbus.trigger('map:clicked', { x: event.coordinate.shift(), y: event.coordinate.shift() });
-        });
-
-        map.on('pointerdrag', function(event) {
-            var target = document.getElementById(map.getTarget());
-            target.style.cursor = 'move';
-        });
     };
 })(this);
