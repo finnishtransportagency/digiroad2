96627a22ed73758d51b592e6e136188ae48ea1d0
anttiahopeltositowise <antti.ahopelto@sitowise.com>
2025-03-12 09:21:13 +0200

    DROTH-4186 increase additional_panel.external_ids column size

--- digiroad2-oracle/src/main/resources/db/migration/V1_51__columns_to_additional_panel.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_51__columns_to_additional_panel.sql
@@ -1,4 +1,4 @@
-ALTER TABLE additional_panel ADD COLUMN external_ids VARCHAR(128);
+ALTER TABLE additional_panel ADD COLUMN external_ids VARCHAR(490);
 ALTER TABLE additional_panel ADD CONSTRAINT unique_external_ids UNIQUE (external_ids);
 
 ALTER TABLE additional_panel ADD COLUMN created_date timestamp;
