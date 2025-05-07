e17a3eaac5de8183e8b9f532685721bb05b919a7
anttiahopeltositowise <antti.ahopelto@sitowise.com>
2025-05-07 09:31:51 +0300

    DROTH-4342 Improve code

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetPartitioner.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/LinearAssetPartitioner.scala
@@ -44,27 +44,23 @@ object LinearAssetPartitioner extends GraphPartitioner {
       roadNumber.orElse(roadNameFi).orElse(roadNameSe)
     }
 
-    val groupedTwoWay = twoWayLinks.groupBy { link =>
-      (extractRoadIdentifier(link), link.administrativeClass, link.value, link.id == 0, link.trafficDirection, link.attributes.get("ROADPARTNUMBER").orElse(None))
+    def partitionLinkGroup(linkGroup:  Seq[T]):Seq[Seq[T]] = {
+      val grouped = linkGroup.groupBy { link =>
+        (extractRoadIdentifier(link), link.administrativeClass, link.value, link.id == 0, link.trafficDirection, link.attributes.get("ROADPARTNUMBER").orElse(None))
+      }
+
+      val (linksToPartition, linksToPass) = grouped.partition { case ((roadIdentifier, _, _, _, _, _), _) => roadIdentifier.isDefined }
+      val clusters = linksToPartition.values.map(p => {
+        clusterLinks(p)
+      }).toSeq.flatten
+
+      val linkPartitions = clusters.map(linksFromCluster)
+      val result = linkPartitions ++ linksToPass.values.flatten.map(x => Seq(x))
+      result
     }
 
-    val groupedOneWay = oneWayLinks.groupBy { link =>
-      (extractRoadIdentifier(link), link.administrativeClass, link.value, link.id == 0, link.trafficDirection, link.attributes.get("ROADPARTNUMBER").orElse(None))
-    }
-
-    val (linksToPartitionTwoWay, linksToPassTwoWay) = groupedTwoWay.partition { case ((roadIdentifier, _, _, _, _, _), _) => roadIdentifier.isDefined }
-    val clustersTwoWay = linksToPartitionTwoWay.values.map(p => {
-      clusterLinks(p)
-    }).toSeq.flatten
-    val linkPartitionsTwoWay = clustersTwoWay.map(linksFromCluster)
-    val twoWayResult = linkPartitionsTwoWay ++ linksToPassTwoWay.values.flatten.map(x => Seq(x))
-
-    val (linksToPartitionOneWay, linksToPassOneWay) = groupedOneWay.partition { case ((roadIdentifier, _, _, _, _, _), _) => roadIdentifier.isDefined }
-    val clustersOneWay = linksToPartitionOneWay.values.map(p => {
-      clusterLinks(p)
-    }).toSeq.flatten
-    val linkPartitionsOneWay = clustersOneWay.map(linksFromCluster)
-    val oneWayResult = linkPartitionsOneWay ++ linksToPassOneWay.values.flatten.map(x => Seq(x))
+    val twoWayResult = partitionLinkGroup(twoWayLinks)
+    val oneWayResult = partitionLinkGroup(oneWayLinks)
 
     twoWayResult ++ oneWayResult
   }
