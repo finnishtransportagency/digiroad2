0be1ab41fdb002c3fda0c280239bd93200f56e1a
SWJPTulijoki <juha-pekka.tulijoki@sitowise.com>
2025-05-20 13:09:10 +0300

    DROTH-4336 editing restrictions based on assetType, municipality and admin class

---
+++ digiroad2-oracle/src/main/resources/db/migration/V1_53__create_editing_restrictions_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE editing_restrictions (
+    id INT PRIMARY KEY DEFAULT nextval('primary_key_seq'),
+    municipality_id INT NOT NULL,
+    state_road_restricted_asset_types VARCHAR(1000),
+    municipality_road_restricted_asset_types VARCHAR(1000),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
