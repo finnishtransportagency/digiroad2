1136999566636e7da336bb85456f7ca6ba5699c1
SWJPTulijoki <juha-pekka.tulijoki@sitowise.com>
2025-05-20 17:45:52 +0300

    DROTH-4336 make municipality unique

--- digiroad2-oracle/src/main/resources/db/migration/V1_53__create_editing_restrictions_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_53__create_editing_restrictions_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE editing_restrictions (
     id INT PRIMARY KEY DEFAULT nextval('primary_key_seq'),
-    municipality_id INT NOT NULL,
+    municipality_id INT UNIQUE NOT NULL,
     state_road_restricted_asset_types VARCHAR(1000),
     municipality_road_restricted_asset_types VARCHAR(1000),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
