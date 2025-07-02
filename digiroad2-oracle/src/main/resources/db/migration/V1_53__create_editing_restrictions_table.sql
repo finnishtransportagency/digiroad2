c1ecc4ba72f7e7ecfdf926122b08d8fc46790276
SWJPTulijoki <juha-pekka.tulijoki@sitowise.com>
2025-07-02 11:42:22 +0300

    DROTH-4337 add fk and remove default sequence from editing restrictions

--- digiroad2-oracle/src/main/resources/db/migration/V1_53__create_editing_restrictions_table.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V1_53__create_editing_restrictions_table.sql
@@ -1,8 +1,12 @@
 CREATE TABLE editing_restrictions (
-    id INT PRIMARY KEY DEFAULT nextval('primary_key_seq'),
+    id INT PRIMARY KEY,
     municipality_id INT UNIQUE NOT NULL,
     state_road_restricted_asset_types VARCHAR(1000),
     municipality_road_restricted_asset_types VARCHAR(1000),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_municipality_restriction
+    FOREIGN KEY (municipality_id)
+    REFERENCES municipality(id)
+    ON DELETE CASCADE
 );
